ec5605352e4cd8041b75dcd9ef440a1e702df9e2
Peter Prochazka <peter.prochazka@evolveum.com>
2011-05-16 15:08:31 +0000

    Fixing problem with repository mock testing and paths. Switche to use JAR sources and not relative path

--- infra/test-util/src/main/java/com/evolveum/midpoint/test/repository/BaseXDatabaseFactory.java
+++ infra/test-util/src/main/java/com/evolveum/midpoint/test/repository/BaseXDatabaseFactory.java
@@ -25,7 +25,22 @@ package com.evolveum.midpoint.test.repository;
 import com.evolveum.midpoint.xml.ns._public.common.common_1.ObjectFactory;
 import com.evolveum.midpoint.xml.ns._public.repository.repository_1.RepositoryPortType;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StreamTokenizer;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
 import javax.xml.bind.JAXBContext;
 import org.basex.core.BaseXException;
 import org.basex.core.Context;
@@ -36,226 +51,305 @@ import org.basex.BaseXServer;
 import org.basex.server.ClientSession;
 
 /**
- * Factory class for embedded BaseX XML database that mocks
- * real midPoint repository.
+ * Factory class for embedded BaseX XML database that mocks real midPoint
+ * repository.
  * 
- * Only one databse is started, but multiple port
- * instances are returned.
+ * Only one databse is started, but multiple port instances are returned.
  * 
- * TODO: This really, really needs refactoring. It is 
- * unreadable and object management is all wrong.
- *
+ * TODO: This really, really needs refactoring. It is unreadable and object
+ * management is all wrong.
+ * 
  * @author $author$
  * @version $Revision$ $Date$
  * @since 1.0.0
  */
 public class BaseXDatabaseFactory {
 
-    public static final String code_id = "$Id$";
+	public static final String code_id = "$Id$";
 
-    /** Database context. */
-    static final Context CONTEXT = new Context();
+	/** Database context. */
+	static final Context CONTEXT = new Context();
 
-    /** Database driver. */
-    public static final String DRIVER = "org.basex.api.xmldb.BXDatabase";
+	/** Database driver. */
+	public static final String DRIVER = "org.basex.api.xmldb.BXDatabase";
 
-    /** Database url. */
-    static final String URI = "xmldb:basex://localhost:1984/midPoint";
+	/** Database url. */
+	static final String URI = "xmldb:basex://localhost:1984/midPoint";
 
-    /** Sample query. */
-    private static final String QUERY = "//object";
-    
-    private static final String DEFAULT_INIT_FILE_PATH = "../../infra/test-util/src/main/resources/test-data/repository/";
+	/** Sample query. */
+	private static final String QUERY = "//object";
 
-    /** Session reference. */
-    private static ClientSession session;
+	//private static final String DEFAULT_INIT_FILE_PATH = "../../infra/test-util/src/main/resources/test-data/repository/";
+	private static final String DEFAULT_INIT_FILE_PATH = "sdfagfr0-tiugfjvakndsq-093";
+	
+	/** Session reference. */
+	private static ClientSession session;
 
-    /** Single Collection. */
-    private static Collection collection = null;
-    // ------------------------------------------------------------------------
-    // Start server on default port 1984.
+	/** Single Collection. */
+	private static Collection collection = null;
+	// ------------------------------------------------------------------------
+	// Start server on default port 1984.
 
-    private static BaseXServer server = null;
+	private static BaseXServer server = null;
 
-    /**
-     * TODO remmove  testClazz attribute
-     * @param testClazz
-     * @return
-     */
-    @Deprecated
-    public static RepositoryPortType getRepositoryPort(Class testClazz) {
-        return getRepositoryPort(new File(DEFAULT_INIT_FILE_PATH));
-    }
-    
-    public static RepositoryPortType getRepositoryPort() {
-        return getRepositoryPort(new File(DEFAULT_INIT_FILE_PATH));
-    }
+	/**
+	 * TODO remmove testClazz attribute
+	 * 
+	 * @param testClazz
+	 * @return
+	 */
+	@Deprecated
+	public static RepositoryPortType getRepositoryPort(Class testClazz) {
+		return getRepositoryPort(new File(DEFAULT_INIT_FILE_PATH));
+	}
 
-    public static RepositoryPortType getRepositoryPort(File base) {
-        RepositoryService port = null;
-        try {
-            
-            if (!base.exists()) {
-                throw new IllegalArgumentException("Repository initialization file does not exists " + base.getAbsolutePath());
-            }
+	public static RepositoryPortType getRepositoryPort() {
 
-            String initPath = base.getAbsolutePath();
-            if (base.isDirectory()) {
-            	initPath = initPath + File.separator; 
-            }
-            XMLServerStart(initPath, new String[]{"-d"});
-            // Register the database.
-            Class<?> c = Class.forName(DRIVER);
-            Database db = (Database) c.newInstance();
-            DatabaseManager.registerDatabase(db);
-            // Collection instance.
-            collection = DatabaseManager.getCollection(URI, "admin", "admin");
-            
-            JAXBContext ctx = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
-            port = new RepositoryService();
-            port.initialis(collection, ctx);
+		return getRepositoryPort(new File(DEFAULT_INIT_FILE_PATH));
+	}
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            XMLServerStop();
-            port = null;
-        }
-        return port;
-    }
+	private static File initializeRepo () {
+		System.out.println("--- START ----");
+		String u = ClassLoader.getSystemResource("test-data/repository")
+				.getPath();
+		System.out.println(u);
 
-    /**
-     * 
-     * TODO: Refactor needed. The method name is wrong.
-     * 
-     * @param args (ignored) command-line arguments
-     * @throws Exception exception
-     */
-    public static void XMLServerStart(String base, final String[] args) throws Exception {
-        if (null == server) {
-            System.out.println("=== Mock midPoint Repository (Embedded BaseX Server) ===");
-            System.out.println("Base: "+base);
-            
-            // ------------------------------------------------------------------------
-            // Start server on default port 1984.
-            server = new BaseXServer(args);
+		File file = new File(u);
 
-            // ------------------------------------------------------------------------
-            // Create a client session with host name, port, user name and password
-            System.out.println("\n* Create a client session.");
+		if (!file.isDirectory()) {
+			System.out.println("Not a directory !! expanding");
+			u = u.replace("file:", "").split("!")[0];
 
-            session = new ClientSession("localhost", 1984, "admin", "admin");
+			System.out.println(u);
 
-            // ------------------------------------------------------------------------
-            // Create a database
-            String DBPath = String.format("CREATE DB midPoint %s", base);
+			JarFile jf = null;
+			try {
+				jf = new JarFile(u);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			new File ("target/xmlDB").mkdirs();
+			
+			for (Enumeration en = jf.entries(); en.hasMoreElements();) {
+				JarEntry je = (JarEntry) en.nextElement();
+				if (je.getName().contains("test-data/repository/")) {
+					String srcName = je.getName();
+					String dstName = srcName.replace("test-data/repository/", "");
+					if ( dstName.length() < 3 ) continue ;
+					System.out.println("++ " + srcName + " -->" + dstName );
+					if (new File ("target/xmlDB/"+ dstName).exists()) continue;
+					InputStream is = null;
+					try {
+						OutputStream out = new FileOutputStream("target/xmlDB/" + dstName);
+						byte buf[] = new byte[65536];
+						is = jf.getInputStream(je);
+						int len;
+						while ((len = is.read(buf)) >0) {
+							
+							out.write(buf,0,len);
+						}
+						out.close();
+						is.close();
+						
+					} catch (FileNotFoundException e) {
+						throw new IllegalStateException(e);
 
-            System.out.println("\n* Create a database: " + DBPath);
-            
-            session.execute(DBPath);
-        }
-    }
-    
-    /**
-     * Shut down the mock repository.
-     * 
-     * TODO: Refactor needed. The method name is wrong.
-     * 
-     * @param args (ignored) command-line arguments
-     * @throws Exception exception
-     */
-    public static void XMLServerStop() {
-        if (null != collection) {
-            // ------------------------------------------------------------------------
-            // Close the collection
-            System.out.println("\n* Close the collection.");
-            try {
-                collection.close();
-            } catch (XMLDBException ex) {
-            } finally {
-                collection = null;
-            }
-        }
-        if (null != session) {
-            try {
-                // ------------------------------------------------------------------------
-                // Drop the database
-                System.out.println("\n* Close and drop the database.");
-                session.execute("DROP DB midPoint");
+					} catch (IOException e) {
+						throw new IllegalStateException(e);
+					}
+				}
+			}
+			return new File ("target/xmlDB");
+		} else {
+			return new File(u);
+		}
+		
+	}
+	
+	public static RepositoryPortType getRepositoryPort(File base) {
 
-                // ------------------------------------------------------------------------
-                // Close the client session
-                System.out.println("\n* Close the client session.");
+		if (!base.exists()) {
+			base = initializeRepo();
+		}
 
-                session.close();
-            } catch (IOException ex) {
-            	// TODO!!!
-            } catch (BaseXException ex) {
-            	// TODO!!!!
-            } finally {
-                session = null;
-            }
-        }
-        if (null != server) {
+		RepositoryService port = null;
+		try {
 
-            // ------------------------------------------------------------------------
-            // Stop the server
-            System.out.println("\n* Stop the server.");
+			if (!base.exists()) {
+				throw new IllegalArgumentException(
+						"Repository initialization file does not exists "
+								+ base.getAbsolutePath());
+			}
 
-            server.stop();
-            server = null;
-        }
-    }
+			String initPath = base.getAbsolutePath();
+			if (base.isDirectory()) {
+				initPath = initPath + File.separator;
+			}
+			XMLServerStart(initPath, new String[] { "-d" });
+			// Register the database.
+			Class<?> c = Class.forName(DRIVER);
+			Database db = (Database) c.newInstance();
+			DatabaseManager.registerDatabase(db);
+			// Collection instance.
+			collection = DatabaseManager.getCollection(URI, "admin", "admin");
 
-    /**
-     * TODO: Refactor needed. The method name is wrong.
-     * 
-     * @param args (ignored) command-line arguments
-     * @throws Exception exception
-     */
-    public void XMLDBQuery() throws Exception {
+			JAXBContext ctx = JAXBContext.newInstance(ObjectFactory.class
+					.getPackage().getName());
+			port = new RepositoryService();
+			port.initialis(collection, ctx);
 
-        System.out.println("=== XMLDBQuery ===");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			XMLServerStop();
+			port = null;
+		}
+		return port;
+	}
 
-        System.out.println("\n* Run query via XML:DB:");
+	/**
+	 * 
+	 * TODO: Refactor needed. The method name is wrong.
+	 * 
+	 * @param args
+	 *            (ignored) command-line arguments
+	 * @throws Exception
+	 *             exception
+	 */
+	public static void XMLServerStart(String base, final String[] args)
+			throws Exception {
+		if (null == server) {
+			System.out
+					.println("=== Mock midPoint Repository (Embedded BaseX Server) ===");
+			System.out.println("Base: " + base);
 
-        // Collection instance.
-        Collection coll = null;
+			// ------------------------------------------------------------------------
+			// Start server on default port 1984.
+			server = new BaseXServer(args);
 
-        try {
-            // Register the database.
-            Class<?> c = Class.forName(DRIVER);
-            Database db = (Database) c.newInstance();
-            DatabaseManager.registerDatabase(db);
+			// ------------------------------------------------------------------------
+			// Create a client session with host name, port, user name and
+			// password
+			System.out.println("\n* Create a client session.");
 
-            // Receive the database.
-            coll = DatabaseManager.getCollection(URI);
+			session = new ClientSession("localhost", 1984, "admin", "admin");
 
-            // Receive the XPath query service.
-            XPathQueryService service = (XPathQueryService) coll.getService("XPathQueryService", "1.0");
+			// ------------------------------------------------------------------------
+			// Create a database
+			String DBPath = String.format("CREATE DB midPoint %s", base);
 
-            // Execute the query and receives all results.
-            ResourceSet set = service.query(QUERY);
+			System.out.println("\n* Create a database: " + DBPath);
 
-            // Create a result iterator.
-            ResourceIterator iter = set.getIterator();
+			session.execute(DBPath);
+		}
+	}
 
-            // Loop through all result items.
-            while (iter.hasMoreResources()) {
-                // Receive the next results.
-                Resource res = iter.nextResource();
+	/**
+	 * Shut down the mock repository.
+	 * 
+	 * TODO: Refactor needed. The method name is wrong.
+	 * 
+	 * @param args
+	 *            (ignored) command-line arguments
+	 * @throws Exception
+	 *             exception
+	 */
+	public static void XMLServerStop() {
+		if (null != collection) {
+			// ------------------------------------------------------------------------
+			// Close the collection
+			System.out.println("\n* Close the collection.");
+			try {
+				collection.close();
+			} catch (XMLDBException ex) {
+			} finally {
+				collection = null;
+			}
+		}
+		if (null != session) {
+			try {
+				// ------------------------------------------------------------------------
+				// Drop the database
+				System.out.println("\n* Close and drop the database.");
+				session.execute("DROP DB midPoint");
 
-                // Write the result to the console.
-                System.out.println(res.getContent());
-            }
-        } catch (final XMLDBException ex) {
-            // Handle exceptions.
-            System.err.println("XML:DB Exception occured " + ex.errorCode);
-        } finally {
-            // Close the collection.
-            if (coll != null) {
-                coll.close();
-            }
-        }
-    }
-    
+				// ------------------------------------------------------------------------
+				// Close the client session
+				System.out.println("\n* Close the client session.");
+
+				session.close();
+			} catch (IOException ex) {
+				// TODO!!!
+			} catch (BaseXException ex) {
+				// TODO!!!!
+			} finally {
+				session = null;
+			}
+		}
+		if (null != server) {
+
+			// ------------------------------------------------------------------------
+			// Stop the server
+			System.out.println("\n* Stop the server.");
+
+			server.stop();
+			server = null;
+		}
+	}
+
+	/**
+	 * TODO: Refactor needed. The method name is wrong.
+	 * 
+	 * @param args
+	 *            (ignored) command-line arguments
+	 * @throws Exception
+	 *             exception
+	 */
+	public void XMLDBQuery() throws Exception {
+
+		System.out.println("=== XMLDBQuery ===");
+
+		System.out.println("\n* Run query via XML:DB:");
+
+		// Collection instance.
+		Collection coll = null;
+
+		try {
+			// Register the database.
+			Class<?> c = Class.forName(DRIVER);
+			Database db = (Database) c.newInstance();
+			DatabaseManager.registerDatabase(db);
+
+			// Receive the database.
+			coll = DatabaseManager.getCollection(URI);
+
+			// Receive the XPath query service.
+			XPathQueryService service = (XPathQueryService) coll.getService(
+					"XPathQueryService", "1.0");
+
+			// Execute the query and receives all results.
+			ResourceSet set = service.query(QUERY);
+
+			// Create a result iterator.
+			ResourceIterator iter = set.getIterator();
+
+			// Loop through all result items.
+			while (iter.hasMoreResources()) {
+				// Receive the next results.
+				Resource res = iter.nextResource();
+
+				// Write the result to the console.
+				System.out.println(res.getContent());
+			}
+		} catch (final XMLDBException ex) {
+			// Handle exceptions.
+			System.err.println("XML:DB Exception occured " + ex.errorCode);
+		} finally {
+			// Close the collection.
+			if (coll != null) {
+				coll.close();
+			}
+		}
+	}
+
 }
