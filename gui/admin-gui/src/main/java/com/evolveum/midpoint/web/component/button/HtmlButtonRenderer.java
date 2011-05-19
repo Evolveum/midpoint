1fd9eae42eafd647f10239bd24dd78eca621e971
Viliam Repan <viliam.repan@evolveum.com>
2011-05-19 08:55:07 +0000

    html button rendering fix, more i18n for template and login page

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/button/HtmlButtonRenderer.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/button/HtmlButtonRenderer.java
@@ -33,65 +33,70 @@ import javax.faces.context.ResponseWriter;
 import javax.faces.render.FacesRenderer;
 
 /**
- *
+ * 
  * @author Vilo Repan
  */
 @FacesRenderer(componentFamily = HtmlCommandLink.COMPONENT_FAMILY, rendererType = "HtmlButtonRenderer")
 public class HtmlButtonRenderer extends CommandLinkRenderer {
 
-    private String styleClasses;
-    private Object value;
-    private ValueExpression valueExpression;
+	private String styleClasses;
+	private Object value;
+	private ValueExpression valueExpression;
 
-    @Override
-    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
-        HtmlButton link = (HtmlButton) component;
-        value = link.getValue();
-        styleClasses = link.getStyleClass();
-        valueExpression = link.getValueExpression("value");
+	@Override
+	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+		HtmlButton link = (HtmlButton) component;
+		value = link.getValue();
+		styleClasses = link.getStyleClass();
+		valueExpression = link.getValueExpression("value");
 
-        link.setValue(null);
-        link.setValueExpression("value", null);
-        link.setStyleClass(styleClasses + " " + link.getButtonType());
+		link.setValue(null);
+		link.setValueExpression("value", null);
+		link.setStyleClass(styleClasses + " " + link.getButtonType());
 
-        ResponseWriter writer = context.getResponseWriter();
-        writer.startElement("div", component);
-        writer.writeAttribute("class", "buttons", null);
+		ResponseWriter writer = context.getResponseWriter();
+		writer.startElement("div", component);
+		writer.writeAttribute("class", "buttons", null);
 
-        super.encodeBegin(context, component);
-    }
+		super.encodeBegin(context, component);
+	}
 
-    @Override
-    public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
-        HtmlButton link = (HtmlButton) component;
+	@Override
+	public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
+		HtmlButton link = (HtmlButton) component;
 
-        ResponseWriter writer = context.getResponseWriter();
-        String src = RenderKitUtils.getImageSource(context, component, "img");
-        if (src != null && !src.isEmpty()) {
-            writer.startElement("img", component);
-            writer.writeAttribute("style", "border: 0px none;", null);
-            writer.writeAttribute("src", src, null);
-            writer.endElement("img");
-        }
+		ResponseWriter writer = context.getResponseWriter();
+		String src = RenderKitUtils.getImageSource(context, component, "img");
+		if (src != null && !src.isEmpty()) {
+			writer.startElement("img", component);
+			writer.writeAttribute("style", "border: 0px none;", null);
+			writer.writeAttribute("src", src, null);
+			writer.endElement("img");
+		}
 
-        super.encodeChildren(context, component);
+		super.encodeChildren(context, component);
 
-        writer.startElement("span", component);
-        writer.writeAttribute("class", link.getButtonType(), null);
-        writer.writeText(value, null);
-        writer.endElement("span");
-    }
+		writer.startElement("span", component);
+		writer.writeAttribute("class", link.getButtonType(), null);
 
-    @Override
-    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
-        super.encodeEnd(context, component);
+		if (valueExpression != null) {
+			writer.writeText((String) valueExpression.getValue(context.getELContext()), null);
+		} else {
+			writer.writeText(value, null);
+		}
+		writer.endElement("span");
+	}
 
-        ResponseWriter writer = context.getResponseWriter();
-        writer.endElement("div");
+	@Override
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+		super.encodeEnd(context, component);
 
-        HtmlButton link = (HtmlButton) component;
-        link.setValue(value);
-        link.setStyleClass(styleClasses);
-        link.setValueExpression("value", valueExpression);
-    }
+		ResponseWriter writer = context.getResponseWriter();
+		writer.endElement("div");
+
+		HtmlButton link = (HtmlButton) component;
+		link.setValue(value);
+		link.setStyleClass(styleClasses);
+		link.setValueExpression("value", valueExpression);
+	}
 }
