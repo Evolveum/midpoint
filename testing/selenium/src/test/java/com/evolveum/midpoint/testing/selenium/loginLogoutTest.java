9c240edc2472b8f9781d63456e0fbf5d5c45bbd6
Peter Prochazka <peter.prochazka@evolveum.com>
2011-06-15 11:59:49 +0000

    Selenium testing testing phase learning how to do.

---
+++ testing/selenium/src/test/java/com/evolveum/midpoint/testing/selenium/loginLogoutTest.java
@@ -0,0 +1,45 @@
+package com.evolveum.midpoint.testing.selenium;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.regex.Pattern;
+
+public class loginLogoutTest {
+
+	WebDriverBackedSelenium selenium;
+	
+	@Before
+	public void setUp() throws Exception {
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://localhost:8080/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+		selenium.start();
+	}
+
+	@Test
+	public void positiveLoginTest() throws Exception {
+		selenium.open("/idm/login.iface");
+		assertEquals("midPoint", selenium.getTitle());
+		selenium.click("css=span.regular");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("midPoint", selenium.getTitle());
+		selenium.click("css=#j_idt24 > span");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("midPoint", selenium.getTitle());
+		selenium.type("loginForm:userName", "huhulak");
+		selenium.type("loginForm:password", "huhulesne");
+		selenium.click("css=span.regular");
+		assertEquals("", selenium.getAttribute("Invalid username and/or password."));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+
+}
