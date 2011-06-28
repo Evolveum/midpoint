d645997bbcd07c2944878e59761961f508e4682f
Radovan Semancik <radovan.semancik@evolveum.com>
2011-06-28 10:52:05 +0000

    First whack at Task Manager Interface. Minimum comments. Work in progress.

---
+++ repo/task-api/src/main/java/com/evolveum/midpoint/task/api/TaskManager.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2011 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted 2011 [name of copyright owner]"
+ * 
+ */
+package com.evolveum.midpoint.task.api;
+
+import java.util.Set;
+
+import com.evolveum.midpoint.xml.ns._public.common.common_1.ObjectModificationType;
+
+/**
+ * @author Radovan Semancik
+ *
+ */
+public interface TaskManager {
+	
+	/**
+	 * Creates new transient, running, claimed task instance.
+	 * 
+	 * This is useful for normal day-to-day tasks that are either
+	 * synchronous or start as a synchronous and are switched to
+	 * asynchronous task later.
+	 * 
+	 * @return transient, running, claimed task instance
+	 */
+	// TODO: parameters
+	public Task createTaskInstance();
+	
+	/**
+	 * Returns a task with specified OID.
+	 * 
+	 * Works only on persistent tasks.
+	 * 
+	 * @param taskOid
+	 * @return
+	 */
+	public Task getTask(String taskOid);
+	
+	public void modifyTask(ObjectModificationType objectChange);
+	
+	public void deteleTask(String taskOid);
+	
+	public void claimTask(Task task);
+	
+	public void releaseTask(Task task);
+	
+	public void switchToBackground(Task task);
+	
+	// TODO: signature
+	public Set<Task> listTasks();
+	
+	// TODO: search
+	
+	public void registerHandler(String uri, TaskHandler handler);
+	
+}
---
+++ repo/task-api/src/main/java/com/evolveum/midpoint/task/api/TaskRunResult.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2011 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted 2011 [name of copyright owner]"
+ * 
+ */
+package com.evolveum.midpoint.task.api;
+
+/**
+ * Single-purpose class to return task run results.
+ * 
+ * More than one value is returned, therefore it is
+ * bundled into a class.
+ * 
+ * @author Radovan Semancik
+ *
+ */
+public final class TaskRunResult {
+
+	private int progress;
+	private TaskExecutionStatus executionStatus;
+	
+	/**
+	 * @return the progress
+	 */
+	public int getProgress() {
+		return progress;
+	}
+	/**
+	 * @param progress the progress to set
+	 */
+	public void setProgress(int progress) {
+		this.progress = progress;
+	}
+	/**
+	 * @return the status
+	 */
+	public TaskExecutionStatus getExecutionStatus() {
+		return executionStatus;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setExecutionStatus(TaskExecutionStatus status) {
+		this.executionStatus = status;
+	}
+	
+}
