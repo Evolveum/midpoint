b1bacdbf63ca9fa9bf57a8c514ba0c22af18d2fa
Viliam Repan <viliam.repan@evolveum.com>
2011-06-28 09:27:33 +0000

    preparing add user with template test finished.

--- infra/common/src/test/java/com/evolveum/midpoint/common/test/XmlAsserts.java
+++ infra/common/src/test/java/com/evolveum/midpoint/common/test/XmlAsserts.java
@@ -18,59 +18,62 @@ import com.evolveum.midpoint.common.diff.MidPointDifferenceListener;
 import com.evolveum.midpoint.common.diff.OidQualifier;
 
 public class XmlAsserts {
-	
-    private static void setupXmlUnitForTest() {
-        //XmlUnit setup
-        //Note: compareUnmatched has to be set to false to calculate diff properly, to avoid matching of nodes that are not comparable
-        XMLUnit.setCompareUnmatched(false);
-        XMLUnit.setIgnoreAttributeOrder(true);
-        XMLUnit.setIgnoreDiffBetweenTextAndCDATA(true);
-        XMLUnit.setIgnoreComments(true);
-        XMLUnit.setIgnoreWhitespace(true);
-        XMLUnit.setNormalize(true);
-        XMLUnit.setNormalizeWhitespace(true);
-    }
 
-    private static String readFileAsString(File fileNewXml) throws java.io.IOException{
-        byte[] buffer = new byte[(int) fileNewXml.length()];
-        FileInputStream f = new FileInputStream(fileNewXml);
-        f.read(buffer);
-        return new String(buffer);
-    }
-    
-    public static void assertPatch(File fileNewXml, String patchedXml) throws Exception {
-    	assertPatch(readFileAsString(fileNewXml), patchedXml);
+	private static void setupXmlUnitForTest() {
+		// XmlUnit setup
+		// Note: compareUnmatched has to be set to false to calculate diff
+		// properly, to avoid matching of nodes that are not comparable
+		XMLUnit.setCompareUnmatched(false);
+		XMLUnit.setIgnoreAttributeOrder(true);
+		XMLUnit.setIgnoreDiffBetweenTextAndCDATA(true);
+		XMLUnit.setIgnoreComments(true);
+		XMLUnit.setIgnoreWhitespace(true);
+		XMLUnit.setNormalize(true);
+		XMLUnit.setNormalizeWhitespace(true);
+	}
 
-    }
+	private static String readFileAsString(File fileNewXml) throws java.io.IOException {
+		byte[] buffer = new byte[(int) fileNewXml.length()];
+		FileInputStream f = new FileInputStream(fileNewXml);
+		f.read(buffer);
+		return new String(buffer);
+	}
 
-    public static void assertPatch(String origXml, String patchedXml) throws Exception {
-        setupXmlUnitForTest();
-        Diff d = new Diff(new InputSource(new StringBufferInputStream(origXml)), new InputSource(new StringBufferInputStream(patchedXml)));
-        DetailedDiff dd = new DetailedDiff(d);
-        dd.overrideElementQualifier(new OidQualifier());
-        dd.overrideDifferenceListener(new MidPointDifferenceListener());
-        List<Difference> differences = dd.getAllDifferences();
+	public static void assertPatch(File fileNewXml, String patchedXml) throws Exception {
+		assertPatch(readFileAsString(fileNewXml), patchedXml);
 
-        for (Difference diff : differences) {
-            switch (diff.getId()) {
-//                case DifferenceConstants.NAMESPACE_PREFIX_ID:
-//                    //ignore namespaces
-//                    //FIXME: ^^^
-//                    break;
-                case DifferenceConstants.ATTR_VALUE_ID:
-                    if (diff.getControlNodeDetail().getNode().getNodeName().contains("type")) {
-                        //ignore attribute values for xsi type, because of namespaces
-                        //FIXME: ^^^
-                        break;
-                    }
-                case DifferenceConstants.CHILD_NODELIST_SEQUENCE_ID:
-                case DifferenceConstants.SCHEMA_LOCATION_ID:
-                    break;
-                default:
-                    fail(diff.toString());
-            }
-        }
+	}
 
-    }
+	public static void assertPatch(String origXml, String patchedXml) throws Exception {
+		setupXmlUnitForTest();
+		Diff d = new Diff(new InputSource(new StringBufferInputStream(origXml)), new InputSource(
+				new StringBufferInputStream(patchedXml)));
+		DetailedDiff dd = new DetailedDiff(d);
+		dd.overrideElementQualifier(new OidQualifier());
+		dd.overrideDifferenceListener(new MidPointDifferenceListener());
+		List<Difference> differences = dd.getAllDifferences();
+
+		for (Difference diff : differences) {
+			switch (diff.getId()) {
+				// case DifferenceConstants.NAMESPACE_PREFIX_ID:
+				// //ignore namespaces
+				// //FIXME: ^^^
+				// break;
+				case DifferenceConstants.ATTR_VALUE_ID:
+					if (diff.getControlNodeDetail().getNode().getNodeName().contains("type")) {
+						// ignore attribute values for xsi type, because of
+						// namespaces
+						// FIXME: ^^^
+						break;
+					}
+				case DifferenceConstants.CHILD_NODELIST_SEQUENCE_ID:
+				case DifferenceConstants.SCHEMA_LOCATION_ID:
+					break;
+				default:
+					fail(diff.toString());
+			}
+		}
+
+	}
 
 }
