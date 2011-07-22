90ed8e527e9a99a18a1bc3da207c8a3c1986d0e6
Igor Farinic <igor.farinic@evolveum.com>
2011-07-22 13:34:52 +0000

    small improvements in existing aspects added back profiling aspect - it is not working yet

--- infra/util/src/main/java/com/evolveum/midpoint/aspect/LoggingAspect.java
+++ infra/util/src/main/java/com/evolveum/midpoint/aspect/LoggingAspect.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2011 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ *
+ * Portions Copyrighted 2011 Igor Farinic
+ */
 package com.evolveum.midpoint.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -7,55 +27,60 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.CodeSignature;
 import org.springframework.core.annotation.Order;
 
+/**
+ * @author Igor Farinic
+ * 
+ */
 @Aspect
-@Order(value=2)
-public class LoggingAspect
-{
-        @Around("repositoryService() || provisioningService() || modelService()")
-        public Object invokeAround(final ProceedingJoinPoint pjp) throws Throwable
-        {
-                final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(pjp.getSignature().getDeclaringType().getName());
+@Order(value = 2)
+public class LoggingAspect {
+	@Around("repositoryService() || provisioningService() || modelService()")
+	public Object logMethodExecution(final ProceedingJoinPoint pjp) throws Throwable {
+		final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(pjp.getSignature()
+				.getDeclaringType().getName());
 
-                final Object[] args = pjp.getArgs();
-                final String[] names = ((CodeSignature) pjp.getSignature()).getParameterNames();
-                @SuppressWarnings("unchecked")
-                final Class<CodeSignature>[] types = ((CodeSignature) pjp.getSignature()).getParameterTypes();
-                final String name = ((CodeSignature) pjp.getSignature()).getName();
-                final StringBuffer methodCallInfo = new StringBuffer();
-                methodCallInfo.append("Entering method: " + name + "(");
+		String name = null;
+		if (logger.isTraceEnabled()) {
+			final Object[] args = pjp.getArgs();
+			final String[] names = ((CodeSignature) pjp.getSignature()).getParameterNames();
+			@SuppressWarnings("unchecked")
+			final Class<CodeSignature>[] types = ((CodeSignature) pjp.getSignature()).getParameterTypes();
+			name = ((CodeSignature) pjp.getSignature()).getName();
+			final StringBuffer methodCallInfo = new StringBuffer();
+			methodCallInfo.append("Entering method: " + name + "(");
 
-                for (int i = 0; i < args.length; i++)
-                {
-                        methodCallInfo.append(types[i].getName() + " : " + names[i] + " = '" + args[i] + "'");
-                        if (args.length == i + 1)
-                        {
-                                methodCallInfo.append(" )");
-                        }
-                        else
-                        {
-                                methodCallInfo.append(",\n");
-                        }
-                }
-                if (args.length == 0)
-                {
-                        methodCallInfo.append(")");
-                }
+			for (int i = 0; i < args.length; i++) {
+				methodCallInfo.append(types[i].getName() + " : " + names[i] + " = '" + args[i] + "'");
+				if (args.length == i + 1) {
+					methodCallInfo.append(" )");
+				} else {
+					methodCallInfo.append(",\n");
+				}
+			}
+			if (args.length == 0) {
+				methodCallInfo.append(")");
+			}
 
-                logger.trace(methodCallInfo.toString());
+			logger.trace(methodCallInfo.toString());
+		}
 
-                final Object tmp = pjp.proceed();
-                logger.trace("Successfully executed method: " + name + "()");
-                return tmp;
-        }
+		final Object tmp = pjp.proceed();
+		if (logger.isTraceEnabled()) {
+			logger.trace("Successfully executed method: " + name + "(). Returned was: " + tmp);
+		}
+		return tmp;
+	}
 
-        @Pointcut("execution(public * com.evolveum.midpoint.repo.api.RepositoryService.*(..))")
-        public void repositoryService() {}
+	@Pointcut("execution(public * com.evolveum.midpoint.repo.api.RepositoryService.*(..))")
+	public void repositoryService() {
+	}
 
-        @Pointcut("execution(public * com.evolveum.midpoint.provisioning.api.ProvisioningService.*(..))")
-        public void provisioningService() {}
-        
-        @Pointcut("execution(public * com.evolveum.midpoint.model.api.ModelService.*(..))")
-        public void modelService() {}
+	@Pointcut("execution(public * com.evolveum.midpoint.provisioning.api.ProvisioningService.*(..))")
+	public void provisioningService() {
+	}
 
-        
+	@Pointcut("execution(public * com.evolveum.midpoint.model.api.ModelService.*(..))")
+	public void modelService() {
+	}
+
 }
---
+++ infra/util/src/main/java/com/evolveum/midpoint/aspect/ProfilingAspect.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2011 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ *
+ * Portions Copyrighted 2011 [name of copyright owner]
+ * Portions Copyrighted 2010 Forgerock
+ */
+
+package com.evolveum.midpoint.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.util.StopWatch;
+
+/**
+ * @author Igor Farinic
+ *
+ */
+@Aspect
+@Order(value = 3)
+public abstract class ProfilingAspect {
+
+    @Around("repositoryService() || provisioningService() || modelService()")
+    public Object profile(final ProceedingJoinPoint pjp) throws Throwable {
+        final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(pjp.getSignature().getDeclaringType().getName());
+    	
+        if (logger.isTraceEnabled()) {
+            StringBuilder message = new StringBuilder();
+            message.append("Entry: ");
+            message.append(getClassName(pjp));
+            message.append(" ");
+            message.append(pjp.getSignature().getName());
+//            message.append(", args: ");
+//            message.append(Arrays.toString(pjp.getArgs()));
+            logger.trace(message.toString());
+        }
+
+        StopWatch sw = new StopWatch(getClass().getSimpleName());
+        Object retValue = null;
+        try {
+            sw.start(pjp.getSignature().getName());
+            retValue = pjp.proceed();
+            return retValue;
+        } finally {
+            sw.stop();
+
+            if (logger.isTraceEnabled()) {
+                StringBuilder message = new StringBuilder();
+                message.append("Exit: ");
+                message.append(getClassName(pjp));
+                message.append(" ");
+                message.append(pjp.getSignature().getName());
+                message.append(", time: ");
+                message.append(sw.getTotalTimeMillis());
+
+                logger.trace(message.toString());
+            }
+        }
+    }
+
+    private String getClassName(ProceedingJoinPoint pjp) {
+        if (pjp.getThis() != null) {
+            return pjp.getThis().getClass().getName();
+        }
+
+        return null;
+    }
+
+    @Pointcut("execution(public * com.evolveum.midpoint.repo.api.RepositoryService.*(..))")
+    public void repositoryService() {}
+
+    @Pointcut("execution(public * com.evolveum.midpoint.provisioning.api.ProvisioningService.*(..))")
+    public void provisioningService() {}
+    
+    @Pointcut("execution(public * com.evolveum.midpoint.model.api.ModelService.*(..))")
+    public void modelService() {}
+    
+}
