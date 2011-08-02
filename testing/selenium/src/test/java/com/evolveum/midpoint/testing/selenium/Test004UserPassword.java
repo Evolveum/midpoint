664bb5c36ab430c55af114ef5be99934f7c356e9
Peter Prochazka <peter.prochazka@evolveum.com>
2011-08-02 19:26:32 +0000

    Adding test based on JIRA MID-188

--- testing/selenium/src/test/java/com/evolveum/midpoint/testing/selenium/Test004UserPassword.java
+++ testing/selenium/src/test/java/com/evolveum/midpoint/testing/selenium/Test004UserPassword.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,11 +13,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverBackedSelenium;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.evolveum.midpoint.testing.Selenium;
+
 import com.evolveum.midpoint.api.logging.Trace;
 import com.evolveum.midpoint.logging.TraceManager;
 
 public class Test004UserPassword {
-	WebDriverBackedSelenium se;
+	Selenium se;
 	static String baseUrl="http://localhost:8080/idm";
 
 	private static final transient Trace logger = TraceManager.getTrace(Test004UserPassword.class);
@@ -23,7 +27,7 @@ public class Test004UserPassword {
 	@Before
 	public void start() {
 		WebDriver driver = new FirefoxDriver();
-		se = new WebDriverBackedSelenium(driver, baseUrl);
+		se = new Selenium(driver, baseUrl);
 		se.open("/");
 		se.waitForPageToLoad("10000");
 	}
@@ -55,6 +59,47 @@ public class Test004UserPassword {
 	public void test020adminChangeUserPassword() {
 		se.type("loginForm:userName", "administrator");
 		se.type("loginForm:password", "secret");
+		se.click("loginForm:loginButton");
+		se.waitForPageToLoad("10000");
+		assertEquals(baseUrl+"/index.iface", se.getLocation());
+		
+		
+		//modify jack (demote)
+		se.click(se.findLink("topAccount"));
+		se.waitForPageToLoad("30000");
+		assertEquals(baseUrl + "/account/index.iface", se.getLocation());
+		assertTrue(se.isTextPresent("New User"));
+		// get hashmap and login
+		HashMap<String, String> h = new HashMap<String, String>();
+		for (String l : se.getAllLinks()) {
+			if (!l.contains("Table") || !l.contains("name"))
+				continue;
+			h.put(se.getText(l), l.replace("name", ""));
+		}
+	
+		se.click(h.get("jack")+"name");
+		se.waitForPageToLoad("30000");
+		assertEquals(baseUrl + "/account/userDetails.iface", se.getLocation());
+		assertTrue(se.isTextPresent("Black Pearl"));
+		se.click("admin-content:editButton");
+		se.waitForText("Save changes",30);
+		
+		for (String s: se.getAllFields() ) {
+			if (s.contains("password")) {
+				se.type(s, "drinkRum");
+			}
+		}
+		
+		se.click("admin-content:saveButton");
+		se.waitForPageToLoad("30000");
+		se.waitForText("Changes saved successfully"); 
+		assertTrue(se.isTextPresent("Changes saved successfully"));
+		
+		se.click("j_idt25"); 	//TODO refactor to logoutlink
+		se.waitForText("User Login");
+		
+		se.type("loginForm:userName", "jack");
+		se.type("loginForm:password", "drinkRum");
 		se.click("loginForm:loginButton");
 		se.waitForPageToLoad("10000");
 		assertEquals(baseUrl+"/index.iface", se.getLocation());
