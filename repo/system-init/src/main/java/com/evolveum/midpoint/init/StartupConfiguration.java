b0b442c42093130f4025c458fa0d8f662393ef1e
Peter Prochazka <peter.prochazka@evolveum.com>
2011-08-18 14:18:59 +0000

    Basic config

--- repo/system-init/src/main/java/com/evolveum/midpoint/init/StartupConfiguration.java
+++ repo/system-init/src/main/java/com/evolveum/midpoint/init/StartupConfiguration.java
@@ -21,9 +21,14 @@
  */
 package com.evolveum.midpoint.init;
 
+import java.net.URL;
+import java.util.Iterator;
+
+import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.ConfigurationFactory;
+import org.apache.commons.configuration.SystemConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.lang.NotImplementedException;
 
 import com.evolveum.midpoint.util.logging.Trace;
@@ -31,33 +36,115 @@ import com.evolveum.midpoint.util.logging.TraceManager;
 
 public class StartupConfiguration {
 
-	private Configuration config = null;
-	
 	private static final Trace logger = TraceManager.getTrace(StartupConfiguration.class);
-	
-	public Configuration getConfiguration( String componentName) {
+
+	private CompositeConfiguration config = null;
+	private String configFilename = null;
+
+	private String getConfigFilename() {
+		if (this.configFilename == null) {
+			configFilename = "config.xml";
+		}
+		return configFilename;
+	}
+
+	public void setConfigFilename(String configFilename) {
+		this.configFilename = configFilename;
+	}
+
+
+	public StartupConfiguration() {
 		loadConfiguration();
-		return null;
+	}
+
+	public StartupConfiguration(String configFilename) {
+		this.configFilename = configFilename;
+		loadConfiguration();
+	}
+
+	
+	/**
+	 * Read subset of configuration for component
+	 * 
+	 * @param componentName
+	 * @return
+	 */
+	public Configuration getConfiguration( String componentName) {
+		return config.subset(componentName);
 	}
 	
 	public void load() {
-		throw new NotImplementedException();
+		loadConfiguration();
 	}
 	
 	public void save() {
 		throw new NotImplementedException();
 	}
 	
+	
+	
 	private void loadConfiguration() {
 		if (config != null) {
 			config.clear();
+		} else {
+			config = new CompositeConfiguration();
 		}
-		ConfigurationFactory configFactory = new ConfigurationFactory("config.xml");
-		try {
-			config = configFactory.getConfiguration();
-		} catch (ConfigurationException e) {
-			logger.error("Error occured during loading configuration file config.xml: " + e.getMessage(), e);
-			System.out.println("Error occured during loading configuration file config.xml: " + e.getMessage());
+		
+		/* loading precednecy and configuration logic */
+		if (null != System.getProperty("midpoint.home") ) {
+			
+			/* Do default loading */
+		} else {
+			try {
+				// system options are priority 1.
+				//config.addConfiguration(new SystemConfiguration());
+				// provided file is priority 2.
+				config.addConfiguration(new XMLConfiguration(this.getConfigFilename()));
+				/*
+				String repoConfigFilename = config.getString("midpoint.repository.class").replace(".","/");
+				//load repository configuration from class
+				URL repoDefaultConf = ClassLoader.getSystemResource(repoConfigFilename + "/default-repository-config.xml");
+				if (repoDefaultConf != null) {
+					config.addConfiguration(repoDefaultConf);
+				}*/
+				
+			} catch (ConfigurationException e) {
+				logger.error("Unable to read configuration file [" + this.getConfigFilename() + "]:" + e.getMessage());
+				System.out.println("Unable to read configuration file [" + this.getConfigFilename() + "]:" + e.getMessage());
+			}
 		}
+		
+		// debug :-)
+		
+		
+	}
+	
+	@Override
+	public String toString() {
+		@SuppressWarnings("unchecked")
+		Iterator<String> i = config.getKeys();
+		StringBuilder sb = new  StringBuilder();
+		while (i.hasNext() ) {
+			String key = i.next();
+			sb.append(key);
+			sb.append(" = ");
+			sb.append(config.getString(key));
+			sb.append("; ");
+		}
+		return sb.toString();
+	}
+	
+	public String dumpConfig(Configuration c) {
+		@SuppressWarnings("unchecked")
+		Iterator<String> i = c.getKeys();
+		StringBuilder sb = new  StringBuilder();
+		while (i.hasNext() ) {
+			String key = i.next();
+			sb.append(key);
+			sb.append(" = ");
+			sb.append(c.getString(key));
+			sb.append("; ");
+		}
+		return sb.toString();
 	}
 }
--- repo/system-init/src/test/java/com/evolveum/midpoint/init/ConfigurationLoadTest.java
+++ repo/system-init/src/test/java/com/evolveum/midpoint/init/ConfigurationLoadTest.java
@@ -1,14 +1,19 @@
 package com.evolveum.midpoint.init;
 
 
-import static org.testng.AssertJUnit.*;
+import static org.testng.Assert.*;
+
+import org.apache.commons.configuration.Configuration;
 import org.testng.annotations.Test;
 
 
 public class ConfigurationLoadTest {
   @Test
   public void sampleTest() {
-	  System.out.println("BUBUBU");
-	  assertTrue(true);
+	  StartupConfiguration sc = new StartupConfiguration();
+	  assertNotNull(sc);
+	  Configuration c = sc.getConfiguration("midpoint.repository");
+	  assertEquals(c.getString("class") , "com.evolveum.midpoint.repo.xml");
+	  System.out.println(sc.dumpConfig(c));
   }
 }
