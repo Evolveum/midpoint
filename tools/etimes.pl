400e368d00edbd80525ff3d23145247fb68f9a20
Radovan Semancik <radovan.semancik@evolveum.com>
2011-10-20 12:02:49 +0000

    Improving the tool

--- tools/etimes.pl
+++ tools/etimes.pl
@@ -1,51 +1,178 @@
 #!/usr/bin/perl
 use strict;
+use Data::Dumper;
 
 my %ops;
+my %cumulative;
+my %subsystems;
+my $line;
+my %threads;
+my $rootStackFrame = { op => "ROOT" };
+my %packageToSubsystem = (
+  repo => "REPOSITORY",
+  provisioning => "PROVISIONING",
+  web => "WEB",
+  mode => "MODEL",
+);
+my %subsystemDependencies = (
+  WEB => [ 'MODEL' ],
+  MODEL => [ 'PROVISIONING', 'REPOSITORY' ],
+  PROVISIONING => [ 'REPOSITORY' ],
+  REPOSITORY => [],
+);
 
-my $line = <>;
-while ($line) {
-  chomp $line;
-  if ($line =~ /^(\S+)\s+(\S+)\s+\[([^]]+)\]\s+(\w+)/) {
-    my %op = ();
-    $op{date} = $1;
-    $op{time} = $2;
-    $op{thread} = $3;
-    $op{level} = $4;
-    my $halfline = $';
-    if ($halfline =~ /####\s+Entry:\s*(\d+)\s+(\S+)/) {
-      my $rest = $';
-      my $opkey = $1;
-      my $op = $ops{$opkey};
-      $op->{key} = $1;
-      $op->{op} = $2;
-      $ops{$opkey} = $op;
-      my @args;
-      $op->{args} = \@args;
-      $line = <>;
-      while ($line =~ /###### args:/) {
-        push @args,$';
-        $line = <>;
-      }
-      next;
-    }
-    if ($halfline =~ /####\s+Exit:\s*(\d+)\s+(\S+)/) {
-      my $rest = $';
-      my $opkey = $1;
-      my $op = $ops{$opkey};
-      $op->{op} = $2;
-      $ops{$opkey} = $op;
-      if ($rest =~ /etime:\s*([\d\.]+)/) {
-        $op->{etime} = $1;
-      }
-    }
-  }
+parse();
+displayIndividualTimes();
+print "\n";
+print ("="x80);
+print "\n\n";
+displayCumulativeTimes();
+print "\n";
+print ("="x80);
+print "\n\n";
+displaySubsystemTimes();
+
+sub parse {
   $line = <>;
+  while ($line) {
+    chomp $line;
+    if ($line =~ /^(\S+)\s+(\S+)\s+\[([^]]+)\]\s+(\w+)/) {
+      my %logdata = ();
+      $logdata{date} = $1;
+      $logdata{time} = $2;
+      $logdata{thread} = $3;
+      $logdata{level} = $4;
+      my $halfline = $';
+      processHalfline($halfline,\%logdata);
+    }
+    if ($line =~ /^(\S+)\s+(\S+)\s+\[([^]]+)\]\s+\[([^]]+)\]\s+(\w+)/) {
+      my %logdata = ();
+      $logdata{date} = $1;
+      $logdata{time} = $2;
+      $logdata{subsystem} = $3;
+      $logdata{thread} = $4;
+      $logdata{level} = $5;
+      my $halfline = $';
+      processHalfline($halfline,\%logdata);
+    }
+    $line = <>;
+#    print Dumper(\%threads);
+  }
 }
 
-foreach my $op (sort { $a->{etime} <=> $b->{etime} } values %ops) {
-  print $op->{etime}.": ".$op->{key}." ".$op->{op}."\n";
-  foreach my $args (@{$op->{args}}) {
-    print "  ".$args."\n";
+sub processHalfline {
+  my ($halfline,$logdata) = @_;
+  
+  my $thread = $logdata->{thread};
+  
+  if ($halfline =~ /####\s+Entry:\s*(\d+)\s+(\S+)/) {
+    my $rest = $';
+    my $opkey = $1;
+    my $op = $ops{$opkey};
+    $op->{key} = $1;
+    $op->{op} = $2;
+    if ($logdata->{subsystem}) {
+      $op->{subsystem} = $logdata->{subsystem};
+    } elsif ($op->{op} =~ /com\.evolveum\.midpoint\.(\w+)\./) {
+      $op->{subsystem} = $packageToSubsystem{$1};
+    }
+    $ops{$opkey} = $op;
+    push @{$threads{$thread}},$op;
+    my @args;
+    $op->{args} = \@args;
+    $line = <>;
+    while ($line =~ /###### args:/) {
+      push @args,$';
+      $line = <>;
+    }
+    next;
   }
+  if ($halfline =~ /####\s+Exit:\s*(\d+)\s+(\S+)/) {
+    my $opname = $2;
+    my $rest = $';
+    my $opkey = $1;
+    my $op = $ops{$opkey};
+    $op->{op} = $opname;
+    $ops{$opkey} = $op;
+    if ($rest =~ /etime:\s*([\d\.]+)/) {
+      $op->{etime} = $1;
+    }
+    my $stackop = pop @{$threads{$thread}};
+    if ($stackop->{op} ne $opname) {
+      print "!!! stack mismatch: ".$stackop->{op}." - ".$opname."\n";
+    }
+    my $parentop = $threads{$thread}->[$#{$threads{$thread}}] || $rootStackFrame;
+    count($op,$parentop);
+  }
+  
+}
+
+sub count {
+  my ($op,$parentop) = @_;
+  
+  if ($parentop->{owntime} > 0 || $op->{owntime} > 0) {
+    warn "Possibly inconsistent data, is the log from two executions?\n";
+    print Dumper($op,$parentop,\%threads);
+  }
+  
+  my $etime = $op->{etime};
+  
+  $parentop->{owntime} -= $etime;
+  $op->{owntime} += $etime;
+  
+  my $opname = $op->{op};
+  $cumulative{$opname}->{op} = $opname;
+  $cumulative{$opname}->{invocations}++;
+  $cumulative{$opname}->{totalTime} += $op->{etime};
+  $cumulative{$opname}->{ownTime} += $op->{owntime};
+  
+  my $subsystem = $op->{subsystem};
+  $subsystems{$subsystem}->{subsystem} = $subsystem;
+  $subsystems{$subsystem}->{invocations}++;
+  $subsystems{$subsystem}->{totalTime} += $op->{etime};
+  $subsystems{$subsystem}->{ownTime} += $op->{owntime};
+}
+
+sub displayIndividualTimes {
+  foreach my $op (sort { $a->{etime} <=> $b->{etime} } values %ops) {
+    printf "%40s(%4s): total: %-15f own: %-15f\n",$op->{op},$op->{key},$op->{etime},$op->{ownTime};
+#    print $op->{etime}.": ".$op->{key}." ".$op->{op}."\n";
+    foreach my $args (@{$op->{args}}) {
+      print "  ".$args."\n";
+    }
+  }
+}
+
+sub displayCumulativeTimes {
+  foreach my $op (sort { $a->{ownTime} <=> $b->{ownTime} } values %cumulative) {
+    printf "%25s: total: %-15f own: %-15f (%d)\n",$op->{op},$op->{totalTime},$op->{ownTime},$op->{invocations};
+#    print $op->{totalTime}.": ".$op->{op}." (".$op->{invocations}.")\n";
+  }
+}
+
+sub displaySubsystemTimes {
+  foreach my $op (sort { $a->{totalTime} <=> $b->{totalTime} } values %subsystems) {
+    my $subsystem = $op->{subsystem};
+    printf "%15s:  total:%-15f  own:%-15f  (%d)\n",$op->{subsystem},$op->{totalTime},$op->{ownTime},$op->{invocations};
+#    print $op->{totalTime}."(total): ".$op->{subsystem}.": total:".$op->{totalTime}." own:".ownTime($subsystem)." (".$op->{invocations}.")\n";
+  }
+  
+  printf "%15s:  total:%-15f  own:%-15f\n","ROOT",$rootStackFrame->{totalTime},$rootStackFrame->{ownTime};
+  
+}
+
+
+
+sub ownTime {
+  my ($subsystem) = @_;
+  
+  my $op = $subsystems{$subsystem};
+  my $totalTime = $op->{totalTime};
+  my $dependencies = $subsystemDependencies{$subsystem};
+  
+  foreach my $dependency (@$dependencies) {
+    $totalTime -= $subsystems{$dependency}->{totalTime};
+  }
+
+  return $totalTime;
 }
