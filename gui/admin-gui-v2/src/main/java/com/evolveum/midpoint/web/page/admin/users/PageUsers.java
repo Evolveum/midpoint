b912622fa360c91f72c651e5dcec4b100d4cfdb3
Viliam Repan <viliam.repan@evolveum.com>
2011-12-16 22:58:06 +0000

    one parameter encoder for gui v2, disabled inbound processor for now.

--- gui/admin-gui-v2/src/main/java/com/evolveum/midpoint/web/page/admin/users/PageUsers.java
+++ gui/admin-gui-v2/src/main/java/com/evolveum/midpoint/web/page/admin/users/PageUsers.java
@@ -22,10 +22,52 @@
 package com.evolveum.midpoint.web.page.admin.users;
 
 import com.evolveum.midpoint.web.page.admin.PageAdmin;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
  * @author lazyman
  */
 public class PageUsers extends PageAdmin {
 
+    public PageUsers() {
+        Form<String> form = new Form<String>("form");
+        final IModel<String> model = new Model<String>();
+        final RequiredTextField<String> text = new RequiredTextField<String>("userId", model);
+        form.add(text);
+
+        AjaxSubmitLink button = new AjaxSubmitLink("button", form) {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                userDetailsPerformed(target, model);
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target, Form<?> form) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        };
+        form.add(button);
+//        AjaxLinkButton link = new AjaxLinkButton("link", new Model<String>("Show user details")) {
+//
+//            @Override
+//            public void onClick(AjaxRequestTarget target) {
+//                System.out.println(text.getModelObject());
+////                userDetailsPerformed(target, model);
+//            }
+//        };
+//        form.add(link);
+        add(form);
+    }
+
+    public void userDetailsPerformed(AjaxRequestTarget target, IModel<String> userIdModel) {
+        PageParameters parameters = new PageParameters();
+        parameters.add(PageUser.PARAM_USER_ID, userIdModel.getObject());
+        setResponsePage(PageUser.class, parameters);
+    }
 }
