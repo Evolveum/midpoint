946f37c86ab51908fda65900c1500487595665c5
Viliam Repan <viliam.repan@evolveum.com>
2012-02-08 09:50:09 +0000

    prepared custom object form for admin gui v2

---
+++ gui/admin-gui-v2/src/main/java/com/evolveum/midpoint/web/component/objectform/PropertyContainerWrapper.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2012 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ *
+ * Portions Copyrighted 2012 [name of copyright owner]
+ */
+
+package com.evolveum.midpoint.web.component.objectform;
+
+import org.apache.commons.lang.Validate;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author lazyman
+ */
+public class PropertyContainerWrapper<T> implements ItemWrapper<T> {
+
+    private PropertyContainer container;
+    private ItemStatus status;
+    private List<ItemWrapper> items;
+
+    public PropertyContainerWrapper(PropertyContainer container, ItemStatus status) {
+        Validate.notNull(container, "Property container must not be null.");
+        Validate.notNull(status, "Status must not be null.");
+
+        this.container = container;
+        this.status = status;
+    }
+
+    @Override
+    public void cleanup() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int getPosition() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String getName() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ObjectDelta<T> createObjectDelta() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ gui/admin-gui-v2/src/main/java/com/evolveum/midpoint/web/component/objectform/PropertyWrapper.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2012 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ *
+ * Portions Copyrighted 2012 [name of copyright owner]
+ */
+
+package com.evolveum.midpoint.web.component.objectform;
+
+import org.apache.commons.lang.Validate;
+import com.evolveum.midpoint.schema.processor.Property;
+
+import java.util.List;
+
+/**
+ * @author lazyman
+ */
+public class PropertyWrapper implements ItemWrapper {
+
+    private Property property;
+    private ItemStatus status;
+    private List<ItemWrapper> items;
+
+    public PropertyContainerWrapper(Property property, ItemStatus status) {
+        Validate.notNull(property, "Property must not be null.");
+        Validate.notNull(status, "Status must not be null.");
+
+        this.property = property;
+        this.status = status;
+    }
+
+    @Override
+    public void cleanup() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int getPosition() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String getName() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ObjectDelta<T> createObjectDelta() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
