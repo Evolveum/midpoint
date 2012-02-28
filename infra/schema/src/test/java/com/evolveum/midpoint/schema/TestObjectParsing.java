68b952dd0ff4079c08e30969ddfa68698302143f
Radovan Semancik <radovan.semancik@evolveum.com>
2012-02-28 10:25:36 +0000

    Forgor this test

---
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/TestObjectParsing.java
@@ -0,0 +1,209 @@
+/**
+ * Copyright (c) 2011 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * Portions Copyrighted 2011 [name of copyright owner]
+ */
+package com.evolveum.midpoint.schema;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertNull;
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.namespace.QName;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.evolveum.midpoint.prism.Objectable;
+import com.evolveum.midpoint.prism.PrismContainer;
+import com.evolveum.midpoint.prism.PrismContainerDefinition;
+import com.evolveum.midpoint.prism.PrismContainerValue;
+import com.evolveum.midpoint.prism.PrismContext;
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.PrismObjectDefinition;
+import com.evolveum.midpoint.prism.PrismProperty;
+import com.evolveum.midpoint.prism.PrismPropertyValue;
+import com.evolveum.midpoint.prism.PrismReferenceDefinition;
+import com.evolveum.midpoint.prism.PropertyPath;
+import com.evolveum.midpoint.prism.PropertyPathSegment;
+import com.evolveum.midpoint.prism.delta.ChangeType;
+import com.evolveum.midpoint.prism.delta.ObjectDelta;
+import com.evolveum.midpoint.prism.schema.PrismSchema;
+import com.evolveum.midpoint.prism.schema.SchemaRegistry;
+import com.evolveum.midpoint.prism.util.PrismAsserts;
+import com.evolveum.midpoint.prism.util.PrismTestUtil;
+import com.evolveum.midpoint.prism.xml.PrismJaxbProcessor;
+import com.evolveum.midpoint.schema.constants.MidPointConstants;
+import com.evolveum.midpoint.util.DOMUtil;
+import com.evolveum.midpoint.util.DebugUtil;
+import com.evolveum.midpoint.util.exception.SchemaException;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.AccountConstructionType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.AccountShadowType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.ActivationType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.AssignmentType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.ExtensibleObjectType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.ObjectReferenceType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.ObjectType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.ResourceObjectShadowAttributesType;
+import com.evolveum.midpoint.xml.ns._public.common.common_1.UserType;
+
+/**
+ * @author semancik
+ *
+ */
+public class TestObjectParsing {
+	
+	public static final File USER_FILE = new File("src/test/resources/diff/user-real-before.xml");
+	
+	@BeforeSuite
+	public void setup() throws SchemaException, SAXException, IOException {
+		DebugUtil.setDefaultNamespacePrefix(MidPointConstants.NS_MIDPOINT_PUBLIC_PREFIX);
+		PrismTestUtil.resetPrismContext(new MidPointPrismContextFactory());
+	}
+	
+	
+	@Test
+	public void testParseUserFile() throws SchemaException {
+		System.out.println("===[ testParseUserFile ]===");
+
+		// GIVEN
+		PrismContext prismContext = PrismTestUtil.getPrismContext();
+		
+		// WHEN
+		PrismObject<UserType> user = prismContext.parseObject(USER_FILE);
+		
+		// THEN
+		System.out.println("Parsed user:");
+		System.out.println(user.dump());
+		
+		assertUser(user);
+	}
+
+	@Test
+	public void testParseUserDom() throws SchemaException {
+		System.out.println("===[ testParseUserDom ]===");
+
+		// GIVEN
+		PrismContext prismContext = PrismTestUtil.getPrismContext();
+		
+		Document document = DOMUtil.parseFile(USER_FILE);
+		Element userElement = DOMUtil.getFirstChildElement(document);
+		
+		// WHEN
+		PrismObject<UserType> user = prismContext.parseObject(userElement);
+		
+		// THEN
+		System.out.println("Parsed user:");
+		System.out.println(user.dump());
+		
+		assertUser(user);
+	}
+
+	@Test
+	public void testPrismParseJaxb() throws JAXBException, SchemaException, SAXException, IOException {
+		System.out.println("===[ testPrismParseJaxb ]===");
+		
+		// GIVEN
+		PrismContext prismContext = PrismTestUtil.getPrismContext();
+		PrismJaxbProcessor jaxbProcessor = prismContext.getPrismJaxbProcessor();
+		
+		// WHEN
+		UserType userType = jaxbProcessor.unmarshalObject(USER_FILE, UserType.class);
+		
+		// THEN
+		assertUser(userType.asPrismObject());
+	}
+	
+	/**
+	 * The definition should be set properly even if the declared type is ObjectType. The Prism should determine
+	 * the actual type.
+	 */
+	@Test
+	public void testPrismParseJaxbObjectType() throws JAXBException, SchemaException, SAXException, IOException {
+		System.out.println("===[ testPrismParseJaxbObjectType ]===");
+		
+		// GIVEN
+		PrismContext prismContext = PrismTestUtil.getPrismContext();
+		PrismJaxbProcessor jaxbProcessor = prismContext.getPrismJaxbProcessor();
+		
+		// WHEN
+		ObjectType userType = jaxbProcessor.unmarshalObject(USER_FILE, ObjectType.class);
+		
+		// THEN
+		assertUser(userType.asPrismObject());
+	}
+	
+	
+	private void assertUser(PrismObject<UserType> user) {
+		
+		assertEquals("Wrong oid", "2f9b9299-6f45-498f-bc8e-8d17c6b93b20", user.getOid());
+//		assertEquals("Wrong version", "42", user.getVersion());
+		PrismObjectDefinition<UserType> usedDefinition = user.getDefinition();
+		assertNotNull("No user definition", usedDefinition);
+		PrismAsserts.assertObjectDefinition(usedDefinition, new QName(SchemaConstants.NS_COMMON, "user"), 
+				UserType.COMPLEX_TYPE, UserType.class);
+		assertEquals("Wrong class in user", UserType.class, user.getCompileTimeClass());
+		
+		assertPropertyValue(user, "fullName", "Jack Sparrow");
+		assertPropertyDefinition(user, "fullName", DOMUtil.XSD_STRING, 1, 1);
+		assertPropertyValue(user, "givenName", "Jack");
+		assertPropertyDefinition(user, "givenName", DOMUtil.XSD_STRING, 1, 1);
+		assertPropertyValue(user, "familyName", "Sparrow");
+		assertPropertyDefinition(user, "familyName", DOMUtil.XSD_STRING, 1, 1);
+		assertPropertyValue(user, "name", "jack");
+		assertPropertyDefinition(user, "name", DOMUtil.XSD_STRING, 0, 1);
+		
+//		PrismContainer extension = user.getExtension();
+//		assertContainerDefinition(extension, "extension", DOMUtil.XSD_ANY, 0, 1);
+//		PrismContainerValue extensionValue = extension.getValue();
+//		assertTrue("Extension parent", extensionValue.getParent() == extension);
+//		assertNull("Extension ID", extensionValue.getId());
+		
+		PropertyPath enabledPath = new PropertyPath(UserType.F_ACTIVATION, ActivationType.F_ENABLED);
+		PrismProperty enabledProperty1 = user.findProperty(enabledPath);
+		PrismAsserts.assertDefinition(enabledProperty1.getDefinition(), ActivationType.F_ENABLED, DOMUtil.XSD_BOOLEAN, 0, 1);
+		assertNotNull("Property "+enabledPath+" not found", enabledProperty1);
+		PrismAsserts.assertPropertyValue(enabledProperty1, true);
+		
+//		PrismProperty validFromProperty = user.findProperty(new PropertyPath(UserType.F_ACTIVATION, ActivationType.F_VALID_FROM));
+//		assertNotNull("Property "+ActivationType.F_VALID_FROM+" not found", validFromProperty);
+//		PrismAsserts.assertPropertyValue(validFromProperty, USER_JACK_VALID_FROM);
+				
+	}
+	
+	private void assertPropertyDefinition(PrismContainer<?> container, String propName, QName xsdType, int minOccurs,
+			int maxOccurs) {
+		QName propQName = new QName(SchemaConstants.NS_COMMON, propName);
+		PrismAsserts.assertPropertyDefinition(container, propQName, xsdType, minOccurs, maxOccurs);
+	}
+	
+	public static void assertPropertyValue(PrismContainer<?> container, String propName, Object propValue) {
+		QName propQName = new QName(SchemaConstants.NS_COMMON, propName);
+		PrismAsserts.assertPropertyValue(container, propQName, propValue);
+	}
+
+}
