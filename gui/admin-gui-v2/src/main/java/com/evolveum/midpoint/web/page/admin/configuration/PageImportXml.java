be7adc607c969a39e616bcad57a2d22a4c4d7acf
Viliam Repan <viliam.repan@evolveum.com>
2012-04-03 10:33:32 +0000

    import from xml, localization

--- gui/admin-gui-v2/src/main/java/com/evolveum/midpoint/web/page/admin/configuration/PageImportXml.java
+++ gui/admin-gui-v2/src/main/java/com/evolveum/midpoint/web/page/admin/configuration/PageImportXml.java
@@ -21,16 +21,62 @@
 
 package com.evolveum.midpoint.web.page.admin.configuration;
 
+import com.evolveum.midpoint.schema.util.MiscSchemaUtil;
+import com.evolveum.midpoint.web.component.button.AjaxSubmitLinkButton;
+import com.evolveum.midpoint.web.component.util.LoadableModel;
 import com.evolveum.midpoint.web.component.xml.ace.AceEditor;
+import com.evolveum.midpoint.xml.ns._public.common.api_types_2.ImportOptionsType;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.Model;
 
 /**
  * @author lazyman
  */
 public class PageImportXml extends PageAdminConfiguration {
+
+    private LoadableModel<ImportOptionsType> model;
     
     public PageImportXml() {
+        model = new LoadableModel<ImportOptionsType>(false) {
+
+            @Override
+            protected ImportOptionsType load() {
+                return MiscSchemaUtil.getDefaultImportOptions();
+            }
+        };
+        initLayout();
+    }
+    
+    private void initLayout() {
+        Form mainForm = new Form("mainForm");
+        add(mainForm);
+
         AceEditor<String> editor = new AceEditor<String>("aceEditor", new Model<String>("aaa"));
-        add(editor);
+        mainForm.add(editor);
+
+        ImportOptionsPanel importOptions = new ImportOptionsPanel("importOptions", model);
+        mainForm.add(importOptions);
+
+        initButtons(mainForm);
+    }
+
+    private void initButtons(final Form mainForm) {
+        AjaxSubmitLinkButton saveButton = new AjaxSubmitLinkButton("importButton",
+                createStringResource("pageImportXml.button.import")) {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                //todo implement
+
+                model.reset();
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target, Form<?> form) {
+                //todo implement
+            }
+        };
+        mainForm.add(saveButton);
     }
 }
