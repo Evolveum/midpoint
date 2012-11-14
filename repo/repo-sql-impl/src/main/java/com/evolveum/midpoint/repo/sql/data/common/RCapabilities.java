3f7028df4fa7b4e643c33d06b2cef46b0a81b987
Viliam Repan <viliam.repan@evolveum.com>
2012-11-14 19:10:49 +0000

    updated capabilities.cachingMetadata. fix for MID-993

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/RCapabilities.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/RCapabilities.java
@@ -3,6 +3,7 @@ package com.evolveum.midpoint.repo.sql.data.common;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import com.evolveum.midpoint.xml.ns._public.common.common_2a.CachingMetadataType;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.Validate;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
@@ -19,23 +20,34 @@ import com.evolveum.midpoint.xml.ns._public.common.common_2a.ResourceType;
 @Embeddable
 public class RCapabilities {
 
+    private String cachingMetadata;
 	private String _native;
 	private String configured;
 
-	@Column(nullable = true)
+    @Column(nullable = true)
+    @Type(type = "org.hibernate.type.TextType")
+    public String getCachingMetadata() {
+        return cachingMetadata;
+    }
+
+    @Column(nullable = true)
 	@Type(type = "org.hibernate.type.TextType")
 	public String getNative() {
 		return _native;
 	}
 
-	public void setNative(String _native) {
-		this._native = _native;
-	}
+    @Column(nullable = true)
+    @Type(type = "org.hibernate.type.TextType")
+    public String getConfigured() {
+        return configured;
+    }
 
-	@Column(nullable = true)
-	@Type(type = "org.hibernate.type.TextType")
-	public String getConfigured() {
-		return configured;
+    public void setCachingMetadata(String cachingMetadata) {
+        this.cachingMetadata = cachingMetadata;
+    }
+
+    public void setNative(String _native) {
+		this._native = _native;
 	}
 
 	public void setConfigured(String configured) {
@@ -51,6 +63,8 @@ public class RCapabilities {
 
 		RCapabilities that = (RCapabilities) o;
 
+        if (cachingMetadata != null ? !cachingMetadata.equals(that.cachingMetadata) : that.cachingMetadata != null)
+            return false;
 		if (_native != null ? !_native.equals(that._native) : that._native != null)
 			return false;
 		if (configured != null ? !configured.equals(that.configured) : that._native != null)
@@ -62,6 +76,7 @@ public class RCapabilities {
 	public int hashCode() {
 		int result = configured != null ? configured.hashCode() : 0;
 		result = 31 * result + (_native != null ? _native.hashCode() : 0);
+        result = 31 * result + (cachingMetadata != null ? cachingMetadata.hashCode() : 0);
 
 		return result;
 	}
@@ -79,6 +94,7 @@ public class RCapabilities {
 		try {
 			repo.setNative(RUtil.toRepo(jaxb.getNative(), prismContext));
 			repo.setConfigured(RUtil.toRepo(jaxb.getConfigured(), prismContext));
+            repo.setCachingMetadata(RUtil.toRepo(jaxb.getCachingMetadata(), prismContext));
 		} catch (Exception ex) {
 			throw new DtoTranslationException(ex.getMessage(), ex);
 		}
@@ -95,8 +111,11 @@ public class RCapabilities {
 		try {
 			jaxb.setNative(RUtil.toJAXB(CapabilitiesType.class, new ItemPath(CapabilitiesType.F__NATIVE),
 					repo.getNative(), CapabilityCollectionType.class, prismContext));
-			jaxb.setConfigured(RUtil.toJAXB(CapabilitiesType.class, new ItemPath(CapabilitiesType.F__NATIVE),
+			jaxb.setConfigured(RUtil.toJAXB(CapabilitiesType.class, new ItemPath(CapabilitiesType.F_CONFIGURED),
 					repo.getConfigured(), CapabilityCollectionType.class, prismContext));
+            jaxb.setCachingMetadata(RUtil.toJAXB(CachingMetadataType.class,
+                    new ItemPath(CapabilitiesType.F_CACHING_METADATA), repo.getCachingMetadata(),
+                    CachingMetadataType.class, prismContext));
 		} catch (Exception ex) {
 			throw new DtoTranslationException(ex.getMessage(), ex);
 		}
