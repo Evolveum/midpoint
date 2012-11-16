26ebe412a619fefc3719ce8933e120aaea7a6255
Michal Serbak <michal.serbak@evolveum.com>
2012-11-16 11:54:45 +0000

    Added preloader for display progress status in GUI

--- gui/admin-gui/src/main/webapp/js/evolveum.js
+++ gui/admin-gui/src/main/webapp/js/evolveum.js
@@ -22,6 +22,7 @@
 var objectFormHelpContainer = null;
 var interval = 0;
 
+window.onload = setupFunc;
 $(document).ready(function(){
 	init();
 	
@@ -234,4 +235,62 @@ function disablePaste(disable) {
 			return !disable;
 		}
 	}
+}
+
+function setupFunc() {
+	document.getElementsByTagName('body')[0].onclick = clickFunc;
+	hideBusysign();
+	Wicket.Ajax.registerPreCallHandler(showBusysign);
+	Wicket.Ajax.registerPostCallHandler(hideBusysign);
+	Wicket.Ajax.registerFailureHandler(hideBusysign);
+}
+
+function hideBusysign() {
+	document.getElementById('bysy_indicator').style.display = 'none';
+}
+
+function showBusysign() {
+	document.getElementById('bysy_indicator').style.display = 'inline';
+}
+
+function clickFunc(eventData) {
+	var clickedElement = (window.event) ? event.srcElement : eventData.target;
+	if (clickedElement.tagName.toUpperCase() == 'BUTTON'
+			|| clickedElement.tagName.toUpperCase() == 'A'
+			|| clickedElement.parentNode.tagName.toUpperCase() == 'A'
+			|| (clickedElement.tagName.toUpperCase() == 'INPUT' && (clickedElement.type
+					.toUpperCase() == 'BUTTON' || clickedElement.type
+					.toUpperCase() == 'SUBMIT'))) {
+		showBusysign();
+	}
+}
+
+if ((clickedElement.tagName.toUpperCase() == 'A'
+		&& ((clickedElement.target == null) || (clickedElement.target.length <= 0))
+		&& (clickedElement.href.lastIndexOf('#') != (clickedElement.href.length - 1))
+		&& (!('nobusy' in clickedElement))
+		&& (clickedElement.href.indexOf('skype') < 0)
+		&& (clickedElement.href.indexOf('mailto') < 0)
+		&& (clickedElement.href.indexOf('WicketAjaxDebug') < 0)
+		&& (clickedElement.href.lastIndexOf('.doc') != (clickedElement.href.length - 4))
+		&& (clickedElement.href.lastIndexOf('.csv') != (clickedElement.href.length - 4))
+		&& (clickedElement.href.lastIndexOf('.xls') != (clickedElement.href.length - 4)) && ((clickedElement.onclick == null) || (clickedElement.onclick
+		.toString().indexOf('window.open') <= 0)))
+		|| (clickedElement.parentNode.tagName.toUpperCase() == 'A'
+				&& ((clickedElement.parentNode.target == null) || (clickedElement.parentNode.target.length <= 0))
+				&& (clickedElement.parentNode.href.indexOf('skype') < 0)
+				&& (clickedElement.parentNode.href.indexOf('mailto') < 0)
+				&& (clickedElement.parentNode.href.lastIndexOf('#') != (clickedElement.parentNode.href.length - 1))
+				&& (clickedElement.parentNode.href.lastIndexOf('.doc') != (clickedElement.parentNode.href.length - 4))
+				&& (clickedElement.parentNode.href.lastIndexOf('.csv') != (clickedElement.parentNode.href.length - 4))
+				&& (clickedElement.parentNode.href.lastIndexOf('.xls') != (clickedElement.parentNode.href.length - 4)) && ((clickedElement.parentNode.onclick == null) || (clickedElement.parentNode.onclick
+				.toString().indexOf('window.open') <= 0)))
+		|| (((clickedElement.onclick == null) || ((clickedElement.onclick
+				.toString().indexOf('confirm') <= 0)
+				&& (clickedElement.onclick.toString().indexOf('alert') <= 0) && (clickedElement.onclick
+				.toString().indexOf('Wicket.Palette') <= 0))) && (clickedElement.tagName
+				.toUpperCase() == 'INPUT' && (clickedElement.type.toUpperCase() == 'BUTTON'
+				|| clickedElement.type.toUpperCase() == 'SUBMIT' || clickedElement.type
+				.toUpperCase() == 'IMAGE')))) {
+	showBusysign();
 }
