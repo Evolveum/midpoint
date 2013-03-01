cf8c59ad30d68b50143371e4931240dcc4d17750
Radovan Semancik <radovan.semancik@evolveum.com>
2013-03-01 11:51:47 +0000

    A bit better run script

---
+++ tools/repo-ninja/src/other/ninja.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MIDPOINT_HOME=/var/opt/midpoint
+NINJA_JAR=repo-ninja-2.1.1-SNAPSHOT.jar
+
+NINJA_HOME="`dirname \"$0\"`"
+
+exec java -Dmidpoint.home=$MIDPOINT_HOME -jar $NINJA_HOME/$NINJA_JAR "$@"
