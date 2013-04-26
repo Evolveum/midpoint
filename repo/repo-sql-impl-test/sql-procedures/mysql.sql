35da25564598674d11771f3292d3540641ec1167
Viliam Repan <viliam.repan@evolveum.com>
2013-04-26 16:25:23 +0000

    sql cleanup procedures fix.

--- repo/repo-sql-impl-test/sql-procedures/mysql.sql
+++ repo/repo-sql-impl-test/sql-procedures/mysql.sql
@@ -15,6 +15,7 @@ DETERMINISTIC
     DELETE FROM m_any_date;
     DELETE FROM m_any_long;
     DELETE FROM m_any_string;
+    DELETE FROM m_any_poly_string;
     DELETE FROM m_any_reference;
     DELETE FROM m_any;
 
--- repo/repo-sql-impl-test/sql-procedures/sqlserver.sql
+++ repo/repo-sql-impl-test/sql-procedures/sqlserver.sql
@@ -19,6 +19,7 @@ AS
     DELETE FROM m_any_date;
     DELETE FROM m_any_long;
     DELETE FROM m_any_string;
+    DELETE FROM m_any_poly_string;
     DELETE FROM m_any_reference;
     DELETE FROM m_any;
 
