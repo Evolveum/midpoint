3368beac712953cde6316b675b831861fd534865
Viliam Repan <viliam.repan@evolveum.com>
2013-05-21 18:04:01 +0000

    licence change. MID-1337

--- infra/prism/src/main/java/com/evolveum/midpoint/prism/polystring/PolyStringNormalizer.java
+++ infra/prism/src/main/java/com/evolveum/midpoint/prism/polystring/PolyStringNormalizer.java
@@ -1,21 +1,17 @@
-/**
- * Copyright (c) 2011 Evolveum
+/*
+ * Copyright (c) 2010-2013 Evolveum
  *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You can obtain a copy of the License at
- * http://www.opensource.org/licenses/cddl1 or
- * CDDLv1.0.txt file in the source code distribution.
- * See the License for the specific language governing
- * permission and limitations under the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * Portions Copyrighted 2011 [name of copyright owner]
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.evolveum.midpoint.prism.polystring;
 
