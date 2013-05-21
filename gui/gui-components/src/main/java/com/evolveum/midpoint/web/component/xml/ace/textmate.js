1e921a1ddfacc62c999884d6432cf9fe4f402156
Viliam Repan <viliam.repan@evolveum.com>
2013-05-21 17:49:42 +0000

    licence change. MID-1337

--- gui/gui-components/src/main/java/com/evolveum/midpoint/web/component/xml/ace/textmate.js
+++ gui/gui-components/src/main/java/com/evolveum/midpoint/web/component/xml/ace/textmate.js
@@ -1,24 +1,3 @@
-/*
- * Copyright (c) 2011 Evolveum
- *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the License at
- * http://www.opensource.org/licenses/cddl1 or
- * CDDLv1.0.txt file in the source code distribution.
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- *
- * Portions Copyrighted 2011 [name of copyright owner]
- */
-
 ace.define('ace/theme/textmate', ['require', 'exports', 'module' , 'ace/lib/dom'], function(require, exports, module) {
 "use strict";
 
