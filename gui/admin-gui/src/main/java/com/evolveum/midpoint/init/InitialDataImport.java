0fc6997c3f095d4d8b8234d1797532fc13b3b739
Viliam Repan <viliam.repan@evolveum.com>
2013-05-21 08:28:38 +0000

    initial import objects cleanup, just a bit.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/init/InitialDataImport.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/init/InitialDataImport.java
@@ -22,35 +22,27 @@
 package com.evolveum.midpoint.init;
 
 import com.evolveum.midpoint.model.api.ModelService;
+import com.evolveum.midpoint.prism.Objectable;
 import com.evolveum.midpoint.prism.PrismContext;
 import com.evolveum.midpoint.prism.PrismObject;
-import com.evolveum.midpoint.prism.delta.ChangeType;
 import com.evolveum.midpoint.prism.delta.ObjectDelta;
 import com.evolveum.midpoint.prism.dom.PrismDomProcessor;
-import com.evolveum.midpoint.prism.xml.PrismJaxbProcessor;
 import com.evolveum.midpoint.schema.constants.SchemaConstants;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.task.api.Task;
 import com.evolveum.midpoint.task.api.TaskManager;
-import com.evolveum.midpoint.util.QNameUtil;
 import com.evolveum.midpoint.util.exception.ObjectNotFoundException;
 import com.evolveum.midpoint.util.logging.LoggingUtils;
 import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.web.util.WebMiscUtil;
-import com.evolveum.midpoint.xml.ns._public.common.common_2a.ObjectType;
-
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.xml.bind.JAXBElement;
-
 import java.io.File;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
+import java.util.List;
 
 /**
  * @author lazyman
@@ -58,11 +50,13 @@ import java.net.URL;
 public class InitialDataImport {
 
     private static final Trace LOGGER = TraceManager.getTrace(InitialDataImport.class);
+
     private static final String DOT_CLASS = InitialDataImport.class.getName() + ".";
     private static final String OPERATION_INITIAL_OBJECTS_IMPORT = DOT_CLASS + "initialObjectsImport";
     private static final String OPERATION_IMPORT_OBJECT = DOT_CLASS + "importObject";
 
-    private final String[] FILES_FOR_IMPORT = new String[]{"globalPasswordPolicy.xml", "systemConfiguration.xml", "admin.xml", "task-cleanup.xml"};
+    private static final String OBJECTS_FILE = "objects.xml";
+
     @Autowired(required = true)
     private transient PrismContext prismContext;
     private ModelService model;
@@ -78,32 +72,31 @@ public class InitialDataImport {
         this.taskManager = taskManager;
     }
 
-    @SuppressWarnings("unchecked")
     public void init() {
         LOGGER.info("Starting initial object import.");
 
         OperationResult mainResult = new OperationResult(OPERATION_INITIAL_OBJECTS_IMPORT);
         Task task = taskManager.createTaskInstance(OPERATION_INITIAL_OBJECTS_IMPORT);
         task.setChannel(SchemaConstants.CHANNEL_GUI_INIT_URI);
-        for (String file : FILES_FOR_IMPORT) {
-            OperationResult result = mainResult.createSubresult(OPERATION_IMPORT_OBJECT);
 
-            try {
-            	File resource = getResource(file);
-                PrismDomProcessor domProcessor = prismContext.getPrismDomProcessor();
-                PrismObject prismObj = domProcessor.parseObject(resource);
+        try {
+            PrismDomProcessor domProcessor = prismContext.getPrismDomProcessor();
+
+            List<PrismObject<? extends Objectable>> objects = domProcessor.parseObjects(getResource(OBJECTS_FILE));
+            for (PrismObject object : objects) {
+                OperationResult result = mainResult.createSubresult(OPERATION_IMPORT_OBJECT);
 
                 boolean importObject = true;
                 try {
-                    model.getObject(prismObj.getCompileTimeClass(), prismObj.getOid(), null, task, result);
+                    model.getObject(object.getCompileTimeClass(), object.getOid(), null, task, result);
                     importObject = false;
                     result.recordSuccess();
                 } catch (ObjectNotFoundException ex) {
                     importObject = true;
                 } catch (Exception ex) {
                     LoggingUtils.logException(LOGGER, "Couldn't get object with oid {} from model", ex,
-                            prismObj.getOid());
-                    result.recordWarning("Couldn't get object with oid '" + prismObj.getOid() + "' from model",
+                            object.getOid());
+                    result.recordWarning("Couldn't get object with oid '" + object.getOid() + "' from model",
                             ex);
                 }
 
@@ -111,31 +104,27 @@ public class InitialDataImport {
                     continue;
                 }
 
-                ObjectDelta delta = ObjectDelta.createAddDelta(prismObj);
+                ObjectDelta delta = ObjectDelta.createAddDelta(object);
                 model.executeChanges(WebMiscUtil.createDeltaCollection(delta), null, task, result);
                 result.recordSuccess();
-            } catch (Exception ex) {
-                LoggingUtils.logException(LOGGER, "Couldn't import file {}", ex, file);
-                result.recordFatalError("Couldn't import file '" + file + "'", ex);
-            } finally {
-//                if (stream != null) {
-//                    IOUtils.closeQuietly(stream);
-//                }
-                result.computeStatus("Couldn't import objects.");
             }
+        } catch (Exception ex) {
+            LoggingUtils.logException(LOGGER, "Couldn't import file {}", ex, OBJECTS_FILE);
+            mainResult.recordFatalError("Couldn't import file '" + OBJECTS_FILE + "'", ex);
         }
-        mainResult.recordSuccess();
+
+        mainResult.recomputeStatus("Couldn't import objects.");
         LOGGER.info("Initial object import finished.");
         LOGGER.info("Initialization status:\n" + mainResult.dump());
     }
 
     private File getResource(String name) {
-        URI path = null;
-		try {
-			path = InitialDataImport.class.getClassLoader().getResource(name).toURI();
-		} catch (URISyntaxException e) {
-			throw new IllegalArgumentException("parameter name = " + name, e);
-		}        
+        URI path;
+        try {
+            path = InitialDataImport.class.getClassLoader().getResource(name).toURI();
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("parameter name = " + name, e);
+        }
         return new File(path);
     }
 }
