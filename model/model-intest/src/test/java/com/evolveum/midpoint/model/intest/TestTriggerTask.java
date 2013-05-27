c3c27262f3fb1c911ec89ba1369fdfe5160cddff
Radovan Semancik <radovan.semancik@evolveum.com>
2013-05-27 09:24:48 +0000

    Disabling the test as it is still work in progress

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestTriggerTask.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestTriggerTask.java
@@ -77,7 +77,8 @@ public class TestTriggerTask extends AbstractInitializedModelIntegrationTest {
 		triggerHandlerRegistry.register(MockTriggerHandler.HANDLER_URI, testTriggerHandler);
 	}
 
-	@Test
+	// WORK IN PROGRESS (MID-1272)
+	@Test(enabled=false)
     public void test100ImportScannerTask() throws Exception {
 		final String TEST_NAME = "test100ImportScannerTask";
         displayTestTile(this, TEST_NAME);
