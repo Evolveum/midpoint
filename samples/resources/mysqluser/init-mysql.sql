5d04e05c1320cf0899ca01e43ebb614f8350eafd
Ivan Noris <ivan.noris@evolveum.com>
2013-06-11 12:55:04 +0000

    Fixed init script for MySQL.

--- samples/resources/mysqluser/init-mysql.sql
+++ samples/resources/mysqluser/init-mysql.sql
@@ -6,7 +6,9 @@
 -- set global general_log = 0;
 -- Administrative user for midPoint
 CREATE USER 'midpointadmin'@'%' IDENTIFIED BY 'secret';
+CREATE USER 'midpointadmin'@'localhost' IDENTIFIED BY 'secret';
 GRANT ALL on *.* TO 'midpointadmin'@'%' WITH GRANT OPTION;
+GRANT ALL on *.* TO 'midpointadmin'@'localhost' WITH GRANT OPTION;
 
 -- Sample prototype user used as User Model in connector
 -- This sample user has global SELECT privilege
