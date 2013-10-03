17520c4d1f0f02180a7c7c4b38145d8e7138190c
Viliam Repan <vilo.repan@evolveum.com>
2013-10-03 23:47:31 +0200

    paging reimplemented.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/data/TablePanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/data/TablePanel.java
@@ -17,20 +17,21 @@
 package com.evolveum.midpoint.web.component.data;
 
 import com.evolveum.midpoint.prism.query.ObjectPaging;
+import com.evolveum.midpoint.web.component.data.paging.NavigatorPanel2;
+import com.evolveum.midpoint.web.component.util.LoadableModel;
 import org.apache.commons.lang.Validate;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
-import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.markup.html.navigation.paging.IPageable;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.data.DataViewBase;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.model.StringResourceModel;
 
 import java.util.List;
 
@@ -39,9 +40,8 @@ import java.util.List;
  */
 public class TablePanel<T> extends Panel {
 
-    private static final String TABLE = "table";
-    private static final String NAV_BOTTOM = "navigatorBottom";
-    private NavigatorPanel bottomNavigator;
+    private static final String ID_TABLE = "table";
+    private static final String ID_PAGING = "paging";
 
     public TablePanel(String id, ISortableDataProvider provider, List<IColumn<T, String>> columns) {
         this(id, provider, columns, 10);
@@ -58,7 +58,7 @@ public class TablePanel<T> extends Panel {
     }
 
     private void initLayout(List<IColumn<T, String>> columns, int itemsPerPage, ISortableDataProvider provider) {
-        DataTable<T, String> table = new SelectableDataTable<T>(TABLE, columns, provider, itemsPerPage);
+        DataTable<T, String> table = new SelectableDataTable<T>(ID_TABLE, columns, provider, itemsPerPage);
         table.setOutputMarkupId(true);
 
         TableHeadersToolbar headers = new TableHeadersToolbar(table, provider);
@@ -67,15 +67,8 @@ public class TablePanel<T> extends Panel {
 
         add(table);
 
-        bottomNavigator = new NavigatorPanel(NAV_BOTTOM, table, showPagedPaging(provider)) {
-
-            @Override
-            protected void onAjaxEvent(AjaxRequestTarget target) {
-                super.onAjaxEvent(target);
-            }
-        };
-
-        add(bottomNavigator);
+        NavigatorPanel2 nb2 = new NavigatorPanel2(ID_PAGING, table, showPagedPaging(provider));
+        add(nb2);
     }
 
     private boolean showPagedPaging(ISortableDataProvider provider) {
@@ -88,7 +81,7 @@ public class TablePanel<T> extends Panel {
     }
 
     public DataTable getDataTable() {
-        return (DataTable) get(TABLE);
+        return (DataTable) get(ID_TABLE);
     }
 
     public void setItemsPerPage(int size) {
@@ -111,7 +104,7 @@ public class TablePanel<T> extends Panel {
     }
 
     public void setShowPaging(boolean showPaging) {
-        Component nav = get(NAV_BOTTOM);
+        Component nav = get(ID_PAGING);
         nav.setVisible(showPaging);
 
         if (!showPaging) {
@@ -133,5 +126,46 @@ public class TablePanel<T> extends Panel {
 
         DataTable table = getDataTable();
         table.add(new AttributeModifier("style", new Model(value)));
+    }
+
+    private IModel<String> createModel(final IPageable pageable) {
+        return new LoadableModel<String>() {
+
+            @Override
+            protected String load() {
+                long from = 0;
+                long to = 0;
+                long count = 0;
+
+                if (pageable instanceof DataViewBase) {
+                    DataViewBase view = (DataViewBase) pageable;
+
+                    from = view.getFirstItemOffset() + 1;
+                    to = from + view.getItemsPerPage() - 1;
+                    long itemCount = view.getItemCount();
+                    if (to > itemCount) {
+                        to = itemCount;
+                    }
+                    count = itemCount;
+                } else if (pageable instanceof DataTable) {
+                    DataTable table = (DataTable) pageable;
+
+                    from = table.getCurrentPage() * table.getItemsPerPage() + 1;
+                    to = from + table.getItemsPerPage() - 1;
+                    long itemCount = table.getItemCount();
+                    if (to > itemCount) {
+                        to = itemCount;
+                    }
+                    count = itemCount;
+                }
+
+                if (count > 0) {
+                    return new StringResourceModel("TablePanel.label", this, null,
+                            new Object[]{from, to, count}).getString();
+                }
+
+                return new StringResourceModel("TablePanel.noFound", this, null).getString();
+            }
+        };
     }
 }
