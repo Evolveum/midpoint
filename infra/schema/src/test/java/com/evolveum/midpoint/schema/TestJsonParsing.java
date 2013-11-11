c9cdd2559d8c83048196bb754220d137dce2a3e9
katkav <k.valalikova@evolveum.com>
2013-11-11 17:03:23 +0100

    fixing build after merge of rest..

--- infra/schema/src/test/java/com/evolveum/midpoint/schema/TestJsonParsing.java
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/TestJsonParsing.java
@@ -2,25 +2,12 @@ package com.evolveum.midpoint.schema;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.Test;
 
-import com.evolveum.midpoint.prism.PrismConstants;
 import com.evolveum.midpoint.prism.PrismContext;
-import com.evolveum.midpoint.prism.PrismInternalTestUtil;
 import com.evolveum.midpoint.prism.PrismObject;
-import com.evolveum.midpoint.prism.foo.ObjectFactory;
 import com.evolveum.midpoint.prism.json.PrismJasonProcessor;
-import com.evolveum.midpoint.prism.schema.SchemaRegistry;
-import com.evolveum.midpoint.prism.util.PrismContextFactory;
-import com.evolveum.midpoint.prism.util.PrismTestUtil;
-import com.evolveum.midpoint.prism.xml.DynamicNamespacePrefixMapper;
-import com.evolveum.midpoint.prism.xml.GlobalDynamicNamespacePrefixMapper;
-import com.evolveum.midpoint.util.exception.SchemaException;
 import com.evolveum.midpoint.xml.ns._public.common.common_2a.UserType;
 
 public class TestJsonParsing {
