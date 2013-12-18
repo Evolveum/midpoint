5e9d54d4708dded67b529e00e91b831ab466b338
Viliam Repan <vilo.repan@evolveum.com>
2013-12-18 20:19:23 +0100

    updated 2.2 to 2.3 upgrade scripts after schema changes, objectTemplate added to system configuration,

--- config/sql/midpoint/2.2/postgresql/postgresql-upgrade-2.2-to-2.3.sql
+++ config/sql/midpoint/2.2/postgresql/postgresql-upgrade-2.2-to-2.3.sql
@@ -8,52 +8,53 @@ ALTER TABLE m_focus ADD disableReason VARCHAR(255);
 ALTER TABLE m_shadow ADD disableReason VARCHAR(255);
 
 CREATE TABLE m_report (
-    name_norm VARCHAR(255),
-    name_orig VARCHAR(255),
-    class_namespace VARCHAR(255),
-    class_localPart VARCHAR(100),
-    query TEXT,
-    reportExport INT4,
-    reportFields TEXT,
-    reportOrientation INT4,
-    reportParameters TEXT,
-    reportTemplateJRXML TEXT,
-    reportTemplateStyleJRTX TEXT,
-    id INT8 NOT NULL,
-    oid VARCHAR(36) NOT NULL,
-    PRIMARY KEY (id, oid),
-    UNIQUE (name_norm)
+  name_norm               VARCHAR(255),
+  name_orig               VARCHAR(255),
+  class_namespace         VARCHAR(255),
+  class_localPart         VARCHAR(100),
+  query                   TEXT,
+  reportExport            INT4,
+  reportFields            TEXT,
+  reportOrientation       INT4,
+  reportParameters        TEXT,
+  reportTemplateJRXML     TEXT,
+  reportTemplateStyleJRTX TEXT,
+  id                      INT8        NOT NULL,
+  oid                     VARCHAR(36) NOT NULL,
+  PRIMARY KEY (id, oid),
+  UNIQUE (name_norm)
 );
 
 CREATE INDEX iReportName ON m_report (name_orig);
 
-ALTER TABLE m_report 
-    ADD CONSTRAINT fk_report 
-    FOREIGN KEY (id, oid) 
-    REFERENCES m_object;
+ALTER TABLE m_report
+ADD CONSTRAINT fk_report
+FOREIGN KEY (id, oid)
+REFERENCES m_object;
 
 CREATE INDEX iAncestorDepth ON m_org_closure (ancestor_id, ancestor_oid, depthValue);
 
 CREATE TABLE m_report_output (
-    name_norm VARCHAR(255),
-    name_orig VARCHAR(255),
-    reportFilePath VARCHAR(255),
-    reportRef_description TEXT,
-    reportRef_filter TEXT,
-    reportRef_relationLocalPart VARCHAR(100),
-    reportRef_relationNamespace VARCHAR(255),
-    reportRef_targetOid VARCHAR(36),
-    reportRef_type INT4,
-    id INT8 NOT NULL,
-    oid VARCHAR(36) NOT NULL,
-    PRIMARY KEY (id, oid),
-    UNIQUE (name_norm)
+  name_norm                   VARCHAR(255),
+  name_orig                   VARCHAR(255),
+  reportFilePath              VARCHAR(255),
+  reportRef_description       TEXT,
+  reportRef_filter            TEXT,
+  reportRef_relationLocalPart VARCHAR(100),
+  reportRef_relationNamespace VARCHAR(255),
+  reportRef_targetOid         VARCHAR(36),
+  reportRef_type              INT4,
+  id                          INT8        NOT NULL,
+  oid                         VARCHAR(36) NOT NULL,
+  PRIMARY KEY (id, oid),
+  UNIQUE (name_norm)
 );
-	
+
 CREATE INDEX iReportOutputName ON m_report_output (name_orig);
 
-ALTER TABLE m_report_output 
-    ADD CONSTRAINT fk_reportoutput 
-    FOREIGN KEY (id, oid) 
-    REFERENCES m_object;
+ALTER TABLE m_report_output
+ADD CONSTRAINT fk_reportoutput
+FOREIGN KEY (id, oid)
+REFERENCES m_object;
 
+ALTER TABLE m_system_configuration ADD objectTemplate TEXT;
