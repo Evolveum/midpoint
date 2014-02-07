de7bd3ce47ed5a6d53b0d545b548112f4fbc306f
Viliam Repan <vilo.repan@evolveum.com>
2014-02-07 13:43:37 +0100

    fixed upgrade scripts from 2.2 to 2.2.1, added missing columns.

--- config/sql/midpoint/2.2/oracle/oracle-upgrade-2.2-to-2.2.1.sql
+++ config/sql/midpoint/2.2/oracle/oracle-upgrade-2.2-to-2.2.1.sql
@@ -6,5 +6,9 @@ ALTER TABLE m_task ADD expectedTotal NUMBER(19, 0);
 ALTER TABLE m_assignment ADD disableReason VARCHAR2(255 CHAR);
 ALTER TABLE m_focus ADD disableReason VARCHAR2(255 CHAR);
 ALTER TABLE m_shadow ADD disableReason VARCHAR2(255 CHAR);
+ALTER TABLE m_audit_delta ADD context CLOB;
+ALTER TABLE m_audit_delta ADD returns CLOB;
+ALTER TABLE m_operation_result ADD context CLOB;
+ALTER TABLE m_operation_result ADD returns CLOB;
 
 CREATE INDEX iAncestorDepth ON m_org_closure (ancestor_id, ancestor_oid, depthValue) INITRANS 30;
