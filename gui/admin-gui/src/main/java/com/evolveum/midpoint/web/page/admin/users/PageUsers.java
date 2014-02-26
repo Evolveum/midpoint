bb299c071fac34d8a2e42af19a2a1316cd1434d7
Viliam Repan <vilo.repan@evolveum.com>
2014-02-26 11:02:56 +0100

    updated search on user list page.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/users/PageUsers.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/users/PageUsers.java
@@ -36,7 +36,7 @@ import com.evolveum.midpoint.util.logging.LoggingUtils;
 import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.web.application.PageDescriptor;
-import com.evolveum.midpoint.web.component.AjaxSubmitButton;
+import com.evolveum.midpoint.web.component.BasicSearchPanel;
 import com.evolveum.midpoint.web.component.DropDownMultiChoice;
 import com.evolveum.midpoint.web.component.data.ObjectDataProvider;
 import com.evolveum.midpoint.web.component.data.TablePanel;
@@ -51,7 +51,6 @@ import com.evolveum.midpoint.web.page.admin.users.dto.UserListItemDto;
 import com.evolveum.midpoint.web.page.admin.users.dto.UsersDto;
 import com.evolveum.midpoint.web.session.UsersStorage;
 import com.evolveum.midpoint.web.util.OnePageParameterEncoder;
-import com.evolveum.midpoint.web.util.SearchFormEnterBehavior;
 import com.evolveum.midpoint.web.util.WebMiscUtil;
 import com.evolveum.midpoint.xml.ns._public.common.common_2a.*;
 import org.apache.commons.lang.StringUtils;
@@ -62,7 +61,6 @@ import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -98,10 +96,8 @@ public class PageUsers extends PageAdminUsers {
     private static final String ID_MAIN_FORM = "mainForm";
     private static final String ID_TABLE = "table";
     private static final String ID_SEARCH_FORM = "searchForm";
-    private static final String ID_SEARCH_TEXT = "searchText";
+    private static final String ID_BASIC_SEARCH = "basicSearch";
     private static final String ID_SEARCH_TYPE = "searchType";
-    private static final String ID_SEARCH_BUTTON = "searchButton";
-    private static final String ID_SEARCH_CLEAR = "searchClear";
 
     private UserListItemDto singleDelete;
     private LoadableModel<UsersDto> model;
@@ -186,7 +182,7 @@ public class PageUsers extends PageAdminUsers {
             }
         });
 
-        IColumn column = new LinkColumn<UserListItemDto>(createStringResource("pageUsers.name"),
+        IColumn column = new LinkColumn<UserListItemDto>(createStringResource("ObjectType.name"),
                 UserType.F_NAME.getLocalPart(), UserListItemDto.F_NAME) {
 
             @Override
@@ -196,19 +192,19 @@ public class PageUsers extends PageAdminUsers {
         };
         columns.add(column);
 
-        column = new PropertyColumn(createStringResource("pageUsers.givenName"),
+        column = new PropertyColumn(createStringResource("UserType.givenName"),
                 UserType.F_GIVEN_NAME.getLocalPart(), UserListItemDto.F_GIVEN_NAME);
         columns.add(column);
 
-        column = new PropertyColumn(createStringResource("pageUsers.familyName"),
+        column = new PropertyColumn(createStringResource("UserType.familyName"),
                 UserType.F_FAMILY_NAME.getLocalPart(), UserListItemDto.F_FAMILY_NAME);
         columns.add(column);
 
-        column = new PropertyColumn(createStringResource("pageUsers.fullName"),
+        column = new PropertyColumn(createStringResource("UserType.fullName"),
                 UserType.F_FULL_NAME.getLocalPart(), UserListItemDto.F_FULL_NAME);
         columns.add(column);
 
-        column = new PropertyColumn(createStringResource("pageUsers.email"), null, UserListItemDto.F_EMAIL);
+        column = new PropertyColumn(createStringResource("UserType.emailAddress"), null, UserListItemDto.F_EMAIL);
         columns.add(column);
 
         column = new PropertyColumn(createStringResource("pageUsers.accounts"), null, UserListItemDto.F_ACCOUNT_COUNT);
@@ -386,39 +382,24 @@ public class PageUsers extends PageAdminUsers {
                 }, options);
         searchForm.add(searchType);
 
-        final AjaxSubmitButton searchButton = new AjaxSubmitButton(ID_SEARCH_BUTTON,
-                createStringResource("pageUsers.button.searchButton")) {
+        BasicSearchPanel<UsersDto> basicSearch = new BasicSearchPanel<UsersDto>(ID_BASIC_SEARCH, model) {
 
             @Override
-            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                searchPerformed(target);
+            protected IModel<String> createSearchTextModel() {
+                return new PropertyModel<String>(model, UsersDto.F_TEXT);
             }
 
             @Override
-            protected void onError(AjaxRequestTarget target, Form<?> form) {
-                target.add(getFeedbackPanel());
-            }
-        };
-        searchForm.add(searchButton);
-
-        final TextField searchText = new TextField(ID_SEARCH_TEXT, new PropertyModel<String>(model,
-                UsersDto.F_TEXT));
-        searchText.add(new SearchFormEnterBehavior(searchButton));
-        searchForm.add(searchText);
-
-        AjaxSubmitButton clearButton = new AjaxSubmitButton(ID_SEARCH_CLEAR) {
-
-            @Override
-            protected void onSubmit(AjaxRequestTarget target, Form<?> form){
-                clearSearchPerformed(target);
+            protected void searchPerformed(AjaxRequestTarget target) {
+                PageUsers.this.searchPerformed(target);
             }
 
             @Override
-            protected void onError(AjaxRequestTarget target, Form<?> form) {
-                target.add(getFeedbackPanel());
+            protected void clearSearchPerformed(AjaxRequestTarget target) {
+                PageUsers.this.clearSearchPerformed(target);
             }
         };
-        searchForm.add(clearButton);
+        searchForm.add(basicSearch);
     }
 
     private void userDetailsPerformed(AjaxRequestTarget target, String oid) {
