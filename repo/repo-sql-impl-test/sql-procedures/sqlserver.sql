42dbb3b16eb4c1e981dfdb33390032fee979ddfc
Viliam Repan <vilo.repan@evolveum.com>
2014-04-05 20:05:47 +0200

    updated cleanup procedures.

--- repo/repo-sql-impl-test/sql-procedures/sqlserver.sql
+++ repo/repo-sql-impl-test/sql-procedures/sqlserver.sql
@@ -14,26 +14,25 @@ AS
 
     DELETE FROM m_audit_delta;
     DELETE FROM m_audit_event;
-
-    DELETE FROM m_any_clob;
-    DELETE FROM m_any_date;
-    DELETE FROM m_any_long;
-    DELETE FROM m_any_string;
-    DELETE FROM m_any_poly_string;
-    DELETE FROM m_any_reference;
-    DELETE FROM m_any;
-
+    DELETE FROM m_object_ext_date;
+    DELETE FROM m_object_ext_long;
+    DELETE FROM m_object_ext_string;
+    DELETE FROM m_object_ext_poly;
+    DELETE FROM m_object_ext_reference;
     DELETE FROM m_reference;
-
+    DELETE FROM m_assignment_ext_date;
+    DELETE FROM m_assignment_ext_long;
+    DELETE FROM m_assignment_ext_poly;
+    DELETE FROM m_assignment_ext_reference;
+    DELETE FROM m_assignment_ext_string;
+    DELETE FROM m_assignment_extension;
+    DELETE FROM m_assignment_reference;
     DELETE FROM m_assignment;
     DELETE FROM m_exclusion;
-    DELETE FROM m_operation_result;
-
     DELETE FROM m_connector_target_system;
     DELETE FROM m_connector;
     DELETE FROM m_connector_host;
     DELETE FROM m_node;
-    DELETE FROM m_sync_situation_description;
     DELETE FROM m_shadow;
     DELETE FROM m_task_dependent;
     DELETE FROM m_task;
@@ -44,8 +43,8 @@ AS
     DELETE FROM m_user_organization;
     DELETE FROM m_user_organizational_unit;
     DELETE FROM m_user;
-	DELETE FROM m_report;
-	DELETE FROM m_report_output;
+    DELETE FROM m_report;
+    DELETE FROM m_report_output;
     DELETE FROM m_org_org_type;
     DELETE FROM m_authorization_action;
     DELETE FROM m_authorization;
@@ -55,17 +54,12 @@ AS
     DELETE FROM m_system_configuration;
     DELETE FROM m_generic_object;
     DELETE FROM m_trigger;
-	DELETE FROM m_security_policy;
-
     DELETE FROM m_org_incorrect;
     DELETE FROM m_org_closure;
-
-    DELETE FROM m_metadata;
-
     DELETE FROM m_focus;
+    DELETE FROM m_security_policy;
     DELETE FROM m_object;
-    DELETE FROM m_container;
 
-    UPDATE hibernate_sequence SET next_val=1;
+    UPDATE hibernate_sequence SET next_val = 1;
   END
 GO
