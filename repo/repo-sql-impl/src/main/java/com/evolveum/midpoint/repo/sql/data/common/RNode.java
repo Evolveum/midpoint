4b7779d6764fa7ec2a50780b649556e899c82386
Viliam Repan <vilo.repan@evolveum.com>
2014-04-07 10:56:21 +0200

    removed unnecessary index and some columns.

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/RNode.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/RNode.java
@@ -39,40 +39,6 @@ public class RNode extends RObject<NodeType> {
 
     private RPolyString name;
     private String nodeIdentifier;
-    private String hostname;
-    private Integer jmxPort;
-    private XMLGregorianCalendar lastCheckInTime;
-    private Boolean running;
-    private Boolean clustered;
-    private String internalNodeIdentifier;
-
-    @Column(nullable = true)
-    public String getHostname() {
-        return hostname;
-    }
-
-    @Column(name = "clusteredNode")
-    public Boolean getClustered() {
-        return clustered;
-    }
-
-    public String getInternalNodeIdentifier() {
-        return internalNodeIdentifier;
-    }
-
-    @Column(nullable = true)
-    public Integer getJmxPort() {
-        return jmxPort;
-    }
-
-    @Column(nullable = true)
-    public XMLGregorianCalendar getLastCheckInTime() {
-        return lastCheckInTime;
-    }
-
-    public Boolean getRunning() {
-        return running;
-    }
 
     public String getNodeIdentifier() {
         return nodeIdentifier;
@@ -91,30 +57,6 @@ public class RNode extends RObject<NodeType> {
         this.nodeIdentifier = nodeIdentifier;
     }
 
-    public void setHostname(String hostname) {
-        this.hostname = hostname;
-    }
-
-    public void setClustered(Boolean clustered) {
-        this.clustered = clustered;
-    }
-
-    public void setInternalNodeIdentifier(String internalNodeIdentifier) {
-        this.internalNodeIdentifier = internalNodeIdentifier;
-    }
-
-    public void setJmxPort(Integer jmxPort) {
-        this.jmxPort = jmxPort;
-    }
-
-    public void setLastCheckInTime(XMLGregorianCalendar lastCheckInTime) {
-        this.lastCheckInTime = lastCheckInTime;
-    }
-
-    public void setRunning(Boolean running) {
-        this.running = running;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -124,16 +66,8 @@ public class RNode extends RObject<NodeType> {
         RNode rNode = (RNode) o;
 
         if (name != null ? !name.equals(rNode.name) : rNode.name != null) return false;
-        if (clustered != null ? !clustered.equals(rNode.clustered) : rNode.clustered != null) return false;
-        if (hostname != null ? !hostname.equals(rNode.hostname) : rNode.hostname != null) return false;
-        if (internalNodeIdentifier != null ? !internalNodeIdentifier.equals(rNode.internalNodeIdentifier) :
-                rNode.internalNodeIdentifier != null) return false;
-        if (jmxPort != null ? !jmxPort.equals(rNode.jmxPort) : rNode.jmxPort != null) return false;
-        if (lastCheckInTime != null ? !lastCheckInTime.equals(rNode.lastCheckInTime) : rNode.lastCheckInTime != null)
-            return false;
         if (nodeIdentifier != null ? !nodeIdentifier.equals(rNode.nodeIdentifier) : rNode.nodeIdentifier != null)
             return false;
-        if (running != null ? !running.equals(rNode.running) : rNode.running != null) return false;
 
         return true;
     }
@@ -143,12 +77,6 @@ public class RNode extends RObject<NodeType> {
         int result = super.hashCode();
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (nodeIdentifier != null ? nodeIdentifier.hashCode() : 0);
-        result = 31 * result + (hostname != null ? hostname.hashCode() : 0);
-        result = 31 * result + (jmxPort != null ? jmxPort.hashCode() : 0);
-        result = 31 * result + (lastCheckInTime != null ? lastCheckInTime.hashCode() : 0);
-        result = 31 * result + (running != null ? running.hashCode() : 0);
-        result = 31 * result + (clustered != null ? clustered.hashCode() : 0);
-        result = 31 * result + (internalNodeIdentifier != null ? internalNodeIdentifier.hashCode() : 0);
         return result;
     }
 
@@ -157,13 +85,7 @@ public class RNode extends RObject<NodeType> {
         RObject.copyFromJAXB(jaxb, repo, prismContext);
 
         repo.setName(RPolyString.copyFromJAXB(jaxb.getName()));
-        repo.setHostname(jaxb.getHostname());
         repo.setNodeIdentifier(jaxb.getNodeIdentifier());
-        repo.setJmxPort(jaxb.getJmxPort());
-        repo.setLastCheckInTime(jaxb.getLastCheckInTime());
-        repo.setRunning(jaxb.isRunning());
-        repo.setClustered(jaxb.isClustered());
-        repo.setInternalNodeIdentifier(jaxb.getInternalNodeIdentifier());
     }
 
     @Override
