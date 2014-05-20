25325eed9069e294250ec4df8bc5caa4d8abdf80
Radovan Semancik <radovan.semancik@evolveum.com>
2014-05-20 11:32:00 +0200

    Fixing spring contexts and test package names.

---
+++ model/model-impl/src/test/java/com/evolveum/midpoint/model/impl/controller/ControllerModifyObjectTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2010-2013 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.evolveum.midpoint.model.impl.controller;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+import com.evolveum.midpoint.model.impl.ModelCrudService;
+import com.evolveum.midpoint.prism.delta.ItemDelta;
+import com.evolveum.midpoint.provisioning.api.ProvisioningService;
+import com.evolveum.midpoint.repo.api.RepositoryService;
+import com.evolveum.midpoint.task.api.TaskManager;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
+
+/**
+ * 
+ * @author lazyman
+ * 
+ */
+@ContextConfiguration(locations = { "classpath:ctx-model-test-no-repo.xml" })
+public class ControllerModifyObjectTest extends AbstractTestNGSpringContextTests {
+
+	private static final Trace LOGGER = TraceManager.getTrace(ControllerModifyObjectTest.class);
+	@Autowired(required = true)
+	private ModelCrudService controller;
+	@Autowired(required = true)
+	@Qualifier("cacheRepositoryService")
+	private RepositoryService repository;
+	@Autowired(required = true)
+	private ProvisioningService provisioning;
+	@Autowired(required = true)
+	private TaskManager taskManager;
+
+	@BeforeMethod
+	public void before() {
+		Mockito.reset(repository, provisioning);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void nullChange() throws Exception {
+		controller.modifyObject(UserType.class, null, null, null, null, null);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void nullChangeOid() throws Exception {
+		controller.modifyObject(UserType.class, null, new ArrayList<ItemDelta>(), null, taskManager.createTaskInstance(), null);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void emptyChangeOid() throws Exception {
+		controller.modifyObject(UserType.class, "", new ArrayList<ItemDelta>(), null, taskManager.createTaskInstance(), null);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void nullResult() throws Exception {
+
+		controller.modifyObject(UserType.class, "1", new ArrayList<ItemDelta>(), null, taskManager.createTaskInstance(), null);
+	}
+}
