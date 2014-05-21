51d94cbe69f5d01ce23fbecdcc8ecc850fe6d3db
Viliam Repan <vilo.repan@evolveum.com>
2014-05-21 16:00:51 +0200

    fix for prism object panel header (error states were not computed correctly)

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/H3Header.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/H3Header.java
@@ -97,9 +97,16 @@ public class H3Header extends SimplePanel<ObjectWrapper> {
             @Override
             public boolean isVisible() {
                 OperationResult fetchResult = getModelObject().getFetchResult();
+                if (fetchResult != null && !WebMiscUtil.isSuccessOrHandledError(fetchResult)) {
+                    return true;
+                }
+
                 OperationResult result = getModelObject().getResult();
-                return !WebMiscUtil.isSuccessOrHandledError(fetchResult)
-                        || !WebMiscUtil.isSuccessOrHandledError(result);
+                if (result != null && !WebMiscUtil.isSuccessOrHandledError(result)) {
+                    return true;
+                }
+
+                return false;
             }
         };
     }
