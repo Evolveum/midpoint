556775fd098f5bad8684826800d440449fd73a1b
Radovan Semancik <radovan.semancik@evolveum.com>
2014-08-15 17:15:08 +0200

    Upgrading testng and surefire

--- tools/test-ng/src/main/java/com/evolveum/midpoint/tools/testng/TestMethodLoggerListener.java
+++ tools/test-ng/src/main/java/com/evolveum/midpoint/tools/testng/TestMethodLoggerListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Evolveum
+ * Copyright (c) 2010-2014 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,11 @@
 
 package com.evolveum.midpoint.tools.testng;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.testng.IConfigurationListener;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
@@ -27,16 +32,34 @@ import org.testng.ITestResult;
  * Time: 16:13
  * To change this template use File | Settings | File Templates.
  */
-public class TestMethodLoggerListener implements IInvokedMethodListener {
+public class TestMethodLoggerListener implements IInvokedMethodListener, IConfigurationListener {
+	
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	
     @Override
     public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
-        System.out.println("-----------------------------------------------------------");
-        System.out.println("TestNG running method: " + method.getTestMethod().toString());
+    	System.out.println(DATE_FORMAT.format(new Date()));
+        System.out.println("----[ TestNG running method: " + method.getTestMethod().toString() + " ("+testResult+") ]----");
     }
 
     @Override
     public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
-        System.out.println("TestNG finish method : " + method.getTestMethod().toString() + " Result: " + (testResult.isSuccess() ? "PASS" : "FAIL"));
-        System.out.println("-----------------------------------------------------------");
+    	System.out.println(DATE_FORMAT.format(new Date()));
+        System.out.println("----[ TestNG finished method : " + method.getTestMethod().toString() + " Result: " + (testResult.isSuccess() ? "PASS" : "FAIL")+" ]----");
     }
+
+	@Override
+	public void onConfigurationFailure(ITestResult res) {
+		System.out.println("--- TestNG configuration failure : " + res.getTestName());
+	}
+
+	@Override
+	public void onConfigurationSkip(ITestResult res) {
+		System.out.println("--- TestNG configuration skip : " + res.getTestName());
+	}
+
+	@Override
+	public void onConfigurationSuccess(ITestResult res) {
+		System.out.println("--- TestNG configuration success : " + res.getTestName());
+	}
 }
