307f53d2e06e473fea5d138f4f93f25768cc9d59
Radovan Semancik <radovan.semancik@evolveum.com>
2014-08-15 18:17:32 +0200

    Removing redundant context load.

--- model/model-impl/src/test/java/com/evolveum/midpoint/model/impl/lens/TestAssignmentProcessor.java
+++ model/model-impl/src/test/java/com/evolveum/midpoint/model/impl/lens/TestAssignmentProcessor.java
@@ -75,8 +75,6 @@ import static org.testng.AssertJUnit.*;
 /**
  * @author semancik
  */
-@ContextConfiguration(locations = {"classpath:ctx-model-test-main.xml"})
-@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class TestAssignmentProcessor extends AbstractLensTest {
 
     private static final ItemPath ATTRIBUTES_PARENT_PATH = new ItemPath(ShadowType.F_ATTRIBUTES);
