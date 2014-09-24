e11129591db337e6d6563ba08d7ef1dffa5f513d
Erik Suta <erik.suta@evolveum.com>
2014-09-24 13:08:29 +0200

    Fix for Google Chrome password auto-complete behavior.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/configuration/component/SystemConfigPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/configuration/component/SystemConfigPanel.java
@@ -28,13 +28,18 @@ import com.evolveum.midpoint.web.page.admin.dto.ObjectViewDto;
 import com.evolveum.midpoint.web.util.InfoTooltipBehavior;
 import com.evolveum.midpoint.web.util.WebMiscUtil;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.MailTransportSecurityType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectPolicyConfigurationType;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.form.EnumChoiceRenderer;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
+import com.evolveum.midpoint.web.component.form.Form;
 
 import java.util.List;
 
@@ -42,8 +47,6 @@ import java.util.List;
  * @author lazyman
  */
 public class SystemConfigPanel extends SimplePanel<SystemConfigurationDto> {
-
-    private static final Trace LOGGER = TraceManager.getTrace(SystemConfigPanel.class);
 
     private static final String ID_GLOBAL_PASSWORD_POLICY_CHOOSER = "passwordPolicyChooser";
     private static final String ID_GLOBAL_USER_TEMPLATE_CHOOSER = "userTemplateChooser";
@@ -63,11 +66,13 @@ public class SystemConfigPanel extends SimplePanel<SystemConfigurationDto> {
     private static final String ID_TRANSPORT_SECURITY = "transportSecurity";
     private static final String ID_REDIRECT_TO_FILE = "redirectToFile";
 
+    private static final String ID_MAIN_FORM = "mainForm";
+
     private static final String ID_LABEL_SIZE = "col-lg-4";
     private static final String ID_INPUT_SIZE = "col-lg-4";
 
-    private ChooseTypePanel passPolicyChoosePanel;
-    private ChooseTypePanel userTemplateChoosePanel;
+//    private ChooseTypePanel passPolicyChoosePanel;
+//    private ChooseTypePanel userTemplateChoosePanel;
 
     public SystemConfigPanel(String id, IModel<SystemConfigurationDto> model) {
         super(id, model);
@@ -75,17 +80,21 @@ public class SystemConfigPanel extends SimplePanel<SystemConfigurationDto> {
 
     @Override
     protected void initLayout(){
-        passPolicyChoosePanel = new ChooseTypePanel(ID_GLOBAL_PASSWORD_POLICY_CHOOSER,
+        Form form = new Form(ID_MAIN_FORM, true);
+        form.setOutputMarkupId(true);
+        add(form);
+
+        ChooseTypePanel passPolicyChoosePanel = new ChooseTypePanel(ID_GLOBAL_PASSWORD_POLICY_CHOOSER,
                 new PropertyModel<ObjectViewDto>(getModel(), SystemConfigurationDto.F_PASSWORD_POLICY));
-        userTemplateChoosePanel = new ChooseTypePanel(ID_GLOBAL_USER_TEMPLATE_CHOOSER,
+        ChooseTypePanel userTemplateChoosePanel = new ChooseTypePanel(ID_GLOBAL_USER_TEMPLATE_CHOOSER,
                 new PropertyModel<ObjectViewDto>(getModel(), SystemConfigurationDto.F_OBJECT_TEMPLATE));
 
-        add(passPolicyChoosePanel);
-        add(userTemplateChoosePanel);
+        form.add(passPolicyChoosePanel);
+        form.add(userTemplateChoosePanel);
 
         ObjectPolicyConfigurationEditor objectPolicyEditor = new ObjectPolicyConfigurationEditor(ID_OBJECT_POLICY_EDITOR,
                 new PropertyModel<List<ObjectPolicyConfigurationTypeDto>>(getModel(), SystemConfigurationDto.F_OBJECT_POLICY_LIST));
-        add(objectPolicyEditor);
+        form.add(objectPolicyEditor);
 
         DropDownChoice<AEPlevel> aepLevel = new DropDownChoice<>(ID_GLOBAL_AEP,
                 new PropertyModel<AEPlevel>(getModel(), SystemConfigurationDto.F_AEP_LEVEL),
@@ -95,15 +104,15 @@ public class SystemConfigPanel extends SimplePanel<SystemConfigurationDto> {
         if(aepLevel.getModel().getObject() == null){
             aepLevel.getModel().setObject(null);
         }
-        add(aepLevel);
+        form.add(aepLevel);
 
         TextField<String> auditRecordsField = new TextField<>(ID_CLEANUP_AUDIT_RECORDS, new PropertyModel<String>(getModel(), SystemConfigurationDto.F_AUDIT_CLEANUP));
         TextField<String> closedTasksField = new TextField<>(ID_CLEANUP_CLOSED_TASKS, new PropertyModel<String>(getModel(), SystemConfigurationDto.F_TASK_CLEANUP));
-        add(auditRecordsField);
-        add(closedTasksField);
+        form.add(auditRecordsField);
+        form.add(closedTasksField);
 
-        createTooltip(ID_CLEANUP_AUDIT_RECORDS_TOOLTIP, this);
-        createTooltip(ID_CLEANUP_CLOSED_TASKS_TOOLTIP, this);
+        createTooltip(ID_CLEANUP_AUDIT_RECORDS_TOOLTIP, form);
+        createTooltip(ID_CLEANUP_CLOSED_TASKS_TOOLTIP, form);
 
         TextField<String> defaultFromField = new TextField<>(ID_DEFAULT_FROM, new PropertyModel<String>(getModel(), "notificationConfig.defaultFrom"));
         CheckBox debugCheck = new CheckBox(ID_DEBUG, new PropertyModel<Boolean>(getModel(), "notificationConfig.debug"));
@@ -123,20 +132,19 @@ public class SystemConfigPanel extends SimplePanel<SystemConfigurationDto> {
 
         TextField<String> redirectToFileField = new TextField<>(ID_REDIRECT_TO_FILE, new PropertyModel<String>(getModel(), "notificationConfig.redirectToFile"));
 
-        IModel choices = WebMiscUtil.createReadonlyModelFromEnum(MailTransportSecurityType.class);
-        IChoiceRenderer renderer = new EnumChoiceRenderer();
-        DropDownFormGroup transportSecurity = new DropDownFormGroup(ID_TRANSPORT_SECURITY, new PropertyModel(getModel(),
-                "notificationConfig.mailTransportSecurityType"), choices, renderer,
-                createStringResource("SystemConfigPanel.mail.transportSecurity"),ID_LABEL_SIZE, ID_INPUT_SIZE, false);
+        DropDownFormGroup transportSecurity = new DropDownFormGroup<>(ID_TRANSPORT_SECURITY, new PropertyModel<MailTransportSecurityType>(getModel(),
+                "notificationConfig.mailTransportSecurityType"), WebMiscUtil.createReadonlyModelFromEnum(MailTransportSecurityType.class),
+                new EnumChoiceRenderer<MailTransportSecurityType>(this), createStringResource("SystemConfigPanel.mail.transportSecurity"),
+                ID_LABEL_SIZE, ID_INPUT_SIZE, false);
 
-        add(defaultFromField);
-        add(debugCheck);
-        add(hostField);
-        add(portField);
-        add(userNameField);
-        add(passwordField);
-        add(redirectToFileField);
-        add(transportSecurity);
+        form.add(defaultFromField);
+        form.add(debugCheck);
+        form.add(hostField);
+        form.add(portField);
+        form.add(userNameField);
+        form.add(passwordField);
+        form.add(redirectToFileField);
+        form.add(transportSecurity);
     }
 
     private void createTooltip(String id, WebMarkupContainer parent) {
