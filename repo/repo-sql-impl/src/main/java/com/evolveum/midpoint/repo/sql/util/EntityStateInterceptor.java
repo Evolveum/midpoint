578f3e0f7b622ce0e8dce0c2b25af986dc4c68e6
Viliam Repan <vilo.repan@evolveum.com>
2014-12-01 11:51:42 +0100

    audit operation improved, no select queries needed.

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/util/EntityStateInterceptor.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/util/EntityStateInterceptor.java
@@ -1,5 +1,6 @@
 package com.evolveum.midpoint.repo.sql.util;
 
+import com.evolveum.midpoint.repo.sql.data.audit.RObjectDeltaOperation;
 import com.evolveum.midpoint.repo.sql.data.common.RObject;
 import com.evolveum.midpoint.repo.sql.data.common.RObjectReference;
 import com.evolveum.midpoint.repo.sql.data.common.RUserPhoto;
