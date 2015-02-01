0c047c98686f2256203218d33989d0f3625b3485
Viliam Repan <vilo.repan@evolveum.com>
2015-02-01 13:35:49 +0100

    updated mysql scripts

--- config/sql/midpoint/3.1/mysql/mysql-3.1-closure-upgrade.sql
+++ config/sql/midpoint/3.1/mysql/mysql-3.1-closure-upgrade.sql
@@ -11,23 +11,23 @@ drop table if exists m_org_incorrect;
 --
 
 CREATE TABLE m_org_closure (
+  ancestor_oid   VARCHAR(36) NOT NULL,
   descendant_oid VARCHAR(36) NOT NULL,
-  ancestor_oid   VARCHAR(36) NOT NULL ,
-  val            INTEGER NOT NULL ,
-  PRIMARY KEY (descendant_oid, ancestor_oid)
+  val            INTEGER,
+  PRIMARY KEY (ancestor_oid, descendant_oid)
 )
   DEFAULT CHARACTER SET utf8
   COLLATE utf8_bin
-  ENGINE =InnoDB;
+  ENGINE = InnoDB;
+
+CREATE INDEX iDescendantAncestor ON m_org_closure (descendant_oid, ancestor_oid);
 
 ALTER TABLE m_org_closure
-ADD INDEX fk_ancestor (ancestor_oid),
 ADD CONSTRAINT fk_ancestor
 FOREIGN KEY (ancestor_oid)
 REFERENCES m_object (oid);
 
 ALTER TABLE m_org_closure
-ADD INDEX fk_descendant (descendant_oid),
 ADD CONSTRAINT fk_descendant
 FOREIGN KEY (descendant_oid)
 REFERENCES m_object (oid);
