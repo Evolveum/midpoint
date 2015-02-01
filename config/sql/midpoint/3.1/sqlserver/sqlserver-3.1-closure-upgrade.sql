71294d3f0cdf277e2a6ff7309add433a4416fa30
Viliam Repan <vilo.repan@evolveum.com>
2015-02-01 14:32:07 +0100

    updated sql server scripts

--- config/sql/midpoint/3.1/sqlserver/sqlserver-3.1-closure-upgrade.sql
+++ config/sql/midpoint/3.1/sqlserver/sqlserver-3.1-closure-upgrade.sql
@@ -4,15 +4,17 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'm_org_inc
   DROP TABLE m_org_incorrect;
 
 CREATE TABLE m_org_closure (
-  descendant_oid NVARCHAR(36) COLLATE database_default NOT NULL,
   ancestor_oid   NVARCHAR(36) COLLATE database_default NOT NULL,
-  val            INT NOT NULL,
-  PRIMARY KEY (descendant_oid, ancestor_oid)
+  descendant_oid NVARCHAR(36) COLLATE database_default NOT NULL,
+  val            INT,
+  PRIMARY KEY (ancestor_oid, descendant_oid)
 );
 
 CREATE INDEX iAncestor ON m_org_closure (ancestor_oid);
 
 CREATE INDEX iDescendant ON m_org_closure (descendant_oid);
+
+CREATE INDEX iDescendantAncestor ON m_org_closure (descendant_oid, ancestor_oid);
 
 ALTER TABLE m_org_closure
 ADD CONSTRAINT fk_ancestor
