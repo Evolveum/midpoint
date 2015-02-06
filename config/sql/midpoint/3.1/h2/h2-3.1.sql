043bd280a81f5d17d3e2c140f30cf0f153b12117
Viliam Repan <vilo.repan@evolveum.com>
2015-02-06 10:48:26 +0100

    updated h2 and oracle scripts

--- config/sql/midpoint/3.1/h2/h2-3.1.sql
+++ config/sql/midpoint/3.1/h2/h2-3.1.sql
@@ -222,6 +222,23 @@ CREATE TABLE m_generic_object (
   PRIMARY KEY (oid)
 );
 
+CREATE TABLE m_lookup_table (
+  name_norm VARCHAR(255),
+  name_orig VARCHAR(255),
+  oid       VARCHAR(36) NOT NULL,
+  PRIMARY KEY (oid)
+);
+
+CREATE TABLE m_lookup_table_row (
+  row_key             VARCHAR(255) NOT NULL,
+  owner_oid           VARCHAR(36)  NOT NULL,
+  label_norm          VARCHAR(255),
+  label_orig          VARCHAR(255),
+  lastChangeTimestamp TIMESTAMP,
+  row_value           VARCHAR(255),
+  PRIMARY KEY (row_key, owner_oid)
+);
+
 CREATE TABLE m_node (
   name_norm      VARCHAR(255),
   name_orig      VARCHAR(255),
@@ -569,6 +586,9 @@ CREATE INDEX iFocusEffective ON m_focus (effectiveStatus);
 ALTER TABLE m_generic_object
 ADD CONSTRAINT uc_generic_object_name UNIQUE (name_norm);
 
+ALTER TABLE m_lookup_table
+ADD CONSTRAINT uc_lookup_name UNIQUE (name_norm);
+
 ALTER TABLE m_node
 ADD CONSTRAINT uc_node_name UNIQUE (name_norm);
 
@@ -731,6 +751,16 @@ ALTER TABLE m_generic_object
 ADD CONSTRAINT fk_generic_object
 FOREIGN KEY (oid)
 REFERENCES m_object;
+
+ALTER TABLE m_lookup_table
+ADD CONSTRAINT fk_lookup_table
+FOREIGN KEY (oid)
+REFERENCES m_object;
+
+ALTER TABLE m_lookup_table_row
+ADD CONSTRAINT fk_lookup_table
+FOREIGN KEY (owner_oid)
+REFERENCES m_lookup_table;
 
 ALTER TABLE m_node
 ADD CONSTRAINT fk_node
