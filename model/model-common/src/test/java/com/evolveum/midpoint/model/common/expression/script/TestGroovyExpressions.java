01466fef3705b0ce4302a69cc171825d2420e29b
Radovan Semancik <radovan.semancik@evolveum.com>
2015-03-02 17:11:56 +0100

    Tests for groovy polystrings (MID-2204)

--- model/model-common/src/test/java/com/evolveum/midpoint/model/common/expression/script/TestGroovyExpressions.java
+++ model/model-common/src/test/java/com/evolveum/midpoint/model/common/expression/script/TestGroovyExpressions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Evolveum
+ * Copyright (c) 2010-2015 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 package com.evolveum.midpoint.model.common.expression.script;
 
+import com.evolveum.midpoint.model.common.expression.ExpressionVariables;
 import com.evolveum.midpoint.model.common.expression.script.ScriptEvaluator;
 import com.evolveum.midpoint.model.common.expression.script.jsr223.Jsr223ScriptEvaluator;
 import com.evolveum.midpoint.model.common.expression.script.xpath.XPathScriptEvaluator;
@@ -43,6 +44,7 @@ import org.xml.sax.SAXException;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
+import javax.xml.namespace.QName;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -73,5 +75,292 @@ public class TestGroovyExpressions extends AbstractScriptTest {
 		return new File(BASE_TEST_DIR, "groovy");
 	}
 
+	@Test
+    public void testExpressionPolyStringEquals101() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-1.xml", 
+				"testExpressionPolyStringEquals101", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOO",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals102() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-1.xml", 
+				"testExpressionPolyStringEquals102", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOOBAR",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals111() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-1.xml", 
+				"testExpressionPolyStringEquals111", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals112() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-1.xml", 
+				"testExpressionPolyStringEquals112", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+	
+	@Test
+    public void testExpressionPolyStringEquals121() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-1.xml", 
+				"testExpressionPolyStringEquals121", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals122() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-1.xml", 
+				"testExpressionPolyStringEquals122", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+	
+	@Test
+    public void testExpressionPolyStringEquals201() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-2.xml", 
+				"testExpressionPolyStringEquals201", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOO",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals202() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-2.xml", 
+				"testExpressionPolyStringEquals202", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOOBAR",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals211() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-2.xml", 
+				"testExpressionPolyStringEquals211", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals212() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-2.xml", 
+				"testExpressionPolyStringEquals212", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals221() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-2.xml", 
+				"testExpressionPolyStringEquals221", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEquals222() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-2.xml", 
+				"testExpressionPolyStringEquals222", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+	
+	@Test
+    public void testExpressionPolyStringEqualsStringify101() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-1.xml", 
+				"testExpressionPolyStringEqualsStringify101", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOO",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify102() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-1.xml", 
+				"testExpressionPolyStringEqualsStringify102", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOOBAR",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify111() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-1.xml", 
+				"testExpressionPolyStringEqualsStringify111", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify112() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-1.xml", 
+				"testExpressionPolyStringEqualsStringify112", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify121() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-1.xml", 
+				"testExpressionPolyStringEqualsStringify121", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify122() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-1.xml", 
+				"testExpressionPolyStringEqualsStringify122", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify201() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-2.xml", 
+				"testExpressionPolyStringEqualsStringify201", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOO",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify202() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-2.xml", 
+				"testExpressionPolyStringEqualsStringify202", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), "FOOBAR",
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify211() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-2.xml", 
+				"testExpressionPolyStringEqualsStringify211", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify212() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-2.xml", 
+				"testExpressionPolyStringEqualsStringify212", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyString("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify221() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-2.xml", 
+				"testExpressionPolyStringEqualsStringify221", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOO"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.TRUE);
+    }
+
+	@Test
+    public void testExpressionPolyStringEqualsStringify222() throws Exception {
+		evaluateAndAssertBooleanScalarExpresssion(
+				"expression-polystring-equals-stringify-2.xml", 
+				"testExpressionPolyStringEqualsStringify222", 
+				ExpressionVariables.create(
+						new QName(NS_X, "foo"), PrismTestUtil.createPolyStringType("FOOBAR"),
+						new QName(NS_Y, "bar"), "BAR"
+				),
+				Boolean.FALSE);
+    }
 
 }
