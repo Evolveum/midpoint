b5f40b6b0ae43341b0bd4c142cc40e0dcd5a432e
Erik Suta <erik.suta@evolveum.com>
2015-03-02 11:03:45 +0100

    MID-2117 - validation plugin interface initial implementation.

---
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/util/validation/MidpointFormValidator.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2010-2015 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolveum.midpoint.web.util.validation;
+
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.delta.ObjectDelta;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
+
+import java.util.Collection;
+
+/**
+ *  TODO - implement
+ * */
+public class MidpointFormValidator implements IMidpointFormValidator{
+
+    @Override
+    public Collection<SimpleValidationError> validateObject(PrismObject<? extends ObjectType> object) {
+        return null;
+    }
+
+    @Override
+    public Collection<SimpleValidationError> validateObject(Collection<ObjectDelta<? extends ObjectType>> deltas) {
+        return null;
+    }
+
+    @Override
+    public Collection<SimpleValidationError> validateObject(PrismObject<? extends ObjectType> object, Collection<ObjectDelta<? extends ObjectType>> deltas) {
+        return null;
+    }
+}
