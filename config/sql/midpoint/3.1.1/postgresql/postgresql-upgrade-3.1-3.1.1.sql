b695530122bdb587c822be1bca17b3fb4e263f42
Pavol Mederly <mederly@evolveum.com>
2015-03-27 16:06:19 +0100

    INT2->INT4 for container ids for PostgreSQL.

--- config/sql/midpoint/3.1.1/postgresql/postgresql-upgrade-3.1-3.1.1.sql
+++ config/sql/midpoint/3.1.1/postgresql/postgresql-upgrade-3.1-3.1.1.sql
@@ -36,3 +36,16 @@ ADD CONSTRAINT m_assignment_reference_pkey PRIMARY KEY (owner_id, owner_owner_oi
 ALTER TABLE m_reference
 DROP CONSTRAINT m_reference_pkey,
 ADD CONSTRAINT m_reference_pkey PRIMARY KEY (owner_oid, reference_type, relation, targetOid);
+
+ALTER TABLE m_assignment ALTER COLUMN id TYPE INT4;
+ALTER TABLE m_assignment ALTER COLUMN extId TYPE INT4;
+ALTER TABLE m_assignment_ext_date ALTER COLUMN anyContainer_owner_id TYPE INT4;
+ALTER TABLE m_assignment_ext_long ALTER COLUMN anyContainer_owner_id TYPE INT4;
+ALTER TABLE m_assignment_ext_poly ALTER COLUMN anyContainer_owner_id TYPE INT4;
+ALTER TABLE m_assignment_ext_reference ALTER COLUMN anyContainer_owner_id TYPE INT4;
+ALTER TABLE m_assignment_ext_string ALTER COLUMN anyContainer_owner_id TYPE INT4;
+ALTER TABLE m_assignment_extension ALTER COLUMN owner_id TYPE INT4;
+ALTER TABLE m_assignment_reference ALTER COLUMN owner_id TYPE INT4;
+ALTER TABLE m_exclusion ALTER COLUMN id TYPE INT4;
+ALTER TABLE m_lookup_table_row ALTER COLUMN id TYPE INT4;
+ALTER TABLE m_trigger ALTER COLUMN id TYPE INT4;
