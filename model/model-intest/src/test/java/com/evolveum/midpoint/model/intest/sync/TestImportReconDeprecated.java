532bd9f9c3a934b6550000ab4af3d2bdec87b632
Radovan Semancik <radovan.semancik@evolveum.com>
2015-07-15 14:56:10 +0200

    Eliminating non-relevant tests

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/sync/TestImportReconDeprecated.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/sync/TestImportReconDeprecated.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Evolveum
+ * Copyright (c) 2010-2015 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -109,9 +109,26 @@ public class TestImportReconDeprecated extends TestImportRecon {
 	}
 
 	@Override
+	public void test600SearchAllDummyAccounts() throws Exception {
+		// Not relevant for deprecated syntax
+	}
+
+	@Override
+	public void test610SearchDummyAccountsNameSubstring() throws Exception {
+		// Not relevant for deprecated syntax
+	}
+
+	@Override
 	public void test900DeleteDummyShadows() throws Exception {
 		// Not relevant for deprecated syntax
 	}
+
+	@Override
+	public void test910DeleteDummyAccounts() throws Exception {
+		// Not relevant for deprecated syntax
+	}
+	
+	
 	
 	
 }
