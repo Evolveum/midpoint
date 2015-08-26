13ef8f6c2e8d2788f546103cadbdb56253eae75f
honchar <honchar@evolveum.com>
2015-08-26 17:39:12 +0200

    Basic GUI automation tests suite

--- testing/selenidetest/src/test/java/com/evolveum/midpoint/testing/selenide/tests/user/SimpleUserTests.java
+++ testing/selenidetest/src/test/java/com/evolveum/midpoint/testing/selenide/tests/user/SimpleUserTests.java
@@ -1,14 +1,16 @@
 package com.evolveum.midpoint.testing.selenide.tests.user;
 
-import com.evolveum.midpoint.testing.selenide.tests.BaseTest;
-import com.evolveum.midpoint.testing.selenide.tests.LoginTest;
-import com.evolveum.midpoint.testing.selenide.tests.Util;
+import com.evolveum.midpoint.testing.selenide.tests.AbstractSelenideTest;
 import org.openqa.selenium.By;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.close;
 
@@ -16,150 +18,113 @@ import static com.codeborne.selenide.Selenide.close;
  * Created by Kate on 14.08.2015.
  */
 @Component
-public class SimpleUserTests extends BaseTest {
-
-    @Autowired
-    UserUtil userUtil;
-
-    @Autowired
-    LoginTest loginTest;
-
-    @Autowired
-    Util util;
-
+public class SimpleUserTests extends AbstractSelenideTest {
     //test data
     public static final String UPDATED_STRING = "_updated";
-    public static final String USER_DESCRIPTION = "description";
-    public static final String USER_FULL_NAME = "full name";
-    public static final String USER_GIVEN_NAME = "given name";
-    public static final String USER_FAMILY_NAME = "family name";
-    public static final String USER_NICKNAME = "nickname";
     public static final String SIMPLE_USER_NAME = "SimpleUserName";
+    public static final String NOT_UPDATED_USER_NAME = "NotUpdatedUserName";
+    public static final String EXISTING_USER_NAME = "ExistingUserName";
+    public static final String ALL_FIELDS_USER_NAME = "AllFieldsUserName";
+    public static final String USER_LIST_XML_FILEPATH = "src/test/resources/user/user-list.xml";
 
 
     /**
-     * Create user test.
+     * Create user test with Name field only
      */
-    @Test (priority = 1)
-    public void createUserWithUserNameOnlyTest() {
+    @Test (priority = 0)
+    public void test001createUserWithUserNameOnlyTest() {
         close();
-        loginTest.login();
+        login();
 
         //check if welcome message appears after user logged in
-        $(By.cssSelector("html.no-js body div.mp-main-container div.row.mainContainer div.page-header h1 small"))
-                .shouldHave(text("welcome to midPoint"));
+        $(byText("welcome to midPoint")).shouldBe(visible);
 
         //create user with filled user name only
-        userUtil.createUser(SIMPLE_USER_NAME);
+        createUser(SIMPLE_USER_NAME, new HashMap<String, String>());
 
         //check if Success message appears after user saving
-        $(By.xpath("/html/body/div[4]/div/div[2]/div[1]/ul/li/div/div[1]/div[1]/span")).shouldHave(text("Success"));
+        $(byText("Success")).shouldBe(visible);
 
         //search for the created in users list
-        util.searchForElement(SIMPLE_USER_NAME, "/html/body/div[4]/div/div[4]/form/span/a");
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr/td[3]/div/a/span"))
-                .shouldBe(visible).click();
+        searchForElement(SIMPLE_USER_NAME);
+        $(By.linkText(SIMPLE_USER_NAME)).shouldBe(visible).click();
+    }
 
-        close();
+   /**
+     * Create user test with all fields filled in
+     */
+    @Test (priority = 1)
+    public void test002createUserWithAllFieldsTest() {
+        //create user with filled user name only
+        Map<String, String> userAttributesMap = getAllUserAttributesMap();
+        createUser(ALL_FIELDS_USER_NAME, userAttributesMap);
+
+        //check if Success message appears after user saving
+        $(byText("Success")).shouldBe(visible);
+
+        //search for the created user in users list
+        searchForElement(ALL_FIELDS_USER_NAME);
+        //check if all fields are saved with values
+        $(By.linkText(ALL_FIELDS_USER_NAME)).shouldBe(visible).click();
+        checkObjectAttributesValues(userAttributesMap);
     }
 
     /**
      * Updating user fields
      */
-    @Test(dependsOnMethods = {"createUserWithUserNameOnlyTest"}, priority = 3)
-    public void editUserTest() {
-        close();
-        loginTest.login();
-        //check if welcome message appears after user logged in
-        $(By.cssSelector("html.no-js body div.mp-main-container div.row.mainContainer div.page-header h1 small"))
-                .shouldHave(text("welcome to midPoint"));
-
+    @Test(dependsOnMethods = {"test001createUserWithUserNameOnlyTest"}, priority = 3)
+    public void test003editUserTest() {
         //open user's Edit page
-        userUtil.openUsersEditPage(SIMPLE_USER_NAME);
+        openUsersEditPage(SIMPLE_USER_NAME);
 
         //click on the menu icon in the User details section
-        $(By.xpath("/html/body/div[4]/div/form/div[3]/div[1]/div/div/div/div[2]/div[2]/ul/li/a")).shouldBe(visible).click();
+        $(byText("User details")).parent().parent().find(byAttribute("about", "dropdownMenu"))
+                .shouldBe(visible).click();
         //click on Show empty fields menu item
         $(By.linkText("Show empty fields")).shouldBe(visible).click();
 
-        //update Name field
-        $(By.name("userForm:body:containers:0:container:properties:0:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).clear();
-        $(By.name("userForm:body:containers:0:container:properties:0:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).setValue(SIMPLE_USER_NAME + "_updated");
-        //update Description field
-        $(By.name("userForm:body:containers:0:container:properties:1:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).setValue(USER_DESCRIPTION + UPDATED_STRING);
-        //update Full Name field
-        $(By.name("userForm:body:containers:0:container:properties:2:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).setValue(USER_FULL_NAME + UPDATED_STRING);
-        //update Given Name field
-        $(By.name("userForm:body:containers:0:container:properties:3:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).setValue(USER_GIVEN_NAME + UPDATED_STRING);
-        //update Family Name field
-        $(By.name("userForm:body:containers:0:container:properties:4:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).setValue(USER_FAMILY_NAME + UPDATED_STRING);
-        //update Nickname field
-        $(By.name("userForm:body:containers:0:container:properties:6:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).setValue(USER_NICKNAME + UPDATED_STRING);
-
+        Map<String, String> userFieldsMap = new HashMap<>();
+        userFieldsMap.put(USER_NAME_FIELD_NAME, SIMPLE_USER_NAME + UPDATED_STRING);
+        userFieldsMap.put(DESCRIPTION_FIELD_NAME, DESCRIPTION_FIELD_VALUE + UPDATED_STRING);
+        userFieldsMap.put(FULL_NAME_FIELD_NAME, FULL_NAME_FIELD_VALUE + UPDATED_STRING);
+        userFieldsMap.put(GIVEN_NAME_FIELD_NAME, GIVEN_NAME_FIELD_VALUE + UPDATED_STRING);
+        userFieldsMap.put(FAMILY_NAME_FIELD_NAME, FAMILY_NAME_FIELD_VALUE + UPDATED_STRING);
+        userFieldsMap.put(NICKNAME_FIELD_NAME, NICKNAME_FIELD_VALUE + UPDATED_STRING);
+        //update user details
+        setFieldValues(userFieldsMap);
         //click Save button
-        $(By.xpath("/html/body/div[4]/div/form/div[6]/a[2]")).shouldHave(text("Save")).click();
+        $(By.linkText("Save")).shouldBe(visible).click();
 
         //check if Success message appears after user saving
-        $(By.xpath("/html/body/div[4]/div/div[2]/div[1]/ul/li/div/div[1]/div[1]/span")).shouldHave(text("Success"));
+        $(byText("Success")).shouldBe(visible);
 
         //search for user in users list
-        util.searchForElement(SIMPLE_USER_NAME + UPDATED_STRING, "/html/body/div[4]/div/div[4]/form/span/a");
+        searchForElement(SIMPLE_USER_NAME + UPDATED_STRING);
 
         //check if updated values are displayed in the users list
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr[1]/td[3]/div"))
-                .shouldHave(text(SIMPLE_USER_NAME + UPDATED_STRING));
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr[1]/td[4]/div"))
-                .shouldHave(text(USER_GIVEN_NAME + UPDATED_STRING));
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr[1]/td[5]/div"))
-                .shouldHave(text(USER_FAMILY_NAME + UPDATED_STRING));
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr[1]/td[6]/div"))
-                .shouldHave(text(USER_FULL_NAME + UPDATED_STRING));
+        $(byText(GIVEN_NAME_FIELD_VALUE + UPDATED_STRING)).shouldBe(visible);
+        $(byText(FAMILY_NAME_FIELD_VALUE + UPDATED_STRING)).shouldBe(visible);
+        $(byText(FULL_NAME_FIELD_VALUE + UPDATED_STRING)).shouldBe(visible);
 
         //click on the user link
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr/td[3]/div/a/span"))
-                .shouldBe(visible).click();
+        $(By.linkText(SIMPLE_USER_NAME + UPDATED_STRING)).shouldBe(visible).click();
 
         //check if updated values are displayed on the user's Edit page
-        $(By.name("userForm:body:containers:0:container:properties:0:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).shouldHave(value(SIMPLE_USER_NAME + "_updated"));
-        $(By.name("userForm:body:containers:0:container:properties:1:property:values:0:value:valueContainer:input:input"))
-                .shouldHave(value(USER_DESCRIPTION + UPDATED_STRING));
-        $(By.name("userForm:body:containers:0:container:properties:2:property:values:0:value:valueContainer:input:input"))
-                .shouldHave(value(USER_FULL_NAME + UPDATED_STRING));
-        $(By.name("userForm:body:containers:0:container:properties:3:property:values:0:value:valueContainer:input:input"))
-                .shouldHave(value(USER_GIVEN_NAME + UPDATED_STRING));
-        $(By.name("userForm:body:containers:0:container:properties:4:property:values:0:value:valueContainer:input:input"))
-                .shouldHave(value(USER_FAMILY_NAME + UPDATED_STRING));
-        $(By.name("userForm:body:containers:0:container:properties:6:property:values:0:value:valueContainer:input:input"))
-                .shouldHave(value(USER_NICKNAME + UPDATED_STRING));
-
-        close();
+        checkObjectAttributesValues(userFieldsMap);
     }
 
     /**
      * Cancelling of user update
      */
-    @Test(dependsOnMethods = {"createUserWithUserNameOnlyTest"}, priority = 2)
-    public void cancelUserUpdateTest() {
-        close();
-        loginTest.login();
-        //check if welcome message appears after user logged in
-        $(By.cssSelector("html.no-js body div.mp-main-container div.row.mainContainer div.page-header h1 small"))
-                .shouldHave(text("welcome to midPoint"));
-
+    @Test(dependsOnMethods = {"test001createUserWithUserNameOnlyTest"}, priority = 2)
+    public void test004cancelUserUpdateTest() {
+        //create user
+        createUser(NOT_UPDATED_USER_NAME, new HashMap<String, String>());
         //open user's Edit page
-        userUtil.openUsersEditPage(SIMPLE_USER_NAME);
-
-
+        openUsersEditPage(NOT_UPDATED_USER_NAME);
         //click on the menu icon in the User details section
-        $(By.xpath("/html/body/div[4]/div/form/div[3]/div[1]/div/div/div/div[2]/div[2]/ul/li/a")).shouldBe(visible).click();
+        $(byText("User details")).parent().parent().find(byAttribute("about", "dropdownMenu"))
+                .shouldBe(visible).click();
         //click on Show empty fields menu item
         $(By.linkText("Show empty fields")).shouldBe(visible).click();
 
@@ -167,98 +132,97 @@ public class SimpleUserTests extends BaseTest {
         $(By.name("userForm:body:containers:0:container:properties:0:property:values:0:value:valueContainer:input:input"))
                 .shouldBe(visible).clear();
         $(By.name("userForm:body:containers:0:container:properties:0:property:values:0:value:valueContainer:input:input"))
-                .shouldBe(visible).setValue(SIMPLE_USER_NAME + "_updated");
+                .shouldBe(visible).setValue(NOT_UPDATED_USER_NAME + "_updated");
 
-        //click Save button
-        $(By.xpath("/html/body/div[4]/div/form/div[6]/span/a")).shouldHave(text("Back")).click();
+        //click Back button
+        $(By.linkText("Back")).shouldBe(visible).click();
 
         //search for user in users list
-        util.searchForElement(SIMPLE_USER_NAME, "/html/body/div[4]/div/div[4]/form/span/a");
+        searchForElement(NOT_UPDATED_USER_NAME);
 
         //check if user name wasn't updated
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr[1]/td[3]/div"))
-                .shouldHave(text(SIMPLE_USER_NAME));
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr[1]/td[3]/div"))
-                .shouldHave(not(text(UPDATED_STRING)));
-        close();
+        $(byText(NOT_UPDATED_USER_NAME)).shouldBe(visible);
     }
 
     /**
      * Attempt to create user with all empty fields
      */
     @Test(alwaysRun = true, priority = 4)
-    public void createUserWithEmptyFieldsTest() {
+    public void test005createUserWithEmptyFieldsTest() {
         close();
-        loginTest.login();
+        login();
         //create user with all empty fields
-        userUtil.createUser("");
+        createUser("", new HashMap<String, String>());
 
         //check if error message appears after user saving
-        $(By.xpath("/html/body/div[4]/div/div[2]/div[1]/ul/li/div/div[1]")).shouldHave(text("No name in new object"));
-        close();
+        $(byAttribute("title", "Partial error")).shouldHave(text("No name in new object"));
     }
 
     /**
      * Attempt to create user with existing name
      */
     @Test(alwaysRun = true, priority = 5)
-    public void createUserWithExistingNameTest() {
-        close();
-        loginTest.login();
-        //check if welcome message appears after user logged in
-        $(By.cssSelector("html.no-js body div.mp-main-container div.row.mainContainer div.page-header h1 small"))
-                .shouldHave(text("welcome to midPoint"));
-
+    public void test006createUserWithExistingNameTest() {
         //create user
-        String userName = "Existing name";
-        userUtil.createUser(userName);
+        createUser(EXISTING_USER_NAME, new HashMap<String, String>());
         //check if Success message appears after user saving
-        $(By.xpath("/html/body/div[4]/div/div[2]/div[1]/ul/li/div/div[1]/div[1]/span")).shouldHave(text("Success"));
+        $(byText("Success")).shouldBe(visible);
         //try to create user with the same name
-        userUtil.createUser(userName);
+        createUser(EXISTING_USER_NAME, new HashMap<String, String>());
         //check if error message appears
-        $(By.xpath("/html/body/div[4]/div/div[2]/div[1]/ul/li/div/div[1]/div[1]/span")).shouldBe(visible)
-                .shouldHave(text("Error processing focus"));
-
-        close();
+        $(byAttribute("title", "Partial error")).find(By.tagName("span")).shouldHave(text("Error processing focus"));
     }
 
     @Test(alwaysRun = true, priority = 6)
-    public void deleteUserTest() {
-        close();
-        loginTest.login();
-        //check if welcome message appears after user logged in
-        $(By.cssSelector("html.no-js body div.mp-main-container div.row.mainContainer div.page-header h1 small"))
-                .shouldHave(text("welcome to midPoint"));
-
+    public void test007deleteUserTest() {
         //open Users -> List users
-        userUtil.openListUsersPage();
+        openListUsersPage();
 
         //search for user in users list
-        util.searchForElement(SIMPLE_USER_NAME + UPDATED_STRING, "/html/body/div[4]/div/div[4]/form/span/a");
+        searchForElement(SIMPLE_USER_NAME + UPDATED_STRING);
 
         //select checkbox next to the found user
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tbody/tr/td[1]/div/input")).shouldBe(visible).click();
+        $(byAttribute("about", "table")).find(By.tagName("tbody")).find(By.tagName("input")).shouldBe(visible).click();
 
         //click on the menu icon in the upper right corner of the users list
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/thead/tr/th[9]/div/span[1]/ul/li/a")).shouldBe(visible).click();
+        $(byAttribute("about", "dropdownMenu")).shouldBe(visible).click();
         //click on Delete menu item
         $(By.linkText("Delete")).shouldBe(visible).click();
 
         //click on Yes button in the opened "Confirm delete" window
-        $(By.xpath("/html/body/div[6]/form/div/div[2]/div/div/div/div[2]/div/div/div/div/p[2]/a[1]")).shouldBe(visible).click();
+        $(By.linkText("Yes")).shouldBe(visible).click();
 
         //check if operation success message appears
-        $(By.xpath("/html/body/div[4]/div/div[2]/div[1]/ul/li/div/div[1]/div[1]/span")).shouldHave(text("Success"));
-        $(By.xpath("/html/body/div[4]/div/div[2]/div[1]/ul/li/div/div[1]/div[2]/ul/li/div/span"))
-                .shouldHave(text("Delete users (Gui)"));
-
+        $(byText("Success")).shouldBe(visible);
         //search for user in users list
-        util.searchForElement(SIMPLE_USER_NAME + UPDATED_STRING, "/html/body/div[4]/div/div[4]/form/span/a");
+        searchForElement(SIMPLE_USER_NAME + UPDATED_STRING);
         //check the user was not found during user search
-        $(By.xpath("/html/body/div[4]/div/form/div[2]/table/tfoot/tr/td")).shouldBe(visible)
-                .shouldHave(text("No matching result found."));
-        close();
+        $(byText("No matching result found.")).shouldBe(visible);
+    }
+
+    @Test (priority = 7)
+    public void test008pagingThroughUsersList(){
+        //import 100 users from xml file
+        importObjectFromFile(USER_LIST_XML_FILEPATH);
+        //open user's list page
+        openListUsersPage();
+        //check if administrator user is present on the first list user page
+        $(By.linkText(ADMIN_LOGIN)).shouldBe(visible);
+        //click on the second number page
+        $(By.linkText("2")).shouldBe(visible).click();
+        //check that administrator user isn't present on the second list user page
+        $(By.linkText(ADMIN_LOGIN)).shouldNotBe(visible);
+        //check paging message shows the correct page numbers
+        $(By.name("table:table:bottomToolbars:toolbars:2:td:pageSize:popButton")).parent().shouldHave(text("Displaying 11 to 20"));
+        //click on the next  page button
+        $(By.linkText(">")).shouldBe(visible).click();
+        //check paging message shows the correct page numbers
+        $(By.name("table:table:bottomToolbars:toolbars:2:td:pageSize:popButton")).parent().shouldHave(text("Displaying 21 to 30"));
+        //click on the previous page button
+        $(By.linkText("<")).shouldBe(visible).click();
+        //check paging message shows the correct page numbers
+        $(By.name("table:table:bottomToolbars:toolbars:2:td:pageSize:popButton")).parent().shouldHave(text("Displaying 11 to 20"));
+
     }
 
 
