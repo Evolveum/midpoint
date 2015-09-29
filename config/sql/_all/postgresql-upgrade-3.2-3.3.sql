ac3ab49908d6db384ed55907ed028cbdb76da6d1
Pavol Mederly <mederly@evolveum.com>
2015-09-29 12:25:23 +0200

    Photo-related upgrade script for PostgreSQL.

--- config/sql/_all/postgresql-upgrade-3.2-3.3.sql
+++ config/sql/_all/postgresql-upgrade-3.2-3.3.sql
@@ -15,6 +15,24 @@ alter table m_sequence
 
 alter table m_user_photo rename to m_focus_photo;
 
-alter table m_focus add hasPhoto bit not null;
+alter table m_focus add hasPhoto boolean not null default FALSE;
+update m_focus set hasPhoto = false;
+update m_focus set hasPhoto = (select hasPhoto from m_user where m_user.oid = m_focus.oid)
+  where m_focus.oid in (select oid from m_user);
+alter table m_focus alter column hasPhoto drop default;
+
+alter table m_focus_photo
+  drop constraint m_user_photo_pkey;
+
+alter table m_focus_photo
+  add constraint m_focus_photo_pkey primary key(owner_oid);
+
+alter table m_focus_photo
+  drop constraint fk_user_photo;
+
+alter table m_focus_photo
+  add constraint fk_focus_photo
+  foreign key (owner_oid)
+  references m_focus;
 
 alter table m_user drop column hasPhoto;
