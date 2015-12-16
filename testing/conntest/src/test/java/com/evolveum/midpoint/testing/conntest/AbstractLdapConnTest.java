f813a156f627b39d5b0d2a96992975f99e840687
Radovan Semancik <radovan.semancik@evolveum.com>
2015-12-16 12:23:25 +0100

    Conntest compilation fixes

--- testing/conntest/src/test/java/com/evolveum/midpoint/testing/conntest/AbstractLdapConnTest.java
+++ testing/conntest/src/test/java/com/evolveum/midpoint/testing/conntest/AbstractLdapConnTest.java
@@ -81,6 +81,7 @@ import com.evolveum.midpoint.prism.query.EqualFilter;
 import com.evolveum.midpoint.prism.query.ObjectFilter;
 import com.evolveum.midpoint.prism.query.ObjectPaging;
 import com.evolveum.midpoint.prism.query.ObjectQuery;
+import com.evolveum.midpoint.prism.query.OrderDirection;
 import com.evolveum.midpoint.prism.util.PrismAsserts;
 import com.evolveum.midpoint.prism.util.PrismTestUtil;
 import com.evolveum.midpoint.schema.GetOperationOptions;
@@ -413,7 +414,7 @@ public abstract class AbstractLdapConnTest extends AbstractLdapSynchronizationTe
         ObjectQuery query = ObjectQueryUtil.createResourceAndObjectClassQuery(getResourceOid(), getAccountObjectClass(), prismContext);
         
         ObjectPaging paging = ObjectPaging.createPaging(20, 50);
-        paging.setOrderBy(getAttributeQName(resource, "uid"));
+        paging.setOrdering(getAttributePath(resource, "uid"), OrderDirection.ASCENDING);
 		query.setPaging(paging);
         
 		SearchResultList<PrismObject<ShadowType>> shadows = doSearch(TEST_NAME, query, 50, task, result);
@@ -448,7 +449,7 @@ public abstract class AbstractLdapConnTest extends AbstractLdapSynchronizationTe
         ObjectQuery query = ObjectQueryUtil.createResourceAndObjectClassQuery(getResourceOid(), getAccountObjectClass(), prismContext);
         
         ObjectPaging paging = ObjectPaging.createPaging(20, 222);
-        paging.setOrderBy(getAttributeQName(resource, "uid"));
+        paging.setOrdering(getAttributePath(resource, "uid"), OrderDirection.ASCENDING);
 		query.setPaging(paging);
         
 		SearchResultList<PrismObject<ShadowType>> shadows = doSearch(TEST_NAME, query, 222, task, result);
