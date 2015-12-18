4da9ea72b38c11574a558d0e8d74ae13842248eb
Viliam Repan <vilo.repan@evolveum.com>
2015-12-18 20:46:20 +0100

    update factory for search panel

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/search/SearchFactory.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/search/SearchFactory.java
@@ -19,8 +19,12 @@ package com.evolveum.midpoint.web.component.search;
 import com.evolveum.midpoint.prism.*;
 import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.prism.schema.SchemaRegistry;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ActivationType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.FocusType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,8 +34,25 @@ import java.util.Map;
  */
 public class SearchFactory {
 
+    private static final Map<Class, List<ItemPath>> SEARCHABLE_OBJECTS = new HashMap<>();
+
+    static {
+        SEARCHABLE_OBJECTS.put(ObjectType.class, Arrays.asList(
+                new ItemPath(ObjectType.F_NAME)));
+        SEARCHABLE_OBJECTS.put(FocusType.class, Arrays.asList(
+                new ItemPath(FocusType.F_ACTIVATION, ActivationType.F_ADMINISTRATIVE_STATUS)));
+        SEARCHABLE_OBJECTS.put(UserType.class, Arrays.asList(
+                new ItemPath(UserType.F_GIVEN_NAME),
+                new ItemPath(UserType.F_FAMILY_NAME),
+                new ItemPath(UserType.F_FULL_NAME),
+                new ItemPath(UserType.F_ADDITIONAL_NAME),
+                new ItemPath(UserType.F_COST_CENTER)));
+
+        //todo add other object types and properties which can be used in search
+    }
+
     public static <T extends ObjectType> Search createSearch(Class<T> type, PrismContext ctx) {
-        Map<ItemPath, ItemDefinition> availableDefs = getAvailableDefinitionsForUser(ctx);
+        Map<ItemPath, ItemDefinition> availableDefs = getAvailableDefinitions(type, ctx);
 
         Search search = new Search(type, availableDefs);
 
@@ -44,43 +65,21 @@ public class SearchFactory {
         return search;
     }
 
-    private static Map<ItemPath, ItemDefinition> getAvailableDefinitionsForObject(PrismContext ctx) {
+    private static <T extends ObjectType> Map<ItemPath, ItemDefinition> getAvailableDefinitions(
+            Class<T> type, PrismContext ctx) {
+
         Map<ItemPath, ItemDefinition> map = new HashMap<>();
+        map.putAll(createExtensionDefinitionList(type, ctx));
 
-        Map<ItemPath, ItemDefinition> object = createAvailableDefinitions(UserType.class, ctx,
-                new ItemPath(ObjectType.F_NAME)
-//                new ItemPath(ObjectType.F_PARENT_ORG_REF),
-//                new ItemPath(ObjectType.F_TENANT_REF)
-        );
-        map.putAll(object);
+        Class<T> typeClass = type;
+        while (typeClass != null && !com.evolveum.prism.xml.ns._public.types_3.ObjectType.class.equals(typeClass)) {
+            List<ItemPath> pathList = SEARCHABLE_OBJECTS.get(typeClass);
+            if (pathList != null) {
+                map.putAll(createAvailableDefinitions(typeClass, ctx, pathList));
 
-        return map;
-    }
-
-    private static Map<ItemPath, ItemDefinition> getAvailableDefinitionsForFocus(PrismContext ctx) {
-        Map<ItemPath, ItemDefinition> map = getAvailableDefinitionsForObject(ctx);
-
-        Map<ItemPath, ItemDefinition> user = createAvailableDefinitions(UserType.class, ctx,
-//                new ItemPath(FocusType.F_LINK_REF),
-//                new ItemPath(FocusType.F_ASSIGNMENT, AssignmentType.F_TARGET_REF),
-                new ItemPath(FocusType.F_ACTIVATION, ActivationType.F_ADMINISTRATIVE_STATUS));
-        map.putAll(user);
-
-        return map;
-    }
-
-    private static Map<ItemPath, ItemDefinition> getAvailableDefinitionsForUser(PrismContext ctx) {
-        Map<ItemPath, ItemDefinition> map = getAvailableDefinitionsForFocus(ctx);
-
-        map.putAll(createExtensionDefinitionList(UserType.class, ctx));
-
-        Map<ItemPath, ItemDefinition> user = createAvailableDefinitions(UserType.class, ctx,
-                new ItemPath(UserType.F_GIVEN_NAME),
-                new ItemPath(UserType.F_FAMILY_NAME),
-                new ItemPath(UserType.F_FULL_NAME),
-                new ItemPath(UserType.F_ADDITIONAL_NAME),
-                new ItemPath(UserType.F_COST_CENTER));
-        map.putAll(user);
+                typeClass = (Class<T>) typeClass.getSuperclass();
+            }
+        }
 
         return map;
     }
@@ -109,7 +108,7 @@ public class SearchFactory {
     }
 
     private static <T extends ObjectType> Map<ItemPath, ItemDefinition> createAvailableDefinitions(
-            Class<T> type, PrismContext ctx, ItemPath... paths) {
+            Class<T> type, PrismContext ctx, List<ItemPath> paths) {
 
         Map<ItemPath, ItemDefinition> map = new HashMap<>();
 
