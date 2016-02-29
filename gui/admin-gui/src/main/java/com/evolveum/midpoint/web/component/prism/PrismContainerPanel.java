7555f73ab365cb868702a63327f3be631d1c75de
honchar <honchar@evolveum.com>
2016-02-29 11:38:42 +0100

    fixing Show empty fields behaviour

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/PrismContainerPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/PrismContainerPanel.java
@@ -48,17 +48,15 @@ public class PrismContainerPanel extends Panel {
     private static final String STRIPED_CLASS = "striped";
 
     private boolean showHeader;
-    private boolean showEmptyFields;
     private PageBase pageBase;
 
     public PrismContainerPanel(String id, IModel<ContainerWrapper> model, Form form) {
-        this(id, model, true, false, form, null);
+        this(id, model, true, form, null);
     }
 
-    public PrismContainerPanel(String id, final IModel<ContainerWrapper> model, boolean showHeader, boolean showEmptyFields, Form form, PageBase pageBase) {
+    public PrismContainerPanel(String id, final IModel<ContainerWrapper> model, boolean showHeader, Form form, PageBase pageBase) {
         super(id);
         this.showHeader = showHeader;
-        this.showEmptyFields = showEmptyFields;
         this.pageBase = pageBase;
 
         LOGGER.trace("Creating container panel for {}", model.getObject());
@@ -118,10 +116,9 @@ public class PrismContainerPanel extends Panel {
         AjaxLink showEmptyFieldsButton = new AjaxLink(ID_SHOW_EMPTY_FIELDS) {
             @Override
             public void onClick(AjaxRequestTarget target) {
-                showEmptyFields = !showEmptyFields;
                 ContainerWrapper containerWrapper = model.getObject();
                 ObjectWrapper objectWrapper = containerWrapper.getObject();
-                objectWrapper.setShowEmpty(showEmptyFields);
+                objectWrapper.setShowEmpty(!objectWrapper.isShowEmpty());
                 target.add(PrismContainerPanel.this.findParent(PrismObjectPanel.class));
             }
         };
