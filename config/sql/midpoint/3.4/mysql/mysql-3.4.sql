6498985a7bd92e7b1a3f8f20c6bb81c7f0880774
Viliam Repan <vilo.repan@evolveum.com>
2016-04-05 12:29:58 +0200

    MID-2714 service type repo support, upgrade scripts

--- config/sql/midpoint/3.4/mysql/mysql-3.4.sql
+++ config/sql/midpoint/3.4/mysql/mysql-3.4.sql
@@ -717,6 +717,27 @@ CREATE TABLE m_sequence (
   COLLATE utf8_bin
   ENGINE = InnoDB;
 
+CREATE TABLE m_service (
+  displayOrder  INTEGER,
+  locality_norm VARCHAR(255),
+  locality_orig VARCHAR(255),
+  name_norm     VARCHAR(255),
+  name_orig     VARCHAR(255),
+  oid           VARCHAR(36) NOT NULL,
+  PRIMARY KEY (oid)
+)
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_bin
+  ENGINE = InnoDB;
+
+CREATE TABLE m_service_type (
+  service_oid VARCHAR(36) NOT NULL,
+  serviceType VARCHAR(255)
+)
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_bin
+  ENGINE = InnoDB;
+
 CREATE TABLE m_shadow (
   attemptNumber                INTEGER,
   dead                         BIT,
@@ -1273,6 +1294,16 @@ ALTER TABLE m_sequence
 ADD CONSTRAINT fk_sequence
 FOREIGN KEY (oid)
 REFERENCES m_object (oid);
+
+ALTER TABLE m_service
+  ADD CONSTRAINT fk_service
+FOREIGN KEY (oid)
+REFERENCES m_abstract_role (oid);
+
+ALTER TABLE m_service_type
+  ADD CONSTRAINT fk_service_type
+FOREIGN KEY (service_oid)
+REFERENCES m_service (oid);
 
 ALTER TABLE m_shadow
 ADD CONSTRAINT fk_shadow
