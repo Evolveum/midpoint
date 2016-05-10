bf3bff2c5d181207a800605e787fa5f832543523
Katarina Valalikova <k.valalikova@evolveum.com>
2016-05-10 13:21:30 +0200

    openstack connector - search script upgrade ..support for listing all projects/accounts.

--- samples/resources/scriptedrest/openstack/OpenStack-SearchScript.groovy
+++ samples/resources/scriptedrest/openstack/OpenStack-SearchScript.groovy
@@ -24,115 +24,112 @@ def login(def prName, def keystoneTokenUrl) {
 	def loginBinding = [
         	projectName : prName
         ]
-	
+
 	def engine = new groovy.text.SimpleTemplateEngine();
         request = engine.createTemplate(loginTemplate).make(loginBinding).toString()
         System.out.println("request: " + request);
         connection.setUri(keystoneTokenUrl)
-	
+
         def resp = connection.post( path : "/v2.0/tokens", headers: ['Content-type': 'application/json'], body: engine.createTemplate(loginTemplate).make(loginBinding).toString());
-        
+
 	System.out.println("response: " + resp.getData());
-	System.out.println(resp.data.access.token.id);	
+	System.out.println(resp.data.access.token.id);
 
 	return resp.data;
 }
 
-def getServerInformation(def novaUrl, token, def serverId) {
-        System.out.println("getServerInformation start")
-	connection.setUri(novaUrl)
-	System.out.println("after connection")
 
-        requestPath = '/v2.1/' + token.access.token.tenant.id + '/servers/'+serverId;
-        System.out.println("Path: "+ requestPath)
+def getObject(def url, def token, def requestPath){
+	connection.setUri(url)
+	System.out.println("Get Object Path: "+ requestPath)
 
-        def resp2 = connection.get( path : requestPath, headers: ['Content-type': 'application/json', 'X-Auth-Token': token.access.token.id], body: null);
+	def resp2 = connection.get( path : requestPath, headers: ['Content-type': 'application/json', 'X-Auth-Token': token.access.token.id], body: null);
 
-        json = resp2.getData();
-        System.out.println("Response: " + json)
+	json = resp2.getData();
+	System.out.println("Response: " + json)
 	return json
 }
 
-def getTenant(def keystoneUrl, def token, def projectId) {
-        connection.setUri(keystoneUrl)
-        requestPath = '/v2.0/tenants/' + projectId;
-        System.out.println("Path: "+ requestPath)
-
-        def resp2 = connection.get( path : requestPath, headers: ['Content-type': 'application/json', 'X-Auth-Token': token.access.token.id], body: null);
-
-        json = resp2.getData();
-        System.out.println("Response: " + json)
-        return json
+def getProjects(def uid, def keystoneTokenUrl, def keystoneUrl) {
+			//login for admin tenant scope
+			def foundProjects = [];
+			def token = login("admin", keystoneTokenUrl)
+			try {
+						requestPath = '/v2.0/tenants/'
+						if (uid != null) {
+							 requestPath = requestPath + uid
+						}
+						def projects = getObject(keystoneUrl, token, requestPath)
+						if (uid != null) {
+							foundProjects.add([__UID__: projects.tenant.id, __NAME__: projects.tenant.name, projectId: projects.tenant.id]);
+						} else {
+								for (project in projects.tenants) {
+										foundProjects.add([__UID__: project.id, __NAME__: project.name, projectId: project.id]);
+								}
+						}
+			} catch(groovyx.net.http.HttpResponseException ex) {
+						if ("Not Found".equals(ex.getMessage())) {
+								throw new Exception("Project does not exist");
+						}
+						throw ex;
+			}
+			return foundProjects;
 }
 
-def getUser(def keystoneUrl, def token, def userId) {
-        connection.setUri(keystoneUrl)
-        requestPath = '/v2.0/users/' + userId;
-        System.out.println("Users Get Path: "+ requestPath)
-
-        def resp2 = connection.get( path : requestPath, headers: ['Content-type': 'application/json', 'X-Auth-Token': token.access.token.id], body: null);
-
-        json = resp2.getData();
-        System.out.println("Response: " + json)
-        return json
+def getAccounts(def uid, def keystoneTokenUrl, def keystoneUrl) {
+				//login for admin tenant scope
+				def accounts = []
+				def token = login("admin", keystoneTokenUrl)
+				if (uid != null) {
+							def json = getObject(keystoneUrl, token, '/v2.0/users/' + uid)
+							connection.setUri(keystoneUrl)
+							def resp2 = getObject(keystoneUrl, token, "/v3/users/"+uid+"/projects")
+							accounts.add([__UID__: json.user.id, __NAME__: json.user.name, tenantId: resp2.data.projects.id, email:json.user.email, enabled: json.user.enabled, username: json.user.username, password: json.user.password]);
+				} else {
+							def users = getObject(keystoneUrl, token, '/v2.0/users/')
+							for (json in users.users) {
+									connection.setUri(keystoneUrl)
+									def projects = getObject(keystoneUrl, token, "/v3/users/"+json.id+"/projects")
+									accounts.add([__UID__: json.id, __NAME__: json.name, tenantId: projects.projects.id, email:json.email, enabled: json.enabled, username: json.username, password: json.password]);
+							}
+				}
+				return accounts;
 }
 
+def getServerInformation(def uid, def keystoneTokenUrl, def novaUrl) {
+
+				serverId = uid.tokenize(':')[1]
+				projectName = uid.tokenize(':')[0]
+				System.out.println("projectName : " + projectName)
+				System.out.println("before login")
+				def token = login(projectName, keystoneTokenUrl)
+				System.out.println("Login successful");
+				def json = getObject(novaUrl, token, '/v2.1/' + token.access.token.tenant.id + '/servers/'+serverId)
+				System.out.println("JSON: " + json)
+				System.out.println("IP Address: " + json.server.addresses.private[1].addr);
+				def server
+				server.add([__UID__ : uid.tokenize(':')[0] + ':' +json.server.id, __NAME__ : json.server.name, ipAddress : json.server.addresses.private[1].addr]);
+				return server
+}
 
 def result = []
-
-if (query != null){
-
-switch ( objectClass ) {
-        case "Server":
-                if (query.get("left").equalsIgnoreCase('__UID__')) {
-
-                        projectServerId = query.get('right')
-                        serverId = projectServerId.tokenize(':')[1]
-                        projectName = projectServerId.tokenize(':')[0]
-
-                        System.out.println("projectName : " + projectServerId.tokenize(':')[0])
-			System.out.println("before login")
-                        def token = login(projectName, keystoneTokenUrl)
-			System.out.println("Login successful");
-                        def json = getServerInformation(novaUrl, token, serverId)
-			
-			System.out.println("JSON: " + json)
-			System.out.println("IP Address: " + json.server.addresses.private[1].addr);
-                        result.add([__UID__ : projectServerId.tokenize(':')[0] + ':' +json.server.id, __NAME__ : json.server.name, ipAddress : json.server.addresses.private[1].addr]);
-                } 
-		break;
-	case "Project":
-		def uid = query.get('right')
+def uid
+if (query != null) {
+		uid = query.get('right')
 		System.out.println("uid: "+ uid);
-		//login for admin tenant scope
-		def token = login("admin", keystoneTokenUrl)
-		try {
-			def json = getTenant(keystoneUrl, token, uid)
-		} catch(groovyx.net.http.HttpResponseException ex) {
-			if ("Not Found".equals(ex.getMessage())) {
-				throw new Exception("Project does not exist");
-			}
-			throw ex;
-		}
-		result.add([__UID__: json.tenant.id, __NAME__: json.tenant.name, projectId: json.tenant.id]);		
-		break;
-
-
-        case "Account":
-                def uid = query.get('right')
-                System.out.println("uid: "+ uid);
-                //login for admin tenant scope
-                def token = login("admin", keystoneTokenUrl)
-                def json = getUser(keystoneUrl, token, uid)
-
-		connection.setUri(keystoneUrl)
-		def requestPath = "/v3/users/"+uid+"/projects"		
-
-		def resp2 = connection.get( path : requestPath, headers: ['Content-type': 'application/json', 'X-Auth-Token': token.access.token.id], body: null);
-		println(resp2.getData())
-		
-                result.add([__UID__: json.user.id, __NAME__: json.user.name, tenantId: resp2.data.projects.id, email:json.user.email, enabled: json.user.enabled, username: json.user.username, password: json.user.password]);
-                break;
 	}
-    }
+		switch ( objectClass ) {
+        case "Server":
+								if (uid != null) {
+                		result = getServerInformation(uid, keystoneTokenUrl, novaUrl)
+								}
+								break;
+				case "Project":
+								result = getProjects(uid, keystoneTokenUrl, keystoneUrl)
+								break;
+        case "Account":
+								result = getAccounts(uid, keystoneTokenUrl, keystoneUrl)
+                break;
+			}
+
 return result;
