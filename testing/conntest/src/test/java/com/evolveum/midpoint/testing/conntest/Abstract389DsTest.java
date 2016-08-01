ee44ccf0f6ceb2138449f6de2e9538bbf67b6610
Radovan Semancik <radovan.semancik@evolveum.com>
2016-08-01 09:46:37 +0200

    Fixing 389ds connetests

--- testing/conntest/src/test/java/com/evolveum/midpoint/testing/conntest/Abstract389DsTest.java
+++ testing/conntest/src/test/java/com/evolveum/midpoint/testing/conntest/Abstract389DsTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2015 Evolveum
+ * Copyright (c) 2014-2016 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,6 +103,11 @@ public abstract class Abstract389DsTest extends AbstractLdapConnTest {
 	@Override
 	protected boolean isVlvSearchBeyondEndResurnsLastEntry() {
 		return true;
+	}
+	
+	@Override
+	protected boolean hasAssociationShortcut() {
+		return false;
 	}
 
 	@Override
