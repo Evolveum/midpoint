76f9ac570f084fdf7ba35f3939c065ed59448782
Radovan Semancik <radovan.semancik@evolveum.com>
2016-08-09 16:14:31 +0200

    Fixing 389 conntests?

--- testing/conntest/src/test/java/com/evolveum/midpoint/testing/conntest/Abstract389DsDnTest.java
+++ testing/conntest/src/test/java/com/evolveum/midpoint/testing/conntest/Abstract389DsDnTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2015 Evolveum
+ * Copyright (c) 2014-2016 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,6 +52,11 @@ public abstract class Abstract389DsDnTest extends Abstract389DsTest {
 	@Override
 	protected boolean syncCanDetectDelete() {
 		return true;
+	}
+	
+	@Override
+	protected boolean isUsingGroupShortcutAttribute() {
+		return false;
 	}
 
 	@Test
