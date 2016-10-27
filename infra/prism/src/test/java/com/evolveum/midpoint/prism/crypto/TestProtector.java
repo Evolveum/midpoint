1936097d70397cd21fbab50e534ba5e80acfd657
Katarina Valalikova <k.valalikova@evolveum.com>
2016-10-27 08:33:41 +0200

    fixing tests

--- infra/prism/src/test/java/com/evolveum/midpoint/prism/crypto/TestProtector.java
+++ infra/prism/src/test/java/com/evolveum/midpoint/prism/crypto/TestProtector.java
@@ -24,26 +24,15 @@ import com.ibm.wsdl.extensions.schema.SchemaConstants;
 
 public class TestProtector {
 	
-	public static final String KEYSTORE_PATH = "/Users/katka/work/cvti/midpoint/keystore.jceks";
+	public static final String KEYSTORE_PATH = "src/test/resources/keystore.jceks";
 	public static final String KEYSTORE_PASSWORD = "changeit";
 	
 	private PrismContext prismContext;
 	
-	private static transient Trace LOGGER = TraceManager.getTrace(TestProtector.class);
-	
-//	@BeforeSuite
-//	public void setup() throws SchemaException, SAXException, IOException {
-//		PrettyPrinter.setDefaultNamespacePrefix("c:");
-//		PrismTestUtil.resetPrismContext(MidPointPrismContextFactory.FACTORY);
-//		
-//		prismContext = PrismTestUtil.createInitializedPrismContext();
-//
-//	}
-	
+	private static transient Trace LOGGER = TraceManager.getTrace(TestProtector.class);	
 	
 	public static Protector createProtector(String xmlCipher){
 		AESProtector protector = new AESProtector();
-//		protector.setPrismContext(prismContext);
 		protector.setKeyStorePassword(KEYSTORE_PASSWORD);
 		protector.setKeyStorePath(KEYSTORE_PATH);
 		protector.setEncryptionAlgorithm(xmlCipher);
@@ -55,49 +44,10 @@ public class TestProtector {
   @Test
   public void testProtectorKeyStore() throws Exception{
 	  
-	  ProtectedStringType ps = new ProtectedStringType();
-	  EncryptedDataType encryptedDataType  = new EncryptedDataType();
-	  
-	  KeyInfoType keyInfo = new KeyInfoType();
-	  keyInfo.setKeyName("feQIVWlexgw5NLryfdEdhS1f8Tw=");
-	  encryptedDataType.setKeyInfo(keyInfo);
-	  
-	  EncryptionMethodType encryptedMethod = new EncryptionMethodType();
-	  encryptedMethod.setAlgorithm("http://www.w3.org/2001/04/xmlenc#aes256-cbc");
-	  encryptedDataType.setEncryptionMethod(encryptedMethod);
-	  
-	  CipherDataType cipherValue = new CipherDataType();
-	  byte[] bytes = Base64.decode("TTWYGGaS3ofcGZEASneXZNBre3eKGSpySRWjxXJofsw=");
-	  cipherValue.setCipherValue(bytes);
-	  encryptedDataType.setCipherData(cipherValue);
-	  
-	  ps.setEncryptedData(encryptedDataType);
-	  
-//	  String psAsString = "<value>"
-//	  + "	<t:encryptedData>"
-//	  + "	 	<t:encryptionMethod>"
-//	  + "			<t:algorithm>http://www.w3.org/2001/04/xmlenc#aes256-cbc</t:algorithm>"
-//	  + "		</t:encryptionMethod>"
-//	  + "		<t:keyInfo>"
-//	  + "			<t:keyName>feQIVWlexgw5NLryfdEdhS1f8Tw=</t:keyName>"
-//	  + "		</t:keyInfo>"
-//	  + "		<t:cipherData>"
-//	  + "			<t:cipherValue>TTWYGGaS3ofcGZEASneXZNBre3eKGSpySRWjxXJofsw=</t:cipherValue>"
-//	  + "		</t:cipherData>"
-//	  + "	</t:encryptedData>"
-//	  + "</value>";
-	  
-//	  ProtectedStringType psType = prismContext.parseAtomicValue(psAsString, ProtectedStringType.COMPLEX_TYPE);
-	  
+	
 	  String value = "someValue";
 	
 	  Protector protector256 = createProtector(XMLCipher.AES_256);
-	  
-	  String clearValue = protector256.decryptString(ps);
-	  System.out.println("clear: " + clearValue);
-	  
-//	  String clearValue = protector256.decryptString(psType);
-//	  System.out.println("clear: " + clearValue);
 	  
 	  ProtectedStringType pdt = new ProtectedStringType();
 	  pdt.setClearValue(value);
