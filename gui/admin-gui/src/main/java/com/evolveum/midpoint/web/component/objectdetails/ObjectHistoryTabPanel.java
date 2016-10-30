0a95f76175d357957305da77055a144d7554ace8
honchar <honchar@evolveum.com>
2016-10-30 02:45:55 +0200

    added buttons column to audit event table on the user's page

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/objectdetails/ObjectHistoryTabPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/objectdetails/ObjectHistoryTabPanel.java
@@ -17,13 +17,27 @@ package com.evolveum.midpoint.web.component.objectdetails;
 
 import com.evolveum.midpoint.gui.api.model.LoadableModel;
 import com.evolveum.midpoint.gui.api.page.PageBase;
+import com.evolveum.midpoint.web.component.data.column.DoubleButtonColumn;
+import com.evolveum.midpoint.web.component.data.column.MultiButtonColumn;
 import com.evolveum.midpoint.web.component.form.Form;
 import com.evolveum.midpoint.web.component.prism.ObjectWrapper;
+import com.evolveum.midpoint.web.component.util.SelectableBean;
 import com.evolveum.midpoint.web.page.admin.reports.component.AuditLogViewerPanel;
 import com.evolveum.midpoint.web.page.admin.reports.dto.AuditSearchDto;
+import com.evolveum.midpoint.web.page.admin.users.PageUser;
+import com.evolveum.midpoint.xml.ns._public.common.audit_3.AuditEventRecordType;
 import com.evolveum.midpoint.xml.ns._public.common.audit_3.AuditEventStageType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AccessCertificationDefinitionType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.FocusType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectReferenceType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by honchar.
@@ -37,7 +51,7 @@ public class ObjectHistoryTabPanel<F extends FocusType> extends AbstractObjectTa
         initLayout(focusWrapperModel, page);
     }
 
-    private void initLayout(LoadableModel<ObjectWrapper<F>> focusWrapperModel, PageBase page) {
+    private void initLayout(final LoadableModel<ObjectWrapper<F>> focusWrapperModel, PageBase page) {
         AuditSearchDto searchDto = new AuditSearchDto();
         ObjectReferenceType ort = new ObjectReferenceType();
         ort.setOid(focusWrapperModel.getObject().getOid());
@@ -45,11 +59,72 @@ public class ObjectHistoryTabPanel<F extends FocusType> extends AbstractObjectTa
 
         searchDto.setEventStage(AuditEventStageType.EXECUTION);
 
-        AuditLogViewerPanel panel = new AuditLogViewerPanel(ID_MAIN_PANEL, page, searchDto);
+        AuditLogViewerPanel panel = new AuditLogViewerPanel(ID_MAIN_PANEL, page, searchDto){
+            @Override
+            protected boolean isTargetNameVisible(){
+                return false;
+            }
+
+            @Override
+            protected boolean isEventStageVisible(){
+                return false;
+            }
+
+            @Override
+            protected List<IColumn<AuditEventRecordType, String>> initColumns() {
+                List<IColumn<AuditEventRecordType, String>> columns = super.initColumns();
+                IColumn<AuditEventRecordType, String> column
+                        = new MultiButtonColumn<AuditEventRecordType>(new Model(), 2) {
+
+                    private final DoubleButtonColumn.BUTTON_COLOR_CLASS[] colors = {
+                            DoubleButtonColumn.BUTTON_COLOR_CLASS.INFO,
+                            DoubleButtonColumn.BUTTON_COLOR_CLASS.SUCCESS
+                    };
+
+                    @Override
+                    public String getCaption(int id) {
+                        return "";
+                    }
+
+                    @Override
+                    public String getButtonColorCssClass(int id) {
+                        return colors[id].toString();
+                    }
+
+                   @Override
+                    protected String getButtonCssClass(int id) {
+                       StringBuilder sb = new StringBuilder();
+                       sb.append(DoubleButtonColumn.BUTTON_BASE_CLASS).append(" ");
+                       sb.append(getButtonColorCssClass(id)).append(" ");
+                       switch (id) {
+                           case 0:  sb.append("fa fa-circle-o"); break;
+                           case 1:  sb.append("fa fa-exchange"); break;
+                       }
+                       return sb.toString();
+                    }
+
+                    @Override
+                    public void clickPerformed(int id, AjaxRequestTarget target, IModel<AuditEventRecordType> model) {
+                        switch (id) {
+                            case 0:  currentStateButtonClicked(target, focusWrapperModel);break;
+                            case 1:  break;
+                        }
+                    }
+
+                };
+                columns.add(column);
+
+                return columns;
+            }
+
+            };
         panel.setOutputMarkupId(true);
         add(panel);
     }
 
+    private void currentStateButtonClicked(AjaxRequestTarget target, LoadableModel<ObjectWrapper<F>> focusWrapperModel){
+        setResponsePage(new PageUser(((ObjectWrapper<UserType>)focusWrapperModel.getObject()).getObject()));
+    }
 
 
 }
