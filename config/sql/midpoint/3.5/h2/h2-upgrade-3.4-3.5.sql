cd09964215a2c719bade4e8a3a5ed2302e1f45e3
Viliam Repan <vilo.repan@evolveum.com>
2016-11-17 14:13:29 +0100

    MID-3516 updated sql scripts

--- config/sql/midpoint/3.5/h2/h2-upgrade-3.4-3.5.sql
+++ config/sql/midpoint/3.5/h2/h2-upgrade-3.4-3.5.sql
@@ -1,3 +1,26 @@
-ALTER TABLE m_object ADD lifecycleState VARCHAR(157);
+ALTER TABLE m_object ADD lifecycleState VARCHAR(255);
 
 CREATE INDEX iObjectLifecycleState ON m_object (lifecycleState);
+
+ALTER TABLE m_assignment ADD lifecycleState VARCHAR(255);
+
+CREATE TABLE m_assignment_policy_situation (
+  assignment_id   INTEGER     NOT NULL,
+  assignment_oid  VARCHAR(36) NOT NULL,
+  policySituation VARCHAR(255)
+);
+
+CREATE TABLE m_focus_policy_situation (
+  focus_oid       VARCHAR(36) NOT NULL,
+  policySituation VARCHAR(255)
+);
+
+ALTER TABLE m_assignment_policy_situation
+  ADD CONSTRAINT fk_assignment_policy_situation
+FOREIGN KEY (assignment_id, assignment_oid)
+REFERENCES m_assignment;
+
+ALTER TABLE m_focus_policy_situation
+  ADD CONSTRAINT fk_focus_policy_situation
+FOREIGN KEY (focus_oid)
+REFERENCES m_focus;
