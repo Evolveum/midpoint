e786602a82b78168b33bdc06ba319ffc5c047040
Katarina Valalikova <k.valalikova@evolveum.com>
2017-01-23 18:53:53 +0100

    dynamic forms for password reset, samll cleanup

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/DynamicFormPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/DynamicFormPanel.java
@@ -15,6 +15,14 @@
  */
 package com.evolveum.midpoint.web.component.prism;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
 
 import com.evolveum.midpoint.gui.api.component.BasePanel;
@@ -22,16 +30,24 @@ import com.evolveum.midpoint.gui.api.model.LoadableModel;
 import com.evolveum.midpoint.gui.api.page.PageBase;
 import com.evolveum.midpoint.gui.api.util.WebModelServiceUtils;
 import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.PrismObjectDefinition;
 import com.evolveum.midpoint.prism.delta.ObjectDelta;
+import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.task.api.Task;
 import com.evolveum.midpoint.util.exception.SchemaException;
+import com.evolveum.midpoint.util.logging.LoggingUtils;
 import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
-import com.evolveum.midpoint.web.component.form.Form;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AbstractFormItemType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.FormDefinitionType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.FormFieldGroupType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.FormFieldType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.FormType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
+import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
+import com.evolveum.midpoint.schema.util.FocusTypeUtil;
+import com.evolveum.midpoint.schema.util.FormTypeUtil;
 
 public class DynamicFormPanel<O extends ObjectType> extends BasePanel<ObjectWrapper<O>> {
 
@@ -45,32 +61,70 @@ public class DynamicFormPanel<O extends ObjectType> extends BasePanel<ObjectWrap
 	private static final String ID_FORM_FIELDS = "formFields";
 
 	LoadableModel<ObjectWrapper<O>> wrapperModel = null;
-	
-	public DynamicFormPanel(String id, final IModel<O> model, String formOid, Form<?> mainForm, boolean runPrivileged,
-			final PageBase parentPage) {
-		
+
+	public DynamicFormPanel(String id, final IModel<O> model, String formOid, Form<?> mainForm,
+			boolean runPrivileged, final PageBase parentPage) {
+		this(id, model.getObject().asPrismObject(), formOid, mainForm, runPrivileged, parentPage);
+
+	}
+
+	public DynamicFormPanel(String id, final PrismObject<O> prismObject, String formOid, Form<?> mainForm,
+			boolean runPrivileged, final PageBase parentPage) {
 		super(id);
 		
+		initialize(prismObject, formOid, mainForm, runPrivileged, parentPage);
+
+	}
+
+	public DynamicFormPanel(String id, final QName objectType, String formOid, Form<?> mainForm,
+			boolean runPrivileged, final PageBase parentPage) {
+		super(id);
+		PrismObject<O> prismObject = instantiateObject(objectType, parentPage);
+		initialize(prismObject, formOid, mainForm, runPrivileged, parentPage);
+
+	}
+
+	private PrismObject<O> instantiateObject(QName objectType, PageBase parentPage) {
+		PrismObjectDefinition<O> objectDef = parentPage.getPrismContext().getSchemaRegistry()
+				.findObjectDefinitionByType(objectType);
+		PrismObject<O> prismObject;
+		try {
+			prismObject = objectDef.instantiate();
+		} catch (SchemaException e) {
+			LoggingUtils.logException(LOGGER, "Could not initalize model for forgot password", e);
+			throw new RestartResponseException(getPageBase());
+		}
+		return prismObject;
+	}
+
+	private void initialize(final PrismObject<O> prismObject, String formOid, Form<?> mainForm,
+			boolean runPrivileged, final PageBase parentPage) {
+
+		if (prismObject == null) {
+			getSession().error(getString("DynamicFormPanel.object.must.not.be.null"));
+			throw new RestartResponseException(getPageBase());
+		}
+
 		wrapperModel = new LoadableModel<ObjectWrapper<O>>() {
-			
+
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected ObjectWrapper<O> load() {
 				ObjectWrapperFactory owf = new ObjectWrapperFactory(parentPage);
 				ObjectWrapper<O> objectWrapper = owf.createObjectWrapper("DisplayName", "description",
-						model.getObject().asPrismObject(), (model.getObject().getOid() == null) ? ContainerStatus.ADDING : ContainerStatus.MODIFYING);
+						prismObject,
+						(prismObject.getOid() == null) ? ContainerStatus.ADDING : ContainerStatus.MODIFYING);
 				objectWrapper.setShowEmpty(true);
 				return objectWrapper;
 			}
 		};
-		
-		
+
 		setParent(parentPage);
 
 		initLayout(formOid, runPrivileged, mainForm);
 	}
-	
+
 	@Override
 	public IModel<ObjectWrapper<O>> getModel() {
 		return wrapperModel;
@@ -100,16 +154,17 @@ public class DynamicFormPanel<O extends ObjectType> extends BasePanel<ObjectWrap
 			LOGGER.trace("No form definition defined for dynamic form");
 		}
 
-		DynamicFieldGroupPanel<O> formFields = new DynamicFieldGroupPanel<O>(ID_FORM_FIELDS, getModel(), formDefinitionType, mainForm, getPageBase());
+		DynamicFieldGroupPanel<O> formFields = new DynamicFieldGroupPanel<O>(ID_FORM_FIELDS, getModel(),
+				formDefinitionType, mainForm, getPageBase());
 		formFields.setOutputMarkupId(true);
 		add(formFields);
 
 	}
-	
+
 	public ObjectDelta<O> getObjectDelta() throws SchemaException {
 		return wrapperModel.getObject().getObjectDelta();
 	}
-	
+
 	public PrismObject<O> getObject() throws SchemaException {
 		ObjectDelta<O> delta = wrapperModel.getObject().getObjectDelta();
 		if (delta != null && delta.isAdd()) {
@@ -118,5 +173,27 @@ public class DynamicFormPanel<O extends ObjectType> extends BasePanel<ObjectWrap
 		return wrapperModel.getObject().getObject();
 	}
 
+	public List<ItemPath> getChangedItems() {
+		DynamicFieldGroupPanel<O> formFields = (DynamicFieldGroupPanel<O>) get(ID_FORM_FIELDS);
+		List<AbstractFormItemType> items = formFields.getFormItems();
+		List<ItemPath> paths = new ArrayList<>();
+		collectItemPaths(items, paths);
+		return paths;
+	}
+
+	private Collection<? extends ItemPath> collectItemPaths(List<AbstractFormItemType> items, List<ItemPath> paths) {
+		for (AbstractFormItemType aItem : items) {
+			ItemPathType itemPathType = aItem.getRef();
+			if (itemPathType != null) {
+				paths.add(itemPathType.getItemPath());
+			}
+			
+			if (aItem instanceof FormFieldGroupType) {
+				collectItemPaths(FormTypeUtil.getFormItems(((FormFieldGroupType) aItem).getFormItems()), paths);
+			}
+		}
+		
+		return paths;
+	}
 
 }
--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/forgetpassword/ResetPolicyDto.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/forgetpassword/ResetPolicyDto.java
@@ -9,6 +9,7 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.AbstractCredentialsR
 import com.evolveum.midpoint.xml.ns._public.common.common_3.MailAuthenticationPolicyType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.MailResetPolicyType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.NonceCredentialsPolicyType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectReferenceType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.SecurityPolicyType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.SecurityQuestionsCredentialsPolicyType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.SecurityQuestionsResetPolicyType;
@@ -29,56 +30,61 @@ public class ResetPolicyDto implements Serializable {
 	private SmsAuthenticationPolicyType smsAuthentication;
 	private SecurityQuestionsCredentialsPolicyType securityQuestions;
 	private NonceCredentialsPolicyType noncePolicy;
-
+	private ObjectReferenceType formRef;
 
 	public void initResetPolicyDto(SecurityPolicyType securityPolicyType) throws SchemaException {
 		if (securityPolicyType == null) {
 			return;
 		}
-		
+
 		if (securityPolicyType.getCredentials() != null
 				&& securityPolicyType.getCredentials().getSecurityQuestions() != null) {
 			this.securityQuestions = securityPolicyType.getCredentials().getSecurityQuestions();
 			this.resetMethod = ResetMethod.SECURITY_QUESTIONS;
 			return;
 		}
-		
+
 		if (securityPolicyType.getCredentialsReset() == null) {
 			return;
 		}
-			MailResetPolicyType mailResetPolicy = securityPolicyType.getCredentialsReset().getMailReset();
-			if (mailResetPolicy != null) {
-				this.resetMethod = ResetMethod.MAIL;
-				initResetPolicy(mailResetPolicy, securityPolicyType);
-				return;
-			}
-			
-			SmsResetPolicyType smsResetPolicy = securityPolicyType.getCredentialsReset().getSmsReset();
-			if (smsResetPolicy != null) {
-				this.resetMethod = ResetMethod.SMS;
-				initResetPolicy(smsResetPolicy, securityPolicyType);
-				return;
-			}
-			
-			SecurityQuestionsResetPolicyType securityQuestionsResetPolicy = securityPolicyType.getCredentialsReset().getSecurityQuestionReset();
-			if (securityQuestionsResetPolicy != null) {
-				this.resetMethod = ResetMethod.SECURITY_QUESTIONS;
-				initResetPolicy(securityQuestionsResetPolicy, securityPolicyType);
-				return;
-			}
-			
 		
+		MailResetPolicyType mailResetPolicy = securityPolicyType.getCredentialsReset().getMailReset();
+		if (mailResetPolicy != null) {
+			this.resetMethod = ResetMethod.MAIL;
+			initResetPolicy(mailResetPolicy, securityPolicyType);
+			return;
+		}
+
+		SmsResetPolicyType smsResetPolicy = securityPolicyType.getCredentialsReset().getSmsReset();
+		if (smsResetPolicy != null) {
+			this.resetMethod = ResetMethod.SMS;
+			initResetPolicy(smsResetPolicy, securityPolicyType);
+			return;
+		}
+
+		SecurityQuestionsResetPolicyType securityQuestionsResetPolicy = securityPolicyType
+				.getCredentialsReset().getSecurityQuestionReset();
+		if (securityQuestionsResetPolicy != null) {
+			this.resetMethod = ResetMethod.SECURITY_QUESTIONS;
+			initResetPolicy(securityQuestionsResetPolicy, securityPolicyType);
+			return;
+		}
 
 	}
-	
-	private void initResetPolicy(AbstractCredentialsResetPolicyType resetPolicy, SecurityPolicyType securityPolicyType) throws SchemaException{
-		AbstractAuthenticationPolicyType authPolicy = SecurityPolicyUtil.getAuthenticationPolicy(resetPolicy.getAdditionalAuthenticationName(), securityPolicyType);
+
+	private void initResetPolicy(AbstractCredentialsResetPolicyType resetPolicy,
+			SecurityPolicyType securityPolicyType) throws SchemaException {
+		this.formRef = resetPolicy.getFormRef();
+		AbstractAuthenticationPolicyType authPolicy = SecurityPolicyUtil
+				.getAuthenticationPolicy(resetPolicy.getAdditionalAuthenticationName(), securityPolicyType);
 		if (authPolicy instanceof MailAuthenticationPolicyType) {
 			this.mailAuthentication = (MailAuthenticationPolicyType) authPolicy;
-			noncePolicy = SecurityPolicyUtil.getCredentialPolicy(mailAuthentication.getMailNonce(), securityPolicyType);
+			noncePolicy = SecurityPolicyUtil.getCredentialPolicy(mailAuthentication.getMailNonce(),
+					securityPolicyType);
 		} else if (authPolicy instanceof SmsAuthenticationPolicyType) {
 			this.smsAuthentication = (SmsAuthenticationPolicyType) authPolicy;
-			this.noncePolicy = SecurityPolicyUtil.getCredentialPolicy(smsAuthentication.getSmsNonce(), securityPolicyType);
+			this.noncePolicy = SecurityPolicyUtil.getCredentialPolicy(smsAuthentication.getSmsNonce(),
+					securityPolicyType);
 		}
 		this.name = resetPolicy.getName();
 	}
@@ -131,6 +137,8 @@ public class ResetPolicyDto implements Serializable {
 		this.noncePolicy = noncePolicy;
 	}
 	
-	
+	public ObjectReferenceType getFormRef() {
+		return formRef;
+	}
 
 }
