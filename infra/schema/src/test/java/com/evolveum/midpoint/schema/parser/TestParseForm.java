9eeef96624d20d02ed324ce6d3d492ffaab813f1
Pavol Mederly <mederly@evolveum.com>
2017-01-28 09:59:37 +0100

    Fixed broken form parsing tests.

--- infra/schema/src/test/java/com/evolveum/midpoint/schema/parser/TestParseForm.java
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/parser/TestParseForm.java
@@ -152,7 +152,7 @@ public class TestParseForm extends AbstractObjectParserTest<FormType> {
 		FormItemsType formItems = formDefinition.getFormItems();
 		assertNotNull("no formItems", formItems);
 		assertEquals("wrong # of form items", 3, formItems.getFormItem().size());
-		assertFormItem(formItems, "main list", 0, SchemaConstantsGenerated.C_FORM_FIELD, FormFieldType.class, "Family name");
+		assertFormItem(formItems, "main list", 0, SchemaConstantsGenerated.C_FORM_FIELD, FormFieldType.class, "FamilyName");
 		assertFormItem(formItems, "main list", 1, SchemaConstantsGenerated.C_FORM_FIELD_GROUP, FormFieldGroupType.class, "Address");
 		assertFormItem(formItems, "main list", 2, SchemaConstantsGenerated.C_FORM_FIELD, FormFieldType.class, "Email");
 		FormItemsType itemsInGroup1 = ((FormFieldGroupType) formItems.getFormItem().get(1).getValue()).getFormItems();
@@ -166,7 +166,7 @@ public class TestParseForm extends AbstractObjectParserTest<FormType> {
 		JAXBElement<? extends AbstractFormItemType> itemElement = formItems.getFormItem().get(index);
 		assertTrue(ctx+"Unexpected item name: "+itemElement.getName()+", expected: "+elementName, elementName.equals(itemElement.getName()));
 		assertEquals(ctx+"Wrong class", clazz, itemElement.getValue().getClass());
-		assertEquals(ctx+"Wrong name", name, itemElement.getValue().getName());
+		assertEquals(ctx+"Wrong name", new QName(name), itemElement.getValue().getName());
 	}
 
 }
