c62bb4966ab5031bb9da9b711ffdc9a65de0f27f
Pavol Mederly <mederly@evolveum.com>
2017-02-23 10:09:15 +0100

    First working version (although not configurable for now).

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/RObjectTextInfo.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/RObjectTextInfo.java
@@ -16,17 +16,22 @@
 
 package com.evolveum.midpoint.repo.sql.data.common;
 
+import com.evolveum.midpoint.prism.PrismContext;
 import com.evolveum.midpoint.repo.sql.query2.definition.NotQueryable;
 import com.evolveum.midpoint.repo.sql.util.RUtil;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AbstractRoleType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
+import com.evolveum.prism.xml.ns._public.types_3.PolyStringType;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.ForeignKey;
 import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 import static com.evolveum.midpoint.repo.sql.data.common.RObjectTextInfo.COLUMN_OWNER_OID;
 import static com.evolveum.midpoint.repo.sql.data.common.RObjectTextInfo.TABLE_NAME;
@@ -39,8 +44,13 @@ import static com.evolveum.midpoint.repo.sql.data.common.RObjectTextInfo.TABLE_N
         @Index(name = "iTextInfoOid", columnList = COLUMN_OWNER_OID)})
 public class RObjectTextInfo implements Serializable {
 
+	private static final Trace LOGGER = TraceManager.getTrace(RObjectTextInfo.class);
+
     public static final String TABLE_NAME = "m_object_text_info";
     public static final String COLUMN_OWNER_OID = "owner_oid";
+    public static final String F_TEXT = "text";
+
+    public static final int MAX_TEXT_SIZE = 255;
 
     private Integer id;
 
@@ -52,7 +62,12 @@ public class RObjectTextInfo implements Serializable {
     public RObjectTextInfo() {
     }
 
-    @Id
+	public RObjectTextInfo(RObject owner, String text) {
+		this.owner = owner;
+		this.text = text;
+	}
+
+	@Id
     @GeneratedValue
     public Integer getId() {
         return id;
@@ -112,18 +127,74 @@ public class RObjectTextInfo implements Serializable {
         return Objects.hash(owner, ownerOid, id, text);
     }
 
-    public static <T extends ObjectType> Set<RObjectTextInfo> createSet(@NotNull ObjectType object, @NotNull RObject repo) {
-        Set<RObjectTextInfo> rv = new HashSet<>();
-        if (object.getName() != null) {
-            rv.add(create(repo, object.getName().getOrig()));
-        }
-        if (object.getDescription() != null) {
-            rv.add(create(repo, object.getDescription()));
-        }
-        return rv;
+    public static <T extends ObjectType> Set<RObjectTextInfo> createItemsSet(@NotNull ObjectType object, @NotNull RObject repo,
+			@NotNull PrismContext prismContext) {
+        List<String> allWords = new ArrayList<>();
+        append(allWords, object.getName(), prismContext);
+        append(allWords, object.getDescription(), prismContext);
+        if (object instanceof UserType) {
+			UserType user = (UserType) object;
+			append(allWords, user.getFullName(), prismContext);
+			append(allWords, user.getGivenName(), prismContext);
+			append(allWords, user.getFamilyName(), prismContext);
+			append(allWords, user.getAdditionalName(), prismContext);
+			append(allWords, user.getEmailAddress(), prismContext);
+		} else if (object instanceof AbstractRoleType) {
+			AbstractRoleType role = (AbstractRoleType) object;
+			append(allWords, role.getDisplayName(), prismContext);
+			append(allWords, role.getIdentifier(), prismContext);
+		}
+        return createItemsSet(repo, allWords);
     }
 
-    private static RObjectTextInfo create(RObject repo, String text) {
+	private static Set<RObjectTextInfo> createItemsSet(RObject repo, List<String> allWords) {
+		Set<RObjectTextInfo> rv = new HashSet<>();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < allWords.size(); i++) {
+			String word = allWords.get(i);
+			if (sb.length() + word.length() + 2 <= MAX_TEXT_SIZE) {
+				sb.append(" ").append(word);
+			} else {
+				if (sb.length() > 0) {
+					sb.append(" ");
+					rv.add(new RObjectTextInfo(repo, sb.toString()));
+					sb = new StringBuilder();
+				} else {
+					// a problem - too large string
+					LOGGER.warn("Word too long to be correctly indexed: {}", word);
+					rv.add(new RObjectTextInfo(repo, " " + word.substring(0, MAX_TEXT_SIZE - 2) + " "));
+					allWords.set(i, word.substring(MAX_TEXT_SIZE - 2));
+					i--;		// to reiterate
+				}
+			}
+		}
+		if (sb.length() > 0) {
+			sb.append(" ");
+			rv.add(new RObjectTextInfo(repo, sb.toString()));
+		}
+		return rv;
+	}
+
+	private static void append(List<String> allWords, String text, PrismContext prismContext) {
+    	if (StringUtils.isBlank(text)) {
+    		return;
+		}
+    	String normalized = prismContext.getDefaultPolyStringNormalizer().normalize(text);
+		String[] words = StringUtils.split(normalized);
+		for (String word : words) {
+			if (StringUtils.isNotBlank(word)) {
+				allWords.add(word);
+			}
+		}
+	}
+
+	private static void append(List<String> allWords, PolyStringType text, PrismContext prismContext) {
+    	if (text != null) {
+    		append(allWords, text.getOrig(), prismContext);
+		}
+	}
+
+	private static RObjectTextInfo create(RObject repo, String text) {
         RObjectTextInfo rv = new RObjectTextInfo();
         rv.setOwner(repo);
         rv.setText(text);
