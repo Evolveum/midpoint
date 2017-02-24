b2e5dd601e6168619cb650c7cc467ef0251c0a4a
Pavol Mederly <mederly@evolveum.com>
2017-02-24 17:37:41 +0100

    Migration scripts.

--- config/sql/midpoint/3.6/sqlserver/sqlserver-upgrade-3.5-3.6.sql
+++ config/sql/midpoint/3.6/sqlserver/sqlserver-upgrade-3.5-3.6.sql
@@ -47,3 +47,15 @@ ALTER TABLE m_audit_ref_value
   ADD CONSTRAINT fk_audit_ref_value
 FOREIGN KEY (record_id)
 REFERENCES m_audit_event;
+
+CREATE TABLE m_object_text_info (
+  owner_oid NVARCHAR(36) COLLATE database_default  NOT NULL,
+  text      NVARCHAR(255) COLLATE database_default NOT NULL,
+  PRIMARY KEY (owner_oid, text)
+);
+
+ALTER TABLE m_object_text_info
+  ADD CONSTRAINT fk_object_text_info_owner
+FOREIGN KEY (owner_oid)
+REFERENCES m_object;
+
