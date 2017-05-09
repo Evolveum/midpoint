a88a96b556fb61e1add4b0eaeef434e717c7f996
Radovan Semancik <radovan.semancik@evolveum.com>
2017-05-09 18:48:50 +0200

    Dynamic configuration provisioning tests (failing)

---
+++ provisioning/provisioning-impl/src/test/java/com/evolveum/midpoint/provisioning/impl/dummy/TestDummyExpression.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.evolveum.midpoint.provisioning.impl.dummy;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertNull;
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.io.File;
+
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.evolveum.midpoint.prism.PrismProperty;
+import com.evolveum.midpoint.schema.result.OperationResult;
+import com.evolveum.midpoint.task.api.Task;
+
+/**
+ * Testing expressions in dummy resource configuration.
+ * 
+ * @author Radovan Semancik
+ *
+ */
+@ContextConfiguration(locations = "classpath:ctx-provisioning-test-main.xml")
+@DirtiesContext
+public class TestDummyExpression extends AbstractBasicDummyTest {
+	
+	public static final File TEST_DIR = new File("src/test/resources/impl/dummy-expression/");
+	public static final File RESOURCE_DUMMY_FILE = new File(TEST_DIR, "resource-dummy.xml");
+
+	@Override
+	public void initSystem(Task initTask, OperationResult initResult) throws Exception {
+		super.initSystem(initTask, initResult);
+	}
+
+	@Override
+	protected File getResourceDummyFilename() {
+		return RESOURCE_DUMMY_FILE;
+	}
+	
+	@Override
+	protected <T> void assertConfigurationProperty(PrismProperty<T> confProp) {
+		T val = confProp.getRealValue();
+		switch (confProp.getElementName().getLocalPart()) {
+			case "instanceId":
+				assertEquals("Wrong value for "+confProp, "", val);
+				break;
+				
+			case "uselessString":
+				assertEquals("Wrong value for "+confProp, null, val);
+				assertExpression(confProp, "value");
+				break;
+
+			default:
+				break;
+		}
+	}
+	
+}
