253d2409ba29ee939fa67865c57655d1e1f0b11c
Pavol Mederly <mederly@evolveum.com>
2017-05-10 20:47:09 +0200

    Small cleanup of db query result processing.

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/util/GetContainerableResult.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/util/GetContainerableResult.java
@@ -5,6 +5,9 @@ import org.hibernate.transform.ResultTransformer;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author lazyman
@@ -12,19 +15,30 @@ import java.io.Serializable;
  */
 public class GetContainerableResult implements Serializable {
 
-    public static final ResultTransformer RESULT_TRANSFORMER = new BasicTransformerAdapter() {
+    public static final ResultStyle RESULT_STYLE = new ResultStyle() {
         @Override
-        public Object transformTuple(Object[] tuple, String[] aliases) {
-            return new GetContainerableResult(tuple);       // ownerOid, id, fullObject
+        public ResultTransformer getResultTransformer() {
+            return new BasicTransformerAdapter() {
+                @Override
+                public Object transformTuple(Object[] tuple, String[] aliases) {
+                    return new GetContainerableResult((String) tuple[0], (byte[]) tuple[2]);
+                }
+            };
+        }
+
+        @Override
+        public List<String> getIdentifiers(String rootAlias) {
+            return Arrays.asList(rootAlias + ".ownerOid", rootAlias + ".id");
+        }
+
+        @Override
+        public List<String> getContentAttributes(String rootAlias) {
+            return Collections.singletonList(rootAlias + ".fullObject");
         }
     };
 
     private final String ownerOid;
     private final byte[] fullObject;
-
-    private GetContainerableResult(Object[] values) {
-        this((String) values[0], (byte[]) values[2]);
-    }
 
     private GetContainerableResult(@NotNull String ownerOid, @NotNull byte[] fullObject) {
         this.fullObject = fullObject;
