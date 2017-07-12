b5c484dfe45fa5096c9396f9259f529279df8e51
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-12 04:09:26 -0700

    Take advantage of Java8's @FunctionalInterface

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/projector/MappingExtractor.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/projector/MappingExtractor.java
@@ -23,6 +23,7 @@ import com.evolveum.midpoint.prism.ItemDefinition;
 import com.evolveum.midpoint.prism.PrismValue;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.FocusType;
 
+@FunctionalInterface
 public interface MappingExtractor<V extends PrismValue, D extends ItemDefinition, F extends FocusType> {
 
 	Collection<? extends PrismValueDeltaSetTripleProducer<V,D>> getMappings(Construction<F> construction);
