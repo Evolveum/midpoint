d48847e8fffc25731c941ed3b0e63e4bc786c805
Pavol Mederly <mederly@evolveum.com>
2017-07-13 16:03:06 +0200

    "list simplified resource objects" bulk action sample + a couple of paging-related fixes

--- infra/prism/src/main/java/com/evolveum/midpoint/prism/query/PagingConvertor.java
+++ infra/prism/src/main/java/com/evolveum/midpoint/prism/query/PagingConvertor.java
@@ -16,33 +16,26 @@
 
 package com.evolveum.midpoint.prism.query;
 
-import javax.xml.namespace.QName;
-
-import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
-
-import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.prism.xml.ns._public.query_3.OrderDirectionType;
 import com.evolveum.prism.xml.ns._public.query_3.PagingType;
+import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
 
 
 
 public class PagingConvertor {
 	
 	public static ObjectPaging createObjectPaging(PagingType pagingType){
-		if (pagingType == null){
+		if (pagingType == null) {
 			return null;
 		}
-		
-		QName orderBy = null;
-		if (pagingType.getOrderBy() != null){
-			orderBy = ItemPath.getName(pagingType.getOrderBy().getItemPath().first());
+		if (pagingType.getOrderBy() != null) {
+			return ObjectPaging.createPaging(pagingType.getOffset(), pagingType.getMaxSize(),
+					pagingType.getOrderBy().getItemPath(), toOrderDirection(pagingType.getOrderDirection()));
+		} else {
+			return ObjectPaging.createPaging(pagingType.getOffset(), pagingType.getMaxSize());
 		}
-		
-		return ObjectPaging.createPaging(pagingType.getOffset(), pagingType.getMaxSize(), orderBy, toOrderDirection(pagingType.getOrderDirection()));
-		
 	}
 
-	
 	private static OrderDirection toOrderDirection(OrderDirectionType directionType){
 		if (directionType == null){
 			return null;
