e5c7e78cd30a7c1e4c63daaa230a4ba0e342c712
kate <honchar@evolveum.com>
2017-07-14 16:21:06 +0200

    csv export limit configuration

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/button/CsvDownloadButtonPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/button/CsvDownloadButtonPanel.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 
+import com.evolveum.midpoint.web.component.data.BaseSortableDataProvider;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.export.CSVDataExporter;
@@ -16,40 +17,28 @@ import org.apache.wicket.util.resource.IResourceStream;
 import com.evolveum.midpoint.gui.api.component.BasePanel;
 import com.evolveum.midpoint.web.component.AbstractAjaxDownloadBehavior;
 import com.evolveum.midpoint.web.component.AjaxIconButton;
-import com.evolveum.midpoint.web.page.admin.reports.dto.AuditEventRecordProvider;
 
 public abstract class CsvDownloadButtonPanel extends BasePanel {
 
 	private static final String ID_EXPORT_DATA = "exportData";
 	
    public CsvDownloadButtonPanel(String id) {
-		this(id, false);
-	}
-   
-   //TODO: temporal HACK.. there is no time to do it properly, just reusing the current implementation
-   public CsvDownloadButtonPanel(String id, boolean isAudit) {
 	   super(id);
-	   initLayout(isAudit);
+	   initLayout();
    }
 
 private static final long serialVersionUID = 1L;
 
-	private void initLayout(boolean isAudit) {
+	private void initLayout() {
     	CSVDataExporter csvDataExporter = new CSVDataExporter() {
     		private static final long serialVersionUID = 1L;
     		
     		@Override
     		public <T> void exportData(IDataProvider<T> dataProvider, List<IExportableColumn<T, ?>> columns,
     				OutputStream outputStream) throws IOException {
-    			if (isAudit) {
-    			        ((AuditEventRecordProvider) dataProvider).setExportSize(true);
-                        super.exportData(dataProvider, columns, outputStream);
-                        ((AuditEventRecordProvider) dataProvider).setExportSize(false);
-                
-    			} else {
-    				super.exportData(dataProvider, columns, outputStream);
-    			}
-    			
+				((BaseSortableDataProvider) dataProvider).setExportSize(true);
+				super.exportData(dataProvider, columns, outputStream);
+				((BaseSortableDataProvider) dataProvider).setExportSize(false);
     		}
     	};
         final AbstractAjaxDownloadBehavior ajaxDownloadBehavior = new AbstractAjaxDownloadBehavior() {
