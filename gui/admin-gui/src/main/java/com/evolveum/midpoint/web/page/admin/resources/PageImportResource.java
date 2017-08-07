01bdd2c4e66d70d7f0d132c6a71c8ad47543125a
kate <honchar@evolveum.com>
2017-08-07 15:53:07 +0200

    MID-4100 fix

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/resources/PageImportResource.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/resources/PageImportResource.java
@@ -12,9 +12,10 @@ import com.evolveum.midpoint.web.page.admin.configuration.PageImportObject;
  * menu item to a separate class (to fix menu item enabling issue)
  *
  */
-@PageDescriptor(url = "/admin/config/import", action = {
-        @AuthorizationAction(actionUri = PageAdminConfiguration.AUTH_CONFIGURATION_ALL,
-                label = PageAdminConfiguration.AUTH_CONFIGURATION_ALL_LABEL, description = PageAdminConfiguration.AUTH_CONFIGURATION_ALL_DESCRIPTION),
+@PageDescriptor(url = "/admin/config/importResource", action = {
+        @AuthorizationAction(actionUri = PageAdminResources.AUTH_RESOURCE_ALL,
+                label = PageAdminResources.AUTH_RESOURCE_ALL_LABEL,
+                description = PageAdminResources.AUTH_RESOURCE_ALL_DESCRIPTION),
         @AuthorizationAction(actionUri = AuthorizationConstants.AUTZ_UI_CONFIGURATION_IMPORT_URL,
                 label = "PageImportObject.auth.configImport.label", description = "PageImportObject.auth.configImport.description")})
 public class PageImportResource extends PageImportObject {
