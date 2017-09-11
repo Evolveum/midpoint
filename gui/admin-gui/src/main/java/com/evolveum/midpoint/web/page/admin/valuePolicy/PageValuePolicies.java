6b0fd1156779bac5c14d48db5b9389344c667542
matus <matus.macik@gmail.com>
2017-09-11 15:50:07 +0200

    Addition of license headers, changig deprecated code block

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/valuePolicy/PageValuePolicies.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/valuePolicy/PageValuePolicies.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2017 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.evolveum.midpoint.web.page.admin.valuePolicy;
 
 import com.evolveum.midpoint.gui.api.GuiStyleConstants;
@@ -29,7 +45,11 @@ import java.util.List;
  * Created by matus on 9/8/2017.
  */
 
-@PageDescriptor(url = "/admin/valuepolicies", action = {
+@PageDescriptor(
+       urls = {
+               @Url(mountUrl = "/admin/valuepolicies", matchUrlForSecurity = "/admin/valuepolicies" )
+       },
+        action = {
         @AuthorizationAction(actionUri = PageAdminValuePolicies.AUTH_VALUE_POLICIES_ALL,
                 label = PageAdminValuePolicies.AUTH_VALUE_POLICIES_ALL_LABEL,
                 description = PageAdminValuePolicies.AUTH_VALUE_POLICIES_ALL_DESCRIPTION),
