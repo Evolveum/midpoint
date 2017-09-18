66d8eb775f95c92bd936ca320465ae607a00d9f0
Pavol Mederly <mederly@evolveum.com>
2017-09-18 14:38:03 +0200

    Migrated constraint reference from simple string to extensible structure.

--- infra/schema/src/main/java/com/evolveum/midpoint/schema/util/PolicyRuleTypeUtil.java
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/util/PolicyRuleTypeUtil.java
@@ -123,11 +123,11 @@ public class PolicyRuleTypeUtil {
 				sb.append(abbreviation != null ? abbreviation : name.getLocalPart());
 			}
 		}
-		for (String ref : constraints.getRef()) {
+		for (PolicyConstraintReferenceType ref : constraints.getRef()) {
 			if (sb.length() > 0) {
 				sb.append(join);
 			}
-			sb.append("ref:").append(ref);
+			sb.append("ref:").append(ref.getName());
 		}
 		return sb.toString();
 	}
@@ -567,15 +567,16 @@ public class PolicyRuleTypeUtil {
 	@SuppressWarnings("unchecked")
 	private static void resolveLocalReferences(PolicyConstraintsType pc, ConstraintResolver resolver)
 			throws ObjectNotFoundException, SchemaException {
-		for (String ref : pc.getRef()) {
-			if (StringUtils.isEmpty(ref)) {
+		for (PolicyConstraintReferenceType ref : pc.getRef()) {
+			String refName = ref != null ? ref.getName() : null;
+			if (StringUtils.isEmpty(refName)) {
 				throw new SchemaException("Illegal empty reference: " + ref);
 			}
 			List<String> pathToRoot = getPathToRoot(pc);
-			if (pathToRoot.contains(ref)) {
-				throw new SchemaException("Trying to resolve cyclic reference to constraint '" + ref + "'. Contained in: " + pathToRoot);
+			if (pathToRoot.contains(refName)) {
+				throw new SchemaException("Trying to resolve cyclic reference to constraint '" + refName + "'. Contained in: " + pathToRoot);
 			}
-			JAXBElement<? extends AbstractPolicyConstraintType> resolved = resolver.resolve(ref);
+			JAXBElement<? extends AbstractPolicyConstraintType> resolved = resolver.resolve(refName);
 			QName constraintName = resolved.getName();
 			AbstractPolicyConstraintType constraintValue = resolved.getValue();
 			PrismContainer<? extends AbstractPolicyConstraintType> container = pc.asPrismContainerValue().findOrCreateContainer(constraintName);
