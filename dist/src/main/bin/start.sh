ed68036ec4642916421137426ab690eb996f7407
Kristian <kristian.suchanovsky@evolveum.com>
2017-10-31 20:23:55 +0100

    Spring boot start.sh script added

--- dist/src/main/bin/start.sh
+++ dist/src/main/bin/start.sh
@@ -1,1 +1,4 @@
-# TODO
+#!/bin/bash
+echo "Starting midPoint"
+path=$(cd $(dirname "$0") && pwd -P)/$(basename "$1")
+java -jar -Xms1024M -Xmx2048M -Dmidpoint.home=$path../var $path../lib/midpoint.war
