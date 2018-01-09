deb0e0da15446162b90fc4caa68419bdc51971e0
Viliam Repan <vilo.repan@evolveum.com>
2018-01-09 15:58:27 +0100

    postgres audit upgrade script tested, fixed

--- config/sql/_all/postgresql-upgrade-3.7-3.8.sql
+++ config/sql/_all/postgresql-upgrade-3.7-3.8.sql
@@ -12,3 +12,8 @@ CREATE SEQUENCE m_audit_ref_value_id_seq;
 ALTER TABLE m_audit_ref_value ALTER COLUMN id SET NOT NULL;
 ALTER TABLE m_audit_ref_value ALTER COLUMN id SET DEFAULT nextval('m_audit_ref_value_id_seq');
 ALTER SEQUENCE m_audit_ref_value_id_seq OWNED BY m_audit_ref_value.id;
+
+SELECT setval('m_audit_event_id_seq', (SELECT max(id) + 1 FROM m_audit_event), FALSE);
+SELECT setval('m_audit_prop_value_id_seq', (SELECT max(id) + 1 FROM m_audit_prop_value), FALSE);
+SELECT setval('m_audit_ref_value_id_seq', (SELECT max(id) + 1 FROM m_audit_ref_value), FALSE);
+
