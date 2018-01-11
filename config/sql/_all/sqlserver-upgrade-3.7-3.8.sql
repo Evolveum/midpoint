adf65f25b30d1ff3b72ee0aac45a264d37c45350
Pavol Mederly <mederly@evolveum.com>
2018-01-11 09:17:35 +0100

    Add SQLServer upgrade procedure for audit tables
    Also formatted sqlserver-3.7-all.sql file.

--- config/sql/_all/sqlserver-upgrade-3.7-3.8.sql
+++ config/sql/_all/sqlserver-upgrade-3.7-3.8.sql
@@ -1,23 +1,96 @@
---- todo figure out what to do with audit tables (changed id column, now with identity)
+CREATE TABLE m_audit_event_NEW (
+  id                BIGINT IDENTITY NOT NULL,
+  attorneyName      NVARCHAR(255) COLLATE database_default,
+  attorneyOid       NVARCHAR(36) COLLATE database_default,
+  channel           NVARCHAR(255) COLLATE database_default,
+  eventIdentifier   NVARCHAR(255) COLLATE database_default,
+  eventStage        INT,
+  eventType         INT,
+  hostIdentifier    NVARCHAR(255) COLLATE database_default,
+  initiatorName     NVARCHAR(255) COLLATE database_default,
+  initiatorOid      NVARCHAR(36) COLLATE database_default,
+  initiatorType     INT,
+  message           NVARCHAR(1024) COLLATE database_default,
+  nodeIdentifier    NVARCHAR(255) COLLATE database_default,
+  outcome           INT,
+  parameter         NVARCHAR(255) COLLATE database_default,
+  remoteHostAddress NVARCHAR(255) COLLATE database_default,
+  result            NVARCHAR(255) COLLATE database_default,
+  sessionIdentifier NVARCHAR(255) COLLATE database_default,
+  targetName        NVARCHAR(255) COLLATE database_default,
+  targetOid         NVARCHAR(36) COLLATE database_default,
+  targetOwnerName   NVARCHAR(255) COLLATE database_default,
+  targetOwnerOid    NVARCHAR(36) COLLATE database_default,
+  targetType        INT,
+  taskIdentifier    NVARCHAR(255) COLLATE database_default,
+  taskOID           NVARCHAR(255) COLLATE database_default,
+  timestampValue    DATETIME2,
+  -- no primary key for now
+);
 
-drop table m_audit_ref_value;
-drop table m_audit_prop_value;
-drop table m_audit_item;
-drop table m_audit_delta;
-drop table m_audit_event;
+CREATE TABLE m_audit_prop_value_NEW (
+  id        BIGINT IDENTITY NOT NULL,
+  name      NVARCHAR(255) COLLATE database_default,
+  record_id BIGINT,
+  value     NVARCHAR(1024) COLLATE database_default,
+  -- no primary key for now
+);
 
-create table m_audit_delta (checksum nvarchar(32) collate database_default not null, record_id bigint not null, delta nvarchar(MAX), deltaOid nvarchar(36) collate database_default, deltaType int, fullResult nvarchar(MAX), objectName_norm nvarchar(255) collate database_default, objectName_orig nvarchar(255) collate database_default, resourceName_norm nvarchar(255) collate database_default, resourceName_orig nvarchar(255) collate database_default, resourceOid nvarchar(36) collate database_default, status int, primary key (checksum, record_id));
-create table m_audit_event (id bigint identity not null, attorneyName nvarchar(255) collate database_default, attorneyOid nvarchar(36) collate database_default, channel nvarchar(255) collate database_default, eventIdentifier nvarchar(255) collate database_default, eventStage int, eventType int, hostIdentifier nvarchar(255) collate database_default, initiatorName nvarchar(255) collate database_default, initiatorOid nvarchar(36) collate database_default, initiatorType int, message nvarchar(1024) collate database_default, nodeIdentifier nvarchar(255) collate database_default, outcome int, parameter nvarchar(255) collate database_default, remoteHostAddress nvarchar(255) collate database_default, result nvarchar(255) collate database_default, sessionIdentifier nvarchar(255) collate database_default, targetName nvarchar(255) collate database_default, targetOid nvarchar(36) collate database_default, targetOwnerName nvarchar(255) collate database_default, targetOwnerOid nvarchar(36) collate database_default, targetType int, taskIdentifier nvarchar(255) collate database_default, taskOID nvarchar(255) collate database_default, timestampValue datetime2, primary key (id));
-create table m_audit_item (changedItemPath nvarchar(900) collate database_default not null, record_id bigint not null, primary key (changedItemPath, record_id));
-create table m_audit_prop_value (id bigint identity not null, name nvarchar(255) collate database_default, record_id bigint, value nvarchar(1024) collate database_default, primary key (id));
-create table m_audit_ref_value (id bigint identity not null, name nvarchar(255) collate database_default, oid nvarchar(255) collate database_default, record_id bigint, targetName_norm nvarchar(255) collate database_default, targetName_orig nvarchar(255) collate database_default, type nvarchar(255) collate database_default, primary key (id));
+CREATE TABLE m_audit_ref_value_NEW (
+  id              BIGINT IDENTITY NOT NULL,
+  name            NVARCHAR(255) COLLATE database_default,
+  oid             NVARCHAR(255) COLLATE database_default,
+  record_id       BIGINT,
+  targetName_norm NVARCHAR(255) COLLATE database_default,
+  targetName_orig NVARCHAR(255) COLLATE database_default,
+  type            NVARCHAR(255) COLLATE database_default,
+  -- no primary key for now
+);
 
-create index iTimestampValue on m_audit_event (timestampValue);
-create index iChangedItemPath on m_audit_item (changedItemPath);
-create index iAuditPropValRecordId on m_audit_prop_value (record_id);
-create index iAuditRefValRecordId on m_audit_ref_value (record_id);
+-- copying tables (can take a long time)
+SET IDENTITY_INSERT m_audit_event_NEW ON;
+INSERT INTO m_audit_event_NEW (id, attorneyName, attorneyOid, channel, eventIdentifier, eventStage, eventType, hostIdentifier,
+                               initiatorName, initiatorOid, initiatorType, message, nodeIdentifier, outcome, parameter, remoteHostAddress,
+                               result, sessionIdentifier, targetName, targetOid, targetOwnerName, targetOwnerOid, targetType,
+                               taskIdentifier, taskOID, timestampValue)
+  SELECT
+    id, attorneyName, attorneyOid, channel, eventIdentifier, eventStage, eventType, hostIdentifier, initiatorName, initiatorOid,
+    initiatorType, message, nodeIdentifier, outcome, parameter, remoteHostAddress, result, sessionIdentifier, targetName, targetOid,
+    targetOwnerName, targetOwnerOid, targetType, taskIdentifier, taskOID, timestampValue
+  FROM m_audit_event;
+SET IDENTITY_INSERT m_audit_event_NEW OFF;
 
-alter table m_audit_delta add constraint fk_audit_delta foreign key (record_id) references m_audit_event;
-alter table m_audit_item add constraint fk_audit_item foreign key (record_id) references m_audit_event;
-alter table m_audit_prop_value add constraint fk_audit_prop_value foreign key (record_id) references m_audit_event;
-alter table m_audit_ref_value add constraint fk_audit_ref_value foreign key (record_id) references m_audit_event;
+SET IDENTITY_INSERT m_audit_prop_value_NEW ON;
+INSERT INTO m_audit_prop_value_NEW (id, name, record_id, [value])
+  SELECT id, [name], record_id, [value] FROM m_audit_prop_value;
+SET IDENTITY_INSERT m_audit_prop_value_NEW OFF;
+
+SET IDENTITY_INSERT m_audit_ref_value_NEW ON;
+INSERT INTO m_audit_ref_value_NEW (id, name, oid, record_id, targetName_norm, targetName_orig, type)
+  SELECT id, [name], oid, record_id, targetName_norm, targetName_orig, type FROM m_audit_ref_value;
+SET IDENTITY_INSERT m_audit_ref_value_NEW OFF;
+
+ALTER TABLE m_audit_delta DROP CONSTRAINT fk_audit_delta;
+ALTER TABLE m_audit_item DROP CONSTRAINT fk_audit_item;
+
+-- renaming new tables to old ones
+DROP TABLE m_audit_prop_value;
+EXEC sp_rename 'm_audit_prop_value_NEW', 'm_audit_prop_value'
+DROP TABLE m_audit_ref_value;
+EXEC sp_rename 'm_audit_ref_value_NEW', 'm_audit_ref_value'
+DROP TABLE m_audit_event;
+EXEC sp_rename 'm_audit_event_NEW', 'm_audit_event'
+
+-- restoring indices and foreign key constraints
+ALTER TABLE m_audit_event ADD PRIMARY KEY (id);
+ALTER TABLE m_audit_prop_value ADD PRIMARY KEY (id);
+ALTER TABLE m_audit_ref_value ADD PRIMARY KEY (id);
+
+CREATE INDEX iTimestampValue ON m_audit_event (timestampValue);
+CREATE INDEX iAuditPropValRecordId ON m_audit_prop_value (record_id);
+CREATE INDEX iAuditRefValRecordId ON m_audit_ref_value (record_id);
+
+ALTER TABLE m_audit_delta ADD CONSTRAINT fk_audit_delta FOREIGN KEY (record_id) REFERENCES m_audit_event;
+ALTER TABLE m_audit_item ADD CONSTRAINT fk_audit_item FOREIGN KEY (record_id) REFERENCES m_audit_event;
+ALTER TABLE m_audit_prop_value ADD CONSTRAINT fk_audit_prop_value FOREIGN KEY (record_id) REFERENCES m_audit_event;
+ALTER TABLE m_audit_ref_value ADD CONSTRAINT fk_audit_ref_value FOREIGN KEY (record_id) REFERENCES m_audit_event;
