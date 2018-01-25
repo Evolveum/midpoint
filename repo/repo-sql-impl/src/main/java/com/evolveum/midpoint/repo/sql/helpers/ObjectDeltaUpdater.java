36a094c03ea86d226332ed9bc4ead473f4fc80be
Viliam Repan <vilo.repan@evolveum.com>
2018-01-25 16:29:58 +0100

    more bugfixes, not finished yet

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/helpers/ObjectDeltaUpdater.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/helpers/ObjectDeltaUpdater.java
@@ -42,6 +42,8 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ShadowType;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -216,6 +218,11 @@ public class ObjectDeltaUpdater {
                 extValues.add(new PrismEntityPair(value, extValue));
             }
 
+            if (extValues.isEmpty()) {
+                // no changes in indexed values
+                return;
+            }
+
             RAnyValue first = extValues.iterator().next().repository;
             Class type = first.getClass();
 
@@ -254,6 +261,8 @@ public class ObjectDeltaUpdater {
         if (delta.getValuesToReplace() != null && !delta.getValuesToReplace().isEmpty()) {
             processAnyExtensionDeltaValues(delta.getValuesToReplace(), object, objectOwnerType, assignmentExtension, assignmentExtensionType,
                     (existing, fromDelta) -> {
+                        // todo don't remove all if not necessary, just the ones that don't exist in fromDelta,
+                        // than add only those items from fromDelta that were not in existing
                         existing.clear();
                         markNewOnesTransientAndAddToExisting(existing, (Collection) fromDelta);
                     },
@@ -473,7 +482,56 @@ public class ObjectDeltaUpdater {
         // handle replace
         Collection<PrismEntityPair<?>> valuesToReplace = processDeltaValues(delta.getValuesToReplace(), outputType, delta, bean);
         if (!valuesToReplace.isEmpty()) {
-            collection.clear();
+            // remove all items from existing which don't exist in valuesToReplace
+            // add items from valuesToReplace to existing, only those which aren't already there
+
+            Pair<Collection<Object>, Set<Long>> split = splitPrismEntityPairs(valuesToReplace);
+            Collection<Object> repositoryObjects = split.getLeft();
+            Set<Long> containerIds = split.getRight();
+
+            Collection skipAddingTheseObjects = new ArrayList();
+            Collection skipAddingTheseIds = new ArrayList();
+            Collection toDelete = new ArrayList();
+            for (Object obj : collection) {
+                if (obj instanceof Container) {
+                    Container container = (Container) obj;
+
+                    long id = container.getId().longValue();
+                    if (!containerIds.contains(id)) {
+                        toDelete.add(container);
+                    } else {
+                        skipAddingTheseIds.add(id);
+                    }
+                } else {
+                    // e.g. RObjectReference
+                    if (!repositoryObjects.contains(obj)) {
+                        toDelete.add(obj);
+                    } else {
+                        skipAddingTheseObjects.add(obj);
+                    }
+                }
+            }
+            collection.removeAll(toDelete);
+
+            Iterator<PrismEntityPair<?>> iterator = valuesToReplace.iterator();
+            while (iterator.hasNext()) {
+                PrismEntityPair pair = iterator.next();
+                Object obj = pair.repository;
+                if (obj instanceof Container) {
+                    Container container = (Container) obj;
+
+                    // todo this will fail as container.getId() returns null at this time
+                    // new id was not generated yet
+                    if (skipAddingTheseIds.contains(container.getId())) {
+                        iterator.remove();
+                    }
+                } else {
+                    if (skipAddingTheseObjects.contains(obj)) {
+                        iterator.remove();
+                    }
+                }
+            }
+
             markNewOnesTransientAndAddToExisting(collection, valuesToReplace);
 
             return;
@@ -484,18 +542,12 @@ public class ObjectDeltaUpdater {
         markNewOnesTransientAndAddToExisting(collection, valuesToAdd);
 
         // handle delete
-        Collection<PrismEntityPair> valuesToDelete = processDeltaValues(delta.getValuesToDelete(), outputType, delta, bean);
-        Set<Long> containerIdsToDelete = new HashSet<>();
-        for (PrismEntityPair pair : valuesToDelete) {
-            if (pair.repository instanceof Container) {
-                Container container = (Container) pair.repository;
-
-                long id = container.getId().longValue();
-                containerIdsToDelete.add(id);
-            }
-        }
-
+        Collection<PrismEntityPair<?>> valuesToDelete = processDeltaValues(delta.getValuesToDelete(), outputType, delta, bean);
         if (!valuesToDelete.isEmpty()) {
+            Pair<Collection<Object>, Set<Long>> split = splitPrismEntityPairs(valuesToDelete);
+            Collection<Object> repositoryObjectsToDelete = split.getLeft();
+            Set<Long> containerIdsToDelete = split.getRight();
+
             Collection toDelete = new ArrayList();
             for (Object obj : collection) {
                 if (obj instanceof Container) {
@@ -507,13 +559,30 @@ public class ObjectDeltaUpdater {
                     }
                 } else {
                     // e.g. RObjectReference
-                    if (valuesToDelete.contains(obj)) {
+                    if (repositoryObjectsToDelete.contains(obj)) {
                         toDelete.add(obj);
                     }
                 }
             }
             collection.removeAll(toDelete);
         }
+    }
+
+    private Pair<Collection<Object>, Set<Long>> splitPrismEntityPairs(Collection<PrismEntityPair<?>> collection) {
+        Collection<Object> repositoryObjects = new ArrayList<>();
+        Set<Long> containerIds = new HashSet<>();
+        for (PrismEntityPair pair : collection) {
+            if (pair.repository instanceof Container) {
+                Container container = (Container) pair.repository;
+
+                long id = container.getId().longValue();
+                containerIds.add(id);
+            }
+
+            repositoryObjects.add(pair.repository);
+        }
+
+        return new ImmutablePair<>(repositoryObjects, containerIds);
     }
 
     private void markNewOnesTransientAndAddToExisting(Collection existing, Collection<PrismEntityPair<?>> newOnes) {
@@ -541,6 +610,7 @@ public class ObjectDeltaUpdater {
                     nextId++;
                 }
 
+                usedIds.add(nextId);
                 ((Container) item.repository).setId(nextId);
                 ((PrismContainerValue) item.prism).setId(nextId.longValue());
             }
