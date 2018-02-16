01755f9bf07e087f377258097e689de5c1f0a108
Kristian <ksuchanovsky@evolveum.com>
2018-02-16 19:09:59 +0100

    modified linux scripts for ninja.jar added ninja.sh

--- dist/src/main/bin/import.sh
+++ dist/src/main/bin/import.sh
@@ -17,98 +17,14 @@ done
 
 # Get standard environment variable
 PRGDIR=`dirname "$PRG"`
+EXECUTABLE=ninja.sh
 
-cd "$PRGDIR/.." >/dev/null
+  if [ ! -x "$PRGDIR"/"$EXECUTABLE" ]; then
+    echo "Cannot find $PRGDIR/$EXECUTABLE"
+    echo "The file is absent or does not have execute permission"
+    echo "This file is needed to run this program"
+    exit 1
+  fi
 
-cd "$SCRIPT_PATH/.."
+exec "$PRGDIR"/"$EXECUTABLE" import "$@"
 
-if [ ! -d var ] ; then
-	echo "ERROR: midpoint.home directory desn't exist"
-	exit 1
-else
-	MIDPOINT_HOME=$(cd "$SCRIPT_PATH../var"; pwd)
-fi
-
-[ -z "$MIDPOINT_HOME" ] && MIDPOINT_HOME=`cd "$SCRIPT_PATH../var" >/dev/null; pwd`
-
-cd "$SCRIPT_PATH../lib"
-
-if [ ! -f ninja.jar ] ; then 
-	echo "ERROR: ninja.jar is not in /lib directory"
-	exit 1
-fi 
-
-# Set UMASK unless it has been overridden
-if [ -z "$UMASK" ]; then
-    UMASK="0027"
-fi
-umask $UMASK
-
-if [ -z "$JAVA_HOME" ] ; then
-  _RUNJAVA=java
-else
-  _RUNJAVA="$JAVA_HOME"/bin/java
-fi
-
-
-OPTIONS=""
-IMP_OPTIONS=""
-
-while getopts "c:hm:p:P:sU:u:vVef:i:o:Ort:z" option; do
- case "${option}" in
- c) c="-c $OPTARG "
-	OPTIONS+="${c}"
- 	;;
- h) h="-h "
-	OPTIONS+="${h}"
-	;;
- m) echo "midpoint.home set by default"	
-	;;
- p) p="-p $OPTARG "
-	OPTIONS+="${p}"
-	;;
- P) P="-P $OPTARG "
-	OPTIONS+="${P}"
-	;;
- s) s="-s "
-	OPTIONS+="${s}"
-	;;
- U) U="-U $OPTARG "
-	OPTIONS+="${U}"
-	;;
- u) u="-u $OPTARG "
-	OPTIONS+="${u}"
-	;;
- v) v="-v "
-	OPTIONS+="${v}"
-	;;
- V) V="-V "
-	OPTIONS+="${V}"
-	;;
- e) e="-e "
-	IMP_OPTIONS+="${e}"
-	;;
- f) f="-f $OPTARG "
-	IMP_OPTIONS+="${f}"
-	;;
- i) i="-i $OPTARG "
-	IMP_OPTIONS+="${i}"
-	;;
- o) o="-o $OPTARG "
-	IMP_OPTIONS+="${o}"
-	;;
- O) echo "Overwrite option already set by default"
-	;;
- r) r="-r "
-	IMP_OPTIONS+="${r}"
-	;;
- t) t="-t $OPTARG "
-	IMP_OPTIONS+="${t}"
-	;;
- z) z="-z "
-	IMP_OPTIONS+="${z}"
-	;;
- esac
-done
-
-exec "$_RUNJAVA" -jar $SCRIPT_PATH../lib/ninja.jar $OPTIONS -m $MIDPOINT_HOME import $IMP_OPTIONS -O
---
+++ dist/src/main/bin/ninja.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(cd $(dirname "$0") && pwd -P)/
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variable
+PRGDIR=`dirname "$PRG"`
+
+cd "$PRGDIR/.." >/dev/null
+
+cd "$SCRIPT_PATH/.."
+
+if [ ! -d var ] ; then
+	echo "ERROR: midpoint.home directory desn't exist"
+	exit 1
+else
+	MIDPOINT_HOME=$(cd "$SCRIPT_PATH../var"; pwd)
+fi
+
+[ -z "$MIDPOINT_HOME" ] && MIDPOINT_HOME=`cd "$SCRIPT_PATH../var" >/dev/null; pwd`
+
+cd "$SCRIPT_PATH../lib"
+
+if [ ! -f ninja.jar ] ; then 
+	echo "ERROR: ninja.jar is not in /lib directory"
+	exit 1
+fi 
+
+# Set UMASK unless it has been overridden
+if [ -z "$UMASK" ]; then
+    UMASK="0027"
+fi
+umask $UMASK
+
+if [ -z "$JAVA_HOME" ] ; then
+  _RUNJAVA=java
+else
+  _RUNJAVA="$JAVA_HOME"/bin/java
+fi
+
+DATE=`date '+%Y-%m-%d-%H:%M:%S'`
+command="$1"
+
+OPTIONS=""
+EXP_OPTIONS=""
+
+while [ "$2" != "" ];
+do
+case $2 in
+ -c|--charset) 	
+	OPTIONS+="$2 $3 "
+	;;
+ -h|--help)    
+	OPTIONS+="$2 "
+	;;
+ -m|--midpoint-home) 	
+	echo "INFO: midpoint.home set by default"	
+	;;
+ -p|--password) 	
+	OPTIONS+="$2 $3 "
+	;;
+ -P|--password-ask) 	
+	OPTIONS+="$2 $3 "
+	;;
+ -s|--silent) 	
+	OPTIONS+="$2 "
+	;;
+ -U|--url) 	
+	OPTIONS+="$2 $3 "
+	;;
+ -u|--username) 	
+	OPTIONS+="$2 $3 "
+	;;
+ -v|--verbose) 	
+	OPTIONS+="$2 "
+	;;
+ -V|--version) 	
+	OPTIONS+="$2 "
+	;;
+ -f|--filter)
+	if [ "$command" = "export" ] ; then	
+	EXP_OPTIONS+="$2 $3 "
+	fi
+	if [ "$command" = "import" ] ; then	
+	IMP_OPTIONS+="$2 $3 "
+	fi
+	;;
+ -o|--oid) 
+	if [ "$command" = "export" ] ; then	
+	EXP_OPTIONS+="$2 $3 "
+	fi
+	if [ "$command" = "import" ] ; then	
+	IMP_OPTIONS+="$2 $3 "
+	fi
+	;;
+ -O|--output|--overwrite) 
+	if [ "$command" = "export" ] ; then		
+	echo "INFO: Output file already set by default"
+	fi
+	if [ "$command" = "import" ] ; then
+	echo "INFO: Overwrite option already set by default"	
+	fi
+	;;
+ -r|--raw)
+	if [ "$command" = "export" ] ; then		
+	EXP_OPTIONS+="$2 "
+	fi
+	if [ "$command" = "import" ] ; then
+	IMP_OPTIONS+="$2 "
+	fi
+	;;
+ -t|--type)
+	if [ "$command" = "export" ] ; then		
+	EXP_OPTIONS+="$2 $3 "
+	fi
+	if [ "$command" = "import" ] ; then
+	IMP_OPTIONS+="$2 $3 "
+	fi
+	;;
+ -z|--zip)	
+	if [ "$command" = "export" ] ; then
+	EXP_OPTIONS+="$2 "
+	fi
+	if [ "$command" = "import" ] ; then
+	IMP_OPTIONS+="$2 "
+	fi
+	;;
+ -e|--allowUnencryptedValues)
+	IMP_OPTIONS+="$2 "
+	shift
+	;;
+ -i|--input)
+	IMP_OPTIONS+="$2 $3 "
+	;;
+ -k|--key-password)
+	KEY_OPTIONS+="$2 $3 "
+	;;
+ -K|--key-password-ask)
+	KEY_OPTIONS+="$2 "
+	;;
+esac
+
+shift
+
+done
+
+
+if [ "$command" = "export" ] ; then
+	exec "$_RUNJAVA" -jar $SCRIPT_PATH../lib/ninja.jar $OPTIONS -m $MIDPOINT_HOME export $EXP_OPTIONS -O "export.$DATE.xml"
+fi
+if [ "$command" = "import" ] ; then
+	exec "$_RUNJAVA" -jar $SCRIPT_PATH../lib/ninja.jar $OPTIONS -m $MIDPOINT_HOME import $IMP_OPTIONS -O
+fi
+if [ "$command" = "keys" ] ; then
+	exec "$_RUNJAVA" -jar $SCRIPT_PATH../lib/ninja.jar $OPTIONS -m $MIDPOINT_HOME keys $KEY_OPTIONS
+fi
