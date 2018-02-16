7052f9628a76815d27a119090a97ec57fbdebaec
Radovan Semancik <radovan.semancik@evolveum.com>
2018-02-16 13:04:27 +0100

    Fixing failing model-intest

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/ChangeExecutor.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/ChangeExecutor.java
@@ -916,7 +916,9 @@ public class ChangeExecutor {
 
 		} finally {
 
+			LOGGER.info("RRRR1: {}", result);
 			result.computeStatus();
+			LOGGER.info("RRRR2: {}", result);
 			if (objectContext != null) {
 				if (!objectDelta.hasCompleteDefinition()) {
 					throw new SchemaException("object delta does not have complete definition");
