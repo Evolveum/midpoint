b9815120553e79110c9a2902283bb51887e0b23b
Misagh Moayyed <mmoayyed@unicon.net>
2018-03-05 10:10:08 +0100

    clean up unused imports

--- model/model-common/src/main/java/com/evolveum/midpoint/model/common/util/ProfilingModelInspector.java
+++ model/model-common/src/main/java/com/evolveum/midpoint/model/common/util/ProfilingModelInspector.java
@@ -21,7 +21,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import com.evolveum.midpoint.model.api.context.Mapping;
 import com.evolveum.midpoint.model.api.context.ModelContext;
