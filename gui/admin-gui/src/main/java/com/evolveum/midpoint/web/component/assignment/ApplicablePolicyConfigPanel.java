689b2556801ed45742f857e3cb074028ac126740
kate <honchar@evolveum.com>
2018-03-14 11:55:46 +0100

    applicable policy configuration panel draft

---
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/assignment/ApplicablePolicyConfigPanel.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2015-2018 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.evolveum.midpoint.web.component.assignment;
+
+import com.evolveum.midpoint.gui.api.component.BasePanel;
+import com.evolveum.midpoint.gui.api.model.LoadableModel;
+import com.evolveum.midpoint.gui.api.util.WebModelServiceUtils;
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.schema.result.OperationResult;
+import com.evolveum.midpoint.util.logging.LoggingUtils;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
+import com.evolveum.midpoint.web.component.prism.ContainerWrapper;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+import org.apache.wicket.model.IModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by honchar.
+ */
+public class ApplicablePolicyConfigPanel extends BasePanel<ContainerWrapper<AssignmentType>>{
+    private static final long serialVersionUID = 1L;
+
+    private static final Trace LOGGER = TraceManager.getTrace(ApplicablePolicyConfigPanel.class);
+    private static final String DOT_CLASS = ApplicablePolicyConfigPanel.class.getName() + ".";
+    private final static String OPERATION_LOAD_SYS_CONFIG = DOT_CLASS + "loadSystemConfiguration";
+
+    private LoadableModel<List<ObjectReferenceType>> policyGroupsListModel;
+
+    public ApplicablePolicyConfigPanel(String id, IModel<ContainerWrapper<AssignmentType>> model){
+        super(id, model);
+    }
+
+    @Override
+    protected void onInitialize(){
+        initModels();
+//        initLayout();
+    }
+
+    private void initModels(){
+        policyGroupsListModel = new LoadableModel<List<ObjectReferenceType>>(false) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected List<ObjectReferenceType> load() {
+                List<ObjectReferenceType> policyGroupsList = new ArrayList<>();
+                OperationResult result = new OperationResult(OPERATION_LOAD_SYS_CONFIG);
+                try {
+                    SystemConfigurationType sysConfig = getPageBase().getModelInteractionService().getSystemConfiguration(result);
+                    if (sysConfig == null){
+                        return policyGroupsList;
+                    } else {
+                        List<ObjectPolicyConfigurationType> policiesConfig = sysConfig.getDefaultObjectPolicyConfiguration();
+                        if (policiesConfig == null){
+                            return policyGroupsList;
+                        }
+                        policiesConfig.forEach(policyConfig -> {
+                            if (policyConfig.getType() != null && policyConfig.getType().equals(RoleType.COMPLEX_TYPE)){
+//                                policyGroupsList = policyConfig.getApplicablePolicies();
+                            }
+                        });
+                    }
+                } catch (Exception ex){
+                    LoggingUtils.logUnexpectedException(LOGGER, "Cannot retrieve system configuration", ex);
+                }
+                return policyGroupsList;
+            }
+        };
+    }
+
+}
