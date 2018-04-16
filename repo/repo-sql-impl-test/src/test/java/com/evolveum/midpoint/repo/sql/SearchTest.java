a99f8ffcd919bf6744d867965d7ce71ea6194bc9
Pavol Mederly <mederly@evolveum.com>
2018-04-16 11:06:11 +0200

    Fix missing words in full text indexing (MID-4590)
    (cherry picked from commit 970de28)

--- repo/repo-sql-impl-test/src/test/java/com/evolveum/midpoint/repo/sql/SearchTest.java
+++ repo/repo-sql-impl-test/src/test/java/com/evolveum/midpoint/repo/sql/SearchTest.java
@@ -708,6 +708,11 @@ public class SearchTest extends BaseSQLRepoTest {
 				true, 1);
 
 		assertUsersFound(QueryBuilder.queryFor(UserType.class, prismContext)
+						.fullText("viverra")
+						.build(),
+				true, 1);       // MID-4590
+
+		assertUsersFound(QueryBuilder.queryFor(UserType.class, prismContext)
 						.fullText("sollicitudin")
 						.build(),
 				true, 0);
@@ -755,7 +760,12 @@ public class SearchTest extends BaseSQLRepoTest {
 						.build(),
 				distinct, 1);
 
-		assertUsersFoundBySearch(QueryBuilder.queryFor(UserType.class, prismContext)
+        assertUsersFound(QueryBuilder.queryFor(UserType.class, prismContext)
+                        .fullText("viverra")
+                        .build(),
+                true, 1);       // MID-4590
+
+        assertUsersFoundBySearch(QueryBuilder.queryFor(UserType.class, prismContext)
 						.fullText("sollicitudin")
 						.asc(UserType.F_FULL_NAME)
 						.maxSize(100)
