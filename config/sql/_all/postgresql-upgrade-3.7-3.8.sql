41fae81b8a75a1d690b6c8d2e56be58a6ab8ddec
Viliam Repan <vilo.repan@evolveum.com>
2018-05-21 11:22:28 +0200

    postgres upgrade escape fix

--- config/sql/_all/postgresql-upgrade-3.7-3.8.sql
+++ config/sql/_all/postgresql-upgrade-3.7-3.8.sql
@@ -87,8 +87,8 @@ SELECT setval('m_audit_prop_value_id_seq', (SELECT max(id) + 1 FROM m_audit_prop
 SELECT setval('m_audit_ref_value_id_seq', (SELECT max(id) + 1 FROM m_audit_ref_value), FALSE);
 
 ALTER TABLE m_audit_delta
-  ALTER COLUMN delta TYPE BYTEA USING delta::bytea,
-  ALTER COLUMN fullResult TYPE BYTEA USING fullResult::bytea;
+  ALTER COLUMN delta TYPE BYTEA USING decode(replace(replace(delta, '\', E'\\134'), '''', E'\\047'), 'escape'),
+  ALTER COLUMN fullResult TYPE BYTEA USING decode(replace(replace(fullResult, '\', E'\\134'), '''', E'\\047'),'escape');
 
 ALTER TABLE m_audit_event
   ADD COLUMN targetOwnerType INT4;
