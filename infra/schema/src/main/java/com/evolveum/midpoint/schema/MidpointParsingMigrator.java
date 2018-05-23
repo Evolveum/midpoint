22939cea1fed9f40d1187fbdf1f5f6be97ba0c6e
Pavol Mederly <mederly@evolveum.com>
2018-05-23 12:58:52 +0200

    Fix additionalInformation handling (MID-4017)
    Approval stage additionalInformation has two forms: xsd:string
    and c:InformationType. The latter allows for localizable strings;
    however, it was broken. Also fix the support for multiple information
    pieces.

--- infra/schema/src/main/java/com/evolveum/midpoint/schema/MidpointParsingMigrator.java
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/MidpointParsingMigrator.java
@@ -19,6 +19,9 @@ package com.evolveum.midpoint.schema;
 import com.evolveum.midpoint.prism.ParsingContext;
 import com.evolveum.midpoint.prism.marshaller.ParsingMigrator;
 import com.evolveum.midpoint.prism.xnode.PrimitiveXNode;
+import com.evolveum.midpoint.schema.util.LocalizationUtil;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.InformationPartType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.InformationType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.LocalizableMessageType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.SingleLocalizableMessageType;
 
@@ -28,12 +31,24 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.SingleLocalizableMes
 public class MidpointParsingMigrator implements ParsingMigrator {
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public <T> T tryParsingPrimitiveAsBean(PrimitiveXNode<T> primitive, Class<T> beanClass, ParsingContext pc) {
 		if (LocalizableMessageType.class.equals(beanClass)) {
-			//noinspection unchecked
 			return (T) new SingleLocalizableMessageType().fallbackMessage(primitive.getStringValue());
+		} else if (InformationType.class.equals(beanClass)) {
+			// This is to allow specifying plain text where InformationType is expected. It is not very clean; and
+			// quite experimental for now.
+			return (T) stringToInformationType(primitive.getStringValue());
 		} else {
 			return null;
 		}
+	}
+
+	public static InformationType stringToInformationType(String s) {
+		InformationType info = new InformationType();
+		InformationPartType part = new InformationPartType();
+		part.setLocalizableText(LocalizationUtil.createForFallbackMessage(s));
+		info.getPart().add(part);
+		return info;
 	}
 }
