ec4d1039aecd56fa24fe3c5d037605d213ebd5be
Radovan Semancik <radovan.semancik@evolveum.com>
2018-05-24 17:24:47 +0200

    Attempt to reproduce MID-4631

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/AbstractConfiguredModelIntegrationTest.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/AbstractConfiguredModelIntegrationTest.java
@@ -343,8 +343,11 @@ public class AbstractConfiguredModelIntegrationTest extends AbstractModelIntegra
 	protected static final Date USER_HERMAN_VALID_TO_DATE = MiscUtil.asDate(2233, 3, 23, 18, 30, 00);
 
 	// Has null name, doesn not have given name, no employeeType
-	protected static final String USER_THREE_HEADED_MONKEY_FILENAME = COMMON_DIR + "/user-three-headed-monkey.xml";
+	protected static final File USER_THREE_HEADED_MONKEY_FILE = new File(COMMON_DIR, "/user-three-headed-monkey.xml");
 	protected static final String USER_THREE_HEADED_MONKEY_OID = "c0c010c0-d34d-b33f-f00d-110011001133";
+	protected static final String USER_THREE_HEADED_MONKEY_NAME = "monkey";
+	protected static final String USER_THREE_HEADED_MONKEY_FULL_NAME = "Three-Headed Monkey";
+	
 
 	// Elaine has account on the dummy resources (default, red, blue)
 	// The accounts are also assigned
