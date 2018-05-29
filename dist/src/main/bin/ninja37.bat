6be1f01a034c72a070924cb64eea5ce8e5c25528
Viliam Repan <vilo.repan@evolveum.com>
2018-05-29 12:05:20 +0200

    forgot to add bat for ninja37

---
+++ dist/src/main/bin/ninja37.bat
@@ -0,0 +1,51 @@
+@echo off
+
+setlocal
+
+set "BIN_DIR=%~dp0"
+
+rem if script for execution is in bin directory
+if exist "%BIN_DIR%\ninja37.bat" goto okBoot
+echo %BIN_DIR%
+echo The keys.bat file is not in \bin
+goto end
+:okBoot
+
+rem set midpoint.home
+if not "%MIDPOINT_HOME%" == "" goto gotHome
+cd "%BIN_DIR%.."
+if exist "%BIN_DIR%..\var" goto setHome
+echo %BIN_DIR%
+echo ERROR: midpoint.home directory desn't exist
+goto end
+:setHome
+
+set "MIDPOINT_HOME=%cd%\var"
+echo %MIDPOINT_HOME%
+echo %BIN_DIR%
+:gotHome
+
+rem NINJA_JAR if not defined
+if exist "%cd%\lib\ninja-3.7.2-SNAPSHOT.jar" goto gotJar
+echo The ninja-3.7.2-SNAPSHOT.jar is not in \lib directory
+echo Can not start ninja
+goto end
+:gotJar
+
+if "%MIDPOINT_HOME%" == "%MIDPOINT_HOME:;=%" goto homeNoSemicolon
+echo Using MIDPOINT_HOME:   "%MIDPOINT_HOME%"
+echo Unable to start as MIDPOINT_HOME contains a semicolon (;) character
+goto end
+:homeNoSemicolon
+
+
+rem ----- Execute The Requested Command ---------------------------------------
+
+echo Using MIDPOINT_HOME:   "%MIDPOINT_HOME%"
+
+start /b java -jar "%cd%\lib\ninja-3.7.2-SNAPSHOT.jar" -m "%MIDPOINT_HOME%" %*
+goto end
+
+
+:end
+
