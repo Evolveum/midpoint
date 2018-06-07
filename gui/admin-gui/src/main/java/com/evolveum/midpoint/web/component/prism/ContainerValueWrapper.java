c860ac9c1e2f0fbb5ae7a89355c17e07e16f1c2f
kate <honchar@evolveum.com>
2018-06-07 12:33:20 +0200

    MID-4417 Can't update extension ObjectReferenceType attribute in GUI

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/ContainerValueWrapper.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/ContainerValueWrapper.java
@@ -424,6 +424,9 @@ public class ContainerValueWrapper<C extends Containerable> extends PrismWrapper
 					delta.addModification(pDelta);
 				}
 			} else if (itemWrapper instanceof ReferenceWrapper) {
+				if (containerPath.equals(ItemPath.EMPTY_PATH) && itemWrapper.getPath().getSegments().size() > 1){
+					containerPath = itemWrapper.getPath().allExceptLast();
+				}
 				ReferenceDelta pDelta = computeReferenceDeltas((ReferenceWrapper) itemWrapper, containerPath);
 				if (!pDelta.isEmpty()) {
 					delta.addModification(pDelta);
