94af20368b04dfa28d6f803d760646073be86c3f
Pavol Mederly <mederly@evolveum.com>
2018-07-06 09:52:55 +0200

    Fix reindex after 3.8 repo changes (MID-4757)
    When executeIfNoChanges (a.k.a. "reindex") option is set we employ
    the original algorithm based on JPA merge operation.

--- repo/repo-sql-impl-test/src/test/java/com/evolveum/midpoint/repo/sql/ModifyTestReindex.java
+++ repo/repo-sql-impl-test/src/test/java/com/evolveum/midpoint/repo/sql/ModifyTestReindex.java
@@ -16,9 +16,23 @@
 
 package com.evolveum.midpoint.repo.sql;
 
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.PrismProperty;
+import com.evolveum.midpoint.prism.PrismPropertyDefinition;
+import com.evolveum.midpoint.prism.PrismPropertyDefinitionImpl;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.prism.query.ObjectQuery;
+import com.evolveum.midpoint.prism.query.builder.QueryBuilder;
 import com.evolveum.midpoint.repo.api.RepoModifyOptions;
+import com.evolveum.midpoint.schema.result.OperationResult;
+import com.evolveum.midpoint.test.util.TestUtil;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+
+import static java.util.Collections.emptySet;
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * The same as ModifyTest but with "executeIfNoChanges" (a.k.a. "reindex") option set.
@@ -35,4 +49,34 @@ public class ModifyTestReindex extends ModifyTest {
 	protected RepoModifyOptions getModifyOptions() {
 		return RepoModifyOptions.createExecuteIfNoChanges();
 	}
+
+	@Test
+	public void testReindex() throws Exception {
+		final String TEST_NAME = "testReindex";
+		TestUtil.displayTestTitle(TEST_NAME);
+		OperationResult result = new OperationResult(TEST_NAME);
+
+		PrismObject<UserType> user = prismContext.createObjectable(UserType.class)
+				.name("unstable")
+				.asPrismObject();
+		ItemPath UNSTABLE_PATH = new ItemPath(UserType.F_EXTENSION, "unstable");
+		PrismPropertyDefinition<String> unstableDef = user.getDefinition().findPropertyDefinition(UNSTABLE_PATH);
+		PrismProperty<String> unstable = unstableDef.instantiate();
+		unstable.setRealValue("hi");
+		user.addExtensionItem(unstable);
+
+		String oid = repositoryService.addObject(user, null, result);
+
+		// brutal hack -- may stop working in the future!
+		((PrismPropertyDefinitionImpl) unstableDef).setIndexed(true);
+
+		repositoryService.modifyObject(UserType.class, oid, emptySet(), getModifyOptions(), result);
+
+		ObjectQuery query = QueryBuilder.queryFor(UserType.class, prismContext)
+				.item(UNSTABLE_PATH).eq("hi")
+				.build();
+		int count = repositoryService.countObjects(UserType.class, query, null, result);
+		assertEquals("Wrong # of objects found", 1, count);
+	}
+
 }
