d6e43800503b7c172a3b22bd5fd9b9efa10d424b
Katarina Valalikova <k.valalikova@evolveum.com>
2018-08-06 14:08:35 +0200

    small improvements to item path panel, adding itempathpanel to generated forms, adding support for editing filters in forms..

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/path/ItemPathPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/path/ItemPathPanel.java
@@ -9,6 +9,7 @@ import javax.xml.namespace.QName;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -21,38 +22,75 @@ import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
 import com.evolveum.midpoint.prism.Definition;
 import com.evolveum.midpoint.prism.ItemDefinition;
 import com.evolveum.midpoint.prism.PrismContainerDefinition;
+import com.evolveum.midpoint.prism.PrismContext;
 import com.evolveum.midpoint.prism.PrismObjectDefinition;
+import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.web.component.AjaxButton;
 import com.evolveum.midpoint.web.component.input.DropDownChoicePanel;
 import com.evolveum.midpoint.web.component.input.QNameChoiceRenderer;
+import com.evolveum.midpoint.web.component.input.TextPanel;
 import com.evolveum.midpoint.web.component.util.VisibleEnableBehaviour;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
+import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
 
 public class ItemPathPanel extends BasePanel<ItemPathDto> {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final String ID_ITEM_PATH = "itemPath";
 	private static final String ID_NAMESPACE = "namespace";
 	private static final String ID_DEFINITION = "definition";
+	
+	private static final String ID_ITEM_PATH_CONTAINER = "itemPathContainer";
+	private static final String ID_ITEM_PATH_LABEL = "itemPathLabel";
+	private static final String ID_CHANGE = "change";
 
 	private static final String ID_PLUS = "plus";
 	private static final String ID_MINUS = "minus";
 
-	public ItemPathPanel(String id, IModel<ItemPathDto> model, PageBase parent) {
+	public ItemPathPanel(String id, IModel<ItemPathDto> model) {
 		super(id, model);
-
-		setParent(parent);
-
-
+	}
+	
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
 		initLayout();
-
 	}
 
-	public ItemPathPanel(String id, ItemPathDto model, PageBase parent) {
-		this(id, Model.of(model), parent);
+	public ItemPathPanel(String id, ItemPathDto model) {
+		this(id, Model.of(model));
+
+	}
+	
+	public ItemPathPanel(String id, ItemPathType itemPath) {
+		this(id, Model.of(new ItemPathDto(itemPath)));
 
 	}
 
 	private void initLayout() {
+		initItemPathPanel();
+		
+		initItemPathLabel();
+		
+		setOutputMarkupId(true);
+	}
+	
+	private void initItemPathPanel() {
+		WebMarkupContainer itemPathPanel = new WebMarkupContainer(ID_ITEM_PATH);
+		itemPathPanel.setOutputMarkupId(true);
+		add(itemPathPanel);
+		itemPathPanel.add(new VisibleEnableBehaviour() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isVisible() {
+				return !getModelObject().isPathDefined();
+			}
+		
+		});
+		
 		ItemPathSegmentPanel itemDefPanel = new ItemPathSegmentPanel(ID_DEFINITION,
 				new AbstractReadOnlyModel<ItemPathDto>() {
 
@@ -70,7 +108,7 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 			}
 		};
 		itemDefPanel.setOutputMarkupId(true);
-		add(itemDefPanel);
+		itemPathPanel.add(itemDefPanel);
 
 		AjaxButton plusButton = new AjaxButton(ID_PLUS) {
 			private static final long serialVersionUID = 1L;
@@ -94,7 +132,7 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 			}
 		});
 		plusButton.setOutputMarkupId(true);
-		add(plusButton);
+		itemPathPanel.add(plusButton);
 
 		AjaxButton minusButton = new AjaxButton(ID_MINUS) {
 			private static final long serialVersionUID = 1L;
@@ -102,12 +140,6 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 			@Override
 			public void onClick(AjaxRequestTarget target) {
 				ItemPathDto path = ItemPathPanel.this.getModelObject();
-//				ItemPathDto parent = null;
-//				if (path.getItemDef() == null){
-//					parent = path.getParentPath();
-//				} else {
-//					parent = path;
-//				}
 				refreshItemPathPanel(path, false, target);
 
 			}
@@ -121,7 +153,7 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 			}
 		});
 		minusButton.setOutputMarkupId(true);
-		add(minusButton);
+		itemPathPanel.add(minusButton);
 
 		DropDownChoicePanel<QName> namespacePanel = new DropDownChoicePanel<>(ID_NAMESPACE,
             new PropertyModel<>(getModel(), "objectType"),
@@ -147,11 +179,48 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 			}
 		});
 		namespacePanel.setOutputMarkupId(true);
-		add(namespacePanel);
+		itemPathPanel.add(namespacePanel);
+	}
+	
+	private void initItemPathLabel() {
+		WebMarkupContainer itemPathLabel = new WebMarkupContainer(ID_ITEM_PATH_CONTAINER);
+		itemPathLabel.setOutputMarkupId(true);
+		add(itemPathLabel);
+		itemPathLabel.add(new VisibleEnableBehaviour() { 
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isVisible() {
+				return getModelObject().isPathDefined();
+			}
+			
+		});
+		
+		TextPanel<ItemPath> textPanel = new TextPanel<>(ID_ITEM_PATH_LABEL, new PropertyModel<>(getModel(), "path"));
+		textPanel.setEnabled(false);
+		textPanel.setOutputMarkupId(true);
+		itemPathLabel.add(textPanel);
+		
+		AjaxButton change = new AjaxButton(ID_CHANGE, createStringResource("ItemPathPanel.button.reset")) {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				ItemPathDto newPath = new ItemPathDto();
+				ItemPathPanel.this.getModel().setObject(newPath);
+				target.add(ItemPathPanel.this);
+				onUpdate(newPath);
+			}
+		};
+		
+		change.setOutputMarkupId(true);
+		itemPathLabel.add(change);
 	}
 
 	private void refreshItemPathPanel(ItemPathDto itemPathDto, boolean isAdd, AjaxRequestTarget target) {
-		ItemPathSegmentPanel pathSegmentPanel = (ItemPathSegmentPanel) get(ID_DEFINITION);
+		ItemPathSegmentPanel pathSegmentPanel = (ItemPathSegmentPanel) get(createComponentPath(ID_ITEM_PATH, ID_DEFINITION));
 		if (isAdd && !pathSegmentPanel.validate()) {
 			return;
 		}
@@ -172,9 +241,11 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 			itemPathDto = resultingItem;
 		}
 		// pathSegmentPanel.refreshModel(itemPathDto);
+		
 		this.getModel().setObject(itemPathDto);
 
 		target.add(this);
+		onUpdate(itemPathDto);
 		// target.add(pathSegmentPanel);
 
 	}
@@ -183,6 +254,8 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 
 		this.getModel().setObject(itemPathDto);
 		target.add(this);
+		
+		onUpdate(itemPathDto);
 	}
 
 	private Map<QName, Collection<ItemDefinition<?>>> initNamspaceDefinitionMap() {
@@ -205,6 +278,11 @@ public class ItemPathPanel extends BasePanel<ItemPathDto> {
 			}
 		}
 		return schemaDefinitionsMap;
+	}
+	
+
+	protected void onUpdate(ItemPathDto itemPathDto) {
+		
 	}
 
 }
