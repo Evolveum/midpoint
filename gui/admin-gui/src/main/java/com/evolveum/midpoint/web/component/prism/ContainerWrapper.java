3bf7dbe355b678d07a567804838d09f805c23b6e
Katarina Valalikova <k.valalikova@evolveum.com>
2018-08-07 19:57:03 +0200

    expression validator improvements - adding object as a variable, improving handling on error..

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/ContainerWrapper.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/prism/ContainerWrapper.java
@@ -50,6 +50,8 @@ public class ContainerWrapper<C extends Containerable> extends PrismWrapper impl
 
 	private static final Trace LOGGER = TraceManager.getTrace(ContainerWrapper.class);
 
+	private ObjectWrapper objectWrapper;
+	
 	private String displayName;
 	private PrismContainer<C> container;
 	private ContainerStatus status;
@@ -65,8 +67,12 @@ public class ContainerWrapper<C extends Containerable> extends PrismWrapper impl
 
 	//TODO: HACK to have custom filter for association contianer here becasue of creating new association:
 	private ObjectFilter filter;
+	
+	public ObjectWrapper getObjectWrapper() {
+		return objectWrapper;
+	}
 
-	ContainerWrapper(PrismContainer<C> container, ContainerStatus objectStatus, ContainerStatus status, ItemPath path) {
+	ContainerWrapper(ObjectWrapper objectWrapper, PrismContainer<C> container, ContainerStatus objectStatus, ContainerStatus status, ItemPath path) {
 		Validate.notNull(container, "container must not be null.");
 		Validate.notNull(status, "Container status must not be null.");
 		Validate.notNull(container.getDefinition(), "container definition must not be null.");
@@ -75,11 +81,12 @@ public class ContainerWrapper<C extends Containerable> extends PrismWrapper impl
 		this.objectStatus = objectStatus;
 		this.status = status;
 		this.path = path;
+		this.objectWrapper = objectWrapper;
 
 	}
 
-	ContainerWrapper(PrismContainer<C> container, ContainerStatus objectStatus, ContainerStatus status, ItemPath path, boolean readOnly) {
-		this(container, objectStatus, status, path);
+	ContainerWrapper(ObjectWrapper objectWrapper, PrismContainer<C> container, ContainerStatus objectStatus, ContainerStatus status, ItemPath path, boolean readOnly) {
+		this(objectWrapper, container, objectStatus, status, path);
 		this.readonly = readOnly;
 	}
 
