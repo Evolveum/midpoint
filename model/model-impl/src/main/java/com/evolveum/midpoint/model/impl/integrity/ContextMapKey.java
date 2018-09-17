e326dde9b350e5c7bfabbaee18b0cf1401ae65ef
Pavol Mederly <mederly@evolveum.com>
2018-09-17 20:22:03 +0200

    Test shadow uniqueness objectclass-wide (MID-4852)
    From prehistoric times we checked shadow uniqueness for each shadow
    kind separately. However, shadows have to be unique with respect to
    object classes (usually).

---
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/integrity/ContextMapKey.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2010-2018 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolveum.midpoint.model.impl.integrity;
+
+import javax.xml.namespace.QName;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * @author mederly
+ */
+
+class ContextMapKey implements Serializable {
+
+    final String resourceOid;
+    final QName objectClassName;
+
+	ContextMapKey(String resourceOid, QName objectClassName) {
+        this.resourceOid = resourceOid;
+        this.objectClassName = objectClassName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof ContextMapKey))
+            return false;
+        ContextMapKey that = (ContextMapKey) o;
+        return Objects.equals(resourceOid, that.resourceOid) &&
+                Objects.equals(objectClassName, that.objectClassName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(resourceOid, objectClassName);
+    }
+
+	@Override
+	public String toString() {
+		return "ContextMapKey{" +
+				"resourceOid='" + resourceOid + '\'' +
+				", objectClassName=" + objectClassName +
+				'}';
+	}
+}
