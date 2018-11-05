24626cf03187acb3be8c52a619982173a35fbd50
Pavol Mederly <mederly@evolveum.com>
2018-11-05 23:01:57 +0100

    Remove excessive logging in PageTaskEdit

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/server/PageTaskEdit.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/server/PageTaskEdit.java
@@ -261,7 +261,7 @@ public class PageTaskEdit extends PageAdmin implements Refreshable {
 		}
 		if (tabsVisibilityNew.equals(tabsVisibilityOld)) {
 			// soft version
-			LOGGER.info("Soft version of the page refreshing, tabsVisibilityNew: " + tabsVisibilityNew + "; \n tabsVisibilityOld: " + tabsVisibilityOld);
+			LOGGER.trace("Soft version of the page refreshing, tabsVisibilityNew: " + tabsVisibilityNew + "; \n tabsVisibilityOld: " + tabsVisibilityOld);
 			for (Component component : mainPanel.getTabPanel()) {
 				if (component instanceof TaskTabPanel) {
 					for (Component c : ((TaskTabPanel) component).getComponentsToUpdate()) {
@@ -270,7 +270,7 @@ public class PageTaskEdit extends PageAdmin implements Refreshable {
 				}
 			}
 		} else {
-			LOGGER.info("Hard version of the page refreshing, tabsVisibilityNew: " + tabsVisibilityNew + "; \n tabsVisibilityOld: " + tabsVisibilityOld);
+			LOGGER.trace("Hard version of the page refreshing, tabsVisibilityNew: " + tabsVisibilityNew + "; \n tabsVisibilityOld: " + tabsVisibilityOld);
 			// hard version
 			target.add(getSummaryPanel());
 			target.add(mainPanel.getTabPanel());
@@ -281,10 +281,10 @@ public class PageTaskEdit extends PageAdmin implements Refreshable {
 		refreshDto.recordRefreshed();
 
 		if (isEdit() || !refreshDto.isEnabled()) {
-			LOGGER.info("Stop refreshing, interval=" + refreshDto.getInterval());
+			LOGGER.trace("Stop refreshing, interval=" + refreshDto.getInterval());
 			getRefreshPanel().stopRefreshing(this, target);
 		} else {
-			LOGGER.info("Start refreshing, interval=" + refreshDto.getInterval());
+			LOGGER.trace("Start refreshing, interval=" + refreshDto.getInterval());
 			getRefreshPanel().startRefreshing(this, target);
 		}
 	}
