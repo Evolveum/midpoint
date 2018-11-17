22d00ae1e16c8d160873a1efe5936ed7ef72f325
Pavol Mederly <mederly@evolveum.com>
2018-11-17 18:35:59 +0100

    Create prism-api #4: DeltaBuilder

---
+++ infra/prism/src/main/java/com/evolveum/midpoint/prism/delta/builder/DeltaBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2010-2018 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolveum.midpoint.prism.delta.builder;
+
+import com.evolveum.midpoint.prism.*;
+import com.evolveum.midpoint.prism.delta.*;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.util.exception.SchemaException;
+import org.jetbrains.annotations.NotNull;
+
+import javax.xml.namespace.QName;
+import java.util.*;
+
+/**
+ * Grammar:
+ *
+ *  ObjectDelta ::= (ItemDelta)* ( 'OBJECT-DELTA(oid)' | 'ITEM-DELTA' | 'ITEM-DELTAS' )
+ *
+ *  ItemDelta ::= 'ITEM(...)' ( ( 'ADD-VALUES(...)' 'DELETE-VALUES(...)'? ) | 'DELETE-VALUES(...)' | 'REPLACE-VALUES(...)' )
+ *
+ * @author mederly
+ */
+public interface DeltaBuilder<T extends Containerable> extends S_ItemEntry, S_MaybeDelete, S_ValuesEntry {
+
+    static <C extends Containerable> S_ItemEntry deltaFor(Class<C> objectClass, PrismContext prismContext) throws SchemaException {
+        return new DeltaBuilderImpl<>(objectClass, prismContext);
+    }
+
+}
