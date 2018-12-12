2935460f37e90bde5c9d2d38b2e7771ac6997402
Petr Gašparík <petr@gasparik.cz>
2018-12-12 15:03:45 +0100

    MID-5024 Confirmation for delete one account and more accounts on resource were switched

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/resources/ResourceContentPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/resources/ResourceContentPanel.java
@@ -979,8 +979,8 @@ public abstract class ResourceContentPanel extends Panel {
 		}
 
 		ConfirmationPanel dialog = new ConfirmationPanel(((PageBase) getPage()).getMainPopupBodyId(),
-				createDeleteConfirmString(selected, "pageContentAccounts.message.deleteConfirmation",
-						"pageContentAccounts.message.deleteConfirmationSingle")) {
+				createDeleteConfirmString(selected, "pageContentAccounts.message.deleteConfirmationSingle",
+						"pageContentAccounts.message.deleteConfirmation")) {
 			@Override
 			public void yesPerformed(AjaxRequestTarget target) {
 				((PageBase) getPage()).hideMainPopup(target);
