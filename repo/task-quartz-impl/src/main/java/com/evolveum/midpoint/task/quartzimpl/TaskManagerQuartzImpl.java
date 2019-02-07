089ab3277905f281ae03dd691da8eb90fec35c15
Pavol Mederly <mederly@evolveum.com>
2019-02-07 13:19:02 +0100

    Remove task only if in CLOSED state (MID-5033)
    It looks like that when re-scheduling already closed single-run tasks
    they keep their completionTimestamp set. And so they are eventually
    cleaned up. This is an immediate fix, requiring tasks that are to
    be cleaned to have also execution status = CLOSED. See also MID-5133.

--- repo/task-quartz-impl/src/main/java/com/evolveum/midpoint/task/quartzimpl/TaskManagerQuartzImpl.java
+++ repo/task-quartz-impl/src/main/java/com/evolveum/midpoint/task/quartzimpl/TaskManagerQuartzImpl.java
@@ -1802,7 +1802,8 @@ public class TaskManagerQuartzImpl implements TaskManager, BeanFactoryAware {
         List<PrismObject<TaskType>> obsoleteTasks;
         try {
             ObjectQuery obsoleteTasksQuery = QueryBuilder.queryFor(TaskType.class, prismContext)
-					.item(TaskType.F_COMPLETION_TIMESTAMP).le(timeXml)
+					.item(TaskType.F_EXECUTION_STATUS).eq(TaskExecutionStatusType.CLOSED)
+					.and().item(TaskType.F_COMPLETION_TIMESTAMP).le(timeXml)
 					.and().item(TaskType.F_PARENT).isNull()
 					.build();
             obsoleteTasks = repositoryService.searchObjects(TaskType.class, obsoleteTasksQuery, null, result);
