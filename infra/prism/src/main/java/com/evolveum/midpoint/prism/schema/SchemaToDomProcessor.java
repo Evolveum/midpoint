Evolveum/prism

Fixes for XXE vulnerabilities (MID-5285)

infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/schema/SchemaToDomProcessor.java
@@ -597,10 +597,17 @@ public class SchemaToDomProcessor {
 			LOGGER.trace("Using namespace prefix mapper to serialize schema:\n{}",DebugUtil.dump(namespacePrefixMapper));
 		}
 
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		dbf.setNamespaceAware(true);
-		dbf.setValidating(false);
-		DocumentBuilder db = dbf.newDocumentBuilder();
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		documentBuilderFactory.setNamespaceAware(true);
+		documentBuilderFactory.setValidating(false);
+		// XXE
+		documentBuilderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+		documentBuilderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+		documentBuilderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+		documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+		documentBuilderFactory.setXIncludeAware(false);
+		documentBuilderFactory.setExpandEntityReferences(false);
+		DocumentBuilder db = documentBuilderFactory.newDocumentBuilder();
 
 		document = db.newDocument();
 		Element root = createElement(new QName(W3C_XML_SCHEMA_NS_URI, "schema"));

infra/util/src/main/java/com/evolveum/midpoint/util/DOMUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Evolveum
+ * Copyright (c) 2010-2019 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -171,6 +171,13 @@ public class DOMUtil {
 				documentBuilderFactory.setFeature("http://xml.org/sax/features/validation", false);
 				documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
 				documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+				// XXE
+				documentBuilderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+				documentBuilderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+				documentBuilderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+				documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+				documentBuilderFactory.setXIncludeAware(false);
+				documentBuilderFactory.setExpandEntityReferences(false);
 				return documentBuilderFactory.newDocumentBuilder();
 			} catch (ParserConfigurationException e) {
 				throw new RuntimeException(e);

Evolveum/midpoint

infra/prism/src/main/java/com/evolveum/midpoint/prism/schema/SchemaToDomProcessor.java
@@ -598,6 +598,13 @@ public class SchemaToDomProcessor {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		dbf.setNamespaceAware(true);
 		dbf.setValidating(false);
+		// XXE
+		dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+		dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+		dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+		dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+		dbf.setXIncludeAware(false);
+		dbf.setExpandEntityReferences(false);
 		DocumentBuilder db = dbf.newDocumentBuilder();
 
 		document = db.newDocument();

infra/util/src/main/java/com/evolveum/midpoint/util/DOMUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Evolveum
+ * Copyright (c) 2010-2019 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -169,6 +169,13 @@ public class DOMUtil {
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			factory.setNamespaceAware(true);
+			// XXE
+			factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			factory.setXIncludeAware(false);
+			factory.setExpandEntityReferences(false);
 			loader = factory.newDocumentBuilder();
 		} catch (ParserConfigurationException ex) {
 			throw new IllegalStateException("Error creating XML document " + ex.getMessage());
@@ -209,6 +216,13 @@ public class DOMUtil {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
         try {
+            // XXE
+    		factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+    		factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+    		factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+    		factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+    		factory.setXIncludeAware(false);
+    		factory.setExpandEntityReferences(false);
             return factory.newDocumentBuilder();
         } catch (ParserConfigurationException e) {
             throw new IllegalStateException("Error creating document builder " + e.getMessage(), e);
@@ -232,6 +246,13 @@ public class DOMUtil {
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			factory.setNamespaceAware(true);
+			// XXE
+			factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			factory.setXIncludeAware(false);
+			factory.setExpandEntityReferences(false);
 			DocumentBuilder loader = factory.newDocumentBuilder();
 			return loader.parse(file);
 		} catch (SAXException | IOException | ParserConfigurationException ex) {
@@ -249,6 +270,13 @@ public class DOMUtil {
 			factory.setFeature("http://xml.org/sax/features/validation", false);
 			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
 			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			// XXE
+			factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			factory.setXIncludeAware(false);
+			factory.setExpandEntityReferences(false);
 			DocumentBuilder loader = factory.newDocumentBuilder();
 			return loader.parse(inputStream);
 		} catch (SAXException | ParserConfigurationException ex) {