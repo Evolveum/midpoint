bad9f03d67debc78db9f01b1b7a174f071f0bab1
skublik <lskublik@evolveum.com>
2019-05-23 10:58:34 +0200

    adding tests for PageUser

--- gui/admin-gui/src/test/java/com/evolveum/midpoint/gui/TestPageUser.java
+++ gui/admin-gui/src/test/java/com/evolveum/midpoint/gui/TestPageUser.java
@@ -23,6 +23,7 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.testng.annotations.Test;
 
+import com.evolveum.midpoint.gui.impl.component.MultivalueContainerListPanelWithDetailsPanel;
 import com.evolveum.midpoint.gui.impl.prism.PrismContainerPanel;
 import com.evolveum.midpoint.gui.impl.prism.PrismObjectValuePanel;
 import com.evolveum.midpoint.gui.test.TestMidPointSpringApplication;
@@ -33,7 +34,13 @@ import com.evolveum.midpoint.task.api.Task;
 import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.web.AbstractGuiIntegrationTest;
+import com.evolveum.midpoint.web.component.assignment.AssignmentTablePanel;
+import com.evolveum.midpoint.web.component.objectdetails.FocusPersonasTabPanel;
+import com.evolveum.midpoint.web.component.objectdetails.FocusTasksTabPanel;
+import com.evolveum.midpoint.web.component.objectdetails.ObjectHistoryTabPanel;
+import com.evolveum.midpoint.web.component.objectdetails.UserDelegationsTabPanel;
 import com.evolveum.midpoint.web.page.admin.users.PageUser;
+import com.evolveum.midpoint.web.util.OnePageParameterEncoder;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ActivationStatusType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.SystemConfigurationType;
 
@@ -53,7 +60,7 @@ public class TestPageUser extends AbstractGuiIntegrationTest {
 	private static final String TAB_PASSWORD = "mainPanel:mainForm:tabPanel:panel:password";
 	private static final String MAIN_FORM = "mainPanel:mainForm";
 	
-	private static final String PATH_FORM_NAME = "tabPanel:panel:main:value:propertiesLabel:properties:0:property:values:0:valueContainer:form:input:input";
+	private static final String PATH_FORM_NAME = "tabPanel:panel:main:value:propertiesLabel:properties:0:property:values:0:valueContainer:form:input:originValueContainer:origValue:input";
 	private static final String PATH_FORM_ADMINISTRATIVE_STATUS = "tabPanel:panel:activation:values:0:value:propertiesLabel:properties:0:property:values:0:valueContainer:form:input:input";
 	private static final String PATH_PASSWORD_NEW = "tabPanel:panel:password:values:0:value:propertiesLabel:properties:1:property:values:0:valueContainer:form:input:inputContainer:password1";
 	private static final String PATH_PASSWORD_NEW_REPEAT = "tabPanel:panel:password:values:0:value:propertiesLabel:properties:1:property:values:0:valueContainer:form:input:inputContainer:password2";
@@ -109,29 +116,97 @@ public class TestPageUser extends AbstractGuiIntegrationTest {
 	
 	@Test
 	public void test010renderAssignmentsTab() {
+		renderPage(USER_ADMINISTRATOR_OID);
 		
+		clickOnTab(2);
+		String assignmentTable = "mainPanel:mainForm:tabPanel:panel:assignmentsContainer:assignmentsPanel:assignmentsPanel:assignments";
+		tester.assertComponent(assignmentTable, MultivalueContainerListPanelWithDetailsPanel.class);
+		
+		String assignmentTableDetailsLink = assignmentTable + "items:itemsTable:box:tableContainer:table:body:rows:1:cells:3:cell:link";
+		tester.clickLink(assignmentTableDetailsLink);
+		String assignmentTableDetails = assignmentTable + "details:itemDetails";
+	}
+	
+	@Test
+	public void test011renderProjectionsTab() {
 		renderPage();
 		
+		clickOnTab(1);
+		String projectionTable = "mainPanel:mainForm:tabPanel:panel:shadowTable";
+		tester.assertComponent(projectionTable, MultivalueContainerListPanelWithDetailsPanel.class);
+	}
+	
+	private void clickOnTab(int order) {
 		tester.assertRenderedPage(PageUser.class);
+		String tabPath = "mainPanel:mainForm:tabPanel:tabs-container:tabs:" + order + ":link";
+		tester.clickLink(tabPath);
+	}
+	
+	@Test
+	public void test012renderHistoryTab() {
+		renderPage(USER_ADMINISTRATOR_OID);
 		
-		String assignmentPath = "mainPanel:mainForm:tabPanel:tabs-container:tabs:2:link";
-		tester.clickLink(assignmentPath);
+		clickOnTab(3);
+		String historyPanel = "mainPanel:mainForm:tabPanel:panel";
+		tester.assertComponent(historyPanel, ObjectHistoryTabPanel.class);
+	}
+	
+	@Test
+	public void test013renderTasksTab() {
+		renderPage();
 		
-		//TODO assignments table
-//		String assignmentTable = assignmentPath + ":table";
-//		
-//		tester.assertComponent(assignmentTable, MultivalueContainerListPanelWithDetailsPanel.class);
+		clickOnTab(3);
+		String panel = "mainPanel:mainForm:tabPanel:panel";
+		tester.assertComponent(panel, FocusTasksTabPanel.class);
+	}
+	
+	@Test
+	public void test014renderPersonasTab() {
+		renderPage();
+		
+		clickOnTab(4);
+		String panel = "mainPanel:mainForm:tabPanel:panel";
+		tester.assertComponent(panel, FocusPersonasTabPanel.class);
+	}
+	
+	@Test
+	public void test015renderDelegationsTab() {
+		renderPage();
+		
+		clickOnTab(5);
+		String panel = "mainPanel:mainForm:tabPanel:panel";
+		tester.assertComponent(panel, UserDelegationsTabPanel.class);
+	}
+	
+	@Test
+	public void test016renderDelegatedToMeTab() {
+		renderPage();
+		
+		clickOnTab(6);
+		String panel = "mainPanel:mainForm:tabPanel:panel";
+		tester.assertComponent(panel, AssignmentTablePanel.class);
 	}
 	
 	private PageUser renderPage() {
-		LOGGER.info("render page user");
+		return renderPageWithParams(null);
+	}
+	
+	private PageUser renderPage(String userOid) {
 		PageParameters params = new PageParameters();
-		PageUser pageUser = tester.startPage(PageUser.class);
+		params.add(OnePageParameterEncoder.PARAMETER, userOid);
+		return renderPageWithParams(params);
+	}
+	
+	private PageUser renderPageWithParams(PageParameters params) {
+		LOGGER.info("render page user");
+		if(params == null) {
+			params = new PageParameters();
+		}
+		PageUser pageUser = tester.startPage(PageUser.class, params);
 		
 		tester.assertRenderedPage(PageUser.class);
 		
 		return pageUser;
-		
 	}
 	
 }
