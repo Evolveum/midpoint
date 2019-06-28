259373ddc4f3b3c0c23c03c0102edbfd869f2254
Katarina Valalikova <k.valalikova@evolveum.com>
2019-06-28 11:24:50 +0200

    resolving conflics after cherry pick

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/security/AuthenticationEvaluatorImpl.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/security/AuthenticationEvaluatorImpl.java
@@ -102,15 +102,10 @@ public abstract class AuthenticationEvaluatorImpl<C extends AbstractCredentialTy
 		CredentialPolicyType credentialsPolicy = getCredentialsPolicy(principal, authnCtx);
 
 		if (checkCredentials(principal, authnCtx, connEnv)) {
-
-			recordPasswordAuthenticationSuccess(principal, connEnv, getCredential(credentials), credentialsPolicy);
-			UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(principal,
-					authnCtx.getEnteredCredential(), principal.getAuthorities());
-			return token;
-
+			recordPasswordAuthenticationSuccess(principal, connEnv, getCredential(credentials));
+			return new UsernamePasswordAuthenticationToken(principal, authnCtx.getEnteredCredential(), principal.getAuthorities());
 		} else {
 			recordPasswordAuthenticationFailure(principal, connEnv, getCredential(credentials), credentialsPolicy, "password mismatch");
-
 			throw new BadCredentialsException("web.security.provider.invalid");
 		}
 	}
@@ -436,8 +431,8 @@ public abstract class AuthenticationEvaluatorImpl<C extends AbstractCredentialTy
 		return clock.isPast(lockedUntilTimestamp);
 	}
 
-	private void recordPasswordAuthenticationSuccess(MidPointPrincipal principal, ConnectionEnvironment connEnv,
-			C passwordType, CredentialPolicyType passwordCredentialsPolicy) {
+	public void recordPasswordAuthenticationSuccess(MidPointPrincipal principal, ConnectionEnvironment connEnv,
+			C passwordType) {
 		Integer failedLogins = passwordType.getFailedLogins();
 		if (failedLogins != null && failedLogins > 0) {
 			passwordType.setFailedLogins(0);
@@ -466,7 +461,7 @@ public abstract class AuthenticationEvaluatorImpl<C extends AbstractCredentialTy
 		securityHelper.auditLoginSuccess(principal.getUser(), connEnv);
 	}
 
-	private void recordPasswordAuthenticationFailure(@NotNull MidPointPrincipal principal, @NotNull ConnectionEnvironment connEnv,
+	public void recordPasswordAuthenticationFailure(@NotNull MidPointPrincipal principal, @NotNull ConnectionEnvironment connEnv,
 			@NotNull C passwordType, CredentialPolicyType credentialsPolicy, String reason) {
 		Integer failedLogins = passwordType.getFailedLogins();
 		LoginEventType lastFailedLogin = passwordType.getLastFailedLogin();
