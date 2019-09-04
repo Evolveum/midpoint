b1d460a32cd2af3ef827c086951012ee03675f60
skublik <lskublik@evolveum.com>
2019-09-04 09:24:09 +0200

    fix for number of 'my work items' (MID-5643)

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/page/PageBase.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/page/PageBase.java
@@ -396,9 +396,8 @@ public abstract class PageBase extends WebPage implements ModelServiceLocator {
                     ObjectQuery query = QueryUtils.filterForAssignees(q, getPrincipal(),
                             OtherPrivilegesLimitationType.F_APPROVAL_WORK_ITEMS, getRelationRegistry())
                             .and()
-                            .not()
-                            .item(PrismConstants.T_PARENT, CaseType.F_STATE)
-                            .eq(SchemaConstants.CASE_STATE_CLOSED)
+                            .item(CaseWorkItemType.F_CLOSE_TIMESTAMP)
+                            .isNull()
                             .build();
                     return getModelService().countContainers(CaseWorkItemType.class, query, null, task, task.getResult());
                 } catch (SchemaException | SecurityViolationException | ExpressionEvaluationException | ObjectNotFoundException | CommunicationException | ConfigurationException e) {
