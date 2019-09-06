8eac39e2553501a59221ba7a13afc4b13a7dff88
Radovan Semancik <radovan.semancik@evolveum.com>
2019-09-06 15:20:43 +0200

    Dual-licensing

--- dist/src/main/bin/start.sh
+++ dist/src/main/bin/start.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This work is dual-licensed under the Apache License 2.0 
+# and European Union Public License. See LICENSE file for details.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Parts of this file Copyright (c) 2017 Evolveum
+# Parts of this file Copyright (c) 2017 Evolveum and contributors
 #
 
 # resolve links - $0 may be a softlink
--- icf-connectors/dummy-connector/src/main/java/com/evolveum/icf/dummy/connector/Utils.java
+++ icf-connectors/dummy-connector/src/main/java/com/evolveum/icf/dummy/connector/Utils.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2010-2019 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0 
+ * and European Union Public License. See LICENSE file for details.
+ */
 /*
  * Copyright (c) 2010-201ž Evolveum
  *
