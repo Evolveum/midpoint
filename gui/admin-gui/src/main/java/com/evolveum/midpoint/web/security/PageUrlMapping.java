ba8e066c8511af60d7542c3471a8b40dd051b66b
skublik <lskublik@evolveum.com>
2019-09-18 10:05:45 +0200

    modify of authorizations for spring boot actuator endpoints and adding of dependency

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/security/PageUrlMapping.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/security/PageUrlMapping.java
@@ -66,32 +66,30 @@ public enum PageUrlMapping {
                     "PageCaseWorkItems.authUri.guiAll.label", "PageAdminRoles.authUri.guiAll.description"),
             new AuthorizationActionValue(AUTZ_GUI_ALL_DEPRECATED_URL,
                     "PageCaseWorkItems.authUri.guiAll.label", "PageAdminRoles.authUri.guiAll.description")
-    }),
+    }), 
     ACTUATOR("/actuator/**", new DisplayableValue[]{
-            new AuthorizationActionValue(AUTZ_GUI_ALL_URL,
-                    "Actuator.authUri.guiAll.label", "Actuator.authUri.guiAll.description"),
             new AuthorizationActionValue(AUTZ_ACTUATOR_ALL_URL,
-                    "Actuator.authUri.actuatorAll.label", "Actuator.authUri.actuatorAll.description")
+                    "ActuatorEndpoint.authActuator.all.label", "ActuatorEndpoint.authActuator.all.description")
     }),
     ACTUATOR_THREAD_DUMP("/actuator/threaddump", new DisplayableValue[]{
             new AuthorizationActionValue(AUTZ_ACTUATOR_THREAD_DUMP_URL,
-                    "PageCaseWorkItems.authUri.actuatorThreadDump.label", "PageAdminRoles.authUri.actuatorThreadDump.description")
+                    "ActuatorEndpoint.authActuator.threadDump.label", "ActuatorEndpoint.authActuator.threadDump.description")
     }),
     ACTUATOR_HEAP_DUMP("/actuator/heapdump", new DisplayableValue[]{
             new AuthorizationActionValue(AUTZ_ACTUATOR_HEAP_DUMP_URL,
-                    "PageCaseWorkItems.authUri.actuatorHeapDump.label", "PageAdminRoles.authUri.actuatorHeapDump.description")
+                    "ActuatorEndpoint.authActuator.heapDump.label", "ActuatorEndpoint.authActuator.heapDump.description")
     }),
     ACTUATOR_ENV_DUMP("/actuator/env", new DisplayableValue[]{
             new AuthorizationActionValue(AUTZ_ACTUATOR_ENV_URL,
-                    "PageCaseWorkItems.authUri.actuatorEnv.label", "PageAdminRoles.authUri.actuatorEnv.description")
+                    "ActuatorEndpoint.authActuator.env.label", "ActuatorEndpoint.authActuator.env.description")
     }),
     ACTUATOR_INFO_DUMP("/actuator/info", new DisplayableValue[]{
             new AuthorizationActionValue(AUTZ_ACTUATOR_INFO_URL,
-                    "PageCaseWorkItems.authUri.actuatorInfo.label", "PageAdminRoles.authUri.actuatorInfo.description")
+                    "ActuatorEndpoint.authActuator.info.label", "ActuatorEndpoint.authActuator.info.description")
     }),
     ACTUATOR_METRICS_DUMP("/actuator/metrics", new DisplayableValue[]{
             new AuthorizationActionValue(AUTZ_ACTUATOR_METRICS_URL,
-                    "PageCaseWorkItems.authUri.actuatorMetrics.label", "PageAdminRoles.authUri.actuatorMetrics.description")
+                    "ActuatorEndpoint.authActuator.metrics.label", "ActuatorEndpoint.authActuator.metrics.description")
     });
 
     private String url;
