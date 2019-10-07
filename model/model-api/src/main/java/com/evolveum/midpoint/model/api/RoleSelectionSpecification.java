b9aa5e4ac8fe90e77bf6dee188c456d73f2e810e
kate <honchar@evolveum.com>
2019-10-07 22:32:33 +0200

    assignable relations filter for gui part of shopping cart

--- model/model-api/src/main/java/com/evolveum/midpoint/model/api/RoleSelectionSpecification.java
+++ model/model-api/src/main/java/com/evolveum/midpoint/model/api/RoleSelectionSpecification.java
@@ -31,6 +31,10 @@ public class RoleSelectionSpecification implements DebugDumpable {
 		return relationMap.size();
 	}
 
+	public Map<QName, ObjectFilter> getRelationMap() {
+		return relationMap;
+	}
+
 	public ObjectFilter getGlobalFilter() {
 		return globalFilter;
 	}
