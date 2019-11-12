67af746c87aba553f77018d9eba96ee693fc5196
kate <honchar@evolveum.com>
2019-11-12 09:39:24 +0100

    attempt to fix synchronization schrodinger tests

--- tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/MidPoint.java
+++ tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/MidPoint.java
@@ -27,7 +27,7 @@ public class MidPoint {
     public static final long TIMEOUT_DEFAULT_2_S = 2000;
     public static final long TIMEOUT_MEDIUM_6_S = 6000;
     public static final long TIMEOUT_LONG_1_M = 60000;
-    public static final long TIMEOUT_EXTRA_LONG_1_M = 120000;
+    public static final long TIMEOUT_EXTRA_LONG_1_M = 180000;
 
     private static final String SCHRODINGER_PROPERTIES = "../../testing/schrodingertest/src/test/resources/configuration/schrodinger.properties";
 
