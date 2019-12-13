cbaa350e8edba4ea18c842357cbe4502a8abe691
kate <honchar@evolveum.com>
2019-12-13 15:33:03 +0100

    "no application attached" fix for dashboard page

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/self/PageSelfDashboard.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/self/PageSelfDashboard.java
@@ -631,7 +631,7 @@ public class PageSelfDashboard extends PageSelf {
             return new AssignmentItemDto(null, null, null, null);
         }
 
-        String name = WebComponentUtil.getDisplayNameOrName(value, true, getLocalizationService());
+        String name = WebComponentUtil.getDisplayNameOrName(value, false);
         AssignmentEditorDtoType type = AssignmentEditorDtoType.getType(value.getCompileTimeClass());
         String relation = refValue.getRelation() != null ? refValue.getRelation().getLocalPart() : null;
 
