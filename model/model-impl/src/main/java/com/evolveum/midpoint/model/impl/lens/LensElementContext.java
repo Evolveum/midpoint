4ba7c95a39aaa37419d9dde7d96291477ef01576
Pavol Mederly <mederly@evolveum.com>
2020-01-09 20:30:34 +0100

    Add re-freezing of primary deltas
    After old values are set, the new delta - i.e. a clone of (immutable)
    primary delta - was left mutable.
    
    This led to mistakenly overwriting primary delta OID in some cases e.g.
    TestConsistencyMechanism.test142ModifyObjectNotFoundAssignedAccount.
    
    Now the deltas are correctly re-frozen after any such changes.

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/LensElementContext.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/LensElementContext.java
@@ -447,6 +447,8 @@ public abstract class LensElementContext<O extends ObjectType> implements ModelE
         if (primaryDelta != null && !primaryDelta.isImmutable()) {
             primaryDelta.setOid(oid);
         }
+        // TODO What if primary delta is immutable ADD delta and objectNew was taken from it?
+        //  It would be immutable as well in that case. We will see.
         if (objectNew != null) {
             objectNew.setOid(oid);
         }
