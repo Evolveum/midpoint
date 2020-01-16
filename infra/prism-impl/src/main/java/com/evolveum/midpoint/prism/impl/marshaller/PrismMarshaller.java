352063a2ca3e6c228a8426a0feb529670798106d
Pavol Mederly <mederly@evolveum.com>
2020-01-16 17:50:42 +0100

    Add support for "incomplete" flag in XML/JSON/YAML
    This is an experimental feature for now. It is intended mainly as
    helping hand for repository to know that e.g. user photo is present
    but has not been retrieved.
    
    See MID-5772, maybe MID-5912.

--- infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/marshaller/PrismMarshaller.java
+++ infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/marshaller/PrismMarshaller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018 Evolveum and contributors
+ * Copyright (c) 2010-2020 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
@@ -172,12 +172,17 @@ public class PrismMarshaller {
     private XNodeImpl marshalItemContent(@NotNull Item<?, ?> item,
             ItemDefinition itemDefinition, SerializationContext context,
             Collection<? extends QName> itemsToSkip) throws SchemaException {
-        if (item.size() == 1) {
+        if (item.isEmpty() && item.isIncomplete()) {
+            return new IncompleteMarkerXNodeImpl();
+        } else if (item.size() == 1 && !item.isIncomplete()) {
             return marshalItemValue(item.getAnyValue(), itemDefinition, null, context, itemsToSkip);
         } else {
             ListXNodeImpl xlist = new ListXNodeImpl();
             for (PrismValue val : item.getValues()) {
                 xlist.add(marshalItemValue(val, itemDefinition, null, context, itemsToSkip));
+            }
+            if (item.isIncomplete()) {
+                xlist.add(new IncompleteMarkerXNodeImpl());
             }
             return xlist;
         }
