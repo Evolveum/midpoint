9093ca33a1316b64d7bb19654e7c7c8e1b55e699
kate <honchar@evolveum.com>
2020-01-24 17:56:34 +0100

    audit log viewer sorting (post-merge )

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/reports/PageAuditLogViewer.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/reports/PageAuditLogViewer.java
@@ -69,6 +69,11 @@ public class PageAuditLogViewer extends PageBase {
             private static final long serialVersionUID = 1L;
 
             @Override
+            protected AuditLogStorage getAuditLogStorage(){
+                return PageAuditLogViewer.this.getAuditLogStorage();
+            }
+
+            @Override
             protected void updateAuditSearchStorage(AuditSearchDto searchDto) {
                 getAuditLogStorage().setSearchDto(searchDto);
                 getAuditLogStorage().setPageNumber(0);
@@ -78,17 +83,6 @@ public class PageAuditLogViewer extends PageBase {
             protected void resetAuditSearchStorage() {
                 getAuditLogStorage().setSearchDto(new AuditSearchDto());
 
-            }
-
-            @Override
-            protected void updateCurrentPage(long current) {
-                getAuditLogStorage().setPageNumber(current);
-
-            }
-
-            @Override
-            protected long getCurrentPage() {
-                return getAuditLogStorage().getPageNumber();
             }
 
         };
