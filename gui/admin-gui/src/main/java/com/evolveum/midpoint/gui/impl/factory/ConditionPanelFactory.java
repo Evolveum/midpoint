4ab32e0dfaf2f369520131942a499c984419562d
lskublik <lskublik@evolveum.com>
2020-02-26 16:15:29 +0100

    fix for change height to max for conditional panel

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/factory/ConditionPanelFactory.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/factory/ConditionPanelFactory.java
@@ -35,7 +35,12 @@ public class ConditionPanelFactory extends AbstractGuiComponentFactory<Expressio
 
     @Override
     protected Panel getPanel(PrismPropertyPanelContext<ExpressionType> panelCtx) {
-        AceEditorPanel conditionPanel  =  new AceEditorPanel(panelCtx.getComponentId(), null, new ConditionExpressionModel(panelCtx.getRealValueModel(), panelCtx.getPageBase()), 200);
+        AceEditorPanel conditionPanel  =  new AceEditorPanel(panelCtx.getComponentId(), null, new ConditionExpressionModel(panelCtx.getRealValueModel(), panelCtx.getPageBase()), 200){
+            @Override
+            protected boolean isResizeToMaxHeight() {
+                return false;
+            }
+        };
 //        conditionPanel.getEditor().add(new OnChangeAjaxBehavior() {
 //            @Override
 //            protected void onUpdate(AjaxRequestTarget ajaxRequestTarget) {
