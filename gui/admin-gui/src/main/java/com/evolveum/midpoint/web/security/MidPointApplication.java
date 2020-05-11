efc943841290db97fadcc0588f81ac9c725286ce
Katarina Valalikova <k.valalikova@evolveum.com>
2020-05-11 09:01:55 +0200

    evolveum AdminLTE webjar.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/security/MidPointApplication.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/security/MidPointApplication.java
@@ -276,7 +276,7 @@ public class MidPointApplication extends AuthenticatedWebApplication implements 
 
         getJavaScriptLibrarySettings().setJQueryReference(
                 new PackageResourceReference(MidPointApplication.class,
-                        "../../../../../webjars/AdminLTE/2.4.18/plugins/jQueryUI/jquery-ui.js")); //todo no jquery.js is found
+                        "../../../../../webjars/AdminLTE/2.4.18/bower_components/jquery/dist/jquery.min.js")); //todo no jquery.js is found
 
         getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContext, true));
 
