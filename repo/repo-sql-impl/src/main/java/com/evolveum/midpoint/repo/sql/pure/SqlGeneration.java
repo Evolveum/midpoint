69963bff24592bc35e59935d724137da49747870
Richard Richter <richard.richter@evolveum.com>
2020-06-23 21:41:33 +0200

    MID-6319: metadata experiments, querydsl-sql-codegen commented out

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/pure/SqlGeneration.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/pure/SqlGeneration.java
@@ -1,27 +1,31 @@
 package com.evolveum.midpoint.repo.sql.pure;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import com.myproject.mydomain.QMAuditEvent;
 import com.querydsl.sql.Configuration;
 import com.querydsl.sql.H2Templates;
 import com.querydsl.sql.SQLQueryFactory;
 import com.querydsl.sql.SQLTemplates;
-import com.querydsl.sql.codegen.MetaDataExporter;
-import org.h2.Driver;
 
 // TODO MID-6319 must go after done
 @Deprecated
 public class SqlGeneration {
-    public static void main(String[] args) throws SQLException {
+
+    public static void main(String[] args) {
+        /* this requires querydsl-sql-codegen
         Driver.load();
         Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost:5437/midpoint", "sa", "");
         MetaDataExporter exporter = new MetaDataExporter();
         exporter.setPackageName("com.myproject.mydomain");
         exporter.setTargetFolder(new File("target/generated-sources/java"));
         exporter.export(conn.getMetaData());
+        */
+
+        System.out.println(QMAuditEvent.mAuditEvent);
+        System.out.println(QMAuditEvent.mAuditEvent.getColumns());
+        System.out.println(QMAuditEvent.mAuditEvent.getAnnotatedElement());
+        System.out.println(QMAuditEvent.mAuditEvent.getForeignKeys());
+        System.out.println(QMAuditEvent.mAuditEvent.getInverseForeignKeys());
+
     }
 
     private void depUnusedFix() {
