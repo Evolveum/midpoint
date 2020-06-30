1dea9d62f9438adeff288164091e8580e5664b69
kate <honchar@evolveum.com>
2020-06-30 09:49:12 +0200

    filter config component for admin gui configuration

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/factory/panel/SearchFilterPanelFactory.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/factory/panel/SearchFilterPanelFactory.java
@@ -7,29 +7,33 @@
 
 package com.evolveum.midpoint.gui.impl.factory.panel;
 
+import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.xml.namespace.QName;
 
-import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerValueWrapper;
-import com.evolveum.midpoint.gui.api.prism.wrapper.PrismPropertyWrapper;
-
-import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
-import com.evolveum.midpoint.web.page.admin.reports.component.SearchFilterConfigurationPanel;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectCollectionType;
-
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
 
 import org.apache.wicket.markup.html.panel.Panel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.evolveum.midpoint.gui.api.factory.AbstractGuiComponentFactory;
-import com.evolveum.midpoint.gui.api.prism.wrapper.ItemWrapper;
-import com.evolveum.midpoint.gui.api.registry.GuiComponentRegistry;
+import com.evolveum.midpoint.gui.api.prism.wrapper.*;
+import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.util.exception.SchemaException;
 import com.evolveum.midpoint.web.page.admin.reports.component.AceEditorPanel;
+import com.evolveum.midpoint.web.page.admin.reports.component.SearchFilterConfigurationPanel;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.GuiObjectListViewType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectCollectionType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.SearchItemType;
 import com.evolveum.prism.xml.ns._public.query_3.SearchFilterType;
 
 @Component
 public class SearchFilterPanelFactory extends AbstractGuiComponentFactory<SearchFilterType> {
+
+    private static final transient Trace LOGGER = TraceManager.getTrace(SearchFilterPanelFactory.class);
 
     @PostConstruct
     public void register() {
@@ -45,11 +49,37 @@ public class SearchFilterPanelFactory extends AbstractGuiComponentFactory<Search
     protected Panel getPanel(PrismPropertyPanelContext<SearchFilterType> panelCtx) {
         PrismPropertyWrapper<SearchFilterType> searchFilterItemWrapper = panelCtx.unwrapWrapperModel();
         PrismContainerValueWrapper containerWrapper = searchFilterItemWrapper.getParent();
-        if (containerWrapper != null && containerWrapper.getRealValue() instanceof ObjectCollectionType){
-            ObjectCollectionType collectionObj = (ObjectCollectionType) containerWrapper.getRealValue();
+        //todo do we want to use search filter configuration component all over the gui?
+        if (containerWrapper != null){
+            QName filterType = null;
+            if (containerWrapper.getRealValue() instanceof ObjectCollectionType){
+                ObjectCollectionType collectionObj = (ObjectCollectionType) containerWrapper.getRealValue();
+                filterType = collectionObj.getType() != null ? collectionObj.getType() : ObjectType.COMPLEX_TYPE;
+            } else if (containerWrapper.getRealValue() instanceof SearchItemType){
+                try {
+                    PrismObjectWrapper ow = containerWrapper.getParent().findObjectWrapper();
+                    ItemPath searchItemPath = containerWrapper.getPath();
+                    ItemPath objectCollectionItemPath = searchItemPath.subPath(0, 3);
+                    Long containerId = Long.parseLong(searchItemPath.subPath(3, 4).toString());
+                    PrismContainerWrapper pcw = ow.findContainer(objectCollectionItemPath);
+                    List<PrismContainerValueWrapper> values = pcw.getValues();
+                    for (PrismContainerValueWrapper val : values) {
+                        if (val.getPath().last() instanceof Long && Long.parseLong(val.getPath().last().toString()) == containerId) {
+                            Object obj = val.getRealValue();
+                            if (obj instanceof GuiObjectListViewType) {
+                                filterType = ((GuiObjectListViewType) obj).getType();
+                            } else if (obj instanceof ObjectCollectionType) {
+                                filterType = ((ObjectCollectionType) obj).getType();
+                            }
+                            break;
+                        }
+                    }
+                } catch (SchemaException ex){
+                    LOGGER.warn("Unable to find search filter type");
+                }
+            }
             return new SearchFilterConfigurationPanel(panelCtx.getComponentId(), panelCtx.getRealValueModel(),
-                    (Class<? extends ObjectType>)WebComponentUtil.qnameToClass(panelCtx.getPageBase().getPrismContext(),
-                            collectionObj.getType() != null ? collectionObj.getType() : ObjectType.COMPLEX_TYPE));
+                    WebComponentUtil.qnameToClass(panelCtx.getPageBase().getPrismContext(), filterType));
         }
         return new AceEditorPanel(panelCtx.getComponentId(), null, new SearchFilterTypeModel(panelCtx.getRealValueModel(), panelCtx.getPageBase()), 10);
     }
