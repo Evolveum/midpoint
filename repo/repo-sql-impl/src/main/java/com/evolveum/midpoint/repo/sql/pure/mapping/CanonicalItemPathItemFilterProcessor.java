55963ec65a521400ce03ee54e9a42e478e070840
Richard Richter <richard.richter@evolveum.com>
2020-07-14 21:07:51 +0200

    MID-6319: added support for filter against F_CHANGED_ITEM
    Implemented with LEFT JOIN, requires DISTINCT option if condition
    matches multiple changeItems per audit record.

---
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/pure/mapping/CanonicalItemPathItemFilterProcessor.java
@@ -0,0 +1,44 @@
+package com.evolveum.midpoint.repo.sql.pure.mapping;
+
+import java.util.function.Function;
+
+import com.querydsl.core.types.EntityPath;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.Predicate;
+
+import com.evolveum.midpoint.prism.query.PropertyValueFilter;
+import com.evolveum.midpoint.repo.sql.pure.SqlPathContext;
+import com.evolveum.midpoint.repo.sql.query.QueryException;
+import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
+
+/**
+ * Filter processor for an {@link ItemPathType} attribute path (Prism item)
+ * with canonicalization of the path before using it as a value for condition.
+ *
+ * @see com.evolveum.midpoint.prism.path.CanonicalItemPath
+ */
+public class CanonicalItemPathItemFilterProcessor
+        extends SinglePathItemFilterProcessor<PropertyValueFilter<ItemPathType>> {
+
+    /**
+     * Returns the mapper creating the item path filter processor from the context.
+     */
+    public static ItemSqlMapper mapper(
+            Function<EntityPath<?>, Path<?>> rootToQueryItem) {
+        return new ItemSqlMapper(ctx ->
+                new CanonicalItemPathItemFilterProcessor(ctx, rootToQueryItem), rootToQueryItem);
+    }
+
+    private CanonicalItemPathItemFilterProcessor(
+            SqlPathContext<?, ?> context, Function<EntityPath<?>, Path<?>> rootToQueryItem) {
+        super(context, rootToQueryItem);
+    }
+
+    @Override
+    public Predicate process(PropertyValueFilter<ItemPathType> filter) throws QueryException {
+        ItemPathType value = getSingleValue(filter);
+        return createBinaryCondition(filter, path, value != null
+                ? context.prismContext().createCanonicalItemPath(value.getItemPath()).asString()
+                : null);
+    }
+}
