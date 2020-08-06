17c73eeb9780c8db17237441fd67063a85e718a6
Richard Richter <richard.richter@evolveum.com>
2020-08-06 11:23:45 +0200

    MID-6319: added copyright/license headers

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/pure/querymodel/mapping/QAuditEventRecordMapping.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/pure/querymodel/mapping/QAuditEventRecordMapping.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2010-2020 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
 package com.evolveum.midpoint.repo.sql.pure.querymodel.mapping;
 
 import static com.evolveum.midpoint.repo.sql.pure.querymodel.QAuditEventRecord.*;
