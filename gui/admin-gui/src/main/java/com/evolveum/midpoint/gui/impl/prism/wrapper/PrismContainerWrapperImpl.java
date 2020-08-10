0721b0cfbf0e23747120cb9f344860dfe310b311
kate <honchar@evolveum.com>
2020-08-10 23:32:52 +0200

    mid-6279 refresh task page fix

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/prism/wrapper/PrismContainerWrapperImpl.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/prism/wrapper/PrismContainerWrapperImpl.java
@@ -173,8 +173,14 @@ public class PrismContainerWrapperImpl<C extends Containerable> extends ItemWrap
     }
 
     @Override
-    public <T extends Containerable> PrismContainerValueWrapper<T> findContainerValue(ItemPath path) {
-        throw new UnsupportedOperationException("Too lazy to implement it. Please, implement by yourself :) ");
+    public PrismContainerValueWrapper<C> findContainerValue(ItemPath path) {
+        if (isSingleValue()){
+            return findValue(0l);
+        } else if (!path.startsWithId()) {
+            throw new UnsupportedOperationException("Cannot get value from multivalue container without specified container id.");
+        } else {
+            return findValue(path.firstToId());
+        }
     }
 
     public PrismContainerDefinition<C> getItemDefinition() {
