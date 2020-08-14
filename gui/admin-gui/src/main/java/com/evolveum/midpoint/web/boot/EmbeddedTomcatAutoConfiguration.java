ff510dca4f2833b626db85b4cbdb5a17683dac6e
Slavek Licehammer <slavek@evolveum.com>
2020-08-14 10:39:36 +0200

    Add Tomcat AJP secret configuration option
    To mitigate Ghostcat venerability in Tomcat AJP it is requiring the
    secret for the AJP protocol by default. This commit add the option to
    configure such secret.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/boot/EmbeddedTomcatAutoConfiguration.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/boot/EmbeddedTomcatAutoConfiguration.java
@@ -13,6 +13,7 @@ import com.evolveum.midpoint.model.common.SystemObjectCache;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.coyote.UpgradeProtocol;
+import org.apache.coyote.ajp.AjpNioProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
@@ -58,6 +59,9 @@ public class EmbeddedTomcatAutoConfiguration {
         @Value( "${server.tomcat.ajp.port:9090}" )
         private int port;
 
+        @Value( "${server.tomcat.ajp.secret:}" )
+        private String secret;
+
         @Value("${server.servlet.context-path}")
         private String contextPath;
 
@@ -70,6 +74,8 @@ public class EmbeddedTomcatAutoConfiguration {
 
             if(enableAjp) {
                 Connector ajpConnector = new Connector("AJP/1.3");
+                AjpNioProtocol protocol = (AjpNioProtocol)ajpConnector.getProtocolHandler();
+                protocol.setSecret(secret);
                 ajpConnector.setPort(port);
                 ajpConnector.setSecure(false);
                 ajpConnector.setScheme("http");
