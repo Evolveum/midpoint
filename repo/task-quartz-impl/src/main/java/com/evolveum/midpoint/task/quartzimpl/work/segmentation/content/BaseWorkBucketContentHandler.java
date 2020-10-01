b867ae2f7b4abc2f10b7e0d0766d3314c0741cd8
Pavol Mederly <mederly@evolveum.com>
2020-10-01 11:43:30 +0200

    Fix default discriminator for non-interval buckets
    For <oidSegmentation> buckets the default discriminator of '#' was used
    only in connection with interval comparison method. Now that's fixed
    and the default is applied also with prefix comparison method.
    
    Somewhat related to MID-6468.

--- repo/task-quartz-impl/src/main/java/com/evolveum/midpoint/task/quartzimpl/work/segmentation/content/BaseWorkBucketContentHandler.java
+++ repo/task-quartz-impl/src/main/java/com/evolveum/midpoint/task/quartzimpl/work/segmentation/content/BaseWorkBucketContentHandler.java
@@ -7,14 +7,32 @@
 
 package com.evolveum.midpoint.task.quartzimpl.work.segmentation.content;
 
+import com.evolveum.midpoint.prism.PrismConstants;
 import com.evolveum.midpoint.prism.PrismContext;
+import com.evolveum.midpoint.prism.path.ItemName;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AbstractWorkBucketContentType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AbstractWorkSegmentationType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.OidWorkSegmentationType;
+import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
+
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * @author mederly
- */
 public abstract class BaseWorkBucketContentHandler implements WorkBucketContentHandler {
 
     @Autowired protected WorkBucketContentHandlerRegistry registry;
     @Autowired protected PrismContext prismContext;
+
+    @NotNull
+    protected ItemPath getDiscriminator(AbstractWorkSegmentationType configuration, AbstractWorkBucketContentType content) {
+        ItemPathType discriminatorPathType = configuration.getDiscriminator();
+        if (discriminatorPathType != null) {
+            return discriminatorPathType.getItemPath();
+        } else if (configuration instanceof OidWorkSegmentationType) {
+            return ItemName.fromQName(PrismConstants.T_ID);     // fixme
+        } else {
+            throw new IllegalStateException("No buckets discriminator defined; bucket content = " + content);
+        }
+    }
 }
