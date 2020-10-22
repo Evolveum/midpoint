7c79c44d494a841d8bc43ffb9e91fd6f0b0044e4
kate <honchar@evolveum.com>
2020-10-22 16:05:02 +0200

    schrodinger: object collection pages coverage

---
+++ tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/page/objectcollection/ListObjectCollectionsPage.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2010-2020 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+package com.evolveum.midpoint.schrodinger.page.objectcollection;
+
+import com.evolveum.midpoint.schrodinger.component.assignmentholder.AssignmentHolderObjectListPage;
+
+public class ListObjectCollectionsPage extends AssignmentHolderObjectListPage<ObjectCollectionsListTable, ObjectCollectionPage> {
+
+    @Override
+    public ObjectCollectionsListTable table() {
+        return new ObjectCollectionsListTable(this, getTableBoxElement());
+    }
+
+    @Override
+    public ObjectCollectionPage getObjectDetailsPage() {
+        return new ObjectCollectionPage();
+    }
+
+}
---
+++ tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/page/objectcollection/ObjectCollectionPage.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2010-2020 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+package com.evolveum.midpoint.schrodinger.page.objectcollection;
+
+import com.evolveum.midpoint.schrodinger.page.AssignmentHolderDetailsPage;
+
+/**
+ * Created by Kate Honchar.
+ */
+public class ObjectCollectionPage extends AssignmentHolderDetailsPage {
+
+}
