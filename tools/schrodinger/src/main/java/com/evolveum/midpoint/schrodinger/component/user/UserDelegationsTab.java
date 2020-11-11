931710fb730a99faee811b47521b43dd42c5d770
kate <honchar@evolveum.com>
2020-11-12 00:30:07 +0100

    schrodinger: coverage for user delegations tab, removed focus delegations tab

--- tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/component/user/UserDelegationsTab.java
+++ tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/component/user/UserDelegationsTab.java
@@ -7,9 +7,21 @@
 
 package com.evolveum.midpoint.schrodinger.component.user;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+
+import com.evolveum.midpoint.schrodinger.MidPoint;
 import com.evolveum.midpoint.schrodinger.component.Component;
+import com.evolveum.midpoint.schrodinger.component.modal.ConfirmationModal;
+import com.evolveum.midpoint.schrodinger.component.modal.ObjectBrowserModal;
 import com.evolveum.midpoint.schrodinger.page.user.UserPage;
+import com.evolveum.midpoint.schrodinger.util.Schrodinger;
+
+import com.evolveum.midpoint.schrodinger.util.Utils;
+
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
 
 /**
  * Created by Viliam Repan (lazyman).
@@ -18,5 +30,41 @@ public class UserDelegationsTab extends Component<UserPage> {
 
     public UserDelegationsTab(UserPage parent, SelenideElement parentElement) {
         super(parent, parentElement);
+    }
+
+    public ObjectBrowserModal<UserDelegationsTab> clickAddDelegation() {
+        SelenideElement button = $(Schrodinger.byDataId("assignmentsMenu")).waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S)
+                .$(Schrodinger.byElementAttributeValue("button", "data-toggle", "dropdown"));
+        button.click();
+        button.waitWhile(Condition.attribute("aria-expanded", "true"), MidPoint.TIMEOUT_MEDIUM_6_S);
+        button.$(By.linkText("Add delegation")).waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S).click();
+        return new ObjectBrowserModal<>(this, Utils.getModalWindowSelenideElement());
+    }
+
+    public ConfirmationModal<UserDelegationsTab> clickDeleteDelegation() {
+        SelenideElement button = $(Schrodinger.byDataId("assignmentsMenu")).waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S)
+                .$(Schrodinger.byElementAttributeValue("button", "data-toggle", "dropdown"));
+        button.click();
+        button.waitUntil(Condition.attribute("aria-expanded", "true"), MidPoint.TIMEOUT_MEDIUM_6_S);
+        button.$(By.linkText("Delete delegation")).waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S).click();
+        return new ConfirmationModal<>(this, Utils.getModalWindowSelenideElement());
+    }
+
+    public UserDelegationsTab clickAllDelegationsCheckBox() {
+        SelenideElement checkbox = $(Schrodinger.byDataId("assignmentsCheckAll")).waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        checkbox.click();
+        checkbox.waitUntil(Condition.attribute("checked", "checked"), MidPoint.TIMEOUT_DEFAULT_2_S);
+        return this;
+    }
+
+    public UserDelegationsTab clickCheckBoxForUserDelegation(String userName) {
+        SelenideElement delegationRow = $(Schrodinger.byAncestorFollowingSiblingDescendantOrSelfElementEnclosedValue("div",
+                "class", "row check-table-header", "class", "name", userName))
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        SelenideElement checkBox = delegationRow.find(Schrodinger.byElementAttributeValue("input", "type", "checkbox"))
+                .waitUntil(Condition.visible, MidPoint.TIMEOUT_DEFAULT_2_S);
+        checkBox.click();
+        checkBox.waitUntil(Condition.attribute("checked", "checked"), MidPoint.TIMEOUT_DEFAULT_2_S);
+        return this;
     }
 }
