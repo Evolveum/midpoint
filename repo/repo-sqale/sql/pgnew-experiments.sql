eadce37cb5f8d94afbb6a456ed9a7a8d1353835a
Richard Richter <richard.richter@evolveum.com>
2020-11-17 12:53:42 +0100

    new-repo DB: split of create script and experiments, more notes/TODOs

--- repo/repo-sqale/sql/pgnew-experiments.sql
+++ repo/repo-sqale/sql/pgnew-experiments.sql
@@ -1,153 +1,41 @@
 -- @formatter:off because of terribly unreliable IDEA reformat for SQL
 
--- TODO: unique oid across tables:
---  http://blog.ioguix.net/postgresql/2015/02/05/Partitionning-and-constraints-part-1.html
---  or having dedicated centralized unrelated m_object_oid table?
+-- various internal PG selects
+SELECT version();
+select * from pg_tables where tableowner='midpoint' order by tablename ;
+select * from pg_tables where schemaname='public' order by tablename ;
+select * from pg_trigger order by tgname;
+select * from pg_available_extensions order by name;
 
--- To support gen_random_uuid() pgcrypto extension must be enabled for the database.
--- select * from pg_available_extensions order by name;
--- create EXTENSION pgcrypto;
+-- DB clean: drop schema does it all with one command
+-- drop schema public CASCADE;
+-- drop table m_object;
+-- DROP TRIGGER m_resource_oid_check_tr ON m_resource;
 
-drop table m_user;
-drop table m_focus;
-drop table m_resource;
-drop table m_object;
-
-CREATE TABLE m_object (
-    oid UUID NOT NULL DEFAULT gen_random_uuid(),
--- will be overridden with GENERATED value in concrete table
-    objectTypeClass INT4 DEFAULT 3,
-    name_norm VARCHAR(255),
-    name_orig VARCHAR(255),
-    fullObject BYTEA,
-    createChannel VARCHAR(255),
-    createTimestamp TIMESTAMP,
-    creatorRef_relation VARCHAR(157),
-    creatorRef_targetOid VARCHAR(36),
-    creatorRef_targetType INT4,
-    lifecycleState VARCHAR(255),
-    modifierRef_relation VARCHAR(157),
-    modifierRef_targetOid VARCHAR(36),
-    modifierRef_targetType INT4,
-    modifyChannel VARCHAR(255),
-    modifyTimestamp TIMESTAMP,
-    tenantRef_relation VARCHAR(157),
-    tenantRef_targetOid VARCHAR(36),
-    tenantRef_targetType INT4,
-    version INT4 NOT NULL,
-
--- prevents inserts to this table, but not to inherited ones
-    CHECK (FALSE) NO INHERIT
-);
-
--- "concrete" table, allows insert and defines "final" objectTypeClass with GENERATED
-CREATE TABLE m_resource (
-    objectTypeClass INT4 GENERATED ALWAYS AS (5) STORED,
-    administrativeState INT4,
-    connectorRef_relation VARCHAR(157),
-    connectorRef_targetOid VARCHAR(36),
-    connectorRef_targetType INT4,
-    o16_lastAvailabilityStatus INT4,
-
-    CONSTRAINT m_resource_pk PRIMARY KEY (OID)
-)
-    INHERITS (m_object);
-
--- extending m_object, but still abstract, hence DEFAULT for objectTypeClass and CHECK (false)
-CREATE TABLE m_focus (
-    -- will be overridden with GENERATED value in concrete table
-    objectTypeClass INT4 DEFAULT 17,
-    administrativeStatus INT4,
-    archiveTimestamp TIMESTAMP,
-    disableReason VARCHAR(255),
-    disableTimestamp TIMESTAMP,
-    effectiveStatus INT4,
-    enableTimestamp TIMESTAMP,
-    validFrom TIMESTAMP,
-    validTo TIMESTAMP,
-    validityChangeTimestamp TIMESTAMP,
-    validityStatus INT4,
-    costCenter VARCHAR(255),
-    emailAddress VARCHAR(255),
-    hasPhoto BOOLEAN DEFAULT FALSE NOT NULL,
-    locale VARCHAR(255),
-    locality_norm VARCHAR(255),
-    locality_orig VARCHAR(255),
-    preferredLanguage VARCHAR(255),
-    telephoneNumber VARCHAR(255),
-    timezone VARCHAR(255),
-    passwordCreateTimestamp TIMESTAMP,
-    passwordModifyTimestamp TIMESTAMP,
-
-    CHECK (FALSE) NO INHERIT
-)
-    INHERITS (m_object);
-
-CREATE TABLE m_user (
-    objectTypeClass INT4 GENERATED ALWAYS AS (10) STORED,
-
-    additionalName_norm VARCHAR(255),
-    additionalName_orig VARCHAR(255),
-    employeeNumber VARCHAR(255),
-    familyName_norm VARCHAR(255),
-    familyName_orig VARCHAR(255),
-    fullName_norm VARCHAR(255),
-    fullName_orig VARCHAR(255),
-    givenName_norm VARCHAR(255),
-    givenName_orig VARCHAR(255),
-    honorificPrefix_norm VARCHAR(255),
-    honorificPrefix_orig VARCHAR(255),
-    honorificSuffix_norm VARCHAR(255),
-    honorificSuffix_orig VARCHAR(255),
-    name_norm VARCHAR(255),
-    name_orig VARCHAR(255),
-    nickName_norm VARCHAR(255),
-    nickName_orig VARCHAR(255),
-    title_norm VARCHAR(255),
-    title_orig VARCHAR(255),
-
-    CONSTRAINT m_user_pk PRIMARY KEY (OID)
-)
-    INHERITS (m_focus);
-
--- See: http://blog.ioguix.net/postgresql/2015/02/05/Partitionning-and-constraints-part-1.html
--- this checks provided OID, we will probably want unique generated OID
-CREATE OR REPLACE FUNCTION object_oid_pk()
-    RETURNS trigger
-    LANGUAGE plpgsql
-AS $function$
-BEGIN
-    PERFORM pg_advisory_xact_lock(hashtext(NEW.oid::text));
-
-    IF count(1) > 1 FROM m_object WHERE oid = NEW.oid THEN
-        RAISE EXCEPTION 'duplicate m_object.OID value "%" while inserting into "%"',
-            NEW.oid, TG_TABLE_NAME;
-    END IF;
-
-    RETURN NULL;
-END
-$function$;
-
-DROP TRIGGER m_resource_oid_check_tr ON m_resource;
-DROP TRIGGER m_user_oid_check_tr ON m_user;
-CREATE TRIGGER m_resource_oid_check_tr AFTER INSERT OR UPDATE ON m_resource
-    FOR EACH ROW EXECUTE PROCEDURE object_oid_pk();
-CREATE TRIGGER m_user_oid_check_tr AFTER INSERT OR UPDATE ON m_user
-    FOR EACH ROW EXECUTE PROCEDURE object_oid_pk();
-
-
+-- DB data initialization (after pgnew-repo.sql)
 -- one user with random name
 INSERT INTO m_user (oid, name_orig, version)
 VALUES (gen_random_uuid(), md5(random()::TEXT), 1);
 
+-- should fail the second time because oid is PK of the table
 insert into m_resource (oid, name_orig, version) VALUES ('66eb4861-867d-4a41-b6f0-41a3874bd48f', 'resource1', 1);
+-- this should fail after previous due to cross-table m_object unique constraint
 insert into m_user (oid, name_orig, version) VALUES ('66eb4861-867d-4a41-b6f0-41a3874bd48f', 'conflict', 1);
 
-SELECT count(*)
--- SELECT *
-FROM m_focus
-;
+-- inner transaction should fail due to cross-table m_object unique constraint
+delete from m_object where oid='66eb4861-867d-4a41-b6f0-41a3874bd48f';
+-- switch Tx to manual in IDE to avoid autocommit
+START TRANSACTION;
+insert into m_resource (oid, name_orig, version) VALUES ('66eb4861-867d-4a41-b6f0-41a3874bd48f', 'resource1', 1);
 
+    START TRANSACTION;
+    insert into m_user (oid, name_orig, version) VALUES ('66eb4861-867d-4a41-b6f0-41a3874bd48f', 'conflict', 1);
+    commit;
+commit;
+-- switch Tx back to Auto if desired - only resource1 should be inserted
+select * from m_object where oid='66eb4861-867d-4a41-b6f0-41a3874bd48f';
+
+-- adding x users
 DO
 $$
     BEGIN
@@ -164,12 +52,31 @@ $$
     END;
 $$;
 
+-- MUST fail on OID constraint if existing OID is used in SET:
+update m_object
+set oid='66eb4861-867d-4a41-b6f0-41a3874bd48f'
+where oid='f7a0362f-37a5-4dea-ac16-9c84dce333dc';
+
+select * from m_user where name_norm is not null;
+
+select * from m_object
+where oid='66eb4861-867d-4a41-b6f0-41a3874bd48f';
+
+-- EXPLAIN selects
 EXPLAIN (ANALYZE, BUFFERS, FORMAT TEXT)
 SELECT *
 FROM m_object
-where oid='50c7746e-3c6d-4e24-8d7b-b18043c6c7bb'
+where oid='cf72947b-f7b5-4b44-a2b1-07452b9056cc'
 ;
 
-select hashtext(gen_random_uuid()::text);
+EXPLAIN (ANALYZE, BUFFERS, FORMAT TEXT)
+SELECT count(*)
+-- SELECT *
+FROM m_focus
+;
 
-select * from pg_trigger
+--------------
+-- sandbox
+
+select ctid, * from m_object
+;
---
+++ repo/repo-sqale/sql/pgnew-repo.sql
@@ -0,0 +1,148 @@
+-- @formatter:off because of terribly unreliable IDEA reformat for SQL
+
+-- just in case PUBLIC schema was dropped (fastest way to remove all midpoint objects)
+CREATE SCHEMA IF NOT EXISTS public;
+
+-- To support gen_random_uuid() pgcrypto extension must be enabled for the database (not for PG 13).
+-- select * from pg_available_extensions order by name;
+DO
+$$
+    BEGIN
+        perform pg_get_functiondef('gen_random_uuid()'::regprocedure);
+        raise notice 'gen_random_uuid already exists, skipping create EXTENSION pgcrypto';
+    EXCEPTION WHEN undefined_function THEN
+        create EXTENSION pgcrypto;
+    END;
+$$;
+
+CREATE TABLE m_object (
+    oid UUID NOT NULL DEFAULT gen_random_uuid(),
+-- will be overridden with GENERATED value in concrete table
+    objectTypeClass INT4 DEFAULT 3,
+    name_norm VARCHAR(255),
+    name_orig VARCHAR(255),
+    fullObject BYTEA,
+    createChannel VARCHAR(255),
+    createTimestamp TIMESTAMP,
+    creatorRef_relation VARCHAR(157),
+    creatorRef_targetOid VARCHAR(36),
+    creatorRef_targetType INT4,
+    lifecycleState VARCHAR(255),
+    modifierRef_relation VARCHAR(157),
+    modifierRef_targetOid VARCHAR(36),
+    modifierRef_targetType INT4,
+    modifyChannel VARCHAR(255),
+    modifyTimestamp TIMESTAMP,
+    tenantRef_relation VARCHAR(157),
+    tenantRef_targetOid VARCHAR(36),
+    tenantRef_targetType INT4,
+    version INT4 NOT NULL,
+
+-- prevents inserts to this table, but not to inherited ones
+    CHECK (FALSE) NO INHERIT
+);
+
+-- See: http://blog.ioguix.net/postgresql/2015/02/05/Partitionning-and-constraints-part-1.html
+-- This checks that provided OID is unique across all tables inherited from m_object.
+-- Docs for trigger functions and triggers: https://www.postgresql.org/docs/13/plpgsql-trigger.html
+CREATE OR REPLACE FUNCTION object_oid_pk()
+    RETURNS trigger
+    LANGUAGE plpgsql
+AS $function$
+BEGIN
+    -- If update doesn't change OID, we don't want the expansive check.
+    -- This can be also done with WHEN on CREATE TRIGGER, but only when INSERT and UPDATE
+    -- triggers are set separately.
+    IF NEW.oid = OLD.oid THEN
+        -- returned VALUE ignored for AFTER trigger
+        RETURN NULL;
+    END IF;
+
+    PERFORM pg_advisory_xact_lock(hashtext(NEW.oid::text));
+
+    IF count(1) > 1 FROM m_object WHERE oid = NEW.oid THEN
+    RAISE EXCEPTION 'duplicate m_object.OID value "%" while inserting into "%"',
+        NEW.oid, TG_TABLE_NAME;
+END IF;
+
+    RETURN NULL;
+END
+$function$;
+
+-- "concrete" table, allows insert and defines "final" objectTypeClass with GENERATED
+CREATE TABLE m_resource (
+    objectTypeClass INT4 GENERATED ALWAYS AS (5) STORED,
+    administrativeState INT4,
+    connectorRef_relation VARCHAR(157),
+    connectorRef_targetOid VARCHAR(36),
+    connectorRef_targetType INT4,
+    o16_lastAvailabilityStatus INT4,
+
+    CONSTRAINT m_resource_pk PRIMARY KEY (oid)
+)
+    INHERITS (m_object);
+
+-- See: http://blog.ioguix.net/postgresql/2015/02/05/Partitionning-and-constraints-part-1.html
+-- It discusses CONSTRAINT TRIGGER and various DEFERRABLE options and the impact.
+CREATE CONSTRAINT TRIGGER m_resource_oid_check_tr AFTER INSERT OR UPDATE ON m_resource
+    DEFERRABLE INITIALLY IMMEDIATE FOR EACH ROW EXECUTE PROCEDURE object_oid_pk();
+
+-- extending m_object, but still abstract, hence DEFAULT for objectTypeClass and CHECK (false)
+CREATE TABLE m_focus (
+    -- will be overridden with GENERATED value in concrete table
+    objectTypeClass INT4 DEFAULT 17,
+    administrativeStatus INT4,
+    archiveTimestamp TIMESTAMP,
+    disableReason VARCHAR(255),
+    disableTimestamp TIMESTAMP,
+    effectiveStatus INT4,
+    enableTimestamp TIMESTAMP,
+    validFrom TIMESTAMP,
+    validTo TIMESTAMP,
+    validityChangeTimestamp TIMESTAMP,
+    validityStatus INT4,
+    costCenter VARCHAR(255),
+    emailAddress VARCHAR(255),
+    hasPhoto BOOLEAN DEFAULT FALSE NOT NULL,
+    locale VARCHAR(255),
+    locality_norm VARCHAR(255),
+    locality_orig VARCHAR(255),
+    preferredLanguage VARCHAR(255),
+    telephoneNumber VARCHAR(255),
+    timezone VARCHAR(255),
+    passwordCreateTimestamp TIMESTAMP,
+    passwordModifyTimestamp TIMESTAMP,
+
+    CHECK (FALSE) NO INHERIT
+)
+    INHERITS (m_object);
+
+CREATE TABLE m_user (
+    objectTypeClass INT4 GENERATED ALWAYS AS (10) STORED,
+
+    additionalName_norm VARCHAR(255),
+    additionalName_orig VARCHAR(255),
+    employeeNumber VARCHAR(255),
+    familyName_norm VARCHAR(255),
+    familyName_orig VARCHAR(255),
+    fullName_norm VARCHAR(255),
+    fullName_orig VARCHAR(255),
+    givenName_norm VARCHAR(255),
+    givenName_orig VARCHAR(255),
+    honorificPrefix_norm VARCHAR(255),
+    honorificPrefix_orig VARCHAR(255),
+    honorificSuffix_norm VARCHAR(255),
+    honorificSuffix_orig VARCHAR(255),
+    nickName_norm VARCHAR(255),
+    nickName_orig VARCHAR(255),
+    title_norm VARCHAR(255),
+    title_orig VARCHAR(255),
+
+    CONSTRAINT m_user_pk PRIMARY KEY (oid)
+)
+    INHERITS (m_focus);
+
+CREATE CONSTRAINT TRIGGER m_user_oid_check_tr AFTER INSERT OR UPDATE ON m_user
+    DEFERRABLE INITIALLY IMMEDIATE FOR EACH ROW EXECUTE PROCEDURE object_oid_pk();
+
+-- TODO other indexes, only PK is defined at the moment
