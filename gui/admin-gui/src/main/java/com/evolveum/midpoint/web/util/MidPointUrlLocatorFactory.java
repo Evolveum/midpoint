7ec00fc18aa41591cf81723e4bc42daf9d776fbd
Katarina Valalikova <k.valalikova@evolveum.com>
2020-12-08 23:31:43 +0100

    fixing less processing

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/util/MidPointUrlLocatorFactory.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/util/MidPointUrlLocatorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2017 Evolveum and contributors
+ * Copyright (c) 2010-2020 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
@@ -8,8 +8,12 @@
 package com.evolveum.midpoint.web.util;
 
 import org.springframework.core.env.PropertyResolver;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
+import ro.isdc.wro.extensions.locator.WebjarUriLocator;
+import ro.isdc.wro.extensions.locator.WebjarsUriLocator;
 import ro.isdc.wro.model.resource.locator.ClasspathUriLocator;
 import ro.isdc.wro.model.resource.locator.ServletContextUriLocator;
+import ro.isdc.wro.model.resource.locator.StandaloneServletContextUriLocator;
 import ro.isdc.wro.model.resource.locator.factory.SimpleUriLocatorFactory;
 
 /**
@@ -19,6 +23,7 @@ public class MidPointUrlLocatorFactory extends SimpleUriLocatorFactory {
 
     public MidPointUrlLocatorFactory(PropertyResolver propertyResolver) {
         addLocator(new ClasspathUriLocator());
+        addLocator(new WebjarsUriLocator());
         addLocator(new ServletContextUriLocator());
         addLocator(new PropertyUrlUriLocator(propertyResolver));
     }
