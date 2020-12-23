243e1a8bff1a9c5cc5964ec9949e561e2cb404fa
kate <honchar@evolveum.com>
2020-12-23 14:27:48 +0100

    name column fix for custom columns

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/data/column/ObjectNameColumn.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/data/column/ObjectNameColumn.java
@@ -48,6 +48,7 @@ public class ObjectNameColumn<O extends ObjectType> extends AbstractColumn<Selec
 
     private ExpressionType expression;
     private PageBase pageBase;
+    private String itemPath;
 
     public ObjectNameColumn(IModel<String> displayModel) {
         this(displayModel, null, null, null, true);
@@ -57,6 +58,7 @@ public class ObjectNameColumn<O extends ObjectType> extends AbstractColumn<Selec
         super(displayModel, useDefaultPath ? ObjectType.F_NAME.getLocalPart() : itemPath);
         this.expression = expression;
         this.pageBase = pageBase;
+        this.itemPath = itemPath;
     }
 
     @Override
@@ -98,14 +100,19 @@ public class ObjectNameColumn<O extends ObjectType> extends AbstractColumn<Selec
                             return cellItem.getString(props.getStatusLabelKey());
                         }
                     } else {
-                        name = WebComponentUtil.getName(value, true);
-                        if (selectableBean.getResult() != null) {
-                            StringBuilder complexName = new StringBuilder();
-                            complexName.append(name);
-                            complexName.append(" (");
-                            complexName.append(selectableBean.getResult().getStatus());
-                            complexName.append(")");
-                            return complexName.toString();
+                        if (itemPath == null || itemPath.equals("name")) {
+                            name = WebComponentUtil.getName(value, true);
+                            if (selectableBean.getResult() != null) {
+                                StringBuilder complexName = new StringBuilder();
+                                complexName.append(name);
+                                complexName.append(" (");
+                                complexName.append(selectableBean.getResult().getStatus());
+                                complexName.append(")");
+                                return complexName.toString();
+                            }
+                        } else {
+                            Object itemPathPropertyValue = new PropertyModel<>(rowModel, "value." + itemPath).getObject();
+                            name = itemPathPropertyValue != null ? itemPathPropertyValue.toString() : "";
                         }
                     }
                         return name;
