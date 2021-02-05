3a7c632f68e1d66045174ef8c08cff4f4cf12e9e
Richard Richter <richard.richter@evolveum.com>
2021-02-05 11:09:17 +0100

    test Starting/Initializing debug line tweaks, @PostConstruct logs too
    @BeforeClass output prints "Starting ...", @PostConstruct methods show
    "Initializing ..." to have at least some header before it potentially
    fails (still can fail during MP startup before the header).
    Unused DerbyController in AbstractIntegrationTest was removed.

--- repo/task-quartz-impl/src/test/java/com/evolveum/midpoint/task/quartzimpl/TestWorkersManagement.java
+++ repo/task-quartz-impl/src/test/java/com/evolveum/midpoint/task/quartzimpl/TestWorkersManagement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018 Evolveum and contributors
+ * Copyright (C) 2010-2021 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
@@ -88,6 +88,7 @@ public class TestWorkersManagement extends AbstractTaskManagerTest {
 
     @PostConstruct
     public void initialize() throws Exception {
+        displayTestTitle("Initializing TEST CLASS: " + getClass().getName());
         super.initialize();
         workStateManager.setFreeBucketWaitIntervalOverride(1000L);
         DebugUtil.setPrettyPrintBeansAs(PrismContext.LANG_YAML);
