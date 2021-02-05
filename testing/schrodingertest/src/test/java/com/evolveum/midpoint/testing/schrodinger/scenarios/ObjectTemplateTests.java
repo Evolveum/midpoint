0a2f11c8615ecc46c0b15c873212469dd131eca4
kate <honchar@evolveum.com>
2021-02-06 00:14:08 +0100

    schrodinger: object templates tests

--- testing/schrodingertest/src/test/java/com/evolveum/midpoint/testing/schrodinger/scenarios/ObjectTemplateTests.java
+++ testing/schrodingertest/src/test/java/com/evolveum/midpoint/testing/schrodinger/scenarios/ObjectTemplateTests.java
@@ -3,6 +3,7 @@ package com.evolveum.midpoint.testing.schrodinger.scenarios;
 import com.codeborne.selenide.Selenide;
 
 import com.evolveum.midpoint.schrodinger.MidPoint;
+import com.evolveum.midpoint.schrodinger.page.login.FormLoginPage;
 import com.evolveum.midpoint.testing.schrodinger.AbstractSchrodingerTest;
 
 import org.testng.annotations.Test;
@@ -13,22 +14,21 @@ import java.util.Map;
 
 public class ObjectTemplateTests extends AbstractSchrodingerTest {
 
-    private static final String COMPONENT_OBJECTS_DIRECTORY = "./src/test/resources/component/objects/";
-    private static final String COMPONENT_OBJECT_TEMPLATE = COMPONENT_OBJECTS_DIRECTORY + "objectTemplate/";
-    private static final File SEARCH_CONFIG_SYSTEM_CONFIG_FILE = new File(COMPONENT_OBJECT_TEMPLATE + "object-template-example-user-simple.xml");
+    private static final String COMPONENT_OBJECTS_DIRECTORY = "./src/test/resources/configuration/objects/";
+    private static final String COMPONENT_OBJECT_TEMPLATE = COMPONENT_OBJECTS_DIRECTORY + "objecttemplate/";
+    private static final String COMPONENT_SYSTEM_CONFIGURATION = COMPONENT_OBJECTS_DIRECTORY + "systemconfig/";
+    private static final File FULL_NAME_OBJECT_TEMPLATE_FILE = new File(COMPONENT_OBJECT_TEMPLATE + "object-template-full-name.xml");
+    private static final File SYSTEM_CONFIGURATION_WITH_OBJ_TEMPLATE_FILE = new File(COMPONENT_SYSTEM_CONFIGURATION + "system-configuration-with-object-template.xml");
+    private static final File SYSTEM_CONFIGURATION_INITIAL_FILE = new File(COMPONENT_SYSTEM_CONFIGURATION + "system-configuration-initial.xml");
 
     @Test
-    public void test00100addObjectTemplateTest() {
+    public void test00100addObjectTemplateCreateUser() {
         Map<String, String> attributesMap = new HashMap<>();
         attributesMap.put("Name", "userWithoutFullname");
         attributesMap.put("Given name", "Mark");
         attributesMap.put("Family name", "Sadman");
         createUser(attributesMap);
 
-        attributesMap.clear();
-        attributesMap.put("Name", "userWithoutFullname");
-        attributesMap.put("Given name", "Mark");
-        attributesMap.put("Family name", "Sadman");
         basicPage
                 .listUsers()
                     .table()
@@ -41,21 +41,20 @@ public class ObjectTemplateTests extends AbstractSchrodingerTest {
                         .assertTableContainsColumnWithValue("UserType.givenName", "Mark")
                         .assertTableColumnValueIsEmpty("UserType.fullName");
 
-        addObjectFromFile(SEARCH_CONFIG_SYSTEM_CONFIG_FILE, true);
+        addObjectFromFile(FULL_NAME_OBJECT_TEMPLATE_FILE, true);
+        addObjectFromFile(SYSTEM_CONFIGURATION_WITH_OBJ_TEMPLATE_FILE, true);
         Selenide.sleep(MidPoint.TIMEOUT_SHORT_4_S);
 
-        basicPage
-                .newUser()
-                    .selectTabBasic()
-                        .form()
-                            .addAttributeValue("Name", "userWithFullname")
-                            .addAttributeValue("Given name", "Emil")
-                            .addAttributeValue("Family name", "Happyman")
-                            .and()
-                        .and()
-                    .clickSave()
-                        .feedback()
-                            .assertSuccess();
+        basicPage.loggedUser().logout();
+        FormLoginPage loginPage = midPoint.formLogin();
+        loginPage.login(getUsername(), getPassword());
+
+        attributesMap.clear();
+        attributesMap.put("Name", "userWithFullname");
+        attributesMap.put("Given name", "Emil");
+        attributesMap.put("Family name", "Happyman");
+        createUser(attributesMap);
+
         basicPage
                 .listUsers()
                     .table()
@@ -68,4 +67,15 @@ public class ObjectTemplateTests extends AbstractSchrodingerTest {
                         .assertTableContainsColumnWithValue("UserType.familyName", "Happyman")
                         .assertTableContainsColumnWithValue("UserType.fullName", "Emil Happyman");
     }
+
+    @Test (dependsOnMethods = {"test00100createUserWithObjectTemplateTest"})
+    public void test00200deleteObjectTemplateCreateUser() {
+
+    }
+
+    @Test
+    public void test00300createArchetypeObjectTemplateExists() {
+
+    }
+
 }
