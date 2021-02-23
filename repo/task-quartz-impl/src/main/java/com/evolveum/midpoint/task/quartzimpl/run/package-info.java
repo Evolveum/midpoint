c005a302def73459d357d63df0170da827e0377f
Pavol Mederly <mederly@evolveum.com>
2021-02-23 20:04:33 +0100

    Add missing copyright in task-quartz-impl

--- repo/task-quartz-impl/src/main/java/com/evolveum/midpoint/task/quartzimpl/run/package-info.java
+++ repo/task-quartz-impl/src/main/java/com/evolveum/midpoint/task/quartzimpl/run/package-info.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2010-2021 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
 /**
  * Implements the task run logic. I.e. all what is needed to run the task from the point of the task itself.
  * (Not from the point of _managing_ the run from the outside.)
