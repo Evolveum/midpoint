23af801e8b7766a4e7aae74375e03904ff671374
Richard Richter <richard.richter@evolveum.com>
2021-02-26 16:43:49 +0100

    MID-6417: cleanup of ROperationExecutionRecordType.java

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/enums/ROperationExecutionRecordType.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/data/common/enums/ROperationExecutionRecordType.java
@@ -6,8 +6,6 @@
  */
 package com.evolveum.midpoint.repo.sql.data.common.enums;
 
-import org.jetbrains.annotations.Nullable;
-
 import com.evolveum.midpoint.repo.sql.query.definition.JaxbType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.OperationExecutionRecordTypeType;
 
@@ -17,27 +15,14 @@ public enum ROperationExecutionRecordType implements SchemaEnum<OperationExecuti
     SIMPLE(OperationExecutionRecordTypeType.SIMPLE),
     COMPLEX(OperationExecutionRecordTypeType.COMPLEX);
 
-    private final OperationExecutionRecordTypeType recordType;
+    private final OperationExecutionRecordTypeType schemaValue;
 
-    ROperationExecutionRecordType(OperationExecutionRecordTypeType recordType) {
-        this.recordType = recordType;
+    ROperationExecutionRecordType(OperationExecutionRecordTypeType schemaValue) {
+        this.schemaValue = schemaValue;
     }
 
     @Override
     public OperationExecutionRecordTypeType getSchemaValue() {
-        return recordType;
-    }
-
-    public static @Nullable ROperationExecutionRecordType fromSchemaValue(
-            @Nullable OperationExecutionRecordTypeType jaxb) {
-        if (jaxb == null) {
-            return null;
-        }
-        for (ROperationExecutionRecordType st : values()) {
-            if (jaxb == st.recordType) {
-                return st;
-            }
-        }
-        throw new IllegalArgumentException("Unknown operation execution record type '" + jaxb + "'.");
+        return schemaValue;
     }
 }
