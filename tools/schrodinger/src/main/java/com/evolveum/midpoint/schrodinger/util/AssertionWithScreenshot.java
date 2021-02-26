c7c1ecb4e1cace99414a71d7ec4d4cc774f0c66b
kate <honchar@evolveum.com>
2021-02-26 12:55:42 +0100

    schrodinger: screenshot file name generation

--- tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/util/AssertionWithScreenshot.java
+++ tools/schrodinger/src/main/java/com/evolveum/midpoint/schrodinger/util/AssertionWithScreenshot.java
@@ -6,12 +6,23 @@ import org.testng.asserts.IAssert;
 
 public class AssertionWithScreenshot extends Assertion {
 
-    private String screenshotFileName = "assertionFailure";
+    private String screenshotFileName;
 
     @Override
     public void onAssertFailure(IAssert<?> var1, AssertionError var2) {
-        Selenide.screenshot(screenshotFileName);
+        Selenide.screenshot(screenshotFileName != null ? screenshotFileName : generateScreenshotName());
         super.onAssertFailure(var1, var2);
+    }
+
+    private String generateScreenshotName() {
+        StackTraceElement[] stack = new Throwable().fillInStackTrace().getStackTrace();
+        for (int i = 0; i < stack.length; i++) {
+            StackTraceElement el = stack[i];
+            if (el.getClassName().contains("testing.schrodinger")) {
+                return el.getClassName().substring(el.getClassName().lastIndexOf(".") + 1) + "_" + el.getMethodName() + "_line" + el.getLineNumber();
+            }
+        }
+        return "assertionError";
     }
 
     public AssertionWithScreenshot screenshotFileName(String screenshotFileName) {
