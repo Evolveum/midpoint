71750cd70040ea1bba27aa11070f15a00613dd0e
lskublik <lskublik@evolveum.com>
2021-03-08 10:40:13 +0100

    adding of icon for autocompleTextField indicating dropdown list (MID-6596)

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/autocomplete/AbstractAutoCompletePanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/autocomplete/AbstractAutoCompletePanel.java
@@ -6,19 +6,41 @@
  */
 package com.evolveum.midpoint.gui.api.component.autocomplete;
 
-import org.apache.wicket.extensions.ajax.markup.html.autocomplete.AutoCompleteSettings;
-
 import com.evolveum.midpoint.web.component.prism.InputPanel;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
+import org.apache.wicket.ajax.attributes.ThrottlingSettings;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.autocomplete.AutoCompleteSettings;
+import org.apache.wicket.util.time.Duration;
 
 /**
  * @author semancik
- *
  */
 public abstract class AbstractAutoCompletePanel extends InputPanel {
     private static final long serialVersionUID = 1L;
 
+    private static final String ID_ICON_BUTTON = "iconButton";
+
     public AbstractAutoCompletePanel(String id) {
         super(id);
+        AjaxLink<String> showChoices = new AjaxLink<String>(ID_ICON_BUTTON) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                target.focusComponent(getBaseFormComponent());
+            }
+
+            @Override
+            protected void updateAjaxAttributes(AjaxRequestAttributes attributes) {
+                super.updateAjaxAttributes(attributes);
+                AutoCompleteSettings settings = createAutoCompleteSettings();
+                attributes.setThrottlingSettings(new ThrottlingSettings(Duration.milliseconds(settings.getThrottleDelay()), true));
+            }
+        };
+        add(showChoices);
     }
 
     protected AutoCompleteSettings createAutoCompleteSettings() {
@@ -29,4 +51,5 @@ public abstract class AbstractAutoCompletePanel extends InputPanel {
         autoCompleteSettings.setShowCompleteListOnFocusGain(true);
         return autoCompleteSettings;
     }
+
 }
