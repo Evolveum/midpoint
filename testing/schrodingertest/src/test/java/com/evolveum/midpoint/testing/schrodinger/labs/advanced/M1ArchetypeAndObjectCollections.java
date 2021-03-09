662507f2a7cc6b3450d0ce308c7678b0aa39f29d
kate <honchar@evolveum.com>
2021-03-09 16:16:50 +0100

    schrodinger: mp initialization for advanced labs with post-initial-objects

--- testing/schrodingertest/src/test/java/com/evolveum/midpoint/testing/schrodinger/labs/advanced/M1ArchetypeAndObjectCollections.java
+++ testing/schrodingertest/src/test/java/com/evolveum/midpoint/testing/schrodinger/labs/advanced/M1ArchetypeAndObjectCollections.java
@@ -2,7 +2,6 @@ package com.evolveum.midpoint.testing.schrodinger.labs.advanced;
 
 import com.evolveum.midpoint.schrodinger.page.login.FormLoginPage;
 import com.evolveum.midpoint.schrodinger.util.Utils;
-import com.evolveum.midpoint.testing.schrodinger.labs.AbstractLabTest;
 
 import com.evolveum.midpoint.testing.schrodinger.scenarios.ScenariosCommons;
 
@@ -15,7 +14,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class M1ArchetypeAndObjectCollections extends AbstractLabTest {
+public class M1ArchetypeAndObjectCollections extends AbstractAdvancedLabTest {
     protected static final String LAB_OBJECTS_DIRECTORY = LAB_ADVANCED_DIRECTORY + "M1/";
     private static final File OBJECT_COLLECTION_ACTIVE_EMP_FILE = new File(LAB_OBJECTS_DIRECTORY + "objectcollections/objectCollection-active-employees.xml");
     private static final File OBJECT_COLLECTION_INACTIVE_EMP_FILE = new File(LAB_OBJECTS_DIRECTORY + "objectcollections/objectCollection-inactive-employees.xml");
@@ -29,8 +28,9 @@ public class M1ArchetypeAndObjectCollections extends AbstractLabTest {
     private static final File CSV_1_SIMPLE_RESOURCE_FILE = new File(LAB_OBJECTS_DIRECTORY + "resources/localhost-csvfile-1-document-access-simple.xml");
     private static final File CSV_2_RESOURCE_FILE = new File(LAB_OBJECTS_DIRECTORY + "resources/localhost-csvfile-2-canteen.xml");
     private static final File CSV_3_RESOURCE_FILE = new File(LAB_OBJECTS_DIRECTORY + "resources/localhost-csvfile-3-ldap.xml");
-    private static final File SYSTEM_CONFIGURATION_FILE_TEST02 = new File(LAB_OBJECTS_DIRECTORY + "systemconfiguration/system-configuration-test02.xml");
+    private static final File SYSTEM_CONFIGURATION_FILE_INIT = new File(LAB_OBJECTS_DIRECTORY + "systemconfiguration/system-configuration-advanced-labs-init.xml");
     private static final File HR_NO_EXTENSION_RESOURCE_FILE = new File(LAB_OBJECTS_DIRECTORY + "resources/localhost-hr-noextension.xml");
+    private static final File ORG_EXAMPLE_FILE = new File(LAB_OBJECTS_DIRECTORY + "org/org-example.xml");
 
     @BeforeClass(alwaysRun = true, dependsOnMethods = { "springTestContextPrepareTestInstance" })
     @Override
@@ -47,12 +47,12 @@ public class M1ArchetypeAndObjectCollections extends AbstractLabTest {
         FileUtils.copyFile(HR_SOURCE_FILE, hrTargetFile);
     }
 
-    @Override
-    protected List<File> getObjectListToImport(){
-        return Arrays.asList(OBJECT_COLLECTION_ACTIVE_EMP_FILE, OBJECT_COLLECTION_INACTIVE_EMP_FILE, OBJECT_COLLECTION_FORMER_EMP_FILE,
-                KIRK_USER_FILE, SECRET_I_ROLE_FILE, SECRET_II_ROLE_FILE, INTERNAL_EMPLOYEE_ROLE_FILE,
-                CSV_1_SIMPLE_RESOURCE_FILE, CSV_2_RESOURCE_FILE, CSV_3_RESOURCE_FILE);
-    }
+//    @Override
+//    protected List<File> getObjectListToImport(){
+//        return Arrays.asList(OBJECT_COLLECTION_ACTIVE_EMP_FILE, OBJECT_COLLECTION_INACTIVE_EMP_FILE, OBJECT_COLLECTION_FORMER_EMP_FILE,
+//                KIRK_USER_FILE, SECRET_I_ROLE_FILE, SECRET_II_ROLE_FILE, INTERNAL_EMPLOYEE_ROLE_FILE,
+//                CSV_1_SIMPLE_RESOURCE_FILE, CSV_2_RESOURCE_FILE, CSV_3_RESOURCE_FILE, SYSTEM_CONFIGURATION_FILE_INIT);
+//    }
 
     @Test(groups={"advancedM1"})
     public void mod01test01environmentInitialization() {
@@ -89,9 +89,6 @@ public class M1ArchetypeAndObjectCollections extends AbstractLabTest {
 
     @Test(groups={"advancedM1"})
     public void mod01test02ArchetypeAndObjectCollection() {
-        addObjectFromFile(ARCHETYPE_EMPLOYEE_FILE);
-        addObjectFromFile(SYSTEM_CONFIGURATION_FILE_TEST02);
-
         basicPage.loggedUser().logout();
         FormLoginPage loginPage = midPoint.formLogin();
         loginPage.loginWithReloadLoginPage(getUsername(), getPassword());
@@ -129,11 +126,30 @@ public class M1ArchetypeAndObjectCollections extends AbstractLabTest {
 
     @Test(groups={"advancedM1"})
     public void mod01test03EnvironmentExamination() {
-        addObjectFromFile(HR_NO_EXTENSION_RESOURCE_FILE, true);
         changeResourceAttribute(HR_RESOURCE_NAME, ScenariosCommons.CSV_RESOURCE_ATTR_FILE_PATH, hrTargetFile.getAbsolutePath(), true);
 
-        getShadowTable(HR_RESOURCE_NAME, "Name", "001212")
-            .clickImport();
-        showUser("X001212");
+        getShadowTable(HR_RESOURCE_NAME, "name", "001212")
+                .selectCheckboxByName("001212")
+                .clickImport();
+        showUser("X001212")
+            .assertGivenName("John")
+            .assertFamilyName("Smith")
+            .assertFullName("John Smith")
+            .selectTabBasic()
+                .form()
+                    .assertPropertySelectValue("isManager", "True")
+                    .assertPropertyInputValue("empStatus", "A")
+                    .and()
+                .and()
+                .selectTabAssignments()
+                    .assertAssignmentsWithRelationExist("Member", "Active Employees", "Inactive Employees",
+                            "Former Employees", "Internal Employee")
+                    .and()
+                .selectTabProjections()
+                    .assertProjectionExist("jsmith")
+                    .assertProjectionExist("cn=John Smith,ou=0300,ou=ExAmPLE,dc=example,dc=com")
+                    .assertProjectionExist("smith");
+
+
     }
 }
