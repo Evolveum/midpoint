040463914c48a2662160ae8d028645ba5d9ba51b
Richard Richter <richard.richter@evolveum.com>
2021-03-18 21:19:37 +0100

    repo-sqale: policySituations changed to INTEGER[] (IDs to M_URI)
    - Jsonb support moved to repo-sqlbase (with PG dep necessary)
    - policySituations added to assignments too
    - not filled by add and not queryable yet

--- repo/repo-sqale/sql/pgnew-experiments.sql
+++ repo/repo-sqale/sql/pgnew-experiments.sql
@@ -86,7 +86,7 @@ SELECT 'resource-' || LPAD(r::text, 10, '0'),
     1
 from generate_series(1, 10) as r;
 
-INSERT INTO m_user (name_norm, name_orig, fullobject, ext, version)
+INSERT INTO m_user (name_norm, name_orig, fullobject, ext, policySituations, version)
 SELECT 'user-' || LPAD(r::text, 10, '0'),
     'user-' || LPAD(r::text, 10, '0'),
     random_bytea(100, 2000),
@@ -108,10 +108,109 @@ SELECT 'user-' || LPAD(r::text, 10, '0'),
             ('{"hired": "' || current_date - width_bucket(random(), 0, 1, 1000) || '",' ||
                 '"hobbies": ' || array_to_json(random_pick(ARRAY['eating', 'books', 'music', 'dancing', 'walking', 'jokes', 'video', 'photo'], 0.5))::text || '}')::jsonb
         END,
+    CASE
+        WHEN r % 10 > 4 THEN
+            -- let's add some policy situations (IDs of M_URI entries)
+            random_pick(ARRAY(SELECT a.n FROM generate_series(1, 100) AS a(n)), r % 10 / 100::decimal)
+        -- ELSE NULL is default and redundant
+        END,
     1
-from generate_series(2000001, 3000000) as r;
+from generate_series(100001,1000000) as r;
 
-select * from m_user;
+EXPLAIN (ANALYZE, VERBOSE, BUFFERS)
+select oid, policysituations from m_user
+where policysituations @> '{10}'
+-- order by name_norm desc
+;
+
+/* 1k rows
+Seq Scan on public.m_user  (cost=0.00..14947.00 rows=3437 width=65) (actual time=0.029..39.071 rows=3567 loops=1)
+"  Output: oid, policysituations"
+  Filter: (m_user.policysituations @> '{10}'::integer[])
+  Rows Removed by Filter: 96433
+  Buffers: shared hit=11650 read=2047
+Planning:
+  Buffers: shared hit=7
+Planning Time: 0.076 ms
+Execution Time: 39.246 ms
+
+1M rows (without analyze 125ms for first 500 rows):
+Gather  (cost=1000.00..146779.83 rows=34406 width=65) (actual time=40.305..2390.356 rows=34949 loops=1)
+"  Output: oid, policysituations"
+  Workers Planned: 2
+  Workers Launched: 2
+  Buffers: shared hit=10990 read=126405 dirtied=89590 written=78799
+  ->  Parallel Seq Scan on public.m_user  (cost=0.00..142339.23 rows=14336 width=65) (actual time=42.050..2216.133 rows=11650 loops=3)
+"        Output: oid, policysituations"
+        Filter: (m_user.policysituations @> '{10}'::integer[])
+        Rows Removed by Filter: 321684
+        Buffers: shared hit=10990 read=126405 dirtied=89590 written=78799
+        Worker 0:  actual time=37.016..2129.323 rows=10913 loops=1
+          JIT:
+            Functions: 4
+"            Options: Inlining false, Optimization false, Expressions true, Deforming true"
+"            Timing: Generation 4.363 ms, Inlining 0.000 ms, Optimization 9.042 ms, Emission 24.312 ms, Total 37.717 ms"
+          Buffers: shared hit=3011 read=39940 dirtied=28314 written=25406
+        Worker 1:  actual time=50.567..2173.420 rows=11177 loops=1
+          JIT:
+            Functions: 4
+"            Options: Inlining false, Optimization false, Expressions true, Deforming true"
+"            Timing: Generation 4.199 ms, Inlining 0.000 ms, Optimization 3.489 ms, Emission 33.603 ms, Total 41.291 ms"
+          Buffers: shared hit=3637 read=40546 dirtied=28917 written=25375
+Planning Time: 0.088 ms
+JIT:
+  Functions: 12
+"  Options: Inlining false, Optimization false, Expressions true, Deforming true"
+"  Timing: Generation 20.183 ms, Inlining 0.000 ms, Optimization 14.305 ms, Emission 87.116 ms, Total 121.604 ms"
+Execution Time: 2411.721 ms
+ */
+DROP INDEX m_user_policySituation_idx;
+/* 1k rows
+Bitmap Heap Scan on public.m_user  (cost=38.63..7971.34 rows=3437 width=65) (actual time=0.895..6.448 rows=3567 loops=1)
+"  Output: oid, policysituations"
+  Recheck Cond: (m_user.policysituations @> '{10}'::integer[])
+  Heap Blocks: exact=3133
+  Buffers: shared hit=2514 read=622
+  ->  Bitmap Index Scan on m_user_policysituation_idx  (cost=0.00..37.77 rows=3437 width=0) (actual time=0.550..0.550 rows=3567 loops=1)
+        Index Cond: (m_user.policysituations @> '{10}'::integer[])
+        Buffers: shared hit=3
+Planning:
+  Buffers: shared hit=25
+Planning Time: 0.228 ms
+Execution Time: 6.603 ms
+
+1M rows (but not parallel, much lower cost (1000 to 330), fast without analyze, 96ms for first 500 rows):
+Bitmap Heap Scan on public.m_user  (cost=326.49..80473.40 rows=34902 width=64) (actual time=23.726..3046.666 rows=34949 loops=1)
+"  Output: oid, policysituations"
+  Recheck Cond: (m_user.policysituations @> '{10}'::integer[])
+  Heap Blocks: exact=31249
+  Buffers: shared hit=13 read=31249 written=4948
+  ->  Bitmap Index Scan on m_user_policysituation_idx  (cost=0.00..317.77 rows=34902 width=0) (actual time=15.035..15.035 rows=34949 loops=1)
+        Index Cond: (m_user.policysituations @> '{10}'::integer[])
+        Buffers: shared hit=13
+Planning:
+  Buffers: shared hit=73
+Planning Time: 0.434 ms
+Execution Time: 3053.032 ms
+ */
+CREATE INDEX m_user_policySituation_idx
+    ON m_user USING GIN(policysituations);
+
+/* 1M rows, clear winner, ~60ms without analyze/select of first 500 rows (also the index built under 1.5s, so it's faster even with its creation):
+Bitmap Heap Scan on public.m_user  (cost=326.48..80471.49 rows=34900 width=64) (actual time=12.360..120.267 rows=34949 loops=1)
+"  Output: oid, policysituations"
+  Recheck Cond: (m_user.policysituations @> '{10}'::integer[])
+  Heap Blocks: exact=31249
+  Buffers: shared hit=450 read=30812 written=165
+  ->  Bitmap Index Scan on m_user_policysituation_idx2  (cost=0.00..317.75 rows=34900 width=0) (actual time=6.909..6.910 rows=34949 loops=1)
+        Index Cond: (m_user.policysituations @> '{10}'::integer[])
+        Buffers: shared hit=13
+Planning:
+  Buffers: shared hit=1
+Planning Time: 0.099 ms
+Execution Time: 122.310 ms
+ */
+CREATE EXTENSION IF NOT EXISTS intarray;
 
 select * from m_object
 where oid<'812c5e7a-8a94-4bd1-944d-389e7294b831'
@@ -271,9 +370,10 @@ AS $$
     FROM generate_series(2, $1 + width_bucket(random(), 0, 1, $2 - $1 + 1));
 $$;
 
--- should return 10 and 20, just to check the ranges
+/* Test: should return 10 and 20, just to check the ranges
 select min(length(i)), max(length(i))
 from (select random_bytea(10, 20) as i from generate_series(1, 200)) q;
+*/
 
 -- returns random element from array (NULL for empty arrays)
 CREATE OR REPLACE FUNCTION random_pick(vals ANYARRAY)
