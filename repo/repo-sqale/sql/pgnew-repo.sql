392974de58844240f74714cae6907c8e12f5a587
Richard Richter <richard.richter@evolveum.com>
2021-03-23 23:06:17 +0100

    repo-sqale: MidScaleNewRepoTest experiments, few test fixes
    - schema is cleared with couple of TRUNCATEs, much faster than DELETE
    - FlexibleRelationalPathBase made non-abstract, so we can "hack" it to
    construct entity path even to stuff like m_object_oid (in test)

--- repo/repo-sqale/sql/pgnew-repo.sql
+++ repo/repo-sqale/sql/pgnew-repo.sql
@@ -618,7 +618,7 @@ ALTER TABLE m_access_cert_campaign
 CREATE INDEX m_access_cert_campaign_ext_idx ON m_access_cert_campaign USING gin (ext);
 
 CREATE TABLE m_access_cert_case (
-    owner_oid UUID NOT NULL REFERENCES m_object_oid(oid),
+    owner_oid UUID NOT NULL REFERENCES m_object_oid(oid) ON DELETE CASCADE,
     containerType ContainerType GENERATED ALWAYS AS ('ACCESS_CERTIFICATION_CASE') STORED,
     administrativeStatus INTEGER,
     archiveTimestamp TIMESTAMPTZ,
@@ -677,6 +677,7 @@ ALTER TABLE m_access_cert_wi
         REFERENCES m_access_cert_case (owner_oid, cid)
             ON DELETE CASCADE;
 
+-- TODO rework to inherit from reference tables
 CREATE TABLE m_access_cert_wi_reference (
     owner_oid UUID NOT NULL, -- PK+FK
     acc_cert_case_cid INTEGER NOT NULL, -- PK+FK
