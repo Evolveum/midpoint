e69d1c09a9b9afa051e4512d17312b9c74eb242a
Richard Richter <richard.richter@evolveum.com>
2021-05-14 09:22:41 +0200

    postgresql-4.3-all.sql: removed superfluous warning-causing commit
    If someone starts the trn before this, it's their call to commit it.

--- config/sql/postgresql-4.3-all.sql
+++ config/sql/postgresql-4.3-all.sql
@@ -1399,5 +1399,3 @@ create index idx_qrtz_ft_j_g on qrtz_fired_triggers(SCHED_NAME,JOB_NAME,JOB_GROU
 create index idx_qrtz_ft_jg on qrtz_fired_triggers(SCHED_NAME,JOB_GROUP);
 create index idx_qrtz_ft_t_g on qrtz_fired_triggers(SCHED_NAME,TRIGGER_NAME,TRIGGER_GROUP);
 create index idx_qrtz_ft_tg on qrtz_fired_triggers(SCHED_NAME,TRIGGER_GROUP);
-
-commit;
