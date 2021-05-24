fddd755054c1416003960bd16938ca94f14888eb
Richard Richter <richard.richter@evolveum.com>
2021-05-24 20:07:38 +0200

    *FilterImpl: toString fixes/enhancements, cleanup, some generics

--- infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/query/LogicalFilterImpl.java
+++ infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/query/LogicalFilterImpl.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2010-2018 Evolveum and contributors
+ * Copyright (C) 2010-2021 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
  */
-
 package com.evolveum.midpoint.prism.impl.query;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+
 import com.evolveum.midpoint.prism.query.LogicalFilter;
 import com.evolveum.midpoint.prism.query.ObjectFilter;
 import com.evolveum.midpoint.prism.query.Visitor;
 import com.evolveum.midpoint.util.DebugUtil;
-import com.google.common.collect.ImmutableList;
 
 public abstract class LogicalFilterImpl extends ObjectFilterImpl implements LogicalFilter {
 
@@ -22,7 +22,7 @@ public abstract class LogicalFilterImpl extends ObjectFilterImpl implements Logi
 
     @Override
     public List<ObjectFilter> getConditions() {
-        if (conditions == null){
+        if (conditions == null) {
             conditions = new ArrayList<>();
         }
         return conditions;
@@ -64,7 +64,7 @@ public abstract class LogicalFilterImpl extends ObjectFilterImpl implements Logi
             return null;
         }
         List<ObjectFilter> clonedConditions = new ArrayList<>(conditions.size());
-        for (ObjectFilter condition: conditions) {
+        for (ObjectFilter condition : conditions) {
             clonedConditions.add(condition.clone());
         }
         return clonedConditions;
@@ -78,14 +78,14 @@ public abstract class LogicalFilterImpl extends ObjectFilterImpl implements Logi
     @Override
     public void checkConsistence(boolean requireDefinitions) {
         if (conditions == null) {
-            throw new IllegalArgumentException("Null conditions in "+this);
+            throw new IllegalArgumentException("Null conditions in " + this);
         }
         if (conditions.isEmpty()) {
-            throw new IllegalArgumentException("Empty conditions in "+this);
+            throw new IllegalArgumentException("Empty conditions in " + this);
         }
-        for (ObjectFilter condition: conditions) {
+        for (ObjectFilter condition : conditions) {
             if (condition == null) {
-                throw new IllegalArgumentException("Null subfilter in "+this);
+                throw new IllegalArgumentException("Null subfilter in " + this);
             }
             condition.checkConsistence(requireDefinitions);
         }
@@ -94,7 +94,7 @@ public abstract class LogicalFilterImpl extends ObjectFilterImpl implements Logi
     @Override
     public void accept(Visitor visitor) {
         super.accept(visitor);
-        for (ObjectFilter condition: getConditions()) {
+        for (ObjectFilter condition : getConditions()) {
             condition.accept(visitor);
         }
     }
@@ -115,9 +115,12 @@ public abstract class LogicalFilterImpl extends ObjectFilterImpl implements Logi
 
     @Override
     public boolean equals(Object obj, boolean exact) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
         LogicalFilterImpl other = (LogicalFilterImpl) obj;
         if (conditions != null) {
             if (conditions.size() != other.conditions.size()) {
@@ -139,7 +142,7 @@ public abstract class LogicalFilterImpl extends ObjectFilterImpl implements Logi
         StringBuilder sb = new StringBuilder();
         DebugUtil.indentDebugDump(sb, indent);
         sb.append(getDebugDumpOperationName()).append(":");
-        for (ObjectFilter filter : getConditions()){
+        for (ObjectFilter filter : getConditions()) {
             sb.append("\n");
             sb.append(filter.debugDump(indent + 1));
         }
@@ -151,10 +154,10 @@ public abstract class LogicalFilterImpl extends ObjectFilterImpl implements Logi
         StringBuilder sb = new StringBuilder();
         sb.append(getDebugDumpOperationName());
         sb.append("(");
-        for (int i = 0; i < getConditions().size(); i++){
+        for (int i = 0; i < getConditions().size(); i++) {
             sb.append(getConditions().get(i));
             if (i != getConditions().size() - 1) {
-                sb.append(",");
+                sb.append("; "); // clearer separation than with ',' used in value filters
             }
         }
         sb.append(")");
