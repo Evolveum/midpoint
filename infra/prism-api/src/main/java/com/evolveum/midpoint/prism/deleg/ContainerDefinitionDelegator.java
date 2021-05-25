4a3912a57dce8979c8b9791047990758136d3ef5
Tony Tkacik <tonydamage@gmail.com>
2021-05-25 19:22:07 +0200

    Prism: Added Delegator interfaces
    Delegator interfaces delegates most of the calls to delegate(),
    useful for implementation of wrappers which modifies only few items.

---
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/deleg/ContainerDefinitionDelegator.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2021 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.prism.deleg;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import javax.xml.namespace.QName;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.evolveum.midpoint.prism.ComplexTypeDefinition;
+import com.evolveum.midpoint.prism.Containerable;
+import com.evolveum.midpoint.prism.ItemDefinition;
+import com.evolveum.midpoint.prism.PrismContainer;
+import com.evolveum.midpoint.prism.PrismContainerDefinition;
+import com.evolveum.midpoint.prism.PrismContainerValue;
+import com.evolveum.midpoint.prism.PrismPropertyDefinition;
+import com.evolveum.midpoint.prism.PrismReferenceDefinition;
+import com.evolveum.midpoint.prism.delta.ContainerDelta;
+import com.evolveum.midpoint.prism.path.ItemName;
+import com.evolveum.midpoint.prism.path.ItemPath;
+
+public interface ContainerDefinitionDelegator<C extends Containerable> extends ItemDefinitionDelegator<PrismContainer<C>>, PrismContainerDefinition<C> {
+
+    @Override
+    PrismContainerDefinition<C> delegate();
+
+    @Override
+    default Class<C> getCompileTimeClass() {
+        return delegate().getCompileTimeClass();
+    }
+
+    @Override
+    default ComplexTypeDefinition getComplexTypeDefinition() {
+        return delegate().getComplexTypeDefinition();
+    }
+
+    @Override
+    default String getDefaultNamespace() {
+        return delegate().getDefaultNamespace();
+    }
+
+    @Override
+    default List<String> getIgnoredNamespaces() {
+        return delegate().getIgnoredNamespaces();
+    }
+
+    @Override
+    default List<? extends ItemDefinition> getDefinitions() {
+        return delegate().getDefinitions();
+    }
+
+    @Override
+    default Collection<ItemName> getItemNames() {
+        return delegate().getItemNames();
+    }
+
+    @Override
+    default <ID extends ItemDefinition> ID findLocalItemDefinition(@NotNull QName name, @NotNull Class<ID> clazz,
+            boolean caseInsensitive) {
+        return delegate().findLocalItemDefinition(name, clazz, caseInsensitive);
+    }
+
+    @Override
+    default <ID extends ItemDefinition> ID findLocalItemDefinition(@NotNull QName name) {
+        return delegate().findLocalItemDefinition(name);
+    }
+
+    @Override
+    default boolean isCompletelyDefined() {
+        return delegate().isCompletelyDefined();
+    }
+
+    @Override
+    default List<PrismPropertyDefinition> getPropertyDefinitions() {
+        return delegate().getPropertyDefinitions();
+    }
+
+    @Override
+    default <ID extends ItemDefinition> ID findItemDefinition(@NotNull ItemPath path) {
+        return delegate().findItemDefinition(path);
+    }
+
+    @Override
+    default PrismReferenceDefinition findReferenceDefinition(@NotNull ItemName name) {
+        return delegate().findReferenceDefinition(name);
+    }
+
+    @Override
+    default PrismContainerDefinition<C> cloneWithReplacedDefinition(QName itemName, ItemDefinition newDefinition) {
+        return delegate().cloneWithReplacedDefinition(itemName, newDefinition);
+    }
+
+    @Override
+    default void replaceDefinition(QName itemName, ItemDefinition newDefinition) {
+        delegate().replaceDefinition(itemName, newDefinition);
+    }
+
+    @Override
+    default PrismContainerValue<C> createValue() {
+        return delegate().createValue();
+    }
+
+    @Override
+    default <C extends Containerable> PrismContainerDefinition<C> findContainerDefinition(@NotNull String name) {
+        return delegate().findContainerDefinition(name);
+    }
+
+    @Override
+    default boolean isEmpty() {
+        return delegate().isEmpty();
+    }
+
+    @Override
+    default boolean canRepresent(@NotNull QName type) {
+        return delegate().canRepresent(type);
+    }
+
+    @Override
+    default <ID extends ItemDefinition> ID findNamedItemDefinition(@NotNull QName firstName, @NotNull ItemPath rest,
+            @NotNull Class<ID> clazz) {
+        return delegate().findNamedItemDefinition(firstName, rest, clazz);
+    }
+
+    @Override
+    default <T> PrismPropertyDefinition<T> findPropertyDefinition(@NotNull ItemPath path) {
+        return delegate().findPropertyDefinition(path);
+    }
+
+    @Override
+    default PrismReferenceDefinition findReferenceDefinition(@NotNull ItemPath path) {
+        return delegate().findReferenceDefinition(path);
+    }
+
+    @Override
+    default <C extends Containerable> PrismContainerDefinition<C> findContainerDefinition(@NotNull ItemPath path) {
+        return delegate().findContainerDefinition(path);
+    }
+
+    @Override
+    default ContainerDelta<C> createEmptyDelta(ItemPath path) {
+        return delegate().createEmptyDelta(path);
+    }
+
+    @Override
+    default Class<C> getTypeClass() {
+        return delegate().getTypeClass();
+    }
+
+    @Override
+    default Optional<ComplexTypeDefinition> structuredType() {
+        return delegate().structuredType();
+    }
+
+}
---
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/deleg/ItemDefinitionDelegator.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2021 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.prism.deleg;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import javax.xml.namespace.QName;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.evolveum.midpoint.prism.ComplexTypeDefinition;
+import com.evolveum.midpoint.prism.Item;
+import com.evolveum.midpoint.prism.ItemDefinition;
+import com.evolveum.midpoint.prism.PrismReferenceValue;
+import com.evolveum.midpoint.prism.PrismValue;
+import com.evolveum.midpoint.prism.delta.ItemDelta;
+import com.evolveum.midpoint.prism.path.ItemName;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.util.exception.SchemaException;
+
+public interface ItemDefinitionDelegator<I extends Item<?,?>> extends DefinitionDelegator, ItemDefinition<I> {
+
+    @Override
+    ItemDefinition<I> delegate();
+
+    @Override
+    default boolean canRead() {
+        return delegate().canRead();
+    }
+
+    @Override
+    default @NotNull ItemName getItemName() {
+        return delegate().getItemName();
+    }
+
+    @Override
+    default String getNamespace() {
+        return delegate().getNamespace();
+    }
+
+    @Override
+    default int getMinOccurs() {
+        return delegate().getMinOccurs();
+    }
+
+    @Override
+    default int getMaxOccurs() {
+        return delegate().getMaxOccurs();
+    }
+
+    @Override
+    default boolean isSingleValue() {
+        return delegate().isSingleValue();
+    }
+
+    @Override
+    default boolean isMultiValue() {
+        return delegate().isMultiValue();
+    }
+
+    @Override
+    default boolean isMandatory() {
+        return delegate().isMandatory();
+    }
+
+    @Override
+    default boolean isOptional() {
+        return delegate().isOptional();
+    }
+
+    @Override
+    default boolean isOperational() {
+        return delegate().isOperational();
+    }
+
+    @Override
+    default boolean isIndexOnly() {
+        return delegate().isIndexOnly();
+    }
+
+    @Override
+    default boolean canModify() {
+        return delegate().canModify();
+    }
+
+    @Override
+    default boolean isInherited() {
+        return delegate().isInherited();
+    }
+
+    @Override
+    default boolean isDynamic() {
+        return delegate().isDynamic();
+    }
+
+    @Override
+    default boolean canAdd() {
+        return delegate().canAdd();
+    }
+
+    @Override
+    default QName getSubstitutionHead() {
+        return delegate().getSubstitutionHead();
+    }
+
+    @Override
+    default boolean isHeterogeneousListItem() {
+        return delegate().isHeterogeneousListItem();
+    }
+
+    @Override
+    default PrismReferenceValue getValueEnumerationRef() {
+        return delegate().getValueEnumerationRef();
+    }
+
+    @Override
+    default boolean isValidFor(QName elementQName, Class<? extends ItemDefinition> clazz) {
+        return delegate().isValidFor(elementQName, clazz);
+    }
+
+    @Override
+    default boolean isValidFor(@NotNull QName elementQName, @NotNull Class<? extends ItemDefinition> clazz,
+            boolean caseInsensitive) {
+        return delegate().isValidFor(elementQName, clazz, caseInsensitive);
+    }
+
+    @Override
+    default void adoptElementDefinitionFrom(ItemDefinition otherDef) {
+        delegate().adoptElementDefinitionFrom(otherDef);
+    }
+
+    @Override
+    default @NotNull I instantiate() throws SchemaException {
+        return delegate().instantiate();
+    }
+
+    @Override
+    default @NotNull I instantiate(QName name) throws SchemaException {
+        return delegate().instantiate(name);
+    }
+
+    @Override
+    default <T extends ItemDefinition> T findItemDefinition(@NotNull ItemPath path, @NotNull Class<T> clazz) {
+        if (path.isEmpty()) {
+            if (clazz.isAssignableFrom(this.getClass())) {
+                return (T) this;
+            } else {
+                throw new IllegalArgumentException("Looking for definition of class " + clazz + " but found " + this);
+            }
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    default ItemDelta createEmptyDelta(ItemPath path) {
+        return delegate().createEmptyDelta(path);
+    }
+
+    @Override
+    default ItemDefinition<I> deepClone(boolean ultraDeep, Consumer<ItemDefinition> postCloneAction) {
+        return delegate().deepClone(ultraDeep, postCloneAction);
+    }
+
+    @Override
+    default ItemDefinition<I> deepClone(Map<QName, ComplexTypeDefinition> ctdMap,
+            Map<QName, ComplexTypeDefinition> onThisPath, Consumer<ItemDefinition> postCloneAction) {
+        return delegate().deepClone(ctdMap, onThisPath, postCloneAction);
+    }
+
+    @Override
+    default void debugDumpShortToString(StringBuilder sb) {
+        delegate().debugDumpShortToString(sb);
+    }
+
+    @Override
+    default boolean canBeDefinitionOf(I item) {
+        return delegate().canBeDefinitionOf(item);
+    }
+
+    @Override
+    default boolean canBeDefinitionOf(PrismValue pvalue) {
+        return delegate().canBeDefinitionOf(pvalue);
+    }
+
+    @Override
+    default Optional<ComplexTypeDefinition> structuredType() {
+        return delegate().structuredType();
+    }
+
+
+}
