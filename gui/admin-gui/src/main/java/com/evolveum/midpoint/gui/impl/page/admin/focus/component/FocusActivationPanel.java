b1f6a1c34add9b40423398b02e2ad7e6952423ad
kate <honchar@evolveum.com>
2021-09-17 15:45:43 +0200

    activation panel for focus type details page

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/focus/component/FocusActivationPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/focus/component/FocusActivationPanel.java
@@ -6,6 +6,8 @@
  */
 package com.evolveum.midpoint.gui.impl.page.admin.focus.component;
 
+import org.apache.wicket.model.IModel;
+
 import com.evolveum.midpoint.gui.api.prism.wrapper.PrismObjectWrapper;
 import com.evolveum.midpoint.gui.impl.page.admin.AbstractObjectMainPanel;
 import com.evolveum.midpoint.gui.impl.page.admin.assignmentholder.FocusDetailsModels;
@@ -21,21 +23,19 @@ import com.evolveum.midpoint.web.application.PanelType;
 import com.evolveum.midpoint.web.model.PrismContainerWrapperModel;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 
-import org.apache.wicket.model.IModel;
-
-@PanelType(name = "password")
-@PanelInstance(identifier = "password",
-        applicableForType = UserType.class,
-        display = @PanelDisplay(label = "prismPropertyPanel.name.credentials.password", order = 10))
-public class FocusPasswordPanel<F extends FocusType, FDM extends FocusDetailsModels<F>> extends AbstractObjectMainPanel<F, FDM> {
+@PanelType(name = "activation")
+@PanelInstance(identifier = "activation",
+        applicableForType = FocusType.class,
+        display = @PanelDisplay(label = "ActivationType.title", order = 20))
+public class FocusActivationPanel<F extends FocusType, FDM extends FocusDetailsModels<F>> extends AbstractObjectMainPanel<F, FDM> {
     private static final long serialVersionUID = 1L;
 
-    private static final Trace LOGGER = TraceManager.getTrace(FocusPasswordPanel.class);
+    private static final Trace LOGGER = TraceManager.getTrace(FocusActivationPanel.class);
     private static final String ID_PANEL = "panel";
 
-    private static final String DOT_CLASS = FocusPasswordPanel.class.getName() + ".";
+    private static final String DOT_CLASS = FocusActivationPanel.class.getName() + ".";
 
-    public FocusPasswordPanel(String id, FDM model, ContainerPanelConfigurationType config) {
+    public FocusActivationPanel(String id, FDM model, ContainerPanelConfigurationType config) {
         super(id, model, config);
     }
 
@@ -43,7 +43,7 @@ public class FocusPasswordPanel<F extends FocusType, FDM extends FocusDetailsMod
     protected void initLayout() {
         SingleContainerPanel panel =
                 new SingleContainerPanel<>(ID_PANEL,
-                        PrismContainerWrapperModel.fromContainerWrapper(getObjectWrapperModel(), ItemPath.create(FocusType.F_CREDENTIALS, CredentialsType.F_PASSWORD)),
+                        PrismContainerWrapperModel.fromContainerWrapper(getObjectWrapperModel(), FocusType.F_ACTIVATION),
                         PasswordType.COMPLEX_TYPE);
         add(panel);
     }
