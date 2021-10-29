ff6696788b1793073105342b480812d860c2d815
Katarina Valalikova <k.valalikova@evolveum.com>
2021-10-29 11:25:57 +0200

    helper method for overlay

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/AbstractObjectMainPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/AbstractObjectMainPanel.java
@@ -11,11 +11,17 @@ import com.evolveum.midpoint.gui.api.page.PageBase;
 import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerWrapper;
 import com.evolveum.midpoint.gui.api.prism.wrapper.PrismObjectWrapper;
 import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
+import com.evolveum.midpoint.gui.impl.prism.panel.ItemPanelSettingsBuilder;
 import com.evolveum.midpoint.prism.Containerable;
 import com.evolveum.midpoint.prism.PrismContext;
 import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.prism.polystring.PolyString;
+import com.evolveum.midpoint.util.exception.SchemaException;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
+import com.evolveum.midpoint.web.component.prism.ItemVisibility;
 import com.evolveum.midpoint.web.model.PrismContainerWrapperModel;
+import com.evolveum.midpoint.web.model.PrismPropertyWrapperModel;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ContainerPanelConfigurationType;
 
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
@@ -23,6 +29,7 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
 import com.evolveum.prism.xml.ns._public.types_3.PolyStringType;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.StringResourceModel;
@@ -30,6 +37,8 @@ import org.apache.wicket.model.StringResourceModel;
 import javax.xml.namespace.QName;
 
 public abstract class AbstractObjectMainPanel<O extends ObjectType, M extends ObjectDetailsModels<O>> extends Panel {
+
+    private static final Trace LOGGER = TraceManager.getTrace(AbstractObjectMainPanel.class);
 
     private ContainerPanelConfigurationType panelConfiguration;
     private M objectDetailsModels;
@@ -128,5 +137,30 @@ public abstract class AbstractObjectMainPanel<O extends ObjectType, M extends Ob
         }
 
         return createStringResource(sb.toString());
+    }
+
+    /**
+     * Used in overlay
+     * @param parentComponent
+     * @param id
+     * @param typeName
+     * @param propertyPath
+     * @return
+     */
+    protected Panel addPrismPropertyPanel(MarkupContainer parentComponent, String id, QName typeName, ItemPath propertyPath) {
+
+        try {
+            ItemPanelSettingsBuilder settingsBuilder = new ItemPanelSettingsBuilder();
+            settingsBuilder.visibilityHandler(wrapper -> ItemVisibility.AUTO);
+
+            Panel panel = getPageBase().initItemPanel(id, typeName, PrismPropertyWrapperModel.fromContainerWrapper(getObjectWrapperModel(), propertyPath), settingsBuilder.build());
+            parentComponent.add(panel);
+            return panel;
+        } catch (SchemaException e) {
+            LOGGER.error("Cannot create panel for {}", typeName, e);
+            getSession().error("Cannot create panel for " + typeName + ", reason: " + e.getMessage());
+        }
+
+        return null;
     }
 }
