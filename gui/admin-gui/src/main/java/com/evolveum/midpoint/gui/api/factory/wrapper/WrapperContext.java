dcce4232d729d8a6fe1475829cbba9621550943f
Katarina Valalikova <k.valalikova@evolveum.com>
2021-11-22 13:55:38 +0100

    removing @NotNull annotation since obviously it can be null and rather adding checks for null

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/factory/wrapper/WrapperContext.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/factory/wrapper/WrapperContext.java
@@ -181,7 +181,6 @@ public class WrapperContext {
         return metadataItemProcessingSpec.isFullProcessing(path);
     }
 
-    @NotNull
     public PrismObject<?> getObject() {
         return object;
     }
