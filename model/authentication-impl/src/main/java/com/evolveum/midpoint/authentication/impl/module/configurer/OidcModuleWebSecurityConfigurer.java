a47377abfcbfbb603696be0a516819c17151d889
lskublik <lskublik@evolveum.com>
2022-01-13 09:41:50 +0100

    cleanup code for remote auth module configurer (MID-7488)

--- model/authentication-impl/src/main/java/com/evolveum/midpoint/authentication/impl/module/configurer/OidcModuleWebSecurityConfigurer.java
+++ model/authentication-impl/src/main/java/com/evolveum/midpoint/authentication/impl/module/configurer/OidcModuleWebSecurityConfigurer.java
@@ -7,62 +7,40 @@
 
 package com.evolveum.midpoint.authentication.impl.module.configurer;
 
-import com.evolveum.midpoint.authentication.api.config.MidpointAuthentication;
-import com.evolveum.midpoint.authentication.api.config.ModuleAuthentication;
 import com.evolveum.midpoint.authentication.api.util.AuthUtil;
 import com.evolveum.midpoint.authentication.impl.entry.point.WicketLoginUrlAuthenticationEntryPoint;
-import com.evolveum.midpoint.authentication.impl.factory.channel.AuthChannelRegistryImpl;
-import com.evolveum.midpoint.authentication.impl.factory.module.AuthModuleRegistryImpl;
-import com.evolveum.midpoint.authentication.impl.filter.MidpointAnonymousAuthenticationFilter;
-import com.evolveum.midpoint.authentication.impl.filter.configurers.MidpointExceptionHandlingConfigurer;
 import com.evolveum.midpoint.authentication.impl.handler.MidPointAuthenticationSuccessHandler;
 import com.evolveum.midpoint.authentication.impl.handler.MidpointAuthenticationFailureHandler;
-import com.evolveum.midpoint.authentication.impl.module.authentication.ModuleAuthenticationImpl;
 import com.evolveum.midpoint.authentication.impl.module.authentication.RemoteModuleAuthenticationImpl;
 import com.evolveum.midpoint.authentication.impl.module.configuration.OidcModuleWebSecurityConfiguration;
 import com.evolveum.midpoint.authentication.impl.oidc.OidcClientLogoutSuccessHandler;
 import com.evolveum.midpoint.authentication.impl.oidc.OidcLoginConfigurer;
 import com.evolveum.midpoint.model.api.ModelAuditRecorder;
-import com.evolveum.midpoint.prism.PrismContext;
 import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
-import java.util.UUID;
 
 /**
  * @author skublik
  */
 
-public class OidcModuleWebSecurityConfigurer<C extends OidcModuleWebSecurityConfiguration> extends ModuleWebSecurityConfigurer<C> {
+public class OidcModuleWebSecurityConfigurer<C extends OidcModuleWebSecurityConfiguration> extends RemoteModuleWebSecurityConfigurer<C> {
 
     private static final Trace LOGGER = TraceManager.getTrace(OidcModuleWebSecurityConfigurer.class);
     public static final String OIDC_LOGIN_PATH = "/oidc/select";
 
     @Autowired
     private ModelAuditRecorder auditProvider;
-
-    @Autowired
-    private AuthModuleRegistryImpl authRegistry;
-
-    @Autowired
-    private AuthChannelRegistryImpl authChannelRegistry;
 
     public OidcModuleWebSecurityConfigurer(C configuration) {
         super(configuration);
@@ -71,21 +49,6 @@ public class OidcModuleWebSecurityConfigurer<C extends OidcModuleWebSecurityConf
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-
-        http.antMatcher(AuthUtil.stripEndingSlashes(getPrefix()) + "/**");
-        http.csrf().disable();
-
-        MidpointExceptionHandlingConfigurer exceptionConfigurer = new MidpointExceptionHandlingConfigurer() {
-            @Override
-            protected Authentication createNewAuthentication(AnonymousAuthenticationToken anonymousAuthenticationToken) {
-                if (anonymousAuthenticationToken.getDetails() instanceof OAuth2LoginAuthenticationToken) {
-                    return (OAuth2LoginAuthenticationToken) anonymousAuthenticationToken.getDetails();
-                }
-                return null;
-            }
-        };
-        getOrApply(http, exceptionConfigurer)
-                .authenticationEntryPoint(new WicketLoginUrlAuthenticationEntryPoint(OIDC_LOGIN_PATH));
 
         OidcLoginConfigurer configurer = new OidcLoginConfigurer(auditProvider);
         configurer.midpointFailureHandler(new MidpointAuthenticationFailureHandler())
@@ -102,14 +65,18 @@ public class OidcModuleWebSecurityConfigurer<C extends OidcModuleWebSecurityConf
             LOGGER.error("Couldn't initialize authentication manager for saml2 module");
         }
         getOrApply(http, configurer);
+    }
 
+    @Override
+    protected AuthenticationEntryPoint getAuthEntryPoint() {
+        return new WicketLoginUrlAuthenticationEntryPoint(OIDC_LOGIN_PATH);
+    }
+
+    @Override
+    protected LogoutSuccessHandler getLogoutRequestSuccessHandler() {
         OidcClientLogoutSuccessHandler logoutRequestSuccessHandler = new OidcClientLogoutSuccessHandler(clientRegistrationRepository());
         logoutRequestSuccessHandler.setPostLogoutRedirectUri(getConfiguration().getPrefixOfSequence());
-        http.logout().clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher(getPrefix() + "/logout"))
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessHandler(logoutRequestSuccessHandler);
+        return logoutRequestSuccessHandler;
     }
 
     private InMemoryClientRegistrationRepository clientRegistrationRepository() {
@@ -117,44 +84,7 @@ public class OidcModuleWebSecurityConfigurer<C extends OidcModuleWebSecurityConf
     }
 
     @Override
-    protected AnonymousAuthenticationFilter createAnonymousFilter() {
-        AnonymousAuthenticationFilter filter = new MidpointAnonymousAuthenticationFilter(authRegistry, authChannelRegistry, PrismContext.get(),
-                UUID.randomUUID().toString(), "anonymousUser",
-                AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS")){
-            @Override
-            protected void processAuthentication(ServletRequest req) {
-                if (SecurityContextHolder.getContext().getAuthentication() instanceof MidpointAuthentication) {
-                    MidpointAuthentication mpAuthentication = (MidpointAuthentication) SecurityContextHolder.getContext().getAuthentication();
-                    ModuleAuthenticationImpl moduleAuthentication = (ModuleAuthenticationImpl) mpAuthentication.getProcessingModuleAuthentication();
-                    if (moduleAuthentication != null
-                            && (moduleAuthentication.getAuthentication() == null
-                            || moduleAuthentication.getAuthentication() instanceof OAuth2LoginAuthenticationToken)) {
-                        Authentication authentication = createBasicAuthentication((HttpServletRequest) req);
-                        moduleAuthentication.setAuthentication(authentication);
-                        mpAuthentication.setPrincipal(authentication.getPrincipal());
-                    }
-                }
-            }
-        };
-
-        filter.setAuthenticationDetailsSource(new OidcAuthenticationDetailsSource());
-        return filter;
-    }
-
-    private static class OidcAuthenticationDetailsSource implements AuthenticationDetailsSource<HttpServletRequest, Object> {
-
-        private final WebAuthenticationDetailsSource detailsSource = new WebAuthenticationDetailsSource();
-
-        @Override
-        public Object buildDetails(HttpServletRequest context) {
-            if (SecurityContextHolder.getContext().getAuthentication() instanceof MidpointAuthentication) {
-                MidpointAuthentication mpAuthentication = (MidpointAuthentication) SecurityContextHolder.getContext().getAuthentication();
-                ModuleAuthentication moduleAuthentication = mpAuthentication.getProcessingModuleAuthentication();
-                if (moduleAuthentication != null && moduleAuthentication.getAuthentication() instanceof OAuth2LoginAuthenticationToken) {
-                    return moduleAuthentication.getAuthentication();
-                }
-            }
-            return detailsSource.buildDetails(context);
-        }
+    protected Class<? extends Authentication> getAuthTokenClass() {
+        return OAuth2LoginAuthenticationToken.class;
     }
 }
--- model/authentication-impl/src/main/java/com/evolveum/midpoint/authentication/impl/module/configurer/SamlModuleWebSecurityConfigurer.java
+++ model/authentication-impl/src/main/java/com/evolveum/midpoint/authentication/impl/module/configurer/SamlModuleWebSecurityConfigurer.java
@@ -8,38 +8,20 @@
 package com.evolveum.midpoint.authentication.impl.module.configurer;
 
 import java.util.Collections;
-import java.util.UUID;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 
-import com.evolveum.midpoint.authentication.impl.factory.channel.AuthChannelRegistryImpl;
-import com.evolveum.midpoint.authentication.impl.factory.module.AuthModuleRegistryImpl;
 import com.evolveum.midpoint.authentication.impl.handler.MidPointAuthenticationSuccessHandler;
 import com.evolveum.midpoint.authentication.impl.handler.MidpointAuthenticationFailureHandler;
-import com.evolveum.midpoint.authentication.impl.module.authentication.ModuleAuthenticationImpl;
 import com.evolveum.midpoint.authentication.impl.entry.point.SamlAuthenticationEntryPoint;
-import com.evolveum.midpoint.authentication.impl.filter.MidpointAnonymousAuthenticationFilter;
-import com.evolveum.midpoint.authentication.impl.filter.configurers.MidpointExceptionHandlingConfigurer;
 import com.evolveum.midpoint.authentication.impl.saml.MidpointMetadataRelyingPartyRegistrationResolver;
 import com.evolveum.midpoint.authentication.impl.saml.MidpointSaml2LoginConfigurer;
 import com.evolveum.midpoint.authentication.impl.saml.MidpointSaml2LogoutRequestResolver;
 import com.evolveum.midpoint.authentication.impl.saml.MidpointSaml2LogoutRequestSuccessHandler;
-import com.evolveum.midpoint.authentication.api.util.AuthUtil;
-import com.evolveum.midpoint.authentication.api.config.MidpointAuthentication;
 import com.evolveum.midpoint.authentication.impl.module.configuration.SamlModuleWebSecurityConfiguration;
 
-import com.evolveum.midpoint.authentication.api.config.ModuleAuthentication;
-
-import com.evolveum.midpoint.prism.PrismContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticationToken;
 import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
 import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
@@ -50,8 +32,7 @@ import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilt
 import org.springframework.security.saml2.provider.service.web.authentication.logout.OpenSaml4LogoutRequestResolver;
 import org.springframework.security.saml2.provider.service.web.authentication.logout.Saml2LogoutRequestResolver;
 import org.springframework.security.saml2.provider.service.web.authentication.logout.Saml2RelyingPartyInitiatedLogoutSuccessHandler;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -63,19 +44,13 @@ import com.evolveum.midpoint.util.logging.TraceManager;
  * @author skublik
  */
 
-public class SamlModuleWebSecurityConfigurer<C extends SamlModuleWebSecurityConfiguration> extends ModuleWebSecurityConfigurer<C> {
+public class SamlModuleWebSecurityConfigurer<C extends SamlModuleWebSecurityConfiguration> extends RemoteModuleWebSecurityConfigurer<C> {
 
     private static final Trace LOGGER = TraceManager.getTrace(SamlModuleWebSecurityConfigurer.class);
     public static final String SAML_LOGIN_PATH = "/saml2/select";
 
     @Autowired
     private ModelAuditRecorder auditProvider;
-
-    @Autowired
-    private AuthModuleRegistryImpl authRegistry;
-
-    @Autowired
-    private AuthChannelRegistryImpl authChannelRegistry;
 
     public SamlModuleWebSecurityConfigurer(C configuration) {
         super(configuration);
@@ -84,21 +59,6 @@ public class SamlModuleWebSecurityConfigurer<C extends SamlModuleWebSecurityConf
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-
-        http.antMatcher(AuthUtil.stripEndingSlashes(getPrefix()) + "/**");
-        http.csrf().disable();
-
-        MidpointExceptionHandlingConfigurer exceptionConfigurer = new MidpointExceptionHandlingConfigurer() {
-            @Override
-            protected Authentication createNewAuthentication(AnonymousAuthenticationToken anonymousAuthenticationToken) {
-                if (anonymousAuthenticationToken.getDetails() instanceof Saml2AuthenticationToken) {
-                    return (Saml2AuthenticationToken) anonymousAuthenticationToken.getDetails();
-                }
-                return null;
-            }
-        };
-        getOrApply(http, exceptionConfigurer)
-                .authenticationEntryPoint(new SamlAuthenticationEntryPoint(SAML_LOGIN_PATH));
 
         MidpointSaml2LoginConfigurer configurer = new MidpointSaml2LoginConfigurer<>(auditProvider);
         configurer.relyingPartyRegistrationRepository(relyingPartyRegistrations())
@@ -113,15 +73,6 @@ public class SamlModuleWebSecurityConfigurer<C extends SamlModuleWebSecurityConf
         }
         getOrApply(http, configurer);
 
-        RelyingPartyRegistrationResolver registrationResolver = new DefaultRelyingPartyRegistrationResolver(relyingPartyRegistrations());
-        LogoutSuccessHandler logoutRequestSuccessHandler = logoutRequestSuccessHandler(registrationResolver);
-
-        http.logout().clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher(getPrefix() + "/logout"))
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessHandler(logoutRequestSuccessHandler);
-
         Saml2MetadataFilter filter = new Saml2MetadataFilter(new MidpointMetadataRelyingPartyRegistrationResolver(relyingPartyRegistrations()),
                 new OpenSamlMetadataResolver());
         filter.setRequestMatcher(new AntPathRequestMatcher( getConfiguration().getPrefixOfModule() + "/metadata/*"));
@@ -129,56 +80,26 @@ public class SamlModuleWebSecurityConfigurer<C extends SamlModuleWebSecurityConf
     }
 
     @Override
-    protected AnonymousAuthenticationFilter createAnonymousFilter() {
-        AnonymousAuthenticationFilter filter = new MidpointAnonymousAuthenticationFilter(authRegistry, authChannelRegistry, PrismContext.get(),
-                UUID.randomUUID().toString(), "anonymousUser",
-                AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS")){
-            @Override
-            protected void processAuthentication(ServletRequest req) {
-                if (SecurityContextHolder.getContext().getAuthentication() instanceof MidpointAuthentication) {
-                    MidpointAuthentication mpAuthentication = (MidpointAuthentication) SecurityContextHolder.getContext().getAuthentication();
-                    ModuleAuthenticationImpl moduleAuthentication = (ModuleAuthenticationImpl) mpAuthentication.getProcessingModuleAuthentication();
-                    if (moduleAuthentication != null
-                            && (moduleAuthentication.getAuthentication() == null
-                            || moduleAuthentication.getAuthentication() instanceof Saml2AuthenticationToken)) {
-                        Authentication authentication = createBasicAuthentication((HttpServletRequest) req);
-                        moduleAuthentication.setAuthentication(authentication);
-                        mpAuthentication.setPrincipal(authentication.getPrincipal());
-                    }
-                }
-            }
-        };
-
-        filter.setAuthenticationDetailsSource(new SamlAuthenticationDetailsSource());
-        return filter;
+    protected AuthenticationEntryPoint getAuthEntryPoint() {
+        return new SamlAuthenticationEntryPoint(SAML_LOGIN_PATH);
     }
 
-    private InMemoryRelyingPartyRegistrationRepository relyingPartyRegistrations() {
-        return getConfiguration().getRelyingPartyRegistrationRepository();
-    }
-
-    private static class SamlAuthenticationDetailsSource implements AuthenticationDetailsSource<HttpServletRequest, Object> {
-
-        private final WebAuthenticationDetailsSource detailsSource = new WebAuthenticationDetailsSource();
-
-        @Override
-        public Object buildDetails(HttpServletRequest context) {
-            if (SecurityContextHolder.getContext().getAuthentication() instanceof MidpointAuthentication) {
-                MidpointAuthentication mpAuthentication = (MidpointAuthentication) SecurityContextHolder.getContext().getAuthentication();
-                ModuleAuthentication moduleAuthentication = mpAuthentication.getProcessingModuleAuthentication();
-                if (moduleAuthentication != null && moduleAuthentication.getAuthentication() instanceof Saml2AuthenticationToken) {
-                    return moduleAuthentication.getAuthentication();
-                }
-            }
-            return detailsSource.buildDetails(context);
-        }
-    }
-
-    private LogoutSuccessHandler logoutRequestSuccessHandler(RelyingPartyRegistrationResolver registrationResolver) {
+    @Override
+    protected LogoutSuccessHandler getLogoutRequestSuccessHandler() {
+        RelyingPartyRegistrationResolver registrationResolver = new DefaultRelyingPartyRegistrationResolver(relyingPartyRegistrations());
         Saml2LogoutRequestResolver logoutRequestResolver = new MidpointSaml2LogoutRequestResolver(
                 new OpenSaml4LogoutRequestResolver(registrationResolver));
         Saml2RelyingPartyInitiatedLogoutSuccessHandler handler = new Saml2RelyingPartyInitiatedLogoutSuccessHandler(logoutRequestResolver);
         return getObjectPostProcessor().postProcess(new MidpointSaml2LogoutRequestSuccessHandler(
                 handler));
+    }
+
+    @Override
+    protected Class<? extends Authentication> getAuthTokenClass() {
+        return Saml2AuthenticationToken.class;
+    }
+
+    private InMemoryRelyingPartyRegistrationRepository relyingPartyRegistrations() {
+        return getConfiguration().getRelyingPartyRegistrationRepository();
     }
 }
