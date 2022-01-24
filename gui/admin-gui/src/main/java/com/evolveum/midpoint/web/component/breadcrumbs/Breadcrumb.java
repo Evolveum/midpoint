c5b6e130faab9707033e6c22988fdd0df4b14685
Viliam Repan <vilo.repan@evolveum.com>
2022-01-24 16:53:36 +0100

    MID-7502 improving breadcrumbs, removed transiency for icon/label models (wrong) and started caching values

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/breadcrumbs/Breadcrumb.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/breadcrumbs/Breadcrumb.java
@@ -7,18 +7,18 @@
 
 package com.evolveum.midpoint.web.component.breadcrumbs;
 
-import java.io.Serializable;
-import java.util.Arrays;
+import com.evolveum.midpoint.util.DebugDumpable;
+import com.evolveum.midpoint.util.DebugUtil;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
 
 import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.evolveum.midpoint.util.DebugDumpable;
-import com.evolveum.midpoint.util.DebugUtil;
-import com.evolveum.midpoint.util.logging.Trace;
-import com.evolveum.midpoint.util.logging.TraceManager;
+import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * Breadcrumb object that is stored in the session. It represents the way "back" to the main menu.
@@ -37,10 +37,9 @@ public class Breadcrumb implements Serializable, DebugDumpable {
 
     private static final Trace LOG = TraceManager.getTrace(Breadcrumb.class);
 
-    private transient IModel<String> labelModel;
-    private String label;
-    private transient IModel<String> iconModel;
-    private String icon;
+    private CachedModel labelModel;
+    private CachedModel iconModel;
+
     private boolean useLink = false;
     private boolean visible = true;
 
@@ -51,9 +50,9 @@ public class Breadcrumb implements Serializable, DebugDumpable {
         this(labelModel, null);
     }
 
-    public Breadcrumb(IModel<String> labelModel, IModel<String> icon) {
-        setLabel(labelModel);
-        setIcon(icon);
+    public Breadcrumb(IModel<String> labelModel, IModel<String> iconModel) {
+        setLabel(new CachedModel(labelModel));
+        setIcon(new CachedModel(iconModel));
     }
 
     public PageParameters getParameters() {
@@ -61,86 +60,29 @@ public class Breadcrumb implements Serializable, DebugDumpable {
     }
 
     public IModel<String> getLabel() {
-        if (labelModel == null && label != null) {
-            labelModel = new IModel<String>() {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public String getObject() {
-                    return label;
-                }
-            };
-        }
         return labelModel;
     }
 
     public void setLabel(final IModel<String> label) {
         if (label == null) {
-            this.labelModel = null;
+            this.labelModel = new CachedModel((String) null);
             return;
         }
 
-        this.labelModel = new IModel<String>() {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public String getObject() {
-                try {
-                    return label.getObject();
-                } catch (Exception ex) {
-                    LOG.warn("Couldn't load breadcrumb model value", ex);
-                    return null;
-                }
-            }
-
-            @Override
-            public void detach() {
-                label.detach();
-            }
-
-        };
+        this.labelModel = new CachedModel(label);
     }
 
     public IModel<String> getIcon() {
-        if (iconModel == null && icon != null) {
-            iconModel = new IModel<String>() {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public String getObject() {
-                    return icon;
-                }
-            };
-        }
         return iconModel;
     }
 
     public void setIcon(final IModel<String> icon) {
         if (icon == null) {
-            this.iconModel = null;
+            this.iconModel = new CachedModel((String) null);
             return;
         }
 
-        this.iconModel = new IModel<String>() {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public String getObject() {
-                try {
-                    return icon.getObject();
-                } catch (Exception ex) {
-                    LOG.warn("Couldn't load breadcrumb model value", ex);
-                    return null;
-                }
-            }
-
-            @Override
-            public void detach() {
-                Breadcrumb.this.icon = icon.getObject();
-                Breadcrumb.this.iconModel = null;
-            }
-
-        };
+        this.iconModel = new CachedModel(icon);
     }
 
     public boolean isUseLink() {
@@ -167,35 +109,10 @@ public class Breadcrumb implements Serializable, DebugDumpable {
         throw new UnsupportedOperationException("Should be implemented in a subclass");
     }
 
-    private <T extends Serializable> IModel<T> wrapModel(final IModel<T> model) {
-        if (model == null) {
-            return null;
-        }
-
-        return new IModel<T>() {
-
-            @Override
-            public T getObject() {
-                try {
-                    return model.getObject();
-                } catch (Exception ex) {
-                    LOG.warn("Couldn't load breadcrumb model value", ex);
-                    return null;
-                }
-            }
-
-            @Override
-            public void detach() {
-                model.getObject();
-            }
-
-        };
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) { return true; }
-        if (o == null || getClass() != o.getClass()) { return false; }
+        if (this == o) {return true;}
+        if (o == null || getClass() != o.getClass()) {return false;}
 
         //we don't compare label/icon models, we would need to compare models values
         return true;
@@ -218,9 +135,7 @@ public class Breadcrumb implements Serializable, DebugDumpable {
         sb.append(this.getClass().getSimpleName());
         sb.append("\n");
         DebugUtil.debugDumpWithLabelLn(sb, "labelModel", labelModel == null ? "" : labelModel.toString(), indent + 1);
-        DebugUtil.debugDumpWithLabelLn(sb, "label", label, indent + 1);
         DebugUtil.debugDumpWithLabelLn(sb, "iconModel", iconModel == null ? "" : iconModel.toString(), indent + 1);
-        DebugUtil.debugDumpWithLabelLn(sb, "icon", icon, indent + 1);
         DebugUtil.debugDumpWithLabelLn(sb, "useLink", useLink, indent + 1);
         DebugUtil.debugDumpWithLabel(sb, "visible", visible, indent + 1);
         extendsDebugDump(sb, indent);
