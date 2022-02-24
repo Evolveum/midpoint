14e4d6b751e8250d6f050e7dfe188872a810633c
lskublik <lskublik@evolveum.com>
2022-02-24 15:38:49 +0100

    adding tests for option skipping of updating auth focus behavior to set (MID-7687)

--- testing/rest/src/test/java/com/evolveum/midpoint/testing/rest/authentication/TestAbstractAuthentication.java
+++ testing/rest/src/test/java/com/evolveum/midpoint/testing/rest/authentication/TestAbstractAuthentication.java
@@ -10,12 +10,19 @@ import com.evolveum.midpoint.common.rest.MidpointAbstractProvider;
 import com.evolveum.midpoint.common.rest.MidpointJsonProvider;
 import com.evolveum.midpoint.model.common.SystemObjectCache;
 import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.schema.internals.InternalMonitor;
+import com.evolveum.midpoint.schema.internals.InternalsConfig;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.task.api.Task;
+import com.evolveum.midpoint.testing.rest.AbstractRestServiceInitializer;
 import com.evolveum.midpoint.testing.rest.RestServiceInitializer;
 
 import com.evolveum.midpoint.util.exception.CommonException;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.RoleType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.SecurityPolicyType;
+
+import com.evolveum.midpoint.xml.ns._public.common.common_3.SystemConfigurationType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.AfterMethod;
@@ -24,10 +31,32 @@ import javax.ws.rs.core.MediaType;
 import java.io.File;
 import java.io.IOException;
 
-public abstract class TestAbstractAuthentication extends RestServiceInitializer {
+public abstract class TestAbstractAuthentication extends AbstractRestServiceInitializer {
 
     protected static final File BASE_AUTHENTICATION_DIR = new File("src/test/resources/authentication/");
     protected static final File BASE_REPO_DIR = new File(BASE_AUTHENTICATION_DIR,"repo/");
+
+    public static final File SECURITY_POLICY_DEFAULT = new File(BASE_REPO_DIR, "security-policy-default.xml");
+
+    @Override
+    public void initSystem(Task initTask, OperationResult result) throws Exception {
+        super.initSystem(initTask, result);
+        logger.trace("initSystem");
+
+        InternalsConfig.encryptionChecks = false;
+
+        addObject(parseObject(ROLE_SUPERUSER_FILE), executeOptions().overwrite(), initTask, result);
+        addObject(parseObject(USER_ADMINISTRATOR_FILE), executeOptions().overwrite().raw(), initTask, result);
+        addObject(parseObject(VALUE_POLICY_GENERAL), executeOptions().overwrite(), initTask, result);
+        addObject(parseObject(SECURITY_POLICY_DEFAULT), executeOptions().overwrite(), initTask, result);
+        addObject(parseObject(SYSTEM_CONFIGURATION_FILE), executeOptions().overwrite(), initTask, result);
+
+        InternalMonitor.reset();
+
+        getModelService().postInit(result);
+
+        result.computeStatus();
+    }
 
     @Autowired
     protected MidpointJsonProvider jsonProvider;
