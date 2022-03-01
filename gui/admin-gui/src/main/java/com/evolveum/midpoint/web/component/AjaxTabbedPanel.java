9217fa7c8786af1e9d4e7f4e11b6229cc521dc39
Viliam Repan <vilo.repan@evolveum.com>
2022-03-01 09:38:14 +0100

    MID-7550 NoSuchElementException fix

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/AjaxTabbedPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/AjaxTabbedPanel.java
@@ -9,7 +9,7 @@ package com.evolveum.midpoint.web.component;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
-import org.apache.wicket.extensions.markup.html.tabs.*;
+import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.IModel;
 
@@ -20,15 +20,12 @@ import java.util.Optional;
  * Ajaxified version of the tabbed panel. Uses AjaxFallbackLink instead of regular wicket links so
  * it can update itself inplace.
  *
+ * @param <T> The type of panel to be used for this component's tabs. Just use {@link org.apache.wicket.extensions.markup.html.tabs.ITab} if you
+ * have no special needs here.
  * @author Igor Vaynberg (ivaynberg)
  * @author shood
- *
- * @param <T>
- *            The type of panel to be used for this component's tabs. Just use {@link org.apache.wicket.extensions.markup.html.tabs.ITab} if you
- *            have no special needs here.
  */
-public class AjaxTabbedPanel<T extends ITab> extends TabbedPanel<T>
-{
+public class AjaxTabbedPanel<T extends ITab> extends TabbedPanel<T> {
     private static final long serialVersionUID = 1L;
 
     /**
@@ -37,8 +34,7 @@ public class AjaxTabbedPanel<T extends ITab> extends TabbedPanel<T>
      * @param id
      * @param tabs
      */
-    public AjaxTabbedPanel(final String id, final List<T> tabs)
-    {
+    public AjaxTabbedPanel(final String id, final List<T> tabs) {
         this(id, tabs, null);
     }
 
@@ -47,8 +43,7 @@ public class AjaxTabbedPanel<T extends ITab> extends TabbedPanel<T>
      *
      * @param id
      * @param tabs
-     * @param model
-     *            model holding the index of the selected tab
+     * @param model model holding the index of the selected tab
      */
     public AjaxTabbedPanel(final String id, final List<T> tabs, IModel<Integer> model) {
         this(id, tabs, model, null);
@@ -62,23 +57,21 @@ public class AjaxTabbedPanel<T extends ITab> extends TabbedPanel<T>
     }
 
     @Override
-    protected WebMarkupContainer newLink(final String linkId, final int index)
-    {
-        return new AjaxFallbackLink<Void>(linkId)
-        {
+    protected WebMarkupContainer newLink(final String linkId, final int index) {
+        return new AjaxFallbackLink<Void>(linkId) {
 
             private static final long serialVersionUID = 1L;
 
             @Override
-            public void onClick(final Optional<AjaxRequestTarget> target)
-            {
+            public void onClick(final Optional<AjaxRequestTarget> target) {
                 setSelectedTab(index);
                 onTabChange(index);
-                if (target != null && target.isPresent() && target.get() != null)
-                {
+
+                if (target != null && target.isPresent()) {
                     target.get().add(AjaxTabbedPanel.this);
                 }
-                onAjaxUpdate(target.get());
+
+                onAjaxUpdate(target);
             }
         };
     }
@@ -92,9 +85,9 @@ public class AjaxTabbedPanel<T extends ITab> extends TabbedPanel<T>
      * <code>target</code> argument can be null when the client browser does not support ajax and
      * the fallback mode is used. See {@link AjaxFallbackLink} for details.
      *
-     * @param target
-     *            ajax target used to update this component
+     * @param target ajax target used to update this component
      */
-    protected void onAjaxUpdate(final AjaxRequestTarget target){}
+    protected void onAjaxUpdate(final Optional<AjaxRequestTarget> target) {
+    }
 }
 
