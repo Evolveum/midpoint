2753c1725d28ab3f588a9b6f414878ce9d9bb675
lskublik <lskublik@evolveum.com>
2022-03-16 07:55:11 +0100

    Merge remote-tracking branch 'origin/tmp/mid-7737'

--- model/report-impl/src/main/java/com/evolveum/midpoint/report/impl/activity/ExportActivitySupport.java
+++ model/report-impl/src/main/java/com/evolveum/midpoint/report/impl/activity/ExportActivitySupport.java
@@ -8,7 +8,6 @@
 package com.evolveum.midpoint.report.impl.activity;
 
 import com.evolveum.midpoint.prism.Containerable;
-import com.evolveum.midpoint.prism.PrismObject;
 import com.evolveum.midpoint.prism.query.ObjectQuery;
 import com.evolveum.midpoint.repo.common.ObjectResolver;
 import com.evolveum.midpoint.repo.common.activity.run.ActivityRunException;
@@ -21,11 +20,10 @@ import com.evolveum.midpoint.schema.GetOperationOptions;
 import com.evolveum.midpoint.schema.SearchResultList;
 import com.evolveum.midpoint.schema.SelectorOptions;
 import com.evolveum.midpoint.schema.result.OperationResult;
+import com.evolveum.midpoint.util.Handler;
 import com.evolveum.midpoint.xml.ns._public.common.audit_3.AuditEventRecordType;
 import com.evolveum.midpoint.util.exception.CommonException;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
@@ -69,24 +67,34 @@ public class ExportActivitySupport extends ReportActivitySupport {
 
     /**
      * Search container objects for iterative task.
-     * Temporary until will be implemented iterative search for audit records and containerable objects.
      */
-    public List<? extends Containerable> searchRecords(Class<? extends Containerable> type,
+    public void searchRecordsIteratively(
+            Class<? extends Containerable> type,
             ObjectQuery query,
+            Handler<Containerable> handler,
             Collection<SelectorOptions<GetOperationOptions>> options,
             OperationResult result) throws CommonException {
         if (AuditEventRecordType.class.equals(type)) {
-            @NotNull SearchResultList<AuditEventRecordType> auditRecords = modelAuditService.searchObjects(query, options, runningTask, result);
-            return Objects.requireNonNullElse(auditRecords.getList(), Collections.emptyList());
+            modelAuditService.searchObjectsIterative(
+                    query,
+                    options,
+                    (value, lResult) -> handler.handle(value),
+                    runningTask,
+                    result);
         } else if (ObjectType.class.isAssignableFrom(type)) {
             Class<? extends ObjectType> objectType = type.asSubclass(ObjectType.class);
-            SearchResultList<? extends PrismObject<? extends ObjectType>> results = modelService.searchObjects(objectType, query, options, runningTask, result);
-            List<ObjectType> list = new ArrayList<>();
-            results.forEach(object -> list.add(object.asObjectable()));
-            return list;
+            modelService.searchObjectsIterative(
+                    objectType,
+                    query,
+                    (object, lResult) -> handler.handle(object.asObjectable()),
+                    options,
+                    runningTask,
+                    result);
         } else {
-            SearchResultList<? extends Containerable> containers = modelService.searchContainers(type, query, options, runningTask, result);
-            return Objects.requireNonNullElse(containers.getList(), Collections.emptyList());
+            // Temporary - until iterative search is available
+            SearchResultList<? extends Containerable> containers =
+                    modelService.searchContainers(type, query, options, runningTask, result);
+            containers.forEach(handler::handle);
         }
     }
 
