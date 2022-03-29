4a5a4c2b8c70812eda8bfb903d4fc33c7ee4665c
lskublik <lskublik@evolveum.com>
2022-03-29 12:30:25 +0200

    create tests for service page

---
+++ gui/admin-gui/src/test/java/com/evolveum/midpoint/gui/TestPageService.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2010-2018 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+package com.evolveum.midpoint.gui;
+
+import com.evolveum.midpoint.gui.impl.page.admin.service.PageService;
+import com.evolveum.midpoint.gui.test.TestMidPointSpringApplication;
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.schema.result.OperationResult;
+import com.evolveum.midpoint.task.api.Task;
+import com.evolveum.midpoint.web.AbstractInitializedGuiIntegrationTest;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.RoleType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ServiceType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.SystemConfigurationType;
+
+import org.apache.wicket.util.tester.FormTester;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * @author skublik
+ */
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+@ActiveProfiles("test")
+@SpringBootTest(classes = TestMidPointSpringApplication.class)
+public class TestPageService extends AbstractInitializedGuiIntegrationTest {
+
+    @Override
+    public void initSystem(Task initTask, OperationResult initResult) throws Exception {
+        super.initSystem(initTask, initResult);
+        PrismObject<SystemConfigurationType> systemConfig = parseObject(SYSTEM_CONFIGURATION_FILE);
+
+        logger.info("adding system config page");
+        addObject(systemConfig, executeOptions().overwrite(), initTask, initResult);
+    }
+
+    @Test
+    public void test001testPageService() {
+        renderPage(PageService.class);
+    }
+
+    @Test
+    public void test002testAddNewService() throws Exception {
+        renderPage(PageService.class);
+
+        FormTester formTester = tester.newFormTester(MAIN_FORM, false);
+        formTester.setValue(PATH_FORM_NAME, "newService");
+        formTester.submit(FORM_SAVE);
+
+        Thread.sleep(5000);
+
+        PrismObject<ServiceType> newService = findObjectByName(ServiceType.class, "newService");
+        assertNotNull(newService, "New service not created.");
+        logger.info("created service: {}", newService.debugDump());
+    }
+
+    @Test
+    public void test003testEditService() throws Exception {
+        PrismObject<ServiceType> service = createObject(ServiceType.class, "Service003");
+        String serviceOid = addObject(service);
+        renderPage(PageService.class, serviceOid);
+    }
+}
