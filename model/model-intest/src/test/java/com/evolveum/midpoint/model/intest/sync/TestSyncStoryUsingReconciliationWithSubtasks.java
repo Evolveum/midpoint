27a309f7c0050c55b3b4c2c51dd3582a03754b29
Pavol Mederly <mederly@evolveum.com>
2022-04-04 20:17:03 +0200

    Stabilize AbstractSynchronizationStoryTest
    These tests were unstable from the day one. The concurrent nature
    of synchronization tasks led to a whole spectrum of various failures.
    But we don't consider them important, because we want to concentrate
    only on the basic functionality of multi-resource synchronization here.
    
    So now we changed these tests so that concurrent execution is avoided.
    
    Other changes:
    
    1. Some test artifacts (tasks, sync object template) were moved from
    `common` directory to test-specific `sync-story` one. This is to lessen
    the potential for coupling between the tests. (They weren't used in
    other tests, anyway.)
    
    2. The test code was modernized a bit.

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/sync/TestSyncStoryUsingReconciliationWithSubtasks.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/sync/TestSyncStoryUsingReconciliationWithSubtasks.java
@@ -6,35 +6,35 @@
  */
 package com.evolveum.midpoint.model.intest.sync;
 
-import com.evolveum.midpoint.test.TestTask;
+import static com.evolveum.midpoint.model.intest.sync.AbstractSynchronizationStoryTest.Color.*;
+
+import java.util.Map;
 
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.Map;
-
-import static com.evolveum.midpoint.model.intest.sync.AbstractSynchronizationStoryTest.Color.*;
+import com.evolveum.midpoint.test.TestTask;
 
 /**
- * Uses multiple worker tasks.
+ * Uses reconciliation with individual sub-activities run in separate subtasks.
  *
  * Shouldn't be run under H2 because of too much contention.
- * Also, it takes a little longer than standard reconciliation test because of the overhead.
+ * Also, it takes a little longer than standard test because of the overhead.
  *
  * NOTE: The utility of this test is questionable, as the synchronization story test is about the correctness
  * of the synchronization algorithms themselves, not about the distribution features of the reconciliation activity.
  */
-@ContextConfiguration(locations = {"classpath:ctx-model-intest-test-main.xml"})
+@ContextConfiguration(locations = { "classpath:ctx-model-intest-test-main.xml" })
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
-public class TestSyncStoryUsingReconciliationMultiNode extends TestSyncStoryUsingReconciliation {
+public class TestSyncStoryUsingReconciliationWithSubtasks extends TestSyncStoryUsingReconciliation {
 
     private static final TestTask TASK_RECONCILE_DUMMY = new TestTask(
-            TEST_DIR, "task-reconcile-dummy-multinode.xml", "c36de89d-5ee2-469c-935b-ff34560d4e77");
+            TEST_DIR, "task-reconcile-dummy-with-subtasks.xml", "7b801396-a602-48b4-8b39-26d7a48f6501");
     private static final TestTask TASK_RECONCILE_DUMMY_GREEN = new TestTask(
-            TEST_DIR, "task-reconcile-dummy-green-multinode.xml", "87100587-1f5c-468f-8e89-8731650833fd");
+            TEST_DIR, "task-reconcile-dummy-green-with-subtasks.xml", "0ed7212e-368f-4928-9799-2960b87c8840");
     private static final TestTask TASK_RECONCILE_DUMMY_BLUE = new TestTask(
-            TEST_DIR, "task-reconcile-dummy-blue-multinode.xml", "fa8abd8d-c379-46c1-aec8-38afb1a2d469");
+            TEST_DIR, "task-reconcile-dummy-blue-with-subtasks.xml", "ed388ce5-bc16-4489-9764-8b9abec547fb");
 
     @Override
     protected Map<Color, TestTask> getTaskMap() {
