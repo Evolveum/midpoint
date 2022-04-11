45d856e71b37c2fee094e003b33fa35c348ef4e6
Viliam Repan <vilo.repan@evolveum.com>
2022-04-11 17:12:03 +0200

    MID-6271 more cleanup, bootstrap select/multiselect js still not working properly

--- gui/admin-gui/src/main/resources/static/js/index.js
+++ gui/admin-gui/src/main/resources/static/js/index.js
@@ -5,15 +5,15 @@
  * and European Union Public License. See LICENSE file for details.
  */
 
-// import MidPointTheme from './midpoint-theme';
-
 import '../../../../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap';
 import '../../../../../node_modules/admin-lte/dist/js/adminlte';
 
+import '../../../../../node_modules/bootstrap-select';
+import '../../../../../node_modules/knockout';
+import '../../../../../node_modules/bootstrap-multiselect';
+
 import MidPointTheme from './midpoint/midpoint-theme';
 import MidPointAceEditor from "./midpoint/ace-editor";
-
-// Test.abc();
 
 window.MidPointTheme = new MidPointTheme();
 window.MidPointAceEditor = new MidPointAceEditor();
