66fc168ea145677be3621a67e9a06a63b68c4cad
Viliam Repan <vilo.repan@evolveum.com>
2022-04-14 09:57:28 +0200

    MID-6271 ace editor mostly working, bootstrap-multiselect disabled for now (has JS issues)

--- gui/admin-gui/src/main/resources/static/js/index.js
+++ gui/admin-gui/src/main/resources/static/js/index.js
@@ -9,7 +9,7 @@ import '../../../../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap';
 import '../../../../../node_modules/admin-lte/dist/js/adminlte';
 
 import '../../../../../node_modules/bootstrap-select';
-import '../../../../../node_modules/bootstrap-multiselect';
+// import '../../../../../node_modules/bootstrap-multiselect';  // todo enable, used in DropDownMultiChoice (ListMultipleChoicePanel)
 
 import MidPointTheme from './midpoint/midpoint-theme';
 import MidPointAceEditor from "./midpoint/ace-editor";
