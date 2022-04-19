0f55a8ba4c10111ea32050cbc0be567ac78179b2
Viliam Repan <vilo.repan@evolveum.com>
2022-04-19 14:39:07 +0200

    MID-6271 fixed locale panel, used npm package for famfamfam flag icons

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/menu/top/LocalePanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/menu/top/LocalePanel.java
@@ -12,6 +12,7 @@ import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.web.security.LocaleDescriptor;
 import com.evolveum.midpoint.web.security.MidPointApplication;
+
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
@@ -21,7 +22,6 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
 import java.util.Locale;
@@ -30,69 +30,61 @@ import java.util.Locale;
  * @author lazyman
  */
 public class LocalePanel extends Panel {
+
     private static final long serialVersionUID = 1L;
 
     private static final Trace LOGGER = TraceManager.getTrace(LocalePanel.class);
 
+    private static final String ID_ICON = "icon";
     private static final String ID_LOCALES = "locales";
-    private static final String ID_LOCALE_ICON = "localeIcon";
-    private static final String ID_LOCALE_LINK = "localeLink";
-    private static final String ID_LOCALE_ITEM_ICON = "localeItemIcon";
+    private static final String ID_LOCALES_LINK = "localesLink";
+    private static final String ID_LOCALES_ICON = "localesIcon";
+
+    private static final String ID_LOCALES_LABEL = "localesLabel";
 
     public LocalePanel(String id) {
         super(id);
 
         setRenderBodyOnly(true);
 
-        Label image = new Label(ID_LOCALE_ICON);
-        image.add(AttributeModifier.replace("class", new IModel<String>(){
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public String getObject(){
-                StringBuilder sb = new StringBuilder("flag-");
-                sb.append(getSelectedLocaleDescriptor() != null ?
-                        getSelectedLocaleDescriptor().getFlag() : "");
-                return sb.toString();
-            }
-        }));
+        Label image = new Label(ID_ICON);
+        image.add(AttributeModifier.append("class", () -> getFlag()));
         image.setOutputMarkupId(true);
         add(image);
 
-        ListView<LocaleDescriptor> locales = new ListView<LocaleDescriptor>(ID_LOCALES,
-                Model.ofList(MidPointApplication.AVAILABLE_LOCALES)) {
+        ListView<LocaleDescriptor> locales = new ListView<>(ID_LOCALES, Model.ofList(MidPointApplication.AVAILABLE_LOCALES)) {
 
             private static final long serialVersionUID = 1L;
 
             @Override
             protected void populateItem(final ListItem<LocaleDescriptor> item) {
-                final Label image = new Label(ID_LOCALE_ITEM_ICON);
-                image.add(AttributeModifier.append("class", "flag-" + item.getModelObject().getFlag()));
-                image.setOutputMarkupId(true);
-                item.add(image);
-
-                final AjaxLink<String> localeLink = new AjaxLink<String>(ID_LOCALE_LINK) {
+                item.setRenderBodyOnly(true);
+                final AjaxLink<String> localeLink = new AjaxLink<>(ID_LOCALES_LINK) {
 
                     private static final long serialVersionUID = 1L;
-
-                    @Override
-                    public IModel<String> getBody() {
-                        return Model.of(item.getModelObject().getName());
-                    }
 
                     @Override
                     public void onClick(AjaxRequestTarget target) {
                         changeLocale(target, item.getModelObject());
                     }
-
-
                 };
-                localeLink.setOutputMarkupId(true);
                 item.add(localeLink);
+
+                Label image = new Label(ID_LOCALES_ICON);
+                image.add(AttributeModifier.append("class", () -> item.getModelObject().getFlag()));
+                localeLink.add(image);
+
+                Label label = new Label(ID_LOCALES_LABEL, () -> item.getModelObject().getName());
+                label.setRenderBodyOnly(true);
+                localeLink.add(label);
             }
         };
-        locales.setOutputMarkupId(true);
         add(locales);
+    }
+
+    private String getFlag() {
+        LocaleDescriptor descriptor = getSelectedLocaleDescriptor();
+        return descriptor != null ? descriptor.getFlag() : "";
     }
 
     private LocaleDescriptor getSelectedLocaleDescriptor() {
@@ -119,7 +111,7 @@ public class LocalePanel extends Panel {
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
 
-        String selectId = get(ID_LOCALE_ICON).getMarkupId();
+        String selectId = get(ID_ICON).getMarkupId();
         response.render(OnDomReadyHeaderItem.forScript("$('#" + selectId + "').selectpicker({});"));
     }
 
