e85b3ce44ea3176555555eeca6fb83e5411975a0
Viliam Repan <vilo.repan@evolveum.com>
2022-05-25 16:13:57 +0200

    request access ui, menu classes (forgot to commit)

---
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/self/requestAccess/DetailsMenuPanel.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.gui.impl.page.self.requestAccess;
+
+import com.evolveum.midpoint.gui.api.component.BasePanel;
+
+import org.apache.wicket.behavior.AttributeAppender;
+
+/**
+ * Created by Viliam Repan (lazyman).
+ */
+public class DetailsMenuPanel extends BasePanel {
+
+    public DetailsMenuPanel(String id) {
+        super(id);
+
+        initLayout();
+    }
+
+    private void initLayout() {
+        add(AttributeAppender.append("class", "details-menu-panel d-flex rounded bg-white align-self-stretch align-self-sm-start"));
+    }
+}
