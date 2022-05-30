e6d8b9ea7447401227d1d53b4cd2410414745a82
Viliam Repan <vilo.repan@evolveum.com>
2022-05-30 15:22:49 +0200

    table/tiles toggle + role catalog table with sample columns

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/self/requestAccess/RoleCatalogPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/self/requestAccess/RoleCatalogPanel.java
@@ -10,13 +10,27 @@ package com.evolveum.midpoint.gui.impl.page.self.requestAccess;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
 import com.evolveum.midpoint.gui.api.component.BasePanel;
 import com.evolveum.midpoint.gui.api.component.wizard.WizardPanel;
+import com.evolveum.midpoint.web.component.data.BoxedTablePanel;
+import com.evolveum.midpoint.web.component.data.column.CheckBoxHeaderColumn;
+import com.evolveum.midpoint.web.component.data.column.IconColumn;
+import com.evolveum.midpoint.web.component.data.column.LinkColumn;
+import com.evolveum.midpoint.web.component.util.ListDataProvider;
+import com.evolveum.midpoint.web.component.util.VisibleBehaviour;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.DisplayType;
 
 /**
  * Created by Viliam Repan (lazyman).
@@ -25,11 +39,15 @@ public class RoleCatalogPanel extends BasePanel implements WizardPanel {
 
     private static final long serialVersionUID = 1L;
 
-    private static final String ID_AS_LIST = "asList";
-    private static final String ID_AS_TILE = "asTile";
+    private static final String ID_VIEW_TOGGLE = "viewToggle";
     private static final String ID_MENU = "menu";
+    private static final String ID_TILES_CONTAINER = "tilesContainer";
     private static final String ID_TILES = "tiles";
     private static final String ID_TILE = "tile";
+    private static final String ID_TABLE = "table";
+    private static final String ID_TABLE_FOOTER_FRAGMENT = "tableFooterFragment";
+    private static final String ID_ADD_SELECTED = "addSelected";
+    private static final String ID_ADD_ALL = "addAll";
 
     public RoleCatalogPanel(String id) {
         super(id);
@@ -48,11 +66,27 @@ public class RoleCatalogPanel extends BasePanel implements WizardPanel {
     }
 
     private void initLayout() {
+        setOutputMarkupId(true);
+
+        IModel<ViewToggle> viewToggleModel = Model.of(ViewToggle.TILE);
+        ViewTogglePanel viewToggle = new ViewTogglePanel(ID_VIEW_TOGGLE, viewToggleModel) {
+
+            @Override
+            protected void onTogglePerformed(AjaxRequestTarget target, ViewToggle newState) {
+                super.onTogglePerformed(target, newState);
+                target.add(RoleCatalogPanel.this);
+            }
+        };
+        add(viewToggle);
         DetailsMenuPanel menu = new DetailsMenuPanel(ID_MENU);
         add(menu);
 
+        WebMarkupContainer tilesContainer = new WebMarkupContainer(ID_TILES_CONTAINER);
+        tilesContainer.add(new VisibleBehaviour(() -> viewToggleModel.getObject() == ViewToggle.TILE));
+        add(tilesContainer);
+
         List<CatalogTile> list = new ArrayList<>();
-        for (int i=0;i<10;i++) {
+        for (int i = 0; i < 10; i++) {
             CatalogTile t = new CatalogTile("fas fa-building", "Canteen");
             t.setLogo("fas fa-utensils fa-2x");
             t.setDescription("Grants you access to canteen services, coffee bar and vending machines");
@@ -69,6 +103,59 @@ public class RoleCatalogPanel extends BasePanel implements WizardPanel {
                 item.add(tile);
             }
         };
-        add(tiles);
+        tilesContainer.add(tiles);
+
+        ISortableDataProvider provider = new ListDataProvider(this, () -> new ArrayList<>());
+        List<IColumn> columns = createColumns();
+        BoxedTablePanel table = new BoxedTablePanel(ID_TABLE, provider, columns) {
+
+            @Override
+            protected WebMarkupContainer createButtonToolbar(String id) {
+                Fragment fragment = new Fragment(id, ID_TABLE_FOOTER_FRAGMENT, RoleCatalogPanel.this);
+                fragment.add(new AjaxLink<>(ID_ADD_SELECTED) {
+
+                    @Override
+                    public void onClick(AjaxRequestTarget target) {
+                        addSelectedPerformed(target);
+                    }
+                });
+
+                fragment.add(new AjaxLink<>(ID_ADD_ALL) {
+
+                    @Override
+                    public void onClick(AjaxRequestTarget target) {
+                        addAllPerformed(target);
+                    }
+                });
+
+                return fragment;
+            }
+        };
+        table.add(new VisibleBehaviour(() -> viewToggleModel.getObject() == ViewToggle.TABLE));
+        add(table);
+    }
+
+    private List<IColumn> createColumns() {
+        List<IColumn> columns = new ArrayList<>();
+        columns.add(new CheckBoxHeaderColumn());
+        columns.add(new IconColumn(null) {
+            @Override
+            protected DisplayType getIconDisplayType(IModel rowModel) {
+                return new DisplayType();
+            }
+        });
+        columns.add(new PropertyColumn(createStringResource("ObjectType.name"), "name"));
+        columns.add(new PropertyColumn(createStringResource("ObjectType.description"), "name"));
+        columns.add(new LinkColumn(createStringResource("RoleCatalogPanel.details")));
+
+        return columns;
+    }
+
+    protected void addSelectedPerformed(AjaxRequestTarget target) {
+
+    }
+
+    protected void addAllPerformed(AjaxRequestTarget target) {
+
     }
 }
