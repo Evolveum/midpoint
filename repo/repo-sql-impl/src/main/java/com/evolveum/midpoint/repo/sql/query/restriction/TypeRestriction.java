bc45792bd4e07792b5aebaa2eba4afbed7906ba4
Richard Richter <richard.richter@evolveum.com>
2022-07-05 02:50:46 +0200

    old-repo: aliases starting with _, TYPE uses exists only with subfilter
    If TYPE filter doesn't have inner filter, restriction uses "old" style
    without EXISTS which is better in that case.
    Subqueries could produce alias like "or" which is invalid, all aliases
    now start with _ to avoid this.
    QueryInterpreterTest needed plenty of fixes after these changes.

--- repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/query/restriction/TypeRestriction.java
+++ repo/repo-sql-impl/src/main/java/com/evolveum/midpoint/repo/sql/query/restriction/TypeRestriction.java
@@ -1,20 +1,17 @@
 /*
- * Copyright (c) 2010-2015 Evolveum and contributors
+ * Copyright (C) 2010-2022 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
  */
-
 package com.evolveum.midpoint.repo.sql.query.restriction;
 
 import java.util.Collection;
-import javax.xml.namespace.QName;
 
 import com.evolveum.midpoint.prism.query.TypeFilter;
 import com.evolveum.midpoint.repo.sql.data.common.RObject;
 import com.evolveum.midpoint.repo.sql.data.common.other.RObjectType;
 import com.evolveum.midpoint.repo.sql.query.InterpretationContext;
-import com.evolveum.midpoint.repo.sql.query.QueryInterpreter;
 import com.evolveum.midpoint.repo.sql.query.definition.JpaEntityDefinition;
 import com.evolveum.midpoint.repo.sql.query.hqm.HibernateQuery;
 import com.evolveum.midpoint.repo.sql.query.hqm.condition.Condition;
@@ -23,20 +20,21 @@ import com.evolveum.midpoint.repo.sql.query.resolution.HqlEntityInstance;
 import com.evolveum.midpoint.repo.sql.util.ClassMapper;
 import com.evolveum.midpoint.repo.sqlbase.QueryException;
 import com.evolveum.midpoint.schema.constants.ObjectTypes;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.AssignmentHolderType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
 
-/**
- * @author lazyman
- */
 public class TypeRestriction extends Restriction<TypeFilter> {
 
-    public TypeRestriction(InterpretationContext context, TypeFilter filter, JpaEntityDefinition baseEntityDefinition, Restriction parent) {
+    public TypeRestriction(InterpretationContext context, TypeFilter filter,
+            JpaEntityDefinition baseEntityDefinition, Restriction<?> parent) {
         super(context, filter, baseEntityDefinition, parent);
     }
 
     @Override
     public Condition interpret() throws QueryException {
+        if (filter.getFilter() == null) {
+            return objectTypeCondition();
+        }
+
         Class<ObjectType> targetType = ObjectTypes.getObjectTypeClass(filter.getType());
         InterpretationContext subcontext = context.createSubcontext(targetType);
         HqlEntityInstance outerEntity = getBaseHqlEntity(); // parent query
@@ -48,32 +46,17 @@ public class TypeRestriction extends Restriction<TypeFilter> {
         return existsCondition;
     }
 
-    public Condition interpretOld() throws QueryException {
+    private Condition objectTypeCondition() {
         InterpretationContext context = getContext();
         HibernateQuery hibernateQuery = context.getHibernateQuery();
 
         String property = getBaseHqlEntity().getHqlPath() + "." + RObject.F_OBJECT_TYPE_CLASS;
+        Collection<RObjectType> values = ClassMapper.getDescendantsForQName(filter.getType());
 
-        Collection<RObjectType> values = getValues(filter.getType());
-
-        Condition basedOnType;
         if (values.size() > 1) {
-            basedOnType = hibernateQuery.createIn(property, values);
+            return hibernateQuery.createIn(property, values);
         } else {
-            basedOnType = hibernateQuery.createEq(property, values.iterator().next());
+            return hibernateQuery.createEq(property, values.iterator().next());
         }
-
-        if (filter.getFilter() == null) {
-            return basedOnType;
-        }
-
-        QueryInterpreter interpreter = context.getInterpreter();
-        Condition basedOnFilter = interpreter.interpretFilter(context, filter.getFilter(), this);
-
-        return hibernateQuery.createAnd(basedOnType, basedOnFilter);
-    }
-
-    private Collection<RObjectType> getValues(QName typeQName) {
-        return ClassMapper.getDescendantsForQName(typeQName);
     }
 }
