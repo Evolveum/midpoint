6941dcac6380b0b3fc078099734b8919dcda7031
Richard Richter <richard.richter@evolveum.com>
2022-08-25 14:37:26 +0200

    FocusTypeUtil: added languageOrLocale, also used on GUI
    Exception-less version of toLocale moved to LocalizationUtil.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/util/WebComponentUtil.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/util/WebComponentUtil.java
@@ -5139,16 +5139,8 @@ public final class WebComponentUtil {
             if (focus == null) {
                 return MidPointApplication.getDefaultLocale();
             }
-            String prefLang = focus.getPreferredLanguage();
-            if (StringUtils.isBlank(prefLang)) {
-                prefLang = focus.getLocale();
-            }
 
-            try {
-                locale = LocaleUtils.toLocale(prefLang);
-            } catch (Exception ex) {
-                LOGGER.debug("Error occurred while getting user locale, " + ex.getMessage());
-            }
+            locale = LocalizationUtil.toLocale(FocusTypeUtil.languageOrLocale(focus));
         }
 
         if (locale == null) {
