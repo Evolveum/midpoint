3236366f3bc5fc1f03662a2b54a61534766066e7
Katarina Valalikova <k.valalikova@evolveum.com>
2022-09-07 10:46:14 +0200

    handling the situation when no attorney is selected.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/workflow/PageWorkItemsAttorney.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/workflow/PageWorkItemsAttorney.java
@@ -39,16 +39,22 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
         })
 public class PageWorkItemsAttorney extends PageCaseWorkItems {
 
+    public PageWorkItemsAttorney() {
+        this(null);
+    }
+
     public PageWorkItemsAttorney(PageParameters pageParameters) {
         super(pageParameters);
+
+        if (getPowerDonorOid() == null) {
+            getSession().error("PageWorkItemsAttorney.attorney.not.selected");
+            throw redirectBackViaRestartResponseException();
+        }
     }
 
     @Override
     protected ObjectFilter getCaseWorkItemsFilter() {
         String attorneyUserOid = getPowerDonorOid();
-        if (StringUtils.isEmpty(attorneyUserOid) || attorneyUserOid.equals("null")) {
-            return super.getCaseWorkItemsFilter();
-        }
         return getPrismContext().queryFor(CaseWorkItemType.class)
                 .item(CaseWorkItemType.F_ASSIGNEE_REF)
                 .ref(attorneyUserOid, UserType.COMPLEX_TYPE)
