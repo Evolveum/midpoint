d3f1f5697d549d272c0040a8b5d8f0209ba6ea97
Pavol Mederly <mederly@evolveum.com>
2022-09-28 19:55:43 +0200

    Add a test for "no super resource" authorization
    This resolves MID-8004.

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/security/TestSecurityMedium.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/security/TestSecurityMedium.java
@@ -6,18 +6,20 @@
  */
 package com.evolveum.midpoint.model.intest.security;
 
+import com.evolveum.midpoint.model.api.ModelExecuteOptions;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.task.api.Task;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.OrgType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.RoleType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
+import com.evolveum.midpoint.test.TestResource;
+import com.evolveum.midpoint.util.exception.CommonException;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author semancik
@@ -26,10 +28,17 @@ import java.io.File;
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class TestSecurityMedium extends AbstractSecurityTest {
 
-    protected static final File USER_EMPLOYEE_FRED_FILE = new File(TEST_DIR, "user-employee-fred.xml");
+    private static final File USER_EMPLOYEE_FRED_FILE = new File(TEST_DIR, "user-employee-fred.xml");
 
-    protected static final File ROLE_EMPLOYEE_MANAGER_FILE = new File(TEST_DIR, "role-employee-manager.xml");
-    protected static final String ROLE_EMPLOYEE_MANAGER_OID = "5549cb8e-d573-11e9-a61e-7f2eff22715a";
+    private static final File ROLE_EMPLOYEE_MANAGER_FILE = new File(TEST_DIR, "role-employee-manager.xml");
+    private static final String ROLE_EMPLOYEE_MANAGER_OID = "5549cb8e-d573-11e9-a61e-7f2eff22715a";
+
+    private static final TestResource<RoleType> ROLE_RESOURCE_NO_SUPER =
+            new TestResource<>(TEST_DIR, "role-resource-no-super.xml", "127e6393-371d-4a15-952f-e454748bfc09");
+    private static final TestResource<ResourceType> RESOURCE_NO_SUPER =
+            new TestResource<>(TEST_DIR, "resource-no-super.xml", "801c9610-5cb7-411f-af3f-a14b303154ca");
+    private static final TestResource<ResourceType> RESOURCE_WITH_SUPER =
+            new TestResource<>(TEST_DIR, "resource-with-super.xml", "9e785491-7207-4288-8d1e-7f7a21a6455f");
 
     @Override
     public void initSystem(Task initTask, OperationResult initResult) throws Exception {
@@ -37,9 +46,10 @@ public class TestSecurityMedium extends AbstractSecurityTest {
 
         repoAddObjectFromFile(ARCHETYPE_EMPLOYEE_FILE, initResult);
         repoAddObjectFromFile(ROLE_EMPLOYEE_MANAGER_FILE, initResult);
+        repoAdd(ROLE_RESOURCE_NO_SUPER, initResult);
     }
 
-    protected static final int NUMBER_OF_IMPORTED_ROLES = 1;
+    private static final int NUMBER_OF_IMPORTED_ROLES = 2;
 
     protected int getNumberOfRoles() {
         return super.getNumberOfRoles() + NUMBER_OF_IMPORTED_ROLES;
@@ -120,5 +130,38 @@ public class TestSecurityMedium extends AbstractSecurityTest {
         assertModifyDeny();
         assertDeleteDeny();
         assertGlobalStateUntouched();
+    }
+
+    @Test
+    public void test200AutzAddResource() throws CommonException, IOException {
+        cleanupAutzTest(USER_JACK_OID);
+        assignRole(USER_JACK_OID, ROLE_RESOURCE_NO_SUPER.oid);
+        login(USER_JACK_USERNAME);
+
+        then("adding resource with no 'super' is allowed");
+        assertAddAllow(RESOURCE_NO_SUPER.file, ModelExecuteOptions.create().setIsImport());
+
+        then("adding resource with 'super' is denied");
+        assertAddDeny(RESOURCE_WITH_SUPER.file, ModelExecuteOptions.create().setIsImport());
+
+        then("modifying resource item other than 'super' is allowed");
+        assertModifyAllow(ResourceType.class, RESOURCE_NO_SUPER.oid, ResourceType.F_DESCRIPTION, "anything");
+
+        String randomOid = "0a4b3d14-7c39-4117-b029-babacf7b254e";
+
+        then("modifying resource 'super' item is denied");
+        assertModifyDeny(
+                ResourceType.class,
+                RESOURCE_NO_SUPER.oid,
+                ResourceType.F_SUPER,
+                new SuperResourceDeclarationType()
+                        .resourceRef(randomOid, ResourceType.COMPLEX_TYPE));
+
+        then("modifying resource 'super/resourceRef' item is denied");
+        assertModifyDeny(
+                ResourceType.class,
+                RESOURCE_NO_SUPER.oid,
+                ResourceType.F_SUPER.append(SuperResourceDeclarationType.F_RESOURCE_REF),
+                new ObjectReferenceType().oid(randomOid).type(ResourceType.COMPLEX_TYPE));
     }
 }
