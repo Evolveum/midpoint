df7ba30dd73138276c10a31a29d71917885973be
Viliam Repan <vilo.repan@evolveum.com>
2022-10-14 14:02:49 +0200

    MID-8135 override mandatory status for action, type on sysconfig object policies form panelw

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/systemconfiguration/component/ObjectPolicyDetailsPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/systemconfiguration/component/ObjectPolicyDetailsPanel.java
@@ -7,21 +7,36 @@
 
 package com.evolveum.midpoint.gui.impl.page.admin.systemconfiguration.component;
 
-import com.evolveum.midpoint.gui.api.component.DisplayNamePanel;
-import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerValueWrapper;
-import com.evolveum.midpoint.gui.impl.component.MultivalueContainerDetailsPanel;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectPolicyConfigurationType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ContainerPanelConfigurationType;
+import java.util.Arrays;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
+import com.evolveum.midpoint.gui.api.component.DisplayNamePanel;
+import com.evolveum.midpoint.gui.api.prism.wrapper.ItemMandatoryHandler;
+import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerValueWrapper;
+import com.evolveum.midpoint.gui.impl.component.MultivalueContainerDetailsPanel;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+
 /**
  * Created by Viliam Repan (lazyman).
  */
 public class ObjectPolicyDetailsPanel extends MultivalueContainerDetailsPanel<ObjectPolicyConfigurationType> {
+
+    private static final ItemPath[] MANDATORY_OVERRIDE_PATHS = {
+            ItemPath.create(
+                    SystemConfigurationType.F_DEFAULT_OBJECT_POLICY_CONFIGURATION,
+                    ObjectPolicyConfigurationType.F_CONFLICT_RESOLUTION,
+                    ConflictResolutionType.F_ACTION),
+            ItemPath.create(
+                    SystemConfigurationType.F_DEFAULT_OBJECT_POLICY_CONFIGURATION,
+                    ObjectPolicyConfigurationType.F_ADMIN_GUI_CONFIGURATION,
+                    ArchetypeAdminGuiConfigurationType.F_OBJECT_DETAILS,
+                    GuiObjectDetailsPageType.F_TYPE)
+    };
 
     public ObjectPolicyDetailsPanel(String id, IModel<PrismContainerValueWrapper<ObjectPolicyConfigurationType>> model, boolean addDefaultPanel) {
         super(id, model, addDefaultPanel);
@@ -59,5 +74,17 @@ public class ObjectPolicyDetailsPanel extends MultivalueContainerDetailsPanel<Ob
         };
 
         return d;
+    }
+
+    @Override
+    protected ItemMandatoryHandler getMandatoryHandler() {
+        return itemWrapper -> {
+            ItemPath named = itemWrapper.getPath().namedSegmentsOnly();
+            if (Arrays.stream(MANDATORY_OVERRIDE_PATHS).anyMatch(p -> p.equivalent(named))) {
+                return false;
+            }
+
+            return itemWrapper.isMandatory();
+        };
     }
 }
