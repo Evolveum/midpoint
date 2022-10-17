ae7408ffd0fd9a1e34e63be0da7df5231c3d6453
Viliam Repan <vilo.repan@evolveum.com>
2022-10-17 17:15:18 +0200

    MID-7859 disabling mandatory=true for fields in sysconfig/profiling configuration panel for now

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/prism/panel/ProfilingClassLoggerPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/prism/panel/ProfilingClassLoggerPanel.java
@@ -22,7 +22,11 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.ClassLoggerConfigura
 public class ProfilingClassLoggerPanel extends PrismContainerPanel<ClassLoggerConfigurationType, PrismContainerWrapper<ClassLoggerConfigurationType>> {
 
     public ProfilingClassLoggerPanel(String id, IModel<PrismContainerWrapper<ClassLoggerConfigurationType>> model, ItemPanelSettings settings) {
-        super(id, model, new ItemPanelSettingsBuilder().visibilityHandler(itemWrapper -> checkVisibility(itemWrapper, settings.getVisibilityHandler())).build());
+        super(id, model,
+                new ItemPanelSettingsBuilder()
+                        .visibilityHandler(itemWrapper -> checkVisibility(itemWrapper, settings.getVisibilityHandler()))
+                        .mandatoryHandler(itemWrapper -> false)
+                        .build());
     }
 
     private static ItemVisibility checkVisibility(ItemWrapper itemWrapper, ItemVisibilityHandler visibilitytHandler) {
