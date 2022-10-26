d290a1a409bf88efe6926c0647cbe4b0203f1986
Pavol Mederly <mederly@evolveum.com>
2022-10-26 23:06:08 +0200

    Bump ConnId from 1.5.0.19 to 1.5.1.10 (current)

--- provisioning/provisioning-impl/src/test/java/com/evolveum/midpoint/provisioning/impl/TestConnectorManager.java
+++ provisioning/provisioning-impl/src/test/java/com/evolveum/midpoint/provisioning/impl/TestConnectorManager.java
@@ -28,7 +28,7 @@ import com.evolveum.midpoint.test.AbstractIntegrationTest;
 @DirtiesContext
 public class TestConnectorManager extends AbstractIntegrationTest {
 
-    private static final String CONNID_FRAMEWORK_VERSION = "1.5.0.19";
+    private static final String CONNID_FRAMEWORK_VERSION = "1.5.1.10";
 
     @Autowired private ConnectorManager connectorManager;
 
