1e40df3fbb6fc6a7879c0f570c0002ba85f87e1c
Viliam Repan <vilo.repan@evolveum.com>
2022-10-26 15:30:15 +0200

    MID-4568 added test with metarole + inducement + CoD

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestPreviewChangesCoD.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestPreviewChangesCoD.java
@@ -10,21 +10,20 @@ package com.evolveum.midpoint.model.intest;
 import java.io.File;
 import java.util.Arrays;
 
-import com.evolveum.midpoint.model.api.ModelExecuteOptions;
-
-import com.evolveum.midpoint.model.api.context.ModelContext;
-import com.evolveum.midpoint.prism.PrismObject;
-import com.evolveum.midpoint.prism.delta.ObjectDelta;
-
-import com.evolveum.midpoint.xml.ns._public.common.common_3.OrgType;
-
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+import com.evolveum.midpoint.model.api.ModelExecuteOptions;
+import com.evolveum.midpoint.model.api.context.ModelContext;
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.delta.ObjectDelta;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.task.api.Task;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AssignmentType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.OrgType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.RoleType;
 
 /**
  * Created by Viliam Repan (lazyman).
@@ -41,6 +40,10 @@ public class TestPreviewChangesCoD extends AbstractEmptyModelIntegrationTest {
 
     private static final File ORG_CHILD = new File(TEST_DIR, "org-child.xml");
 
+    private static final File ROLE_META = new File(TEST_DIR, "meta-role.xml");
+
+    private static final String ROLE_META_OID = "1ac00214-ffd0-49db-a1b9-51b46a0e9ae1";
+
     @Override
     protected File getSystemConfigurationFile() {
         return SYSTEM_CONFIGURATION_FILE;
@@ -51,38 +54,70 @@ public class TestPreviewChangesCoD extends AbstractEmptyModelIntegrationTest {
         super.initSystem(initTask, initResult);
 
         addObject(OBJECT_TEMPLATE_ORG, initTask, initResult);
+        addObject(ROLE_META, initTask, initResult);
     }
 
     @Test
     public void test100OrgNotProvisioned() throws Exception {
         given();
+
         Task task = getTestTask();
         OperationResult result = task.getResult();
 
-        searchObjectsIterative(OrgType.class, null, null, 0);
+        final Integer ORG_COUNT = modelService.countObjects(OrgType.class, null, null, task, result);
 
         when();
+
         PrismObject<OrgType> orgChild = prismContext.parseObject(ORG_CHILD);
         ObjectDelta delta = orgChild.createAddDelta();
 
         ModelContext<OrgType> context = modelInteractionService.previewChanges(Arrays.asList(delta), ModelExecuteOptions.create(), task, result);
 
         then();
-        AssertJUnit.assertNotNull(context);
 
-        searchObjectsIterative(OrgType.class, null, null, 0);
+        AssertJUnit.assertNotNull(context);
+        AssertJUnit.assertEquals("Orgs were created", ORG_COUNT, modelService.countObjects(OrgType.class, null, null, task, result));
+    }
+
+    @Test
+    public void test150OrgNotProvisionedWithMetarole() throws Exception {
+        given();
+
+        Task task = getTestTask();
+        OperationResult result = task.getResult();
+
+        final Integer ORG_COUNT = modelService.countObjects(OrgType.class, null, null, task, result);
+        final Integer ROLE_COUNT = modelService.countObjects(RoleType.class, null, null, task, result);
+
+        when();
+
+        PrismObject<OrgType> orgChild = prismContext.parseObject(ORG_CHILD);
+        // we'll add assignment to meta role
+        orgChild.asObjectable().getAssignment().add(new AssignmentType().targetRef(ROLE_META_OID, RoleType.COMPLEX_TYPE));
+        ObjectDelta delta = orgChild.createAddDelta();
+
+        System.out.println(delta.debugDump());
+
+        ModelContext<OrgType> context = modelInteractionService.previewChanges(Arrays.asList(delta), ModelExecuteOptions.create(), task, result);
+
+        then();
+
+        AssertJUnit.assertNotNull(context);
+        AssertJUnit.assertEquals("Orgs were created", ORG_COUNT, modelService.countObjects(OrgType.class, null, null, task, result));
+        AssertJUnit.assertEquals("Roles were created", ROLE_COUNT, modelService.countObjects(RoleType.class, null, null, task, result));
     }
 
     @Test
     public void test200EmptyOrgProvisioned() throws Exception {
         given();
+
         Task task = getTestTask();
         OperationResult result = task.getResult();
 
         when();
 
-
         then();
+
     }
 
     @Test
@@ -93,7 +128,16 @@ public class TestPreviewChangesCoD extends AbstractEmptyModelIntegrationTest {
 
         when();
 
-
         then();
+    }
+
+    @Test
+    public void test400AssociationTargetSearch() throws Exception {
+
+    }
+
+    @Test
+    public void test500ReferenceTargetSearch() throws Exception {
+
     }
 }
