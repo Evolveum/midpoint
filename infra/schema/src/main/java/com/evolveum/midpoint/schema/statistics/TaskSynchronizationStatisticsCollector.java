02fd77ffaf1c9fbdc7f82b2ea42899ced140d0c8
Pavol Mederly <mederly@evolveum.com>
2022-12-13 17:16:22 +0100

    Adapt TestThresholdsStoryLiveSyncSimulate
    It is quite questionable how should we report transitions between
    sync states when doing simulated synchronization.

--- infra/schema/src/main/java/com/evolveum/midpoint/schema/statistics/TaskSynchronizationStatisticsCollector.java
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/statistics/TaskSynchronizationStatisticsCollector.java
@@ -24,23 +24,23 @@ public interface TaskSynchronizationStatisticsCollector {
      * We assume that we have a shadow with OID by that time.
      * (If the OID is null we ignore further synchronization situation updates.)
      */
-    void onSynchronizationStart(@Nullable String processingIdentifier, @Nullable String shadowOid,
-            @Nullable SynchronizationSituationType situation);
+    void onSynchronizationStart(
+            @Nullable String processingIdentifier, @Nullable String shadowOid, @Nullable SynchronizationSituationType situation);
 
     /**
      * Informs the task that no synchronization will take place.
      * Note that in theory it is possible that {@link #onSynchronizationStart(String, String, SynchronizationSituationType)} is called first.
      */
-    void onSynchronizationExclusion(@Nullable String processingIdentifier,
-            @NotNull SynchronizationExclusionReasonType exclusionReason);
+    void onSynchronizationExclusion(
+            @Nullable String processingIdentifier, @NotNull SynchronizationExclusionReasonType exclusionReason);
 
     /**
      * Informs the task that sync situation has changed for given shadow OID.
      * There could be more such changes. But we are interested in the last one.
      * If the shadow OID is null, we ignore such updates.
      */
-    void onSynchronizationSituationChange(@Nullable String processingIdentifier, @Nullable String shadowOid,
-            @Nullable SynchronizationSituationType situation);
+    void onSynchronizationSituationChange(
+            @Nullable String processingIdentifier, @Nullable String shadowOid, @Nullable SynchronizationSituationType situation);
 
     /**
      * Starts collecting synchronization statistics within the task.
