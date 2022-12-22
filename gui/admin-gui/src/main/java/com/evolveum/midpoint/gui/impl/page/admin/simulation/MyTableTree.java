4241cf06076ea68271a5a740d760ec058767adaf
Viliam Repan <vilo.repan@evolveum.com>
2022-12-22 11:53:24 +0100

    cleaner table tree component, wip

---
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/MyTableTree.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2010-2022 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.gui.impl.page.admin.simulation;
+
+import java.util.List;
+import java.util.Set;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.tree.ITreeProvider;
+import org.apache.wicket.extensions.markup.html.repeater.tree.TableTree;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.IModel;
+
+/**
+ * Created by Viliam Repan (lazyman).
+ */
+public class MyTableTree<T, S> extends TableTree<T, S> {
+
+    public MyTableTree(String id, List<? extends IColumn<T, S>> columns, ITreeProvider<T> provider, long rowsPerPage) {
+        this(id, columns, provider, rowsPerPage, null);
+    }
+
+    public MyTableTree(String id, List<? extends IColumn<T, S>> columns, ITreeProvider<T> provider, long rowsPerPage, IModel<? extends Set<T>> state) {
+        super(id, columns, provider, rowsPerPage, state);
+    }
+
+    @Override
+    public Component newContentComponent(String id, IModel<T> model) {
+        return new MyTreeColumnPanel<>(id, this, model);
+    }
+
+    @Override
+    protected DataTable<T, S> newDataTable(String id, List<? extends IColumn<T, S>> iColumns, IDataProvider<T> dataProvider, long rowsPerPage) {
+        DataTable<T, S> table = super.newDataTable(id, iColumns, dataProvider, rowsPerPage);
+        table.add(AttributeModifier.replace("class", "table"));
+        return table;
+    }
+}
---
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/MyTreeColumnPanel.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2010-2022 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.gui.impl.page.admin.simulation;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.markup.html.repeater.tree.AbstractTree;
+import org.apache.wicket.markup.html.WebComponent;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.jetbrains.annotations.NotNull;
+
+import com.evolveum.midpoint.gui.api.component.BasePanel;
+import com.evolveum.midpoint.web.component.util.VisibleBehaviour;
+
+/**
+ * Created by Viliam Repan (lazyman).
+ */
+public class MyTreeColumnPanel<T> extends BasePanel<T> {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final String ID_LINK = "link";
+    private static final String ID_CHEVRON = "chevron";
+    private static final String ID_ICON = "icon";
+    private static final String ID_LABEL = "label";
+
+    private final AbstractTree<T> tree;
+
+    public MyTreeColumnPanel(@NotNull String id, @NotNull AbstractTree<T> tree, @NotNull IModel<T> model) {
+        super(id, model);
+
+        this.tree = tree;
+
+        initLayout();
+    }
+
+    private void initLayout() {
+        AjaxLink<T> link = new AjaxLink<>(ID_LINK) {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                onClickPerformed(target);
+            }
+        };
+        add(link);
+
+        WebComponent chevron = new WebComponent(ID_CHEVRON);
+        chevron.add(AttributeModifier.append("class", () -> getChevronCss(getModel())));
+        link.add(chevron);
+
+        WebComponent icon = new WebComponent(ID_ICON);
+        icon.add(AttributeModifier.append("class", () -> getIconCss(getModel())));
+        icon.add(new VisibleBehaviour(() -> getIconCss(getModel()) != null));
+        link.add(icon);
+
+        Label label = new Label(ID_LABEL, getModel());
+        link.add(label);
+    }
+
+    protected String getIconCss(IModel<T> model) {
+        return null;
+    }
+
+    protected String getChevronCss(IModel<T> model) {
+        T t = model.getObject();
+
+        String cssClass = "";
+        if (tree.getProvider().hasChildren(t)) {
+            if (tree.getState(t) == AbstractTree.State.EXPANDED) {
+                cssClass = "fa fa-chevron-down";
+            } else {
+                cssClass = "fa fa-chevron-right";
+            }
+        } else {
+            cssClass = "fa fa-chevron-right invisible";
+        }
+
+        if (isSelected()) {
+            cssClass += " active";
+        }
+
+        return cssClass;
+    }
+
+    protected boolean isSelected() {
+        return false;
+    }
+
+    protected void onClickPerformed(AjaxRequestTarget target) {
+        T t = getModelObject();
+
+        if (tree.getState(t) == AbstractTree.State.EXPANDED) {
+            tree.collapse(t);
+        } else {
+            tree.expand(t);
+        }
+    }
+}
