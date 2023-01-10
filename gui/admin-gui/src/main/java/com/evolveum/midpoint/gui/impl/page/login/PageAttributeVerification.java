81c02ad7d240dd92670875264aaab38092ddc38c
honchar <honchar@evolveum.com>
2023-01-10 17:29:22 +0100

    attributes verification

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/login/PageAttributeVerification.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/login/PageAttributeVerification.java
@@ -12,9 +12,9 @@ import com.evolveum.midpoint.authentication.api.config.MidpointAuthentication;
 import com.evolveum.midpoint.authentication.api.config.ModuleAuthentication;
 import com.evolveum.midpoint.authentication.api.util.AuthUtil;
 import com.evolveum.midpoint.authentication.api.util.AuthenticationModuleNameConstants;
+import com.evolveum.midpoint.prism.PrismProperty;
 import com.evolveum.midpoint.prism.query.ObjectQuery;
 import com.evolveum.midpoint.security.api.MidPointPrincipal;
-import com.evolveum.midpoint.web.component.AjaxButton;
 import com.evolveum.midpoint.web.component.AjaxSubmitButton;
 import com.evolveum.midpoint.web.component.form.MidpointForm;
 import com.evolveum.midpoint.web.component.prism.DynamicFormPanel;
@@ -27,21 +27,23 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
 import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
 import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.HiddenField;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -53,6 +55,7 @@ public class PageAttributeVerification extends PageAuthenticationBase {
 
 
     private static final String ID_MAIN_FORM = "mainForm";
+    private static final String ID_VERIFIED = "verified";
     private static final String ID_ATTRIBUTES = "attributes";
     private static final String ID_ATTRIBUTE_NAME = "attributeName";
     private static final String ID_ATTRIBUTE_VALUE = "attributeValue";
@@ -63,6 +66,7 @@ public class PageAttributeVerification extends PageAuthenticationBase {
     LoadableDetachableModel<List<ItemPathType>> attributesPathModel;
     private LoadableDetachableModel<UserType> userModel;
     private HashMap<ItemPathType, String> attributeValuesMap = new HashMap<>();
+    IModel<Boolean> verificationModel = Model.of(false);
 
     public PageAttributeVerification() {
     }
@@ -131,10 +135,15 @@ public class PageAttributeVerification extends PageAuthenticationBase {
     @Override
     protected void initCustomLayout() {
         MidpointForm<?> form = new MidpointForm<>(ID_MAIN_FORM);
+        form.add(AttributeModifier.replace("action", (IModel<String>) this::getUrlProcessingLogin));
         add(form);
 
         WebMarkupContainer csrfField = SecurityUtils.createHiddenInputForCsrf(ID_CSRF_FIELD);
         form.add(csrfField);
+
+        HiddenField<Boolean> verified = new HiddenField<>(ID_VERIFIED, verificationModel);
+        verified.setOutputMarkupId(true);
+        form.add(verified);
 
         initAttributesLayout(form);
 
@@ -182,7 +191,8 @@ public class PageAttributeVerification extends PageAuthenticationBase {
 
             @Override
             protected void onSubmit(AjaxRequestTarget target) {
-//                verifyAttributes(target);
+                verifyAttributes(target);
+                target.add(getVerifiedField());
             }
 
             @Override
@@ -193,6 +203,32 @@ public class PageAttributeVerification extends PageAuthenticationBase {
         };
         form.add(submit);
         form.add(createBackButton(ID_BACK_BUTTON));
+    }
+
+    private void verifyAttributes(AjaxRequestTarget target) {
+        for (ItemPathType itemPathType : attributesPathModel.getObject()) {
+            if (!attributeValueMatches(itemPathType)) {
+                return;
+            }
+        }
+        verificationModel.setObject(true);
+    }
+
+    private boolean attributeValueMatches(ItemPathType path) {
+        if (!attributeValuesMap.containsKey(path)) {
+            return false;
+        }
+        UserType user = userModel.getObject();
+
+        PrismProperty<?> property = user.asPrismObject().findProperty(path.getItemPath());
+        if (property == null) {
+            return false;
+        }
+        return attributeValuesMap.get(path).equals(property.getRealValue().toString());
+    }
+
+    private Component getVerifiedField() {
+        return  get(ID_MAIN_FORM).get(ID_VERIFIED);
     }
 
     @Override
@@ -206,5 +242,23 @@ public class PageAttributeVerification extends PageAuthenticationBase {
     protected DynamicFormPanel<UserType> getDynamicForm() {
         return null;
     }
+
+    private String getUrlProcessingLogin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof MidpointAuthentication) {
+            MidpointAuthentication mpAuthentication = (MidpointAuthentication) authentication;
+            ModuleAuthentication moduleAuthentication = mpAuthentication.getProcessingModuleAuthentication();
+            if (moduleAuthentication != null
+                    && AuthenticationModuleNameConstants.ATTRIBUTE_VERIFICATION.equals(moduleAuthentication.getModuleTypeName())){
+                String prefix = moduleAuthentication.getPrefix();
+                return AuthUtil.stripSlashes(prefix) + "/spring_security_login";
+            }
+        }
+
+        String key = "web.security.flexAuth.unsupported.auth.type";
+        error(getString(key));
+        return "/midpoint/spring_security_login";
+    }
+
 
 }
