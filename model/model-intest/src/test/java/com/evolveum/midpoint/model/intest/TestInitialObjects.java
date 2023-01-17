e02e98290370e02a9c3486966968a381dcf253e1
Viliam Repan <vilo.repan@evolveum.com>
2023-01-17 22:35:16 +0100

    moved and updated test/validation of initial import objects (from gui to model-intest)

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestInitialObjects.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestInitialObjects.java
@@ -5,23 +5,23 @@
  * and European Union Public License. See LICENSE file for details.
  */
 
-package com.evolveum.midpoint.web;
+package com.evolveum.midpoint.model.intest;
 
 import java.io.File;
 import java.io.IOException;
 
-import com.evolveum.midpoint.prism.path.ItemPath;
-
-import com.evolveum.midpoint.schema.result.OperationResultStatus;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectCollectionType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ReportType;
-
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.Test;
 
 import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.schema.result.OperationResultStatus;
 import com.evolveum.midpoint.schema.validator.ObjectValidator;
 import com.evolveum.midpoint.schema.validator.ValidationItem;
 import com.evolveum.midpoint.schema.validator.ValidationResult;
+import com.evolveum.midpoint.test.AbstractIntegrationTest;
 import com.evolveum.midpoint.util.exception.SchemaException;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
 
@@ -30,23 +30,29 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
  *
  * @author Radovan Semancik
  */
-public class TestInitialObjects extends AbstractGuiUnitTest {
+@ContextConfiguration(locations = { "classpath:ctx-model-intest-test-main.xml" })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class TestInitialObjects extends AbstractIntegrationTest {
 
-    private static final File DIR_INITIAL_OBJECTS = new File("src/main/resources/initial-objects");
+    private static final String INITIAL_OBJECTS_RESOURCE_PATTERN = "classpath*:/initial-objects/**/*.xml";
 
     @Test
     public void testInitialObjects() throws Exception {
-        ObjectValidator validator = new ObjectValidator(getPrismContext());
+        ObjectValidator validator = new ObjectValidator(prismContext);
         validator.setAllWarnings();
 
         StringBuilder errorsSb = new StringBuilder();
 
-        for (File file : DIR_INITIAL_OBJECTS.listFiles()) {
+        Resource[] resources = new PathMatchingResourcePatternResolver()
+                .getResources(INITIAL_OBJECTS_RESOURCE_PATTERN);
+
+        for (Resource resource : resources) {
+            File file = resource.getFile();
             if (file.isFile()) {
                 try {
                     testInitialObject(validator, errorsSb, file);
                 } catch (Throwable e) {
-                    String msg = "Error processing file "+file.getName()+": "+e.getMessage();
+                    String msg = "Error processing file " + file.getName() + ": " + e.getMessage();
                     logger.error(msg, e);
                     displayException(msg, e);
                     throw e;
@@ -60,13 +66,13 @@ public class TestInitialObjects extends AbstractGuiUnitTest {
     }
 
     private <O extends ObjectType> void testInitialObject(ObjectValidator validator, StringBuilder errorsSb, File file) throws SchemaException, IOException {
-        PrismObject<O> object = getPrismContext().parseObject(file);
+        PrismObject<O> object = prismContext.parseObject(file);
         ValidationResult validationResult = validator.validate(object);
         if (validationResult.isEmpty() || isIgnoredWarning(validationResult)) {
-            display("Checked "+object+": no warnings");
+            display("Checked " + object + ": no warnings");
             return;
         }
-        displayDumpable("Validation warnings for "+object, validationResult);
+        displayDumpable("Validation warnings for " + object, validationResult);
         for (ValidationItem valItem : validationResult.getItems()) {
             errorsSb.append(file.getName());
             errorsSb.append(" ");
@@ -78,8 +84,8 @@ public class TestInitialObjects extends AbstractGuiUnitTest {
     }
 
     private boolean isIgnoredWarning(ValidationResult validationResult) {
-        for (ValidationItem item : validationResult.getItems()){
-            if (!item.getStatus().equals(OperationResultStatus.WARNING)){
+        for (ValidationItem item : validationResult.getItems()) {
+            if (!item.getStatus().equals(OperationResultStatus.WARNING)) {
                 return false;
             }
         }
