d81862e9c0245a881c422349440054994a128dae
Richard Richter <richard.richter@evolveum.com>
2023-01-26 13:24:03 +0100

    reporting: low-level support for non-containerable nearly done

--- model/report-impl/src/main/java/com/evolveum/midpoint/report/impl/controller/ReportDataSource.java
+++ model/report-impl/src/main/java/com/evolveum/midpoint/report/impl/controller/ReportDataSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2021 Evolveum and contributors
+ * Copyright (C) 2010-2023 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
@@ -7,15 +7,14 @@
 
 package com.evolveum.midpoint.report.impl.controller;
 
-import com.evolveum.midpoint.prism.Containerable;
+import java.util.Collection;
+
 import com.evolveum.midpoint.prism.query.ObjectQuery;
 import com.evolveum.midpoint.schema.GetOperationOptions;
 import com.evolveum.midpoint.schema.ObjectHandler;
 import com.evolveum.midpoint.schema.SelectorOptions;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.util.exception.CommonException;
-
-import java.util.Collection;
 
 /**
  * Represents a source of data for a collection-based report.
