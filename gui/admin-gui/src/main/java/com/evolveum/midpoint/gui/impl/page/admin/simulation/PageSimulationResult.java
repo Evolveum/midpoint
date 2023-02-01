9ff38be90a02d0231f336f9e122dd59941065f7a
Viliam Repan <vilo.repan@evolveum.com>
2023-02-01 13:09:15 +0100

    simulation ui, breadcrumbs improvements

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/PageSimulationResult.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/PageSimulationResult.java
@@ -14,9 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
@@ -28,11 +30,15 @@ import org.jetbrains.annotations.NotNull;
 import com.evolveum.midpoint.authentication.api.authorization.AuthorizationAction;
 import com.evolveum.midpoint.authentication.api.authorization.PageDescriptor;
 import com.evolveum.midpoint.authentication.api.authorization.Url;
+import com.evolveum.midpoint.gui.api.component.Badge;
 import com.evolveum.midpoint.gui.api.component.wizard.NavigationPanel;
 import com.evolveum.midpoint.gui.api.model.LoadableModel;
+import com.evolveum.midpoint.gui.api.page.PageBase;
 import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
+import com.evolveum.midpoint.gui.api.util.WebModelServiceUtils;
 import com.evolveum.midpoint.gui.impl.component.box.SmallBoxData;
 import com.evolveum.midpoint.gui.impl.page.admin.simulation.widget.MetricWidgetPanel;
+import com.evolveum.midpoint.prism.PrismObject;
 import com.evolveum.midpoint.prism.impl.PrismPropertyValueImpl;
 import com.evolveum.midpoint.schema.util.ValueDisplayUtil;
 import com.evolveum.midpoint.security.api.AuthorizationConstants;
@@ -40,9 +46,7 @@ import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.web.component.util.VisibleEnableBehaviour;
 import com.evolveum.midpoint.web.page.admin.PageAdmin;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.DashboardWidgetType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.SimulationMetricValuesType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.SimulationResultType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 
 /**
  * Created by Viliam Repan (lazyman).
@@ -120,14 +124,21 @@ public class PageSimulationResult extends PageAdmin implements SimulationPage {
 
                     @Override
                     public Component createValueComponent(String id) {
-                        return new Label(id); // todo task status badge. Green if running and endTimestamp is not defined, Gray otherwise
+                        return createTaskStateLabel(id, resultModel, PageSimulationResult.this);
                     }
                 });
                 list.add(new ResultDetail("PageSimulationResult.productionConfiguration", null) {
 
                     @Override
                     public Component createValueComponent(String id) {
-                        Label label = new Label(id);
+                        Label label = new Label(id, () -> {
+                            ConfigurationSpecificationType specification = resultModel.getObject().getConfigurationUsed();
+                            if (specification == null || BooleanUtils.isNotFalse(specification.isProductionConfiguration())) {
+                                return getString("PageSimulationResult.production");
+                            }
+
+                            return getString("PageSimulationResult.development");
+                        });
                         label.add(AttributeModifier.replace("class", "badge badge-success"));
                         return label;
                     }
@@ -231,5 +242,40 @@ public class PageSimulationResult extends PageAdmin implements SimulationPage {
             label.setRenderBodyOnly(true);
             return label;
         }
+    }
+
+    public static Component createTaskStateLabel(String id, IModel<SimulationResultType> model, PageBase page) {
+        IModel<TaskExecutionStateType> stateModel = () -> {
+            SimulationResultType result = model.getObject();
+            if (result == null || result.getRootTaskRef() == null) {
+                return null;
+            }
+
+            PrismObject<TaskType> task = WebModelServiceUtils.loadObject(result.getRootTaskRef(), page);
+            return task != null ? task.asObjectable().getExecutionState() : null;
+        };
+
+        Label label = new Label(id, () -> {
+            if (model.getObject().getEndTimestamp() != null) {
+                return null;
+            }
+
+            TaskExecutionStateType state = stateModel.getObject();
+            if (state == null) {
+                return null;
+            }
+
+            return page.getString(state);
+        });
+        label.add(AttributeAppender.replace("class", () -> {
+            TaskExecutionStateType state = stateModel.getObject();
+            if (state == TaskExecutionStateType.RUNNABLE || state == TaskExecutionStateType.RUNNING) {
+                return Badge.State.SUCCESS.getCss();
+            }
+
+            return Badge.State.SECONDARY.getCss();
+        }));
+
+        return label;
     }
 }
