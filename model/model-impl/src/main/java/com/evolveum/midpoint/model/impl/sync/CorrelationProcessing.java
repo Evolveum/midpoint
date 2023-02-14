Evolveum/midpoint

commit 9a254359bfea939fd0219bb344cc09c310ccf855

Make (experimental) correlation simulation working
Simulation of shadows correlation is now implemented on back-end.
The respective deltas are sent to the simulation result.

Notes:

1. Clearing of correlation state before re-starting the correlation was
removed. It is most probably not necessary; and it would complicate
the simulation process.

2. Shadow-simulation mode is not supported when ID Match correlator
is used.

Work in progress. Some tests may fail.

model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/sync/CorrelationProcessing.java
@@ -104,7 +104,6 @@ class CorrelationProcessing<F extends FocusType> {
         OperationResult result = parentResult.subresult(OP_CORRELATE)
                 .build();
         try {
-            clearExistingCorrelationState(result);
             CompleteCorrelationResult correlationResult = correlateInRootCorrelator(result);
             applyCorrelationResultToShadow(correlationResult);
 
@@ -133,10 +132,11 @@ class CorrelationProcessing<F extends FocusType> {
             return null;
         }
         CorrelationSituationType situation = correlation.getSituation();
-        if (situation == CorrelationSituationType.EXISTING_OWNER && correlation.getResultingOwner() != null) {
-            ObjectType owner = resolveExistingOwner(correlation.getResultingOwner(), result);
+        ObjectReferenceType resultingOwnerRef = correlation.getResultingOwner();
+        if (situation == CorrelationSituationType.EXISTING_OWNER && resultingOwnerRef != null) {
+            ObjectType owner = resolveExistingOwner(resultingOwnerRef, result);
             if (owner != null) {
-                // We are not interested in other candidates here, hence the null values.
+                // We are not interested in other candidates here, hence the null value into candidateOwnersMap.
                 return CompleteCorrelationResult.existingOwner(owner, null, null);
             } else {
                 LOGGER.trace("Owner reference could not be resolved -> retry the correlation.");
@@ -197,19 +197,6 @@ class CorrelationProcessing<F extends FocusType> {
     private void processFinalResult(OperationResult result) throws SchemaException {
         beans.correlationCaseManager.closeCaseIfStillOpen(getShadow(), result);
         // TODO record case close if needed
-    }
-
-    private void clearExistingCorrelationState(OperationResult result) throws SchemaException {
-        if (syncCtx.getShadowedResourceObject().getCorrelation() == null) {
-            return; // nothing to clear
-        }
-        syncCtx.applyShadowDeltas(
-                beans.prismContext.deltaFor(ShadowType.class)
-                        .item(ShadowType.F_CORRELATION)
-                        .replace()
-                        .asItemDeltas());
-        // We commit this delta now to avoid overlapping with the follow-up correlation state deltas that will be added later.
-        syncCtx.getUpdater().commit(result);
     }
 
     private void applyCorrelationResultToShadow(CompleteCorrelationResult correlationResult) throws SchemaException {

model/model-api/src/main/java/com/evolveum/midpoint/model/api/correlator/CorrelatorFactoryRegistry.java
@@ -11,13 +11,11 @@ import javax.xml.namespace.QName;
 
 import org.jetbrains.annotations.NotNull;
 
-import com.evolveum.midpoint.schema.result.OperationResult;
-import com.evolveum.midpoint.task.api.Task;
-import com.evolveum.midpoint.util.exception.ConfigurationException;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.AbstractCorrelatorType;
-
 /**
  * A registry of correlator factories.
+ *
+ * This method resides in model API module just because a custom code may need to register its own correlator
+ * and correlator factory.
  */
 public interface CorrelatorFactoryRegistry {
 
@@ -27,13 +25,4 @@ public interface CorrelatorFactoryRegistry {
      * @param name Name of the configuration item. Must be qualified.
      */
     void registerFactory(@NotNull QName name, @NotNull CorrelatorFactory<?, ?> factory);
-
-    /**
-     * Convenience method to look up a correlator factory based on the specific (typed) configuration,
-     * and then instantiate the correlator.
-     */
-    <CB extends AbstractCorrelatorType> @NotNull Correlator instantiateCorrelator(
-            @NotNull CorrelatorContext<CB> correlatorContext,
-            @NotNull Task task,
-            @NotNull OperationResult result) throws ConfigurationException;
 }

model/model-api/src/main/java/com/evolveum/midpoint/model/api/simulation/ProcessedObject.java
@@ -69,6 +69,8 @@ public interface ProcessedObject<O extends ObjectType> extends DebugDumpable {
     boolean matches(@NotNull SimulationObjectPredicateType predicate, @NotNull Task task, @NotNull OperationResult result)
             throws CommonException;
     void resolveEventMarks(OperationResult result);
+    boolean hasEventMark(@NotNull String eventMarkOid);
+    boolean hasNoEventMarks();
 
     interface Factory {
         <O extends ObjectType> ProcessedObject<O> create(

model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/ModelBeans.java
@@ -9,13 +9,6 @@ package com.evolveum.midpoint.model.impl;
 
 import javax.annotation.PostConstruct;
 
-import com.evolveum.midpoint.model.common.MarkManager;
-import com.evolveum.midpoint.model.common.util.AuditHelper;
-import com.evolveum.midpoint.model.impl.controller.ModelController;
-import com.evolveum.midpoint.model.impl.correlation.CorrelationServiceImpl;
-
-import com.evolveum.midpoint.model.impl.simulation.SimulationResultManagerImpl;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -26,11 +19,15 @@ import com.evolveum.midpoint.common.Clock;
 import com.evolveum.midpoint.model.api.ModelService;
 import com.evolveum.midpoint.model.api.context.ProjectionContextKeyFactory;
 import com.evolveum.midpoint.model.api.correlation.CorrelationService;
-import com.evolveum.midpoint.model.api.correlator.CorrelatorFactoryRegistry;
+import com.evolveum.midpoint.model.common.MarkManager;
 import com.evolveum.midpoint.model.common.ModelCommonBeans;
 import com.evolveum.midpoint.model.common.archetypes.ArchetypeManager;
 import com.evolveum.midpoint.model.common.mapping.MappingFactory;
+import com.evolveum.midpoint.model.common.util.AuditHelper;
+import com.evolveum.midpoint.model.impl.controller.ModelController;
 import com.evolveum.midpoint.model.impl.correlation.CorrelationCaseManager;
+import com.evolveum.midpoint.model.impl.correlation.CorrelationServiceImpl;
+import com.evolveum.midpoint.model.impl.correlator.CorrelatorFactoryRegistryImpl;
 import com.evolveum.midpoint.model.impl.lens.*;
 import com.evolveum.midpoint.model.impl.lens.identities.IdentitiesManager;
 import com.evolveum.midpoint.model.impl.lens.indexing.IndexingManager;
@@ -46,6 +43,7 @@ import com.evolveum.midpoint.model.impl.lens.projector.policy.PolicyRuleSuspendT
 import com.evolveum.midpoint.model.impl.lens.projector.policy.scriptExecutor.PolicyRuleScriptExecutor;
 import com.evolveum.midpoint.model.impl.migrator.Migrator;
 import com.evolveum.midpoint.model.impl.security.SecurityHelper;
+import com.evolveum.midpoint.model.impl.simulation.SimulationResultManagerImpl;
 import com.evolveum.midpoint.model.impl.sync.SynchronizationService;
 import com.evolveum.midpoint.model.impl.sync.reactions.SynchronizationActionFactory;
 import com.evolveum.midpoint.model.impl.sync.tasks.SyncTaskHelper;
@@ -133,7 +131,7 @@ public class ModelBeans {
     @Autowired public PolicyRuleSuspendTaskExecutor policyRuleSuspendTaskExecutor;
     @Autowired public ClockworkHookHelper clockworkHookHelper;
     @Autowired public SecurityHelper securityHelper;
-    @Autowired public CorrelatorFactoryRegistry correlatorFactoryRegistry;
+    @Autowired public CorrelatorFactoryRegistryImpl correlatorFactoryRegistry;
     @Autowired public CorrelationCaseManager correlationCaseManager;
     @Autowired public CorrelationService correlationService;
     @Autowired public CorrelationServiceImpl correlationServiceImpl;

model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/correlation/CorrelationServiceImpl.java
@@ -15,6 +15,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
+import com.evolveum.midpoint.model.impl.correlator.CorrelatorFactoryRegistryImpl;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
@@ -70,7 +72,7 @@ public class CorrelationServiceImpl implements CorrelationService {
     private static final String OP_RESOLVE = CorrelationServiceImpl.class.getName() + ".resolve";
 
     @Autowired ModelBeans beans;
-    @Autowired CorrelatorFactoryRegistry correlatorFactoryRegistry;
+    @Autowired CorrelatorFactoryRegistryImpl correlatorFactoryRegistry;
     @Autowired SystemObjectCache systemObjectCache;
     @Autowired CorrelationCaseManager correlationCaseManager;
     @Autowired @Qualifier("cacheRepositoryService") RepositoryService repositoryService;

model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/correlator/BaseCorrelator.java
@@ -111,7 +111,6 @@ public abstract class BaseCorrelator<CCB extends AbstractCorrelatorType> impleme
             throws ConfigurationException, SchemaException, ExpressionEvaluationException, CommunicationException,
             SecurityViolationException, ObjectNotFoundException;
 
-
     @Override
     public @NotNull CorrelationExplanation explain(
             @NotNull CorrelationContext correlationContext,

model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/correlator/CorrelatorFactoryRegistryImpl.java
@@ -43,7 +43,10 @@ public class CorrelatorFactoryRegistryImpl implements CorrelatorFactoryRegistry 
         factories.put(name, factory);
     }
 
-    @Override
+    /**
+     * Convenience method to look up a correlator factory based on the specific (typed) configuration,
+     * and then instantiate the correlator.
+     */
     public <CB extends AbstractCorrelatorType> @NotNull Correlator instantiateCorrelator(
             @NotNull CorrelatorContext<CB> correlatorContext,
             @NotNull Task task,

model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/correlator/idmatch/IdMatchCorrelator.java
@@ -113,6 +113,13 @@ class IdMatchCorrelator extends BaseCorrelator<IdMatchCorrelatorType> {
                     correlationContext.getPreFocus(),
                     correlationContext.getResourceObject());
         }
+
+        void ensureNotInShadowSimulationMode() {
+            if (task.areShadowChangesSimulated()) {
+                throw new UnsupportedOperationException(
+                        "Shadows-simulation mode cannot be used with ID Match correlator: " + task.getExecutionMode());
+            }
+        }
     }
 
     private class CorrelationLikeOperation extends Operation {
@@ -125,6 +132,7 @@ class IdMatchCorrelator extends BaseCorrelator<IdMatchCorrelatorType> {
                 throws SchemaException, ExpressionEvaluationException, CommunicationException, SecurityViolationException,
                 ConfigurationException, ObjectNotFoundException {
 
+            ensureNotInShadowSimulationMode();
             MatchingResult mResult = executeMatchAndStoreTheResult(result);
             String referenceId = mResult.getReferenceId();
             if (referenceId != null) {
@@ -145,8 +153,7 @@ class IdMatchCorrelator extends BaseCorrelator<IdMatchCorrelatorType> {
 
             // FIXME this assumes we are the top-level correlator!
             IdMatchCorrelatorStateType correlatorState = createCorrelatorState(mResult);
-            correlationContext.setCorrelatorState(correlatorState);
-            // we also need the state in the shadow in the case object
+            correlationContext.setCorrelatorState(correlatorState); // not connected with the resource object
             ShadowUtil.setCorrelatorState(resourceObject, correlatorState.clone());
 
             return mResult;
@@ -426,10 +433,10 @@ class IdMatchCorrelator extends BaseCorrelator<IdMatchCorrelatorType> {
 
         private CorrelatorConfiguration getFollowOnConfiguration(CompositeCorrelatorType configBean)
                 throws ConfigurationException {
-            Collection<CorrelatorConfiguration> followOnConfigs =
-                    CorrelatorConfiguration.getChildConfigurations(configBean);
-            configCheck(followOnConfigs.size() == 1, "Not a single 'follow on' correlator configured: %s",
-                    followOnConfigs);
+            Collection<CorrelatorConfiguration> followOnConfigs = CorrelatorConfiguration.getChildConfigurations(configBean);
+            configCheck(
+                    followOnConfigs.size() == 1,
+                    "Not a single 'follow on' correlator configured: %s", followOnConfigs);
             return followOnConfigs.iterator().next();
         }
     }

commit 4b09bff502c95c38c2635396b2cad7bfb89c7a75

Fix correlation processing
The last start/end timestamp of correlation processing was not updated;
this was broken recently in 9a254359bfea939fd0219bb344cc09c310ccf855,
when the clearing was removed to facilitate the simulations.

Here we introduce at least setting of start and end timestamps,
hoping it will be enough.

This should fix TestFirstSteps.

model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/sync/CorrelationProcessing.java
@@ -94,10 +94,10 @@ class CorrelationProcessing<F extends FocusType> {
 
         assert syncCtx.getLinkedOwner() == null;
 
-        CompleteCorrelationResult existing = getResultFromExistingCorrelationState(parentResult);
+        CompleteCorrelationResult existing = getDefiniteResultFromExistingCorrelationState(parentResult);
         if (existing != null) {
-            LOGGER.debug("Result determined from existing correlation state in shadow: {}", existing.getSituation());
-            markShadowCorrelationFinished();
+            LOGGER.debug("Definite result determined from existing correlation state in shadow: {}", existing.getSituation());
+            setShadowCorrelationEndTime(existing.isDone()); // isDone is true here
             return existing;
         }
 
@@ -108,7 +108,7 @@ class CorrelationProcessing<F extends FocusType> {
             applyCorrelationResultToShadow(correlationResult);
 
             if (correlationResult.isDone()) {
-                processFinalResult(result);
+                closeCaseIfNeeded(result);
             }
             result.addArbitraryObjectAsReturn("correlationResult", correlationResult);
             return correlationResult;
@@ -120,14 +120,17 @@ class CorrelationProcessing<F extends FocusType> {
         }
     }
 
-    private CompleteCorrelationResult getResultFromExistingCorrelationState(OperationResult result) throws SchemaException {
+    /** Returns non-null only if there's certain result. */
+    private CompleteCorrelationResult getDefiniteResultFromExistingCorrelationState(OperationResult result)
+            throws SchemaException {
+        LOGGER.trace("Trying to get existing correlation result from the correlation state");
         ShadowCorrelationStateType correlation = syncCtx.getShadowedResourceObject().getCorrelation();
         if (correlation == null) {
-            LOGGER.trace("No correlation state");
+            LOGGER.trace("-> No correlation state");
             return null;
         }
         if (correlation.getCorrelationEndTimestamp() != null) {
-            LOGGER.trace("Existing correlation state found, but the correlation process is done. Ignoring the state:\n{}",
+            LOGGER.trace("-> Existing correlation state found, but the correlation process is done. Ignoring the state:\n{}",
                     correlation.debugDumpLazily(1));
             return null;
         }
@@ -136,16 +139,18 @@ class CorrelationProcessing<F extends FocusType> {
         if (situation == CorrelationSituationType.EXISTING_OWNER && resultingOwnerRef != null) {
             ObjectType owner = resolveExistingOwner(resultingOwnerRef, result);
             if (owner != null) {
+                LOGGER.trace("-> Found resultingOwnerRef, and resolved it into {}", owner);
                 // We are not interested in other candidates here, hence the null value into candidateOwnersMap.
                 return CompleteCorrelationResult.existingOwner(owner, null, null);
             } else {
-                LOGGER.trace("Owner reference could not be resolved -> retry the correlation.");
+                LOGGER.trace("-> Owner reference could not be resolved -> retry the correlation.");
                 return null;
             }
         } else if (situation == CorrelationSituationType.NO_OWNER) {
+            LOGGER.trace("-> was resolved to 'no owner'");
             return CompleteCorrelationResult.noOwner();
         } else {
-            LOGGER.trace("Neither 'existing owner' nor 'no owner' situation -> retry the correlation.");
+            LOGGER.trace("-> Neither 'existing owner' nor 'no owner' situation -> retry the correlation.");
             return null;
         }
     }
@@ -194,14 +199,16 @@ class CorrelationProcessing<F extends FocusType> {
         return beans.correlatorFactoryRegistry.instantiateCorrelator(rootCorrelatorContext, task, result);
     }
 
-    private void processFinalResult(OperationResult result) throws SchemaException {
+    private void closeCaseIfNeeded(OperationResult result) throws SchemaException {
         beans.correlationCaseManager.closeCaseIfStillOpen(getShadow(), result);
         // TODO record case close if needed
     }
 
     private void applyCorrelationResultToShadow(CompleteCorrelationResult correlationResult) throws SchemaException {
         S_ItemEntry builder = PrismContext.get().deltaFor(ShadowType.class);
-        if (getShadowCorrelationStartTimestamp() == null) {
+        XMLGregorianCalendar lastStart = getShadowCorrelationStartTimestamp();
+        XMLGregorianCalendar lastEnd = getShadowCorrelationEndTimestamp();
+        if (lastStart == null || lastEnd != null) {
             builder = builder
                     .item(ShadowType.F_CORRELATION, ShadowCorrelationStateType.F_CORRELATION_START_TIMESTAMP)
                     .replace(thisCorrelationStart);
@@ -232,17 +239,18 @@ class CorrelationProcessing<F extends FocusType> {
         syncCtx.applyShadowDeltas(
                 builder.asItemDeltas());
 
-        if (correlationResult.isDone()) {
-            markShadowCorrelationFinished();
-        }
+        setShadowCorrelationEndTime(correlationResult.isDone());
     }
 
-    private void markShadowCorrelationFinished() throws SchemaException {
+    private void setShadowCorrelationEndTime(boolean done) throws SchemaException {
+        if (!done && getShadowCorrelationEndTimestamp() == null) {
+            return;
+        }
         syncCtx.applyShadowDeltas(
                 PrismContext.get().deltaFor(ShadowType.class)
                         .item(ShadowType.F_CORRELATION, ShadowCorrelationStateType.F_CORRELATION_END_TIMESTAMP)
                         .replace(
-                                XmlTypeConverter.createXMLGregorianCalendar())
+                                done ? XmlTypeConverter.createXMLGregorianCalendar() : null)
                         .asItemDeltas());
     }
 
@@ -257,6 +265,11 @@ class CorrelationProcessing<F extends FocusType> {
     private @Nullable XMLGregorianCalendar getShadowCorrelationStartTimestamp() {
         ShadowCorrelationStateType state = getShadowCorrelationState();
         return state != null ? state.getCorrelationStartTimestamp() : null;
+    }
+
+    private @Nullable XMLGregorianCalendar getShadowCorrelationEndTimestamp() {
+        ShadowCorrelationStateType state = getShadowCorrelationState();
+        return state != null ? state.getCorrelationEndTimestamp() : null;
     }
 
     private @Nullable CorrelationSituationType getShadowCorrelationSituation() {