e06ec6e51547be7b4cf80fc41818831f2dc049ee
Viliam Repan <vilo.repan@evolveum.com>
2023-02-28 10:16:06 +0100

    fixed styles for assignment path panel

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/data/column/AssignmentPathPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/data/column/AssignmentPathPanel.java
@@ -7,13 +7,8 @@
 
 package com.evolveum.midpoint.web.component.data.column;
 
-import com.evolveum.midpoint.gui.api.component.BasePanel;
-import com.evolveum.midpoint.gui.api.model.LoadableModel;
-import com.evolveum.midpoint.web.component.AjaxButton;
-import com.evolveum.midpoint.web.component.util.VisibleBehaviour;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+import java.util.List;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -22,14 +17,22 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.evolveum.midpoint.gui.api.component.BasePanel;
+import com.evolveum.midpoint.web.component.AjaxIconButton;
+import com.evolveum.midpoint.web.component.util.VisibleBehaviour;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AssignmentPathMetadataType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AssignmentPathSegmentMetadataType;
 
 public class AssignmentPathPanel extends BasePanel<List<AssignmentPathMetadataType>> {
+
+    private static final long serialVersionUID = 1L;
+
     private static final String ID_PATHS = "paths";
-    private static final String ID_PATH = "path";
-    private static final String ID_DETAILED_VIEW_BUTTON = "detailedViewButton";
-    private static final String ID_DETAILED_VIEW_ICON = "detailedViewIcon";
+    private static final String ID_FIRST_SEGMENT = "firstSegment";
+    private static final String ID_OPEN = "open";
+    private static final String ID_BODY = "body";
+    private static final String ID_SEGMENTS = "segments";
+    private static final String ID_SEGMENT = "segment";
 
     public AssignmentPathPanel(String id, IModel<List<AssignmentPathMetadataType>> model) {
         super(id, model);
@@ -42,58 +45,47 @@ public class AssignmentPathPanel extends BasePanel<List<AssignmentPathMetadataTy
     }
 
     private void initLayout() {
+        add(AttributeAppender.append("class", "d-flex flex-column"));
 
-        ListView<AssignmentPathMetadataType> assignmentPaths = new ListView<>(ID_PATHS, getModel()) {
+        ListView<AssignmentPathMetadataType> paths = new ListView<>(ID_PATHS, getModel()) {
+
             @Override
-            protected void populateItem(ListItem<AssignmentPathMetadataType> listItem) {
-                IModel<Boolean> showAllSegmentsModel = Model.of(false);
+            protected void populateItem(ListItem<AssignmentPathMetadataType> item) {
+                item.setOutputMarkupId(true);
 
-                IModel<List<AssignmentPathSegmentMetadataType>> segmentsModel = loadSegmentsModel(listItem.getModel());
-                listItem.add(new AssignmentPathSegmentPanel(ID_PATH, segmentsModel) {
+                ObjectReferenceColumnPanel firstSegment = new ObjectReferenceColumnPanel(ID_FIRST_SEGMENT,
+                        () -> item.getModelObject().getSegment().get(0).getTargetRef());
+                item.add(firstSegment);
+
+                IModel<Boolean> openModel = Model.of(false);
+
+                AjaxIconButton open = new AjaxIconButton(ID_OPEN,
+                        () -> openModel.getObject() ? "fa fa-search-minus" : "fa fa-search-plus",
+                        () -> openModel.getObject() ? "AssignmentPathPanel.hideDetails" : "AssignmentPathPanel.showDetails") {
 
                     @Override
-                    protected boolean showAllSegments() {
-                        return showAllSegmentsModel.getObject();
-                    }
-                });
-                listItem.setOutputMarkupId(true);
-
-                AjaxButton detailedViewButton = new AjaxButton(ID_DETAILED_VIEW_BUTTON) {
-                    @Override
-                    public void onClick(AjaxRequestTarget ajaxRequestTarget) {
-                        showAllSegmentsModel.setObject(!showAllSegmentsModel.getObject());
-                        ajaxRequestTarget.add(listItem);
+                    public void onClick(AjaxRequestTarget target) {
+                        openModel.setObject(!openModel.getObject());
+                        target.add(item);
                     }
                 };
-                detailedViewButton.add(new VisibleBehaviour(() -> CollectionUtils.isNotEmpty(segmentsModel.getObject())));
-                detailedViewButton.setOutputMarkupId(true);
+                item.add(open);
 
-                WebMarkupContainer detailedViewIcon = new WebMarkupContainer(ID_DETAILED_VIEW_ICON);
-                detailedViewIcon.add(AttributeAppender.append("class", getIconStyleModel(showAllSegmentsModel)));
-                detailedViewButton.add(detailedViewIcon);
-                listItem.add(detailedViewButton);
+                WebMarkupContainer body = new WebMarkupContainer(ID_BODY);
+                body.add(new VisibleBehaviour(() -> openModel.getObject()));
+                item.add(body);
+
+                ListView<AssignmentPathSegmentMetadataType> segments = new ListView<>(ID_SEGMENTS, () -> item.getModelObject().getSegment().subList(1, item.getModelObject().getSegment().size())) {
+
+                    @Override
+                    protected void populateItem(ListItem<AssignmentPathSegmentMetadataType> item) {
+                        ObjectReferenceColumnPanel segment = new ObjectReferenceColumnPanel(ID_SEGMENT, () -> item.getModelObject().getTargetRef());
+                        item.add(segment);
+                    }
+                };
+                body.add(segments);
             }
         };
-        add(assignmentPaths);
-    }
-
-    private IModel<List<AssignmentPathSegmentMetadataType>> loadSegmentsModel(IModel<AssignmentPathMetadataType> assignmentModel) {
-        return () -> {
-            AssignmentPathMetadataType assignmentPathType = assignmentModel.getObject();
-           List<AssignmentPathSegmentMetadataType> segments = assignmentPathType.getSegment();
-           if (CollectionUtils.isEmpty(segments) || segments.size() == 1) {
-               return new ArrayList<>();
-           }
-           return segments;
-        };
-    }
-
-    private LoadableModel<String> getIconStyleModel(IModel<Boolean> showAllSegmentsModel) {
-        return new LoadableModel<String>() {
-            @Override
-            protected String load() {
-                return showAllSegmentsModel.getObject() ? "fa fa-search-minus" : "fa fa-search-plus";
-            }
-        };
+        add(paths);
     }
 }
