5c5cda88203ffd44ef5a78e1305d9e2eb35b46b7
Viliam Repan <vilo.repan@evolveum.com>
2023-03-07 12:30:54 +0100

    MID-8560 correlation NPE fix

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/workflow/InformationListPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/workflow/InformationListPanel.java
@@ -8,7 +8,9 @@
 package com.evolveum.midpoint.web.page.admin.workflow;
 
 import com.evolveum.midpoint.gui.api.component.BasePanel;
+import com.evolveum.midpoint.web.component.util.VisibleBehaviour;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.InformationType;
+
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
@@ -26,10 +28,13 @@ public class InformationListPanel extends BasePanel<List<InformationType>> {
     }
 
     private void initLayout() {
-        ListView<InformationType> list = new ListView<InformationType>(ID_INFORMATION_LIST, getModel()) {
+        ListView<InformationType> list = new ListView<>(ID_INFORMATION_LIST, getModel()) {
             @Override
             protected void populateItem(ListItem<InformationType> item) {
-                item.add(new InformationPanel(ID_INFORMATION, item.getModel()));
+                InformationPanel information = new InformationPanel(ID_INFORMATION, item.getModel());
+                information.add(new VisibleBehaviour(() -> item.getModelObject() != null));
+
+                item.add(information);
             }
         };
         add(list);
