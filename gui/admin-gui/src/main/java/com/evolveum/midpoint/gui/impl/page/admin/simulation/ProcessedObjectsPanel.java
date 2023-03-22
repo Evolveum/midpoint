049efcacd4597d1c69a953a0d91de88fb8a2d5a9
Viliam Repan <vilo.repan@evolveum.com>
2023-03-22 22:47:41 +0100

    simulations, processed objects (PO) results/statuses ui support. result displayed only for non success PO

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/ProcessedObjectsPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/simulation/ProcessedObjectsPanel.java
@@ -7,30 +7,12 @@
 
 package com.evolveum.midpoint.gui.impl.page.admin.simulation;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import javax.xml.namespace.QName;
-
-import com.google.common.collect.Lists;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.wicket.Component;
-import org.apache.wicket.RestartResponseException;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.LambdaColumn;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.jetbrains.annotations.NotNull;
-
 import com.evolveum.midpoint.gui.api.GuiStyleConstants;
 import com.evolveum.midpoint.gui.api.component.ObjectBrowserPanel;
 import com.evolveum.midpoint.gui.api.component.data.provider.ISelectableDataProvider;
 import com.evolveum.midpoint.gui.api.component.result.OperationResultPopupPanel;
 import com.evolveum.midpoint.gui.api.page.PageBase;
+import com.evolveum.midpoint.gui.api.util.LocalizationUtil;
 import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
 import com.evolveum.midpoint.gui.impl.component.ContainerableListPanel;
 import com.evolveum.midpoint.gui.impl.component.icon.CompositedIconBuilder;
@@ -46,6 +28,7 @@ import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.prism.query.ObjectFilter;
 import com.evolveum.midpoint.schema.DeltaConvertor;
 import com.evolveum.midpoint.schema.result.OperationResult;
+import com.evolveum.midpoint.schema.result.OperationResultStatus;
 import com.evolveum.midpoint.task.api.Task;
 import com.evolveum.midpoint.util.DisplayableValue;
 import com.evolveum.midpoint.util.MiscUtil;
@@ -62,6 +45,24 @@ import com.evolveum.midpoint.web.component.util.SelectableBean;
 import com.evolveum.midpoint.web.session.UserProfileStorage;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 import com.evolveum.prism.xml.ns._public.types_3.ObjectDeltaType;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.Component;
+import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.LambdaColumn;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.jetbrains.annotations.NotNull;
+
+import javax.xml.namespace.QName;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by Viliam Repan (lazyman).
@@ -169,14 +170,26 @@ public abstract class ProcessedObjectsPanel extends ContainerableListPanel<Simul
 
                     @Override
                     protected IModel<String> createIconCssModel() {
-                        // todo add condition
-                        return () -> GuiStyleConstants.CLASS_OP_RESULT_STATUS_ICON_WARNING_COLORED;
+                        return () -> {
+                            OperationResultStatus status = model.getObject().getResultStatus();
+                            if (status == null) {
+                                return null;
+                            }
+
+                            return status.isConsideredSuccess() ? null : GuiStyleConstants.CLASS_OP_RESULT_STATUS_ICON_WARNING_COLORED;
+                        };
                     }
 
                     @Override
                     protected IModel<String> createIconTitleModel() {
-                        // todo icon title
-                        return super.createIconTitleModel();
+                        return () -> {
+                            OperationResultStatus status = model.getObject().getResultStatus();
+                            if (status == null) {
+                                return null;
+                            }
+
+                            return getString(LocalizationUtil.createKeyForEnum(status));
+                        };
                     }
                 });
             }
@@ -186,7 +199,7 @@ public abstract class ProcessedObjectsPanel extends ContainerableListPanel<Simul
     private void showOperationResult(AjaxRequestTarget target, IModel<ProcessedObject<?>> model) {
         PageBase page = getPageBase();
 
-        IModel<OperationResult> result = () -> null;        // todo fix result
+        IModel<OperationResult> result = () -> model.getObject().getResult();
 
         page.showMainPopup(new OperationResultPopupPanel(page.getMainPopupBodyId(), result), target);
     }
@@ -379,7 +392,7 @@ public abstract class ProcessedObjectsPanel extends ContainerableListPanel<Simul
         return new AbstractColumn<>(displayModel, SimulationResultProcessedObjectType.F_STATE.getLocalPart()) {
             @Override
             public void populateItem(Item<ICellPopulator<SelectableBean<SimulationResultProcessedObjectType>>> item, String id,
-                    IModel<SelectableBean<SimulationResultProcessedObjectType>> row) {
+                                     IModel<SelectableBean<SimulationResultProcessedObjectType>> row) {
 
                 item.add(SimulationsGuiUtil.createProcessedObjectStateLabel(id, () -> row.getObject().getValue()));
             }
@@ -426,7 +439,7 @@ public abstract class ProcessedObjectsPanel extends ContainerableListPanel<Simul
     }
 
     private void markObjects(IModel<SelectableBean<SimulationResultProcessedObjectType>> rowModel, List<String> markOids,
-            AjaxRequestTarget target) {
+                             AjaxRequestTarget target) {
         OperationResult result = new OperationResult(OPERATION_MARK_SHADOW);
         Task task = getPageBase().createSimpleTask(OPERATION_MARK_SHADOW);
 
@@ -462,8 +475,8 @@ public abstract class ProcessedObjectsPanel extends ContainerableListPanel<Simul
                                 statements.toArray(new PolicyStatementType[0]));
                 getPageBase().getModelService().executeChanges(MiscUtil.createCollection(delta), null, task, result);
             } catch (ObjectAlreadyExistsException | ObjectNotFoundException | SchemaException
-                    | ExpressionEvaluationException | CommunicationException | ConfigurationException
-                    | PolicyViolationException | SecurityViolationException e) {
+                     | ExpressionEvaluationException | CommunicationException | ConfigurationException
+                     | PolicyViolationException | SecurityViolationException e) {
                 result.recordPartialError(
                         createStringResource(
                                 "ResourceContentPanel.message.markShadowPerformed.partialError", shadow)
