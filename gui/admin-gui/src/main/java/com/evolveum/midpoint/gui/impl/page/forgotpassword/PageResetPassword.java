bb820a587c129ed65fcf611e7c557a87ed907b3c
lskublik <lskublik@evolveum.com>
2023-04-13 09:59:41 +0200

    MID-8696:showing of  success message after changing of password without showing result

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/forgotpassword/PageResetPassword.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/forgotpassword/PageResetPassword.java
@@ -108,7 +108,7 @@ public class PageResetPassword extends AbstractPageLogin {
                         }
                     }
 
-                    showResult(result);
+                    success(getString("PageResetPassword.reset.successful"));
                     AuthUtil.clearMidpointAuthentication();
                     throw new RestartResponseException(PageLogin.class);
                 } else if (showFeedback) {
