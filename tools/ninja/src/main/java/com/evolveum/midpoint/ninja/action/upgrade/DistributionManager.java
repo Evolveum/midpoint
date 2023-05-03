d97b96ccdd968877459e119940ad5a3b249f715a
Viliam Repan <vilo.repan@evolveum.com>
2023-05-03 13:56:13 +0200

    MID-8842 distribution manager, wip

---
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/upgrade/DistributionManager.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2010-2023 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.ninja.action.upgrade;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * URL format: https://download.evolveum.com/midpoint/<VERSION>/midpoint-<VERSION_NUMBER>-dist.zip
+ * VERSION can be: 3.4.1, ..., 4.7, latest
+ * VERSION_NUMBER can be: 3.4.1, ..., 4.7, SNAPSHOT
+ */
+public class DistributionManager {
+
+    private static final String DOWNLOAD_URL = "https://download.evolveum.com/midpoint/";
+
+    private static final String LATEST_VERSION = "latest";
+
+    private static final String LATEST_VERSION_NUMBER = "SNAPSHOT";
+
+    public File downloadDistribution(@NotNull String version, ProgressListener listener) throws IOException {
+        String versionNumber = LATEST_VERSION.equals(version) ? LATEST_VERSION_NUMBER : version;
+
+        String distributionFile = createFileName(versionNumber);
+
+        File file = new File(FileUtils.getTempDirectory(), "midpoint-upgrade/" + System.currentTimeMillis() + "-" + distributionFile);
+        FileUtils.forceMkdirParent(file);
+        file.createNewFile();
+
+        StringBuilder url = new StringBuilder();
+        url.append(DOWNLOAD_URL);
+        url.append(version);
+        url.append("/");
+        url.append(distributionFile);
+
+        Request request = new Request.Builder()
+                .url(url.toString())
+                .build();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addNetworkInterceptor(chain -> {
+                    Response originalResponse = chain.proceed(chain.request());
+                    return originalResponse.newBuilder()
+                            .body(new ProgressResponseBody(originalResponse.body(), listener))
+                            .build();
+                })
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            try (InputStream is = response.body().byteStream()) {
+                FileUtils.copyInputStreamToFile(is, file);
+            }
+        }
+
+        return file;
+    }
+
+    private String createFileName(String versionNumber) {
+        return "midpoint-" + versionNumber + "-dist.zip";
+    }
+}
---
+++ tools/ninja/src/test/java/com/evolveum/midpoint/ninja/upgrade/DistributionManagerTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2010-2023 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.ninja.upgrade;
+
+import java.io.File;
+
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import com.evolveum.midpoint.ninja.action.upgrade.ConsoleProgressListener;
+import com.evolveum.midpoint.ninja.action.upgrade.DistributionManager;
+import com.evolveum.midpoint.ninja.action.upgrade.ProgressListener;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
+
+public class DistributionManagerTest {
+
+    private static final Trace LOGGER = TraceManager.getTrace(DistributionManagerTest.class);
+
+    @Test(enabled = false)
+    public void downloadLTS() throws Exception {
+        AnsiConsole.systemInstall();
+
+        final ProgressListener listener = new ConsoleProgressListener();
+
+        File file = new DistributionManager().downloadDistribution("4.4.4", listener);
+        AssertJUnit.assertTrue(file.exists());
+        LOGGER.info("File size: " + file.length());
+        LOGGER.info("File path: " + file.getAbsolutePath());
+
+        AnsiConsole.systemUninstall();
+    }
+
+    @Test(enabled = false)
+    public void testJANSI() {
+        AnsiConsole.systemInstall();
+
+        System.out.println(Ansi.ansi().fgBlue().a("Start").reset());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Ansi.ansi().eraseLine().fgGreen().a(i).reset());
+        }
+        System.out.println(Ansi.ansi().fgRed().a("Complete").reset());
+
+        AnsiConsole.systemUninstall();
+    }
+}
