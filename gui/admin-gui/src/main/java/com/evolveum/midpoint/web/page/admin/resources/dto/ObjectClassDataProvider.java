3033eeb86e7c308136d5ce5b8744cbfd41c6a823
lskublik <lskublik@evolveum.com>
2023-05-17 19:46:48 +0200

    clean up old resource wizard classes

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/resources/dto/ObjectClassDataProvider.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/admin/resources/dto/ObjectClassDataProvider.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2010-2020 Evolveum and contributors
+ * Copyright (C) 2010-2023 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
  */
-package com.evolveum.midpoint.web.component.wizard.resource.dto;
+package com.evolveum.midpoint.web.page.admin.resources.dto;
 
 import java.util.ArrayList;
 import java.util.Iterator;
