5c60dcee6679fb45dc18b78b03a88cdf8f3a4ae3
lskublik <lskublik@evolveum.com>
2023-05-26 08:55:25 +0200

    adding last rest test of oidc auth module for keycloak

--- testing/rest/src/test/java/com/evolveum/midpoint/testing/rest/authentication/oidc/TestAbstractOidcRestModule.java
+++ testing/rest/src/test/java/com/evolveum/midpoint/testing/rest/authentication/oidc/TestAbstractOidcRestModule.java
@@ -14,8 +14,10 @@ import com.evolveum.midpoint.testing.rest.authentication.TestAbstractAuthenticat
 import com.evolveum.midpoint.xml.ns._public.common.common_3.SystemObjectsType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.UserType;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.text.StringEscapeUtils;
+import org.apache.cxf.common.util.Base64Utility;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.keycloak.authorization.client.AuthzClient;
 import org.keycloak.representations.AccessTokenResponse;
@@ -35,12 +37,20 @@ public abstract class TestAbstractOidcRestModule extends TestAbstractAuthenticat
 
     public static final String USER_ADMINISTRATOR_USERNAME = "administrator";
 
-    public static final File SECURITY_POLICY_ISSUER_URI = new File(BASE_REPO_DIR, "security-policy-issuer-uri.xml");
-    public static final File SECURITY_POLICY_JWS_URI = new File(BASE_REPO_DIR, "security-policy-jws-uri.xml");
-    public static final File SECURITY_POLICY_JWS_URI_WRONG_ALG = new File(BASE_REPO_DIR, "security-policy-jws-uri-wrong-alg.xml");
-    public static final File SECURITY_POLICY_PUBLIC_KEY = new File(BASE_REPO_DIR, "security-policy-public-key.xml");
-    public static final File SECURITY_POLICY_PUBLIC_KEY_WRONG_ALG = new File(BASE_REPO_DIR, "security-policy-public-key-wrong-alg.xml");
-    public static final File SECURITY_POLICY_PUBLIC_KEY_KEYSTORE = new File(BASE_REPO_DIR, "security-policy-public-key-keystore.xml");
+    public static final File SECURITY_POLICY_ISSUER_URI =
+            new File(BASE_REPO_DIR, "security-policy-issuer-uri.xml");
+    public static final File SECURITY_POLICY_JWS_URI =
+            new File(BASE_REPO_DIR, "security-policy-jws-uri.xml");
+    public static final File SECURITY_POLICY_JWS_URI_WRONG_ALG =
+            new File(BASE_REPO_DIR, "security-policy-jws-uri-wrong-alg.xml");
+    public static final File SECURITY_POLICY_PUBLIC_KEY =
+            new File(BASE_REPO_DIR, "security-policy-public-key.xml");
+    public static final File SECURITY_POLICY_PUBLIC_KEY_WRONG_ALG =
+            new File(BASE_REPO_DIR, "security-policy-public-key-wrong-alg.xml");
+    public static final File SECURITY_POLICY_PUBLIC_KEY_KEYSTORE =
+            new File(BASE_REPO_DIR, "security-policy-public-key-keystore.xml");
+    public static final File SECURITY_POLICY_WRONG_ATTRIBUTE_NAME =
+            new File(BASE_REPO_DIR, "security-policy-wrong-attribute-name.xml");
 
     private static final String MIDPOINT_USER_PASSWORD_KEY = "midpointUserPassword";
     protected static final String CLIENT_ID_KEY = "clientId";
@@ -146,7 +156,7 @@ public abstract class TestAbstractOidcRestModule extends TestAbstractAuthenticat
     @Test
     public void test005oidcAuthByPublicKeyWithWrongAlg() throws Exception {
         String securityContent = getSecurityPolicy(SECURITY_POLICY_PUBLIC_KEY_WRONG_ALG);
-        securityContent = securityContent.replace(createTag(TRUSTING_ASYMMETRIC_CERT_KEY), getProperty(TRUSTING_ASYMMETRIC_CERT_KEY));
+        securityContent = securityContent.replace(createTag(TRUSTING_ASYMMETRIC_CERT_KEY), getPublicKey());
         replaceSecurityPolicy(securityContent);
 
         WebClient client = prepareClient();
@@ -161,13 +171,22 @@ public abstract class TestAbstractOidcRestModule extends TestAbstractAuthenticat
     @Test
     public void test006OidcAuthByPublicKeyAsKeystore() throws Exception {
         KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keystore.load(null);
         String publicKey = getPublicKey();
 
+        byte[] certBytes;
+        if (Base64.isBase64(publicKey)) {
+            boolean isBase64Url = publicKey.contains("-") || publicKey.contains("_");
+            certBytes = Base64Utility.decode(publicKey, isBase64Url);
+        } else {
+            certBytes = publicKey.getBytes();
+        }
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        Certificate certificate = cf.generateCertificate(new ByteArrayInputStream(publicKey.getBytes(StandardCharsets.UTF_8)));
+        Certificate certificate = cf.generateCertificate(new ByteArrayInputStream(certBytes));
         keystore.setCertificateEntry("cert", certificate);
 
-        File file = new File(MIDPOINT_HOME + "/test-keystore");
+        String pathToKeystore = MIDPOINT_HOME + "/test-keystore";
+        File file = new File(pathToKeystore);
         file.deleteOnExit();
         if (file.exists()) {
             file.delete();
@@ -178,7 +197,7 @@ public abstract class TestAbstractOidcRestModule extends TestAbstractAuthenticat
         out.close();
 
         String securityContent = getSecurityPolicy(SECURITY_POLICY_PUBLIC_KEY_KEYSTORE);
-        securityContent = securityContent.replace(createTag(KEY_STORE_PATH_KEY), getPublicKey());
+        securityContent = securityContent.replace(createTag(KEY_STORE_PATH_KEY), pathToKeystore);
         replaceSecurityPolicy(securityContent);
 
         WebClient client = prepareClient();
@@ -188,6 +207,21 @@ public abstract class TestAbstractOidcRestModule extends TestAbstractAuthenticat
 
         then();
         assertSuccess(response);
+    }
+
+    @Test
+    public void test007WrongAttributeName() throws Exception {
+        String securityContent = getSecurityPolicy(SECURITY_POLICY_WRONG_ATTRIBUTE_NAME);
+        securityContent = securityContent.replace(createTag(ISSUER_URI_KEY), getProperty(ISSUER_URI_KEY));
+        replaceSecurityPolicy(securityContent);
+
+        WebClient client = prepareClient();
+
+        when();
+        Response response = client.get();
+
+        then();
+        assertUnsuccess(response, 0);
     }
 
     protected abstract String getPublicKey();
@@ -221,10 +255,16 @@ public abstract class TestAbstractOidcRestModule extends TestAbstractAuthenticat
     }
 
     protected void assertUnsuccess(Response response) {
+        assertUnsuccess(response, 1);
+    }
+
+    protected void assertUnsuccess(Response response, int expectedAuditRecords) {
         assertStatus(response, 401);
 
-        displayDumpable("Audit", getDummyAuditService());
-        getDummyAuditService().assertRecords(1);
-        getDummyAuditService().assertFailedLogin(SchemaConstants.CHANNEL_REST_URI);
+        if (expectedAuditRecords != 0) {
+            displayDumpable("Audit", getDummyAuditService());
+            getDummyAuditService().assertRecords(expectedAuditRecords);
+            getDummyAuditService().assertFailedLogin(SchemaConstants.CHANNEL_REST_URI);
+        }
     }
 }
