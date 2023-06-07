0615e89ab5026696bafc7245929edec1cf901139
Viliam Repan <vilo.repan@evolveum.com>
2023-06-07 14:49:18 +0200

    MID-8842 upgrade, added global metadata info to RepoDiag

---
+++ repo/repo-sqale/src/main/java/com/evolveum/midpoint/repo/sqale/qmodel/common/MGlobalMetadata.java
@@ -0,0 +1,16 @@
+package com.evolveum.midpoint.repo.sqale.qmodel.common;
+
+public class MGlobalMetadata {
+
+    public String name;
+
+    public String value;
+
+    @Override
+    public String toString() {
+        return "MGlobalMetadata{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
