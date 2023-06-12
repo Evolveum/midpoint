eac6bfb6e238a91cbf55ee33c5e5d7f6808c422e
Pavol Mederly <mederly@evolveum.com>
2023-06-12 21:07:15 +0200

    Deduplicate code in SelectorToFilterTranslator
    There was a duplication of selector-to-filter translating code between
    linked objects and security enforcer since 4.2. Now, with the advent of
    ValueSelector, it became easy to fix. This commit does that.

--- repo/repo-common/src/main/java/com/evolveum/midpoint/repo/common/query/LinkedSelectorToFilterTranslator.java
+++ repo/repo-common/src/main/java/com/evolveum/midpoint/repo/common/query/LinkedSelectorToFilterTranslator.java
@@ -7,28 +7,22 @@
 
 package com.evolveum.midpoint.repo.common.query;
 
-import static java.util.Collections.singletonList;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import com.evolveum.midpoint.prism.PrismContext;
 import com.evolveum.midpoint.prism.PrismReferenceValue;
 import com.evolveum.midpoint.prism.query.ObjectFilter;
-import com.evolveum.midpoint.repo.common.expression.ExpressionFactory;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.task.api.Task;
 import com.evolveum.midpoint.util.annotation.Experimental;
 import com.evolveum.midpoint.util.exception.*;
+import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.AssignmentHolderType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.LinkedObjectSelectorType;
-
-import org.jetbrains.annotations.Nullable;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
 
 /**
  * Translates a linked selector (LinkedObjectSelectorType) to appropriate ObjectFilter.
@@ -41,40 +35,40 @@ public class LinkedSelectorToFilterTranslator {
     @NotNull private final PrismReferenceValue targetObjectRef;
     @NotNull private final SelectorToFilterTranslator objectSelectorTranslator;
 
-    @NotNull private final List<ObjectFilter> components = new ArrayList<>();
-
-    public LinkedSelectorToFilterTranslator(@Nullable LinkedObjectSelectorType selector, @NotNull PrismReferenceValue targetObjectRef,
-            @NotNull String contextDescription, @NotNull PrismContext prismContext, @NotNull ExpressionFactory expressionFactory,
-            @NotNull Task task, @NotNull OperationResult result) {
-        this.selector = selector != null ? selector : new LinkedObjectSelectorType(prismContext);
+    public LinkedSelectorToFilterTranslator(
+            @Nullable LinkedObjectSelectorType selector,
+            @NotNull PrismReferenceValue targetObjectRef,
+            @NotNull String contextDescription,
+            @NotNull Trace logger,
+            @NotNull Task task) throws ConfigurationException {
+        this.selector = selector != null ? selector : new LinkedObjectSelectorType();
         this.targetObjectRef = targetObjectRef;
-        this.objectSelectorTranslator = new SelectorToFilterTranslator(this.selector, AssignmentHolderType.class,
-                contextDescription, prismContext, expressionFactory, task, result);
+        this.objectSelectorTranslator =
+                new SelectorToFilterTranslator(this.selector, AssignmentHolderType.class, contextDescription, logger, task);
     }
 
     @NotNull
-    public ObjectFilter createFilter() throws SchemaException, ConfigurationException, ObjectNotFoundException,
+    public ObjectFilter createFilter(OperationResult result) throws SchemaException, ConfigurationException, ObjectNotFoundException,
             CommunicationException, SecurityViolationException, ExpressionEvaluationException {
-        components.add(objectSelectorTranslator.createFilter());
-        components.add(
-                objectSelectorTranslator.prismContext.queryFor(objectSelectorTranslator.getNarrowedTargetType())
+        return PrismContext.get().queryFactory().createAndOptimized(
+                objectSelectorTranslator.createFilter(result),
+                PrismContext.get().queryFor(AssignmentHolderType.class)
                         .item(AssignmentHolderType.F_ROLE_MEMBERSHIP_REF)
                         .ref(getTargetObjectRefValues())
                         .buildFilter());
-        return objectSelectorTranslator.queryFactory.createAndOptimized(components);
     }
 
     private List<PrismReferenceValue> getTargetObjectRefValues() {
         if (selector.getRelation().isEmpty()) {
-            return singletonList(targetObjectRef);
+            return List.of(targetObjectRef);
         } else {
             return selector.getRelation().stream()
                     .map(relation -> targetObjectRef.clone().relation(relation))
-                    .collect(Collectors.toList());
+                    .toList();
         }
     }
 
-    public Class<? extends ObjectType> getNarrowedTargetType() {
+    public Class<? extends ObjectType> getNarrowedTargetType() throws ConfigurationException {
         return objectSelectorTranslator.getNarrowedTargetType();
     }
 }
