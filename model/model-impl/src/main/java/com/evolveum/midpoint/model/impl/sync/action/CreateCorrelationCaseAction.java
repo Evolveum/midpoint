228b6a6eadc3fd9484674b9d100611628bf58ebd
Pavol Mederly <mederly@evolveum.com>
2023-06-12 19:54:39 +0200

    Skip creation/update of corr cases when simulating
    Correlation cases should be created or updated only when running
    in full execution mode.
    
    This resolves MID-8848.

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/sync/action/CreateCorrelationCaseAction.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/sync/action/CreateCorrelationCaseAction.java
@@ -12,11 +12,15 @@ import com.evolveum.midpoint.model.impl.sync.reactions.ActionDefinitionClass;
 import com.evolveum.midpoint.model.impl.sync.reactions.ActionInstantiationContext;
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.util.exception.CommonException;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.CreateCorrelationCaseSynchronizationActionType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.FocusType;
 
 @ActionDefinitionClass(CreateCorrelationCaseSynchronizationActionType.class)
 public class CreateCorrelationCaseAction<F extends FocusType> extends BaseAction<F> {
+
+    private static final Trace LOGGER = TraceManager.getTrace(CreateCorrelationCaseAction.class);
 
     private static final String OP_HANDLE = CreateCorrelationCaseAction.class.getName() + ".handle";
 
@@ -28,12 +32,18 @@ public class CreateCorrelationCaseAction<F extends FocusType> extends BaseAction
     public void handle(@NotNull OperationResult parentResult) throws CommonException {
         OperationResult result = parentResult.subresult(OP_HANDLE).build();
         try {
-            beans.correlationCaseManager.createOrUpdateCase(
-                    syncCtx.getShadowedResourceObject(),
-                    syncCtx.getResource(),
-                    syncCtx.getPreFocus(),
-                    syncCtx.getTask(),
-                    result);
+            if (syncCtx.isFullMode()) {
+                beans.correlationCaseManager.createOrUpdateCase(
+                        syncCtx.getShadowedResourceObject(),
+                        syncCtx.getResource(),
+                        syncCtx.getPreFocus(),
+                        syncCtx.getTask(),
+                        result);
+            } else {
+                LOGGER.debug("Not creating nor updating correlation case because not in full execution mode: {}",
+                        syncCtx.getExecutionMode());
+                result.recordNotApplicable("Not in full execution mode");
+            }
         } catch (Throwable t) {
             result.recordFatalError(t);
             throw t;
