a1d94859b79e3fd399c2042fcace2e32bc31d78d
Viliam Repan <vilo.repan@evolveum.com>
2023-07-07 14:15:20 +0200

    MID-8842 ninja - fixed message for few options

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/upgrade/UpgradeCommonOptions.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/upgrade/UpgradeCommonOptions.java
@@ -3,13 +3,15 @@ package com.evolveum.midpoint.ninja.action.upgrade;
 import java.io.File;
 
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
 import org.apache.commons.io.FileUtils;
 
+@Parameters(resourceBundle = "messages")
 public class UpgradeCommonOptions {
 
     public static final String P_TEMP_DIR_LONG = "--temp-dir";
 
-    @Parameter(names = { P_TEMP_DIR_LONG }, descriptionKey = "upgrade.tempDir") // todo fix key
+    @Parameter(names = { P_TEMP_DIR_LONG }, descriptionKey = "upgradeDistribution.tempDir") // todo fix key
     private File tempDirectory;
 
     public File getTempDirectory() {
