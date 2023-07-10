88431330d002ea67b3318d04cca05f5e7bcf7b28
Viliam Repan <vilo.repan@evolveum.com>
2023-07-10 13:35:10 +0200

    MID-8842 ninja - DB schema version skip check option + some logging added (debug level)
    (cherry picked from commit 44e334df92a2e216ec56db9076f6a93ace3b8f6d)

--- infra/common/src/main/java/com/evolveum/midpoint/common/configuration/api/MidpointConfiguration.java
+++ infra/common/src/main/java/com/evolveum/midpoint/common/configuration/api/MidpointConfiguration.java
@@ -19,6 +19,8 @@ public interface MidpointConfiguration {
     String MIDPOINT_LOGGING_ALT_FILENAME_PROPERTY = "midpoint.logging.alt.filename";
     String MIDPOINT_LOGGING_ALT_PREFIX_PROPERTY = "midpoint.logging.alt.prefix";
 
+    String MIDPOINT_SKIP_VERSION_CHECK="midpoint.skipVersionCheck";
+
     String USER_HOME_PROPERTY = "user.home";
 
     String MIDPOINT_CONFIG_FILE_PROPERTY = "midpoint.configFile";
