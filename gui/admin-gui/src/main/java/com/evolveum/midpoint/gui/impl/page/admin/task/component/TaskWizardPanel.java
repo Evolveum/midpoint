8fad2f3b169b3623df29b2270efbb9ccc1c487cb
Katarina Valalikova <k.valalikova@evolveum.com>
2023-07-12 10:49:33 +0200

    separated panels for task wizard steps

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/task/component/TaskWizardPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/task/component/TaskWizardPanel.java
@@ -11,32 +11,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.model.IModel;
 
 import com.evolveum.midpoint.gui.api.component.wizard.WizardModel;
 import com.evolveum.midpoint.gui.api.component.wizard.WizardPanel;
 import com.evolveum.midpoint.gui.api.component.wizard.WizardStep;
-import com.evolveum.midpoint.gui.api.prism.wrapper.ItemVisibilityHandler;
-import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerWrapper;
 import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
-import com.evolveum.midpoint.gui.impl.component.wizard.AbstractFormWizardStepPanel;
 import com.evolveum.midpoint.gui.impl.component.wizard.AbstractWizardPanel;
 import com.evolveum.midpoint.gui.impl.component.wizard.WizardPanelHelper;
 import com.evolveum.midpoint.gui.impl.page.admin.task.TaskDetailsModel;
+import com.evolveum.midpoint.gui.impl.page.admin.task.component.wizard.*;
 import com.evolveum.midpoint.prism.path.ItemName;
-import com.evolveum.midpoint.prism.path.ItemPath;
-import com.evolveum.midpoint.web.component.prism.ItemVisibility;
 import com.evolveum.midpoint.web.component.util.VisibleEnableBehaviour;
-import com.evolveum.midpoint.web.model.PrismContainerWrapperModel;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.SystemObjectsType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.TaskType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.WorkDefinitionsType;
 
 public class TaskWizardPanel extends AbstractWizardPanel<TaskType, TaskDetailsModel> {
 
-    private static final String BASIC_PANEL_TYPE = "tw-basic";
-    private static final String SCHEDULE_PANEL_TYPE = "tw-schedule";
-    private static final String SIMULATE_PANEL_TYPE = "tw-simulation";
-    private static final String DISTRIBUTION_PANEL_TYPE = "tw-distribution";
-    private static final String WORK_PANEL_TYPE = "tw-work";
 
     public TaskWizardPanel(String id, WizardPanelHelper<TaskType, TaskDetailsModel> helper) {
         super(id, helper);
@@ -51,232 +42,31 @@ public class TaskWizardPanel extends AbstractWizardPanel<TaskType, TaskDetailsMo
     private List<WizardStep> createBasicSteps() {
         List<WizardStep> steps = new ArrayList<>();
 
-        steps.add(new AbstractFormWizardStepPanel<>(getAssignmentHolderModel()) {
-            @Override
-            protected String getPanelType() {
-                return BASIC_PANEL_TYPE;
-            }
-
-            @Override
-            protected ItemVisibilityHandler getVisibilityHandler() {
-                return wrapper -> {
-                    if (wrapper.getItemName().equals(TaskType.F_DIAGNOSTIC_INFORMATION)
-                            || wrapper.getItemName().equals(TaskType.F_EFFECTIVE_MARK_REF)
-                            || wrapper.getItemName().equals(TaskType.F_HANDLER_URI)
-                            || wrapper.getItemName().equals(TaskType.F_LIFECYCLE_STATE)
-                            || wrapper.getItemName().equals(TaskType.F_INDESTRUCTIBLE)
-                            || wrapper.getItemName().equals(TaskType.F_RECURRENCE)){
-                        return ItemVisibility.HIDDEN;
-                    }
-                    return ItemVisibility.AUTO;
-                };
-            }
-
-            @Override
-            protected String getIcon() {
-                return "fa fa-wrench";
-            }
-
-            @Override
-            public IModel<String> getTitle() {
-                return createStringResource("TaskWizardPanel.wizard.step.basic");
-            }
-
-            @Override
-            protected IModel<String> getSubTextModel() {
-                return createStringResource("TaskWizardPanel.wizard.step.basic.help");
-            }
-
-            @Override
-            public String getStepId() {
-                return "basic";
-            }
-        });
+        steps.add(new TaskBasicWizardPanel(getAssignmentHolderModel()));
 
         TaskType task = getAssignmentHolderModel().getObjectType();
-        boolean isImport = WebComponentUtil.hasArchetypeAssignment(task, SystemObjectsType.ARCHETYPE_IMPORT_TASK.value());
 
         ItemName activityName = getActivityName(task);
         if (activityName != null) {
-            steps.add(new AbstractFormWizardStepPanel<>(getAssignmentHolderModel()) {
-
-                @Override
-                protected IModel<? extends PrismContainerWrapper> getContainerFormModel() {
-                    return PrismContainerWrapperModel.fromContainerWrapper(getAssignmentHolderModel().getObjectWrapperModel(),
-                            ItemPath.create(TaskType.F_ACTIVITY, ActivityDefinitionType.F_WORK,
-                                    activityName, ReconciliationWorkDefinitionType.F_RESOURCE_OBJECTS));
-                }
-
-                @Override
-                protected String getPanelType() {
-                    return WORK_PANEL_TYPE;
-                }
-
-                @Override
-                protected ItemVisibilityHandler getVisibilityHandler() {
-                    return wrapper -> {
-                        if (wrapper.getItemName().equals(ResourceObjectSetType.F_QUERY)
-                                || wrapper.getItemName().equals(ResourceObjectSetType.F_QUERY_APPLICATION)){
-                            return ItemVisibility.HIDDEN;
-                        }
-                        return ItemVisibility.AUTO;
-                    };
-                }
-
-                @Override
-                protected String getIcon() {
-                    return "fa fa-wrench";
-                }
-
-                @Override
-                public IModel<String> getTitle() {
-                    return createStringResource("TaskWizardPanel.wizard.step.work.resourceObjects");
-                }
-
-                @Override
-                protected IModel<String> getSubTextModel() {
-                    return createStringResource("TaskWizardPanel.wizard.step.work.resourceObjects.help");
-                }
-
-                @Override
-                public String getStepId() {
-                    return "resourceObjects";
-                }
-            });
+            steps.add(new TaskResourceObjectsWizardPanel(activityName, getAssignmentHolderModel()));
         }
 
         //TODO this is not very clean, should be somehow passed to the wizard
         boolean isSimulationTask = task.getActivity().getExecution() != null && task.getActivity().getExecution().getMode() != null;
-
         if (isSimulationTask) {
-            steps.add(new AbstractFormWizardStepPanel<>(getAssignmentHolderModel()) {
-
-                @Override
-                protected IModel<? extends PrismContainerWrapper> getContainerFormModel() {
-                    return PrismContainerWrapperModel.fromContainerWrapper(getAssignmentHolderModel().getObjectWrapperModel(), ItemPath.create(TaskType.F_ACTIVITY, ActivityDefinitionType.F_EXECUTION));
-                }
-
-                @Override
-                protected String getPanelType() {
-                    return SIMULATE_PANEL_TYPE;
-                }
-
-                @Override
-                protected ItemVisibilityHandler getVisibilityHandler() {
-                    return super.getVisibilityHandler();
-                }
-
-                @Override
-                protected String getIcon() {
-                    return "fa fa-wrench";
-                }
-
-                @Override
-                public IModel<String> getTitle() {
-                    return createStringResource("TaskWizardPanel.wizard.step.execution");
-                }
-
-                @Override
-                protected IModel<String> getSubTextModel() {
-                    return createStringResource("TaskWizardPanel.wizard.step.execution.help");
-                }
-
-                @Override
-                public String getStepId() {
-                    return "execution";
-                }
-            });
+            steps.add(new TaskExecutionWizardPanel(getAssignmentHolderModel()));
         }
 
+        boolean isImport = WebComponentUtil.hasArchetypeAssignment(task, SystemObjectsType.ARCHETYPE_IMPORT_TASK.value());
         if (!isImport) {
-            steps.add(new AbstractFormWizardStepPanel<>(getAssignmentHolderModel()) {
-
-                @Override
-                protected IModel<? extends PrismContainerWrapper> getContainerFormModel() {
-                    return PrismContainerWrapperModel.fromContainerWrapper(getAssignmentHolderModel().getObjectWrapperModel(), TaskType.F_SCHEDULE);
-                }
-
-                @Override
-                protected String getPanelType() {
-                    return SCHEDULE_PANEL_TYPE;
-                }
-
-                @Override
-                protected ItemVisibilityHandler getVisibilityHandler() {
-                    return wrapper -> {
-                        if (wrapper.getItemName().equals(ScheduleType.F_EARLIEST_START_TIME)
-                                || wrapper.getItemName().equals(ScheduleType.F_LATEST_START_TIME)
-                                || wrapper.getItemName().equals(ScheduleType.F_MISFIRE_ACTION)
-                                || wrapper.getItemName().equals(ScheduleType.F_RECURRENCE)){
-                            return ItemVisibility.HIDDEN;
-                        }
-                        return ItemVisibility.AUTO;
-                    };
-                    //cron-like pattern - better help, maybe some example how to configure it
-                }
-
-                @Override
-                protected String getIcon() {
-                    return "fa fa-calendar";
-                }
-
-                @Override
-                public IModel<String> getTitle() {
-                    return createStringResource("TaskWizardPanel.wizard.step.schedule");
-                }
-
-                @Override
-                protected IModel<String> getSubTextModel() {
-                    return createStringResource("TaskWizardPanel.wizard.step.schedule.help");
-                }
-
-                @Override
-                public String getStepId() {
-                    return "schedule";
-                }
-            });
+            steps.add(new TaskScheduleWizardPanel(getAssignmentHolderModel()));
         }
 
-        steps.add(new AbstractFormWizardStepPanel<>(getAssignmentHolderModel()) {
-
-            @Override
-            protected IModel<? extends PrismContainerWrapper> getContainerFormModel() {
-                return PrismContainerWrapperModel.fromContainerWrapper(getAssignmentHolderModel().getObjectWrapperModel(), ItemPath.create(TaskType.F_ACTIVITY, ActivityDefinitionType.F_DISTRIBUTION));
-            }
-
-            @Override
-            protected String getPanelType() {
-                return DISTRIBUTION_PANEL_TYPE;
-            }
-
-            @Override
-            protected ItemVisibilityHandler getVisibilityHandler() {
-                return super.getVisibilityHandler();
-            }
-
-            @Override
-            protected String getIcon() {
-                return "fa fa-wrench";
-            }
-
-            @Override
-            public IModel<String> getTitle() {
-                return createStringResource("TaskWizardPanel.wizard.step.distribution");
-            }
-
-            @Override
-            protected IModel<String> getSubTextModel() {
-                return createStringResource("TaskWizardPanel.wizard.step.distribution.help");
-            }
+        steps.add(new TaskDistributionWizardPanel(getAssignmentHolderModel()) {
 
             @Override
             protected void onSubmitPerformed(AjaxRequestTarget target) {
                 getHelper().onSaveObjectPerformed(target);
-            }
-
-            @Override
-            public String getStepId() {
-                return "distribution";
             }
         });
 
