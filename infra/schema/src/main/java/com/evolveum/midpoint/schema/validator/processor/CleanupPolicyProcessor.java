652de9fcf2def92fa38f4ed49b8727a6067edb8f
Viliam Repan <vilo.repan@evolveum.com>
2023-07-24 09:26:46 +0200

    MID-8842 ninja - cleanup policies processor
    (cherry picked from commit adebe739d505b59ae52b079b8edb8207a3bb1c1a)

---
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/validator/processor/CleanupPolicyProcessor.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2010-2023 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.schema.validator.processor;
+
+import com.evolveum.midpoint.prism.PrismContainer;
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.schema.validator.UpgradeObjectProcessor;
+import com.evolveum.midpoint.schema.validator.UpgradePhase;
+import com.evolveum.midpoint.schema.validator.UpgradePriority;
+import com.evolveum.midpoint.schema.validator.UpgradeType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+
+public class CleanupPolicyProcessor implements UpgradeObjectProcessor<ObjectType> {
+
+    @Override
+    public UpgradePhase getPhase() {
+        return UpgradePhase.BEFORE;
+    }
+
+    @Override
+    public UpgradePriority getPriority() {
+        return UpgradePriority.NECESSARY;
+    }
+
+    @Override
+    public UpgradeType getType() {
+        return UpgradeType.SEAMLESS;
+    }
+
+    @Override
+    public boolean isApplicable(PrismObject<?> object, ItemPath path) {
+        return matchObjectTypeAndPathTemplate(
+                object, path, SystemConfigurationType.class, ItemPath.create(
+                        SystemConfigurationType.F_CLEANUP_POLICY,
+                        CleanupPoliciesType.F_OBJECT_RESULTS))
+                ||
+                matchObjectTypeAndPathTemplate(
+                        object, path, TaskType.class, ItemPath.create(
+                                TaskType.F_ACTIVITY,
+                                ActivityDefinitionType.F_WORK,
+                                WorkDefinitionsType.F_CLEANUP,
+                                CleanupWorkDefinitionType.F_POLICIES,
+                                CleanupPoliciesType.F_OBJECT_RESULTS));
+    }
+
+    @Override
+    public boolean process(PrismObject<ObjectType> object, ItemPath path) throws Exception {
+        ItemPath allExceptLast = path.allExceptLast();
+        PrismContainer<CleanupPoliciesType> container = object.findContainer(allExceptLast);
+        CleanupPoliciesType policies = container.getRealValue();
+
+        CleanupPolicyType policy = policies.getObjectResults();
+        policies.setObjectResults(null);
+        if (policies.getSimpleOperationExecutions() == null) {
+            policies.setSimpleOperationExecutions(policy.clone());
+        }
+
+        if (policies.getComplexOperationExecutions() == null) {
+            policies.setComplexOperationExecutions(policy.clone());
+        }
+
+        return true;
+    }
+}
