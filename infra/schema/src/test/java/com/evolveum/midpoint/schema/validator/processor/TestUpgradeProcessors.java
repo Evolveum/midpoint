2310d845a93606e644aac7d245941a83be60dacf
Viliam Repan <vilo.repan@evolveum.com>
2023-07-24 14:36:46 +0200

    MID-8842 ninja - activity tracing processor
    (cherry picked from commit d3de1410204df1891ceb497287c9b5cca79a3c34)

--- infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
@@ -218,7 +218,7 @@ public class TestUpgradeProcessors extends AbstractSchemaTest {
     public void test80TaskRecomputation() throws Exception {
         testUpgradeValidator("task-recomputation.xml", result -> {
             Assertions.assertThat(result.getItems())
-                    .hasSize(2);
+                    .hasSize(3);
 
             Assertions.assertThat(result.hasChanges()).isTrue();
         });
