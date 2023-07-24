88d3bd226675d0d49110a987c1755c309e8d83c8
Viliam Repan <vilo.repan@evolveum.com>
2023-07-24 13:21:52 +0200

    MID-8842 ninja - use legacy approvers processor

---
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/validator/processor/UseLegacyApproversProcessor.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2010-2023 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.schema.validator.processor;
+
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.schema.validator.UpgradeObjectProcessor;
+import com.evolveum.midpoint.schema.validator.UpgradePhase;
+import com.evolveum.midpoint.schema.validator.UpgradePriority;
+import com.evolveum.midpoint.schema.validator.UpgradeType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.SystemConfigurationType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.WfConfigurationType;
+
+public class UseLegacyApproversProcessor implements UpgradeObjectProcessor<SystemConfigurationType> {
+
+    @Override
+    public UpgradePhase getPhase() {
+        return UpgradePhase.BEFORE;
+    }
+
+    @Override
+    public UpgradePriority getPriority() {
+        return UpgradePriority.NECESSARY;
+    }
+
+    @Override
+    public UpgradeType getType() {
+        return UpgradeType.SEAMLESS;
+    }
+
+    @Override
+    public boolean isApplicable(PrismObject<?> object, ItemPath path) {
+        return matchParentType(object, path, WfConfigurationType.class);
+    }
+
+    @Override
+    public boolean process(PrismObject<SystemConfigurationType> object, ItemPath path) throws Exception {
+        WfConfigurationType wfConfiguration = getItemParent(object, path);
+        if (wfConfiguration == null) {
+            return false;
+        }
+
+        wfConfiguration.setUseLegacyApproversSpecification(null);
+
+        wfConfiguration.asPrismContainerValue();
+        if (wfConfiguration.asPrismContainerValue().isEmpty()) {
+            object.asObjectable().setWorkflowConfiguration(null);
+        }
+
+        return true;
+    }
+}
