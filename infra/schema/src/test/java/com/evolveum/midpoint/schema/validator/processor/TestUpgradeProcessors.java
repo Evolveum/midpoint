dc29ef481fdc868d18b5bbfcfb8e11d8a0283103
Viliam Repan <vilo.repan@evolveum.com>
2023-07-25 15:14:50 +0200

    MID-8842 ninja - file transport processor, "notificationConfiguration/file" marked deprecated (type was already deprecated

--- infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
@@ -165,7 +165,7 @@ public class TestUpgradeProcessors extends AbstractSchemaTest {
     @Test
     public void test30TestSystemConfig() throws Exception {
         testUpgradeValidator("system-configuration.xml", result -> {
-            Assertions.assertThat(result.getItems()).hasSize(10);
+            Assertions.assertThat(result.getItems()).hasSize(11);
 
             UpgradeValidationItem item = assertGetItem(result, getProcessorIdentifier(RoleCatalogCollectionsProcessor.class));
             Assertions.assertThat(item.getDelta().getModifiedItems()).hasSize(2);
