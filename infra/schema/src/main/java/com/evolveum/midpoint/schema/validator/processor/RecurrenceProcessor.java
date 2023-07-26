6ad3ef174f763dc6864d1204c74b90a40d334793
Viliam Repan <vilo.repan@evolveum.com>
2023-07-26 11:16:44 +0200

    MID-8842 ninja - task recurrence processor + test
    (cherry picked from commit e8cf6f09537e60f931f0738fd48b9cf127e6ecc9)

---
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/validator/processor/RecurrenceProcessor.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2010-2023 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.schema.validator.processor;
+
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.schema.validator.UpgradeObjectProcessor;
+import com.evolveum.midpoint.schema.validator.UpgradePhase;
+import com.evolveum.midpoint.schema.validator.UpgradePriority;
+import com.evolveum.midpoint.schema.validator.UpgradeType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.ScheduleType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.TaskRecurrenceType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.TaskType;
+
+@SuppressWarnings("unused")
+public class RecurrenceProcessor implements UpgradeObjectProcessor<TaskType> {
+
+    @Override
+    public UpgradePhase getPhase() {
+        return UpgradePhase.BEFORE;
+    }
+
+    @Override
+    public UpgradePriority getPriority() {
+        return UpgradePriority.CRITICAL;
+    }
+
+    @Override
+    public UpgradeType getType() {
+        return UpgradeType.SEAMLESS;
+    }
+
+    @Override
+    public boolean isApplicable(PrismObject<?> object, ItemPath path) {
+        return matchParentTypeAndItemName(object, path, TaskType.class, TaskType.F_RECURRENCE);
+    }
+
+    @Override
+    public boolean process(PrismObject<TaskType> object, ItemPath path) throws Exception {
+        TaskType task = object.asObjectable();
+        TaskRecurrenceType recurrence = task.getRecurrence();
+
+        ScheduleType schedule = task.getSchedule();
+        if (schedule == null) {
+            schedule = new ScheduleType();
+            task.setSchedule(schedule);
+        }
+
+        schedule.setRecurrence(recurrence);
+        task.setRecurrence(null);
+
+        return true;
+    }
+}
