f75485f1bbe243b4098f7e247053a14186575d64
Viliam Repan <vilo.repan@evolveum.com>
2023-07-26 15:56:45 +0200

    MID-8842 ninja - authentication name/identifier processor + test
    (cherry picked from commit 528ad1697c1dbf84e6c55d3e71b758d5e19bd331)

---
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/validator/processor/AuthenticationNameProcessor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2010-2023 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.schema.validator.processor;
+
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.schema.validator.UpgradeObjectProcessor;
+import com.evolveum.midpoint.schema.validator.UpgradePhase;
+import com.evolveum.midpoint.schema.validator.UpgradePriority;
+import com.evolveum.midpoint.schema.validator.UpgradeType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AbstractAuthenticationModuleType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AuthenticationSequenceModuleType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.AuthenticationSequenceType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.SecurityPolicyType;
+
+@SuppressWarnings("unused")
+public class AuthenticationNameProcessor implements UpgradeObjectProcessor<SecurityPolicyType> {
+
+    @Override
+    public UpgradePhase getPhase() {
+        return UpgradePhase.BEFORE;
+    }
+
+    @Override
+    public UpgradePriority getPriority() {
+        return UpgradePriority.NECESSARY;
+    }
+
+    @Override
+    public UpgradeType getType() {
+        return UpgradeType.SEAMLESS;
+    }
+
+    @Override
+    public boolean isApplicable(PrismObject<?> object, ItemPath path) {
+        return matchParentTypeAndItemName(
+                object, path, AuthenticationSequenceType.class, AuthenticationSequenceType.F_NAME)
+                || matchParentTypeAndItemName(
+                object, path, AuthenticationSequenceModuleType.class, AuthenticationSequenceModuleType.F_NAME)
+                || matchParentTypeAndItemName(
+                object, path, AbstractAuthenticationModuleType.class, AbstractAuthenticationModuleType.F_NAME);
+    }
+
+    @Override
+    public boolean process(PrismObject<SecurityPolicyType> object, ItemPath path) throws Exception {
+        Object parent = getItemParent(object, path);
+        if (parent instanceof AuthenticationSequenceType) {
+            AuthenticationSequenceType auth = (AuthenticationSequenceType) parent;
+            auth.setIdentifier(auth.getName());
+            return true;
+        }
+
+        if (parent instanceof AuthenticationSequenceModuleType) {
+            AuthenticationSequenceModuleType module = (AuthenticationSequenceModuleType) parent;
+            module.setIdentifier(module.getName());
+            return true;
+        }
+
+        if (parent instanceof AbstractAuthenticationModuleType) {
+            AbstractAuthenticationModuleType module = (AbstractAuthenticationModuleType) parent;
+            module.setIdentifier(module.getName());
+            return true;
+        }
+
+        return false;
+    }
+}
