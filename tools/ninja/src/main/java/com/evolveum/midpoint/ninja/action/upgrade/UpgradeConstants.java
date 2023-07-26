d59bbcb2439bf95af88f148be8492afc9d753b28
Viliam Repan <vilo.repan@evolveum.com>
2023-07-26 20:49:21 +0200

    MID-8842 ninja - fixed upgrade constants

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/upgrade/UpgradeConstants.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/upgrade/UpgradeConstants.java
@@ -2,12 +2,11 @@ package com.evolveum.midpoint.ninja.action.upgrade;
 
 public class UpgradeConstants {
 
-    public static final String SUPPORTED_VERSION_LTS = "4.4.5";
+    public static final String SUPPORTED_VERSION_LTS = "4.4.6";
 
-    public static final String SUPPORTED_VERSION_FEATURE = "4.7.1";
+    public static final String SUPPORTED_VERSION_FEATURE = "4.7.2";
 
-    // todo, master doesn't have a version to upgrade to..., support branches do have  this one
-    public static final String SUPPORTED_VERSION_TARGET = null;
+    public static final String SUPPORTED_VERSION_TARGET = "4.8";
 
     public static final String[] SUPPORTED_VERSIONS = { SUPPORTED_VERSION_LTS, SUPPORTED_VERSION_FEATURE };
 
@@ -17,7 +16,7 @@ public class UpgradeConstants {
 
     public static final String LABEL_SCHEMA_AUDIT_CHANGE_NUMBER = "schemaAuditChangeNumber";
 
-    public static final String SUPPORTED_SCHEMA_CHANGE_NUMBER = Integer.toString(15);
+    public static final String SUPPORTED_SCHEMA_CHANGE_NUMBER = Integer.toString(1);
 
-    public static final String SUPPORTED_SCHEMA_AUDIT_CHANGE_NUMBER = Integer.toString(4);
+    public static final String SUPPORTED_SCHEMA_AUDIT_CHANGE_NUMBER = Integer.toString(1);
 }
