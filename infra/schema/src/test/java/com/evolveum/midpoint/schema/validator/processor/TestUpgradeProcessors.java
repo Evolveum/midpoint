eaa6a7cdb037094a9b081cc37468f685535806f4
Viliam Repan <vilo.repan@evolveum.com>
2023-08-01 16:30:31 +0200

    MID-8842 ninja - container manual processor + test
    (cherry picked from commit 302d876a25ddb1dab24163c315b405d56a663986)

--- infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
@@ -165,7 +165,7 @@ public class TestUpgradeProcessors extends AbstractSchemaTest {
     @Test
     public void test30TestSystemConfig() throws Exception {
         testUpgradeValidator("system-configuration.xml", result -> {
-            Assertions.assertThat(result.getItems()).hasSize(9);
+            Assertions.assertThat(result.getItems()).hasSize(10);
         });
     }
 
