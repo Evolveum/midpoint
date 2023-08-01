fdfb5506984d9aa029f60685694a113d3d823970
Viliam Repan <vilo.repan@evolveum.com>
2023-08-01 13:53:18 +0200

    MID-8842 ninja - task category processor
    (cherry picked from commit 2eb8cb30b8e3364c1744d0906149eda667f9cf29)

---
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/validator/processor/TaskCategoryProcessor.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2010-2023 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.schema.validator.processor;
+
+import com.evolveum.midpoint.prism.PrismObject;
+import com.evolveum.midpoint.prism.path.ItemPath;
+import com.evolveum.midpoint.schema.validator.UpgradeObjectProcessor;
+import com.evolveum.midpoint.schema.validator.UpgradePhase;
+import com.evolveum.midpoint.schema.validator.UpgradePriority;
+import com.evolveum.midpoint.schema.validator.UpgradeType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.TaskType;
+
+@SuppressWarnings("unused")
+public class TaskCategoryProcessor implements UpgradeObjectProcessor<TaskType> {
+
+    @Override
+    public UpgradePhase getPhase() {
+        return UpgradePhase.BEFORE;
+    }
+
+    @Override
+    public UpgradePriority getPriority() {
+        return UpgradePriority.NECESSARY;
+    }
+
+    @Override
+    public UpgradeType getType() {
+        return UpgradeType.SEAMLESS;
+    }
+
+    @Override
+    public boolean isApplicable(PrismObject<?> object, ItemPath path) {
+        return matchObjectTypeAndPathTemplate(object, path, TaskType.class, TaskType.F_CATEGORY);
+    }
+
+    @Override
+    public boolean process(PrismObject<TaskType> object, ItemPath path) throws Exception {
+        object.asObjectable().setCategory(null);
+
+        return true;
+    }
+}
