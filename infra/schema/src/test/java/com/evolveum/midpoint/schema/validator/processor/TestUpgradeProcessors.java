c130c4c6f2baa46cd40de78ff0da21e373990562
Viliam Repan <vilo.repan@evolveum.com>
2023-08-01 14:32:56 +0200

    MID-8842 ninja - objects forms manual processor
    (cherry picked from commit a362a6b7ee7e70347f20aef1563ac903271a138d)

--- infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
@@ -165,7 +165,7 @@ public class TestUpgradeProcessors extends AbstractSchemaTest {
     @Test
     public void test30TestSystemConfig() throws Exception {
         testUpgradeValidator("system-configuration.xml", result -> {
-            Assertions.assertThat(result.getItems()).hasSize(2);
+            Assertions.assertThat(result.getItems()).hasSize(3);
         });
     }
 
