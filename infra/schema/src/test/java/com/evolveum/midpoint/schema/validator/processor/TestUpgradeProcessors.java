2dd10c892766e471d3882fd7b2c3209f3a154924
Viliam Repan <vilo.repan@evolveum.com>
2023-08-01 15:32:03 +0200

    MID-8842 ninja - search object type processor + test

--- infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
+++ infra/schema/src/test/java/com/evolveum/midpoint/schema/validator/processor/TestUpgradeProcessors.java
@@ -165,7 +165,7 @@ public class TestUpgradeProcessors extends AbstractSchemaTest {
     @Test
     public void test30TestSystemConfig() throws Exception {
         testUpgradeValidator("system-configuration.xml", result -> {
-            Assertions.assertThat(result.getItems()).hasSize(17);
+            Assertions.assertThat(result.getItems()).hasSize(19);
 
             UpgradeValidationItem item = assertGetItem(result, getProcessorIdentifier(RoleCatalogCollectionsProcessor.class));
             Assertions.assertThat(item.getDelta().getModifiedItems()).hasSize(2);
