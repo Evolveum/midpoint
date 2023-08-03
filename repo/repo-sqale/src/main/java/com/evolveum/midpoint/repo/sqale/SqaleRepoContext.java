4957bcce4564b1dbdeb3b84571fb4a934878c85d
Viliam Repan <vilo.repan@evolveum.com>
2023-08-03 14:01:17 +0200

    MID-8842 ninja - repository skip version check fix
    (cherry picked from commit 4a188c431f77abf42c1b7e4036c6e4b30c7beeec)

--- repo/repo-sqale/src/main/java/com/evolveum/midpoint/repo/sqale/SqaleRepoContext.java
+++ repo/repo-sqale/src/main/java/com/evolveum/midpoint/repo/sqale/SqaleRepoContext.java
@@ -15,26 +15,13 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import javax.xml.namespace.QName;
 
-import com.evolveum.midpoint.common.configuration.api.MidpointConfiguration;
-import com.evolveum.midpoint.repo.sqale.qmodel.common.MGlobalMetadata;
-import com.evolveum.midpoint.repo.sqale.qmodel.common.QGlobalMetadata;
-import com.evolveum.midpoint.repo.sqlbase.JdbcSession;
-
-import com.evolveum.midpoint.util.logging.Trace;
-
-import com.evolveum.midpoint.util.logging.TraceManager;
-
-import javax.annotation.PostConstruct;
-
-import javax.sql.DataSource;
-import javax.xml.namespace.QName;
-
 import com.querydsl.sql.types.ArrayType;
 import com.querydsl.sql.types.EnumAsObjectType;
 import org.apache.commons.lang3.BooleanUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import com.evolveum.midpoint.common.configuration.api.MidpointConfiguration;
 import com.evolveum.midpoint.prism.PrismObject;
 import com.evolveum.midpoint.prism.SerializationOptions;
 import com.evolveum.midpoint.prism.delta.ChangeType;
@@ -42,6 +29,8 @@ import com.evolveum.midpoint.prism.delta.ItemDelta;
 import com.evolveum.midpoint.repo.sqale.jsonb.Jsonb;
 import com.evolveum.midpoint.repo.sqale.jsonb.QuerydslJsonbType;
 import com.evolveum.midpoint.repo.sqale.qmodel.common.MContainerType;
+import com.evolveum.midpoint.repo.sqale.qmodel.common.MGlobalMetadata;
+import com.evolveum.midpoint.repo.sqale.qmodel.common.QGlobalMetadata;
 import com.evolveum.midpoint.repo.sqale.qmodel.common.QUri;
 import com.evolveum.midpoint.repo.sqale.qmodel.ext.MExtItem;
 import com.evolveum.midpoint.repo.sqale.qmodel.ext.MExtItemCardinality;
@@ -49,6 +38,7 @@ import com.evolveum.midpoint.repo.sqale.qmodel.ext.MExtItemHolderType;
 import com.evolveum.midpoint.repo.sqale.qmodel.object.MObjectType;
 import com.evolveum.midpoint.repo.sqale.qmodel.ref.MReferenceType;
 import com.evolveum.midpoint.repo.sqlbase.JdbcRepositoryConfiguration;
+import com.evolveum.midpoint.repo.sqlbase.JdbcSession;
 import com.evolveum.midpoint.repo.sqlbase.SqlRepoContext;
 import com.evolveum.midpoint.repo.sqlbase.mapping.QueryModelMappingRegistry;
 import com.evolveum.midpoint.schema.SchemaConstantsGenerated;
@@ -57,6 +47,8 @@ import com.evolveum.midpoint.schema.util.FullTextSearchUtil;
 import com.evolveum.midpoint.util.QNameUtil;
 import com.evolveum.midpoint.util.exception.SchemaException;
 import com.evolveum.midpoint.util.exception.SystemException;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.xml.ns._public.common.audit_3.AuditEventStageType;
 import com.evolveum.midpoint.xml.ns._public.common.audit_3.AuditEventTypeType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
@@ -130,10 +122,14 @@ public class SqaleRepoContext extends SqlRepoContext {
     public void initialize() {
         // skip version check if option was defined or option value is "true" (equals ignore case)
         String skipVersionCheck = System.getProperty(MidpointConfiguration.MIDPOINT_SKIP_VERSION_CHECK + "1");
-        if ("".equals(skipVersionCheck) || BooleanUtils.isTrue(Boolean.parseBoolean(skipVersionCheck))) {
-            return;
+        if (BooleanUtils.isNotTrue(Boolean.parseBoolean(skipVersionCheck))) {
+            checkDBSchemaVersion();
         }
 
+        clearCaches();
+    }
+
+    private void checkDBSchemaVersion() {
         LOGGER.debug("Checking DB schema version.");
 
         try (JdbcSession session = this.newJdbcSession().startReadOnlyTransaction()) {
@@ -153,8 +149,6 @@ public class SqaleRepoContext extends SqlRepoContext {
 
             LOGGER.debug("DB schema version check OK.");
         }
-
-        clearCaches();
     }
 
     // This has nothing to do with "repo cache" which is higher than this.
