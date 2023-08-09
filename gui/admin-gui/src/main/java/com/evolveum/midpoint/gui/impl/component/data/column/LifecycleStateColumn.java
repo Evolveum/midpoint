0c20bd2f1397b5ede39c4815ee7413bca27348ca
lskublik <lskublik@evolveum.com>
2023-08-09 15:32:10 +0200

    MID-8383:expand of capability container (resource wizard)

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/data/column/LifecycleStateColumn.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/data/column/LifecycleStateColumn.java
@@ -16,7 +16,6 @@ import com.evolveum.midpoint.prism.PrismContainerDefinition;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
 
 import org.apache.wicket.Component;
-import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.model.IModel;
 
 public class LifecycleStateColumn<C extends Containerable> extends PrismPropertyWrapperColumn<C, String> {
@@ -27,13 +26,6 @@ public class LifecycleStateColumn<C extends Containerable> extends PrismProperty
 
     @Override
     protected <IW extends ItemWrapper> Component createColumnPanel(String componentId, IModel<IW> rowModel) {
-        LifecycleStatePanel panel =  new LifecycleStatePanel(componentId, (IModel<PrismPropertyWrapper<String>>) rowModel);
-        panel.add(AttributeAppender.append("class", "d-inline"));
-        return panel;
+        return new LifecycleStatePanel(componentId, (IModel<PrismPropertyWrapper<String>>) rowModel);
     }
-
-//    @Override
-//    public Component getHeader(String componentId) {
-//        return new Label(componentId, getPageBase().createStringResource("PrismPropertyWrapperColumn.column.mode"));
-//    }
 }
