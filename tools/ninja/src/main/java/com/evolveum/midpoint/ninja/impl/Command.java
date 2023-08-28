bcb84fc80fc8b3d63414e9ee12d1a94755f0724c
Viliam Repan <vilo.repan@evolveum.com>
2023-08-28 13:11:55 +0200

    MID-8842 ninja - changed named of few commands and options, everything now uses kebab case instead of combination of lowerCamelCase and kebab-case (some-value instead of someValue)

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/impl/Command.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/impl/Command.java
@@ -36,11 +36,11 @@ public enum Command {
 
     INFO("info", InfoOptions.class, InfoRepositoryAction.class),
 
-    IMPORT_AUDIT("importAudit", ImportAuditOptions.class, ImportAuditRepositoryAction.class),
+    IMPORT_AUDIT("import-audit", ImportAuditOptions.class, ImportAuditRepositoryAction.class),
 
-    EXPORT_AUDIT("exportAudit", ExportAuditOptions.class, ExportAuditRepositoryAction.class),
+    EXPORT_AUDIT("export-audit", ExportAuditOptions.class, ExportAuditRepositoryAction.class),
 
-    EXPORT_MINING("exportMining", ExportMiningOptions.class, ExportMiningRepositoryAction.class),
+    EXPORT_MINING("export-mining", ExportMiningOptions.class, ExportMiningRepositoryAction.class),
 
     TRACE("trace", EditTraceOptions.class, EditTraceAction.class),
 
