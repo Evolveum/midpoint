ebedb7f2133380a85f816132add18104cfc95734
Pavol Mederly <mederly@evolveum.com>
2023-09-04 14:16:08 +0200

    Document the mergers a little

--- infra/schema/src/main/java/com/evolveum/midpoint/schema/merger/package-info.java
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/merger/package-info.java
@@ -11,5 +11,38 @@
  * For example, resource inheritance and object type inheritance is implemented here.
  *
  * Other uses will be considered in the future.
+ *
+ * == How to Use
+ *
+ * === Implementors
+ *
+ * When you want to create a new merger, you need to just extend {@link com.evolveum.midpoint.schema.merger.BaseMergeOperation}
+ * and implement the merging logic. In most cases, it involves providing the appropriate root-level
+ * {@link com.evolveum.midpoint.schema.merger.ItemMerger} to the constructor:
+ * {@link com.evolveum.midpoint.schema.merger.BaseMergeOperation#BaseMergeOperation(com.evolveum.midpoint.prism.Containerable,
+ * com.evolveum.midpoint.prism.Containerable, com.evolveum.midpoint.schema.merger.GenericItemMerger)}
+ * Only in rare cases you will need to implement the merging logic yourself, like in
+ * {@link com.evolveum.midpoint.schema.merger.objdef.LimitationsMerger} or
+ * {@link com.evolveum.midpoint.schema.merger.resource.ObjectTypeDefinitionMerger}.
+ *
+ * Some of the mergers operate at the level of objects, for example:
+ *
+ * - {@link com.evolveum.midpoint.schema.merger.resource.ResourceMergeOperation} (for resources)
+ * - {@link com.evolveum.midpoint.schema.merger.template.ObjectTemplateMergeOperation} (for object templates)
+ *
+ * Others are at the level of containerables, like:
+ *
+ * - {@link com.evolveum.midpoint.schema.merger.correlator.CorrelatorMergeOperation} (for correlator definitions)
+ * - {@link com.evolveum.midpoint.schema.merger.simulation.SimulationDefinitionMergeOperation} (for simulation definitions)
+ *
+ * === Users
+ *
+ * After instantiating appropriate {@link com.evolveum.midpoint.schema.merger.BaseMergeOperation}, you just call its
+ * {@link com.evolveum.midpoint.schema.merger.BaseMergeOperation#execute()} method. Just look for the usages in midPoint code
+ * for inspiration.
+ *
+ * == Limitations
+ *
+ * The architecture of this package is not finished yet. It is more-or-less an experiment for now.
  */
 package com.evolveum.midpoint.schema.merger;
