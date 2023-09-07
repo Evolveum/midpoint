5edf01057efc299dd9d3bac86179e7bff5b6ef71
tchrapovic <chrapovic.tadeas@gmail.com>
2023-09-07 12:00:30 +0200

    Fix MID-8916 translate read only fields.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/autocomplete/LookupTableLabelPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/autocomplete/LookupTableLabelPanel.java
@@ -6,19 +6,37 @@
  */
 package com.evolveum.midpoint.gui.api.component.autocomplete;
 
+import com.evolveum.midpoint.gui.api.page.PageBase;
+import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.LookupTableType;
+
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.util.convert.IConverter;
 
 public class LookupTableLabelPanel extends Label {
 
+    private final String lookupTableOid;
 
-    public LookupTableLabelPanel(String id, IModel<?> model) {
+    public LookupTableLabelPanel(String id, IModel<?> model, String lookupTableOid) {
         super(id, model);
+        this.lookupTableOid = lookupTableOid;
     }
 
     @Override
     public <C> IConverter<C> getConverter(Class<C> type) {
-        return new LookupTableConverter<C>(super.getConverter(type), this, true);
+        return new LookupTableConverter<C>(super.getConverter(type), this, true){
+            @Override
+            protected LookupTableType getLookupTable() {
+                if (lookupTableOid != null) {
+                    return WebComponentUtil.loadLookupTable(lookupTableOid, getPageBase());
+                }
+                return null;
+            }
+        };
+    }
+
+    public PageBase getPageBase() {
+        return (PageBase) getPage();
     }
 }
