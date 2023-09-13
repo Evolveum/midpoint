027bc983d1ba0464314f7d7b228a7a4e7ff81b03
Kateryna Honchar <honchar@evolveum.com>
2023-09-13 14:12:05 +0200

    mid-8886 fixed error message while invisible required fields

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/FormWrapperValidator.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/FormWrapperValidator.java
@@ -12,11 +12,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.feedback.ComponentFeedbackMessageFilter;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.validation.IFormValidator;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LambdaModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.util.visit.IVisit;
+import org.apache.wicket.util.visit.IVisitor;
 import org.apache.wicket.validation.Validatable;
 import org.jetbrains.annotations.NotNull;
 
@@ -109,6 +115,46 @@ public abstract class FormWrapperValidator<O extends ObjectType> implements IFor
         };
 
         validator.validate(validatable);
+        if (!validatable.isValid()) {
+            validatable.getErrors().forEach(e -> {
+                Serializable errorMessage = e.getErrorMessage((key, vars) ->
+                        new StringResourceModel(key)
+                                .setModel(new Model<String>())
+                                .setDefaultValue(key)
+                                .getString());
+                if (errorMessage!= null && !hasError(form, errorMessage.toString())) {
+                    form.error(errorMessage);
+                }
+            });
+        }
+    }
+
+    private boolean hasError(Form<?> form, String errorMessage) {
+        if (!form.hasError()) {
+            return false;
+        }
+        Boolean hasError = form.getFeedbackMessages()
+                .messages(new ComponentFeedbackMessageFilter(form))
+                .stream()
+                .anyMatch(m -> errorMessage.equals(m.getMessage().toString()));
+        if (!hasError) {
+            hasError = form.visitChildren(Component.class, new IVisitor<Component, Boolean>() {
+                @Override
+                public void component(final Component component, final IVisit<Boolean> visit) {
+                    if (!component.hasErrorMessage()) {
+                        return;
+                    }
+                    boolean componentErrorExists = component.getFeedbackMessages()
+                            .messages(new ComponentFeedbackMessageFilter(component))
+                            .stream()
+                            .anyMatch(m -> (m != null) && errorMessage.equals(m.getMessage().toString()));
+                    if (componentErrorExists) {
+                        visit.stop(true);
+                    }
+                }
+            });
+        }
+        return Boolean.TRUE.equals(hasError);
     }
 
     protected abstract PrismObjectWrapper<O> getObjectWrapper();
