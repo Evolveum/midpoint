6e14cde4b553588dd1148d48c5437773cdb2806b
Kateryna Honchar <honchar@evolveum.com>
2023-11-22 12:36:35 +0100

    fix for MID-9324 Request access: Cannot change search mode; still the issue stays for Roles of teammate panel

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/self/requestAccess/RoleCatalogPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/self/requestAccess/RoleCatalogPanel.java
@@ -13,6 +13,8 @@ import javax.xml.namespace.QName;
 
 import com.evolveum.midpoint.gui.impl.util.TableUtil;
 
+import com.evolveum.midpoint.web.component.util.*;
+
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -30,6 +32,7 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.IResource;
@@ -75,10 +78,6 @@ import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.web.component.data.column.AjaxLinkPanel;
 import com.evolveum.midpoint.web.component.data.column.CheckBoxHeaderColumn;
 import com.evolveum.midpoint.web.component.data.column.RoundedIconColumn;
-import com.evolveum.midpoint.web.component.util.EnableBehaviour;
-import com.evolveum.midpoint.web.component.util.SelectableBean;
-import com.evolveum.midpoint.web.component.util.SerializableBiConsumer;
-import com.evolveum.midpoint.web.component.util.VisibleEnableBehaviour;
 import com.evolveum.midpoint.web.session.UserProfileStorage;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 import com.evolveum.prism.xml.ns._public.types_3.ItemPathType;
@@ -110,7 +109,7 @@ public class RoleCatalogPanel extends WizardStepPanel<RequestAccess> implements 
 
     private final PageBase page;
 
-    private IModel<Search> searchModel;
+    private LoadableModel<Search> searchModel;
 
     private IModel<ListGroupMenu<RoleCatalogQueryItem>> menuModel;
 
@@ -182,13 +181,17 @@ public class RoleCatalogPanel extends WizardStepPanel<RequestAccess> implements 
     }
 
     private void updateFalseQuery(RoleCatalogQuery query) {
+        updateFalseQuery(query, RoleType.class);
+    }
+
+    private <R extends AbstractRoleType> void updateFalseQuery(RoleCatalogQuery query, Class<R> queryType) {
         ObjectQuery oq = getPrismContext()
-                .queryFor(RoleType.class)
+                .queryFor(queryType)
                 .none()
                 .build();
 
         query.setQuery(oq);
-        query.setType(RoleType.class);
+        query.setType(queryType);
     }
 
     private void updateQueryFromOrgRef(RoleCatalogQuery query, ObjectReferenceType ref, boolean scopeOne) {
@@ -204,7 +207,7 @@ public class RoleCatalogPanel extends WizardStepPanel<RequestAccess> implements 
 
     private void updateQueryForRolesOfTeammate(RoleCatalogQuery query, String userOid) {
         if (userOid == null) {
-            updateFalseQuery(query);
+            updateFalseQuery(query, AbstractRoleType.class);
             return;
         }
 
@@ -332,10 +335,10 @@ public class RoleCatalogPanel extends WizardStepPanel<RequestAccess> implements 
             }
 
             @Override
-            protected Search load() {
+            protected Search<?> load() {
                 Class<? extends ObjectType> type = queryModel.getObject().getType();
 
-                SearchBuilder searchBuilder = new SearchBuilder(type)
+                SearchBuilder<?> searchBuilder = new SearchBuilder<>(type)
                         .modelServiceLocator(page);
 
                 return searchBuilder.build();
@@ -586,9 +589,7 @@ public class RoleCatalogPanel extends WizardStepPanel<RequestAccess> implements 
         }
 
         if (rcq.rolesOfTeammate()) {
-            ObjectReferenceType userRef = teammateModel.getObject();
-            String userOid = userRef != null ? userRef.getOid() : null;
-            updateQueryForRolesOfTeammate(query, userOid);
+            updateQueryForRolesOfTeammate(query, getTeammateUserOid());
             return;
         }
 
@@ -614,20 +615,20 @@ public class RoleCatalogPanel extends WizardStepPanel<RequestAccess> implements 
         updateFalseQuery(query);
     }
 
+    private String getTeammateUserOid() {
+        ObjectReferenceType userRef = teammateModel.getObject();
+        return userRef != null ? userRef.getOid() : null;
+    }
+
     private void onMenuClickPerformed(AjaxRequestTarget target, ListGroupMenuItem<RoleCatalogQueryItem> item) {
         updateQueryModel(item);
+        searchModel.reset();
 
-        RoleCatalogQuery query = queryModel.getObject();
-
-        Search search = searchModel.getObject();
-        if (!Objects.equals(search.getTypeClass(), query.getType())) {
-            SearchBuilder searchBuilder = new SearchBuilder(query.getType())
-                    .modelServiceLocator(page);
-
-            searchModel.setObject(searchBuilder.build());
-        }
+        TileTablePanel<?, ?> tilesTable = (TileTablePanel<?, ?>) get(ID_TILES);
+        tilesTable.initHeaderFragment();
 
         target.add(get(ID_TILES));
+        target.add(get(ID_MENU));
     }
 
     private IModel<ViewToggle> createViewToggleModel() {
