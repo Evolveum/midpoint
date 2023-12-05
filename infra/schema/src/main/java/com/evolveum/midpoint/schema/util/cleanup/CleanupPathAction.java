2ff44a725fa97bbe029067de8f0116800f18d17f
Viliam Repan <vilo.repan@evolveum.com>
2023-12-05 22:06:37 +0100

    cleanup action processor implemented in schema module, to be used by ninja and studio

--- infra/schema/src/main/java/com/evolveum/midpoint/schema/util/cleanup/CleanupPathAction.java
+++ infra/schema/src/main/java/com/evolveum/midpoint/schema/util/cleanup/CleanupPathAction.java
@@ -1,7 +1,5 @@
 package com.evolveum.midpoint.schema.util.cleanup;
 
-import java.util.Arrays;
-
 /**
  * Cleanup action to be taken when a path is encountered.
  */
@@ -10,12 +8,12 @@ public enum CleanupPathAction {
     /**
      * Item is removed from the object.
      */
-    REMOVE("remove"),
+    REMOVE,
 
     /**
      * Item is not removed from the object.
      */
-    IGNORE("ignore"),
+    IGNORE,
 
     /**
      * Consumer is asked what to do.
@@ -23,26 +21,5 @@ public enum CleanupPathAction {
      * @see CleanupEventListener
      * @see CleanupActionProcessor#setListener(CleanupEventListener)
      */
-    ASK("ask");
-
-    private final String value;
-
-    CleanupPathAction(String value) {
-        this.value = value;
-    }
-
-    public String value() {
-        return value;
-    }
-
-    public static CleanupPathAction getState(String value) {
-        if (value == null) {
-            return null;
-        }
-
-        return Arrays.stream(values())
-                .filter(s -> value.equals(s.value()))
-                .findFirst()
-                .orElse(null);
-    }
+    ASK;
 }
