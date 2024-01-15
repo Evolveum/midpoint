b64d859ffcdef6be48d552dfc8f8ffe6d325ae12
Viliam Repan <vilo.repan@evolveum.com>
2024-01-15 14:29:45 +0100

    ninja: added supported version target 4.8->4.9

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/upgrade/UpgradeConstants.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/upgrade/UpgradeConstants.java
@@ -14,7 +14,7 @@ public class UpgradeConstants {
      * This is the version we are upgrading to.
      * For "master" branch it is null - there's nothing newer than master - nothing to upgrade to.
      */
-    public static final String SUPPORTED_VERSION_TARGET = null;
+    public static final String SUPPORTED_VERSION_TARGET = "4.9";
 
     public static final String UPGRADE_TEMP_DIRECTORY = ".upgrade";
 
