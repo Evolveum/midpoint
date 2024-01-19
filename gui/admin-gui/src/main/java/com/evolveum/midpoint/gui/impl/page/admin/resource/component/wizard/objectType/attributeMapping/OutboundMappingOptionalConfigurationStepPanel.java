4b348be3450296c7835cf00653c1b56876b7284e
lskublik <lskublik@evolveum.com>
2024-01-19 17:52:32 +0100

    MID-9419: fix localizations on outbound details panels for resource wizard

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/resource/component/wizard/objectType/attributeMapping/OutboundMappingOptionalConfigurationStepPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/resource/component/wizard/objectType/attributeMapping/OutboundMappingOptionalConfigurationStepPanel.java
@@ -29,15 +29,15 @@ import java.util.List;
 /**
  * @author lskublik
  */
-@PanelInstance(identifier = "rw-attributes-inbound-optional",
+@PanelInstance(identifier = "rw-attributes-outbound-optional",
         applicableForType = ResourceType.class,
         applicableForOperation = OperationTypeType.WIZARD,
-        display = @PanelDisplay(label = "PageResource.wizard.step.attributes.inbound.optional", icon = "fa fa-screwdriver-wrench"),
+        display = @PanelDisplay(label = "PageResource.wizard.step.attributes.outbound.optional", icon = "fa fa-screwdriver-wrench"),
         expanded = true)
 public class OutboundMappingOptionalConfigurationStepPanel<ODM extends ObjectDetailsModels>
         extends AbstractValueFormResourceWizardStepPanel<MappingType, ODM> {
 
-    public static final String PANEL_TYPE = "rw-attributes-inbound-optional";
+    public static final String PANEL_TYPE = "rw-attributes-outbound-optional";
 
     private static final List<ItemName> VISIBLE_ITEMS = List.of(
             MappingType.F_DESCRIPTION,
@@ -63,17 +63,17 @@ public class OutboundMappingOptionalConfigurationStepPanel<ODM extends ObjectDet
 
     @Override
     public IModel<String> getTitle() {
-        return createStringResource("PageResource.wizard.step.attributes.inbound.optional");
+        return createStringResource("PageResource.wizard.step.attributes.outbound.optional");
     }
 
     @Override
     protected IModel<?> getTextModel() {
-        return createStringResource("PageResource.wizard.step.attributes.inbound.optional.text");
+        return createStringResource("PageResource.wizard.step.attributes.outbound.optional.text");
     }
 
     @Override
     protected IModel<?> getSubTextModel() {
-        return createStringResource("PageResource.wizard.step.attributes.inbound.optional.subText");
+        return createStringResource("PageResource.wizard.step.attributes.outbound.optional.subText");
     }
 
     @Override
