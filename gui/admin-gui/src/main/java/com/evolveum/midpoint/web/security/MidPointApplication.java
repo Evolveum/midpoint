c56a1504fdb8c2bd413d6d3c878de85e713ec851
Pavol Mederly <mederly@evolveum.com>
2024-01-25 19:14:28 +0100

    Allow production clustering only with subscription

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/security/MidPointApplication.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/security/MidPointApplication.java
@@ -174,7 +174,6 @@ public class MidPointApplication extends AuthenticatedWebApplication implements 
     private WebApplicationConfiguration webApplicationConfiguration;
 
     private DeploymentInformationType deploymentInfo;
-    private SubscriptionId subscriptionId;
 
     public static final String MOUNT_INTERNAL_SERVER_ERROR = "/error";
     public static final String MOUNT_UNAUTHORIZED_ERROR = "/error/401";
@@ -333,14 +332,7 @@ public class MidPointApplication extends AuthenticatedWebApplication implements 
     }
 
     public @NotNull SubscriptionState getSubscriptionState() {
-        var subscriptionId = this.subscriptionId;
-        if (subscriptionId != null) {
-            // Should be always the case, maybe except the initialization.
-            return subscriptionStateCache.getSubscriptionState(subscriptionId);
-        } else {
-            // This will try to get it from the current system configuration.
-            return subscriptionStateCache.getSubscriptionState();
-        }
+        return subscriptionStateCache.getSubscriptionState();
     }
 
     private void initializeSchrodinger() {
@@ -580,7 +572,6 @@ public class MidPointApplication extends AuthenticatedWebApplication implements 
 
     private void updateDeploymentInfo(@Nullable SystemConfigurationType value) {
         deploymentInfo = value != null ? value.getDeploymentInformation() : null;
-        subscriptionId = SubscriptionStateCache.getSubscriptionId(value);
     }
 
     /* (non-Javadoc)
