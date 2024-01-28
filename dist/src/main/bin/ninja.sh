8def5f7e43bb72b6949be0ecea5f45ae41b953f2
Viliam Repan <vilo.repan@evolveum.com>
2024-01-28 18:51:24 +0100

    Revert "MID-9270 fix in ninja shell script to use same keystore as midpoint"
    This reverts commit a1ae58497f8baef19de3e31d6efb06d8a7e78ac8.

--- dist/src/main/bin/ninja.sh
+++ dist/src/main/bin/ninja.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2017-2023 Evolveum and contributors
+# Copyright (C) 2017-2020 Evolveum and contributors
 #
 # This work is dual-licensed under the Apache License 2.0
 # and European Union Public License. See LICENSE file for details.
@@ -13,19 +13,13 @@ BASE_DIR=$(cd "${SCRIPT_DIR}/.." && pwd -P)
 : "${MIDPOINT_HOME:="${BASE_DIR}/var"}"
 
 if [[ ! -f "${BASE_DIR}/lib/ninja.jar" ]]; then
-    echo "ERROR: ninja.jar not found in ${BASE_DIR}/lib directory" >&2
+  echo "ERROR: ninja.jar not found in ${BASE_DIR}/lib directory"
   exit 1
 fi
 
 # Set UMASK unless it has been overridden
 : "${UMASK:="0027"}"
 umask ${UMASK}
-
-JAVA_def_trustStore="keystore.jceks"
-JAVA_def_trustStoreType="jceks"
-
-if $(echo "${JAVA_OPTS:-}" | grep -v -q "\-Djavax.net.ssl.trustStore="); then JAVA_OPTS="${JAVA_OPTS:-} -Djavax.net.ssl.trustStore=\"${MIDPOINT_HOME}/${JAVA_def_trustStore}\""; fi
-if $(echo "${JAVA_OPTS:-}" | grep -v -q "\-Djavax.net.ssl.trustStoreType="); then JAVA_OPTS="${JAVA_OPTS:-} -Djavax.net.ssl.trustStoreType=${JAVA_def_trustStoreType}"; fi
 
 # can't use -v here because of bash 3.2 support
 if [[ -z "${JAVA_HOME:-}" ]]; then
@@ -46,8 +40,8 @@ done
 # Technically we could do one exec, but then we can't quote -Dloader.path argument, because it
 # would be empty and considered a class name by the "java -jar" command.
 if [ -n "${JDBC_DRIVER:-}" ]; then
-  echo "Using JDBC driver path: ${JDBC_DRIVER}" >&2
-  eval "${_RUNJAVA}" ${JAVA_OPTS} "-Dloader.path=${JDBC_DRIVER}" -jar "${BASE_DIR}/lib/ninja.jar" -m "${MIDPOINT_HOME}" "$@"
+  echo "Using JDBC driver path: ${JDBC_DRIVER}"
+  exec "${_RUNJAVA}" "-Dloader.path=${JDBC_DRIVER}" -jar "${BASE_DIR}/lib/ninja.jar" -m "${MIDPOINT_HOME}" "$@"
 else
-  eval "${_RUNJAVA}" ${JAVA_OPTS} -jar "${BASE_DIR}/lib/ninja.jar" -m "${MIDPOINT_HOME}" "$@"
+  exec "${_RUNJAVA}" -jar "${BASE_DIR}/lib/ninja.jar" -m "${MIDPOINT_HOME}" "$@"
 fi
