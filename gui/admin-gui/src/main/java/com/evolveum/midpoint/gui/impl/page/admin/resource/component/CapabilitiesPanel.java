d3e6a4385b77c4f1fcce2d7cf992fcc430c837df
skublik <lukas.skublik@gmail.com>
2024-03-20 14:06:54 +0100

    adding icon for AssociationsCapabilityType

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/resource/component/CapabilitiesPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/resource/component/CapabilitiesPanel.java
@@ -291,6 +291,9 @@ public class CapabilitiesPanel extends BasePanel<PrismContainerValueWrapper<Reso
         if (SchemaCapabilityType.class.isAssignableFrom(capability)) {
             return "fa fa-table-cells";
         }
+        if (AssociationsCapabilityType.class.isAssignableFrom(capability)) {
+            return "fa fa-shield";
+        }
         return "fa fa-circle";
     }
 
