2c94f1a4c404e6f7abd9357f6f48fcb950ad77f4
Pavol Mederly <mederly@evolveum.com>
2024-08-07 10:56:36 +0200

    Check trigger existence in optimizing creator
    The OptimizingTriggerCreator did not check for trigger existence
    in a strict way, which lead to creation of duplicate triggers
    on some occasions - e.g., in cluster or when caches were cleared
    in the meanwhile.
    
    Although not considered harmful initially, it seems that this
    behavior can lead to relatively massive creation of triggers
    in some environments.
    
    Hence, this commit strengthens the trigger existence checks,
    at the cost of some extra repository read accesses.
    
    Related to MID-9368.

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/expr/MidpointFunctionsImpl.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/expr/MidpointFunctionsImpl.java
@@ -2300,7 +2300,11 @@ public class MidpointFunctionsImpl implements MidpointFunctions {
     @NotNull
     @Override
     public OptimizingTriggerCreator getOptimizingTriggerCreator(long fireAfter, long safetyMargin) {
-        return new OptimizingTriggerCreatorImpl(triggerCreatorGlobalState, this, fireAfter, safetyMargin);
+        return new OptimizingTriggerCreatorImpl(
+                triggerCreatorGlobalState,
+                this,
+                fireAfter,
+                safetyMargin);
     }
 
     @NotNull
