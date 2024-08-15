ef9f2095561d0c7f0797367c9477e04ae6fbede3
KaterynaHonchar <honchar@evolveum.com>
2024-08-15 17:37:19 +0200

    attempt to update assignment wrapper creation (within 9858 ticket)

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/self/requestAccess/ShoppingCartEditPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/self/requestAccess/ShoppingCartEditPanel.java
@@ -25,6 +25,7 @@ import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
 import org.jetbrains.annotations.NotNull;
 
@@ -82,7 +83,7 @@ public class ShoppingCartEditPanel extends BasePanel<ShoppingCartItem> implement
 
     private Fragment footer;
 
-    private IModel<ItemSecurityConstraints> assignmentSecurityConstraints;
+    private LoadableDetachableModel<ItemSecurityConstraints> assignmentSecurityConstraints;
 
     private IModel<RequestAccess> requestAccess;
 
@@ -160,6 +161,7 @@ public class ShoppingCartEditPanel extends BasePanel<ShoppingCartItem> implement
                     context.setCreateIfEmpty(true);
                     context.setReadOnly(false);
                     context.setShowEmpty(true);
+                    context.setSecurityConstraints(assignmentSecurityConstraints.getObject());
 
                     // create whole wrapper, instead of only the concrete container value wrapper
                     PrismObjectWrapper<UserType> userWrapper = userWrapperFactory.createObjectWrapper(user.asPrismObject(), ItemStatus.ADDED, context);
@@ -181,7 +183,7 @@ public class ShoppingCartEditPanel extends BasePanel<ShoppingCartItem> implement
             }
         };
 
-        assignmentSecurityConstraints = new LoadableModel<>() {
+        assignmentSecurityConstraints = new LoadableDetachableModel<>() {
 
             @Override
             protected ItemSecurityConstraints load() {
@@ -265,6 +267,9 @@ public class ShoppingCartEditPanel extends BasePanel<ShoppingCartItem> implement
                     if (path.equivalent(itemWrapper.getPath())) {
                         return ItemVisibility.HIDDEN;
                     }
+                }
+                if (isItemVisible(itemWrapper.getPath())) {
+                    return ItemVisibility.AUTO;
                 }
                 return super.getBasicTabVisibity(itemWrapper);
             }
@@ -473,9 +478,24 @@ public class ShoppingCartEditPanel extends BasePanel<ShoppingCartItem> implement
                 ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_DOCUMENTATION),
                 ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_SUBTYPE),
                 ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_LIFECYCLE_STATE),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_METADATA),
                 ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_TARGET_REF),
-                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_ORDER)
-        );
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_CONSTRUCTION),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_PERSONA_CONSTRUCTION),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_FOCUS_MAPPINGS),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_POLICY_RULE),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_ACTIVATION),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_ORDER),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_ORDER_CONSTRAINT),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_LIMIT_TARGET_CONTENT),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_LIMIT_OTHER_PRIVILEGES),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_CONDITION),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_POLICY_SITUATION),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_TRIGGERED_POLICY_RULE),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_POLICY_EXCEPTION),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_ASSIGNMENT_RELATION),
+                ItemPath.create(UserType.F_ASSIGNMENT, AssignmentType.F_EFFECTIVE_MARK_REF)
+                );
     }
 
     private boolean isDetailsPanelVisible() {
