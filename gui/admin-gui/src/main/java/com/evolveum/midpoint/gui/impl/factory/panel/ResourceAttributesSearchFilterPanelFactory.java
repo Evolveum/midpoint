3edfa5b6abe2af79f4d619b944a36d2a6d8097be
skublik <lukas.skublik@gmail.com>
2024-09-05 14:04:27 +0200

    MID-9967: adding validator for search filter panel

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/factory/panel/ResourceAttributesSearchFilterPanelFactory.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/factory/panel/ResourceAttributesSearchFilterPanelFactory.java
@@ -12,10 +12,12 @@ import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerValueWrapper;
 import com.evolveum.midpoint.gui.api.prism.wrapper.PrismPropertyWrapper;
 import com.evolveum.midpoint.gui.api.prism.wrapper.PrismValueWrapper;
 import com.evolveum.midpoint.gui.impl.factory.panel.searchfilter.SearchFilterPanelFactory;
+import com.evolveum.midpoint.gui.impl.factory.panel.searchfilter.SearchFilterTypeForQueryModel;
 import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.util.exception.SchemaException;
 import com.evolveum.midpoint.util.logging.Trace;
 import com.evolveum.midpoint.util.logging.TraceManager;
+import com.evolveum.midpoint.web.component.prism.InputPanel;
 import com.evolveum.midpoint.web.page.admin.reports.component.SearchFilterConfigurationPanel;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 import com.evolveum.prism.xml.ns._public.query_3.SearchFilterType;
@@ -51,11 +53,11 @@ public class ResourceAttributesSearchFilterPanelFactory extends SearchFilterPane
     }
 
     @Override
-    protected Panel getPanel(PrismPropertyPanelContext<SearchFilterType> panelCtx) {
+    protected InputPanel getPanel(PrismPropertyPanelContext<SearchFilterType> panelCtx) {
         return new SearchFilterConfigurationPanel(
                 panelCtx.getComponentId(), panelCtx.getItemWrapperModel(), panelCtx.getRealValueModel(), null) {
             @Override
-            protected IModel<String> createQueryModel(IModel model, LoadableModel filterTypeModel, boolean useParsing) {
+            protected SearchFilterTypeForQueryModel createQueryModel(IModel model, LoadableModel filterTypeModel, boolean useParsing) {
                 ItemRealValueModel<QName> objectClassModel = new ItemRealValueModel<>((IModel<? extends PrismValueWrapper<QName>>) () -> {
                     try {
 
