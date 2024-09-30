9fbc59e0ada52aa3a31b90cb1fe0bcac35130ee0
Pavol Mederly <mederly@evolveum.com>
2024-09-30 11:48:50 +0200

    Adapt TestModelServiceContractCaching
    Assertions in this test assumed the default cache use policy is
    "use cached". After changing the default to "use fresh" in
    0acb5a1e65c6f0c505935f6da68a7d64b94199d6 they started to fail.
    
    The resolution in this commit is to force "use cached" in the relevant
    resources (dummy, blue, and green). So the test passes for both native
    and generic repo.

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestModelServiceContractCaching.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestModelServiceContractCaching.java
@@ -28,8 +28,9 @@ import com.evolveum.midpoint.xml.ns._public.common.common_3.CachingMetadataType;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ResourceType;
 
 /**
- * @author semancik
+ * Uses dummy, blue, and green resources with the caching enabled and actively used (strategy is "use cached or fresh").
  *
+ * @author semancik
  */
 @ContextConfiguration(locations = {"classpath:ctx-model-intest-test-main.xml"})
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
