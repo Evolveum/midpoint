68bcc478a91b1f6eda24d3f1c0b8d4721bd017f3
Pavol Mederly <mederly@evolveum.com>
2024-10-08 21:18:14 +0200

    Adapt TestInbounds and TestVolatility to caching
    Caching of volatile attributes was turned off.
    
    An inbound mapping for volatile attribute (description) had to be marked
    as strong, to make sure the shadow is loaded when needed.
    
    See also MID-10051.

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestVolatility.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/TestVolatility.java
@@ -10,11 +10,12 @@ import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import com.evolveum.midpoint.prism.polystring.PolyString;
+
+import com.evolveum.midpoint.test.TestTask;
 
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
@@ -44,27 +45,27 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
 
     public static final File TEST_DIR = new File("src/test/resources/volatility");
 
-    protected static final File RESOURCE_DUMMY_HR_FILE = new File(TEST_DIR, "resource-dummy-hr.xml");
-    protected static final String RESOURCE_DUMMY_HR_OID = "10000000-0000-0000-0000-00000000f004";
-    protected static final String RESOURCE_DUMMY_HR_NAME = "hr";
+    private static final File RESOURCE_DUMMY_HR_FILE = new File(TEST_DIR, "resource-dummy-hr.xml");
+    private static final String RESOURCE_DUMMY_HR_OID = "10000000-0000-0000-0000-00000000f004";
+    private static final String RESOURCE_DUMMY_HR_NAME = "hr";
 
-    protected static final File RESOURCE_DUMMY_VOLATILE_FILE = new File(TEST_DIR, "resource-dummy-volatile.xml");
-    protected static final String RESOURCE_DUMMY_VOLATILE_OID = "10000000-0000-0000-0000-00000000f104";
-    protected static final String RESOURCE_DUMMY_VOLATILE_NAME = "volatile";
+    private static final File RESOURCE_DUMMY_VOLATILE_FILE = new File(TEST_DIR, "resource-dummy-volatile.xml");
+    private static final String RESOURCE_DUMMY_VOLATILE_OID = "10000000-0000-0000-0000-00000000f104";
+    private static final String RESOURCE_DUMMY_VOLATILE_NAME = "volatile";
 
-    protected static final File RESOURCE_DUMMY_MONSTERIZED_FILE = new File(TEST_DIR, "resource-dummy-monsterized.xml");
-    protected static final String RESOURCE_DUMMY_MONSTERIZED_OID = "67a954d2-f391-11e6-a1d7-078381fe0e6f";
-    protected static final String RESOURCE_DUMMY_MONSTERIZED_NAME = "monsterized";
+    private static final File RESOURCE_DUMMY_MONSTERIZED_FILE = new File(TEST_DIR, "resource-dummy-monsterized.xml");
+    private static final String RESOURCE_DUMMY_MONSTERIZED_OID = "67a954d2-f391-11e6-a1d7-078381fe0e6f";
+    private static final String RESOURCE_DUMMY_MONSTERIZED_NAME = "monsterized";
 
-    protected static final String ACCOUNT_MANCOMB_DUMMY_USERNAME = "mancomb";
-    protected static final String ACCOUNT_GUYBRUSH_DUMMY_USERNAME = "guybrush";     //Guybrush Threepwood
-    protected static final String ACCOUNT_LARGO_DUMMY_USERNAME = "largo";
+    private static final String ACCOUNT_MANCOMB_DUMMY_USERNAME = "mancomb";
+    protected static final String ACCOUNT_GUYBRUSH_DUMMY_USERNAME = "guybrush"; //Guybrush Threepwood
+    private static final String ACCOUNT_LARGO_DUMMY_USERNAME = "largo";
 
-    protected static final String TASK_LIVE_SYNC_DUMMY_HR_FILENAME = TEST_DIR + "/task-dummy-hr-livesync.xml";
-    protected static final String TASK_LIVE_SYNC_DUMMY_HR_OID = "10000000-0000-0000-5555-55550000f004";
+    private static final TestTask TASK_LIVE_SYNC_DUMMY_HR = new TestTask(
+            TEST_DIR, "task-dummy-hr-livesync.xml", "10000000-0000-0000-5555-55550000f004");
 
-    protected static final File USER_TEMPLATE_FILE = new File(TEST_DIR, "user-template-import-hr.xml");
-    protected static final File USER_LARGO_WITH_ASSIGNMENT_FILE = new File(TEST_DIR, "user-largo-with-assignment.xml");
+    private static final File USER_TEMPLATE_FILE = new File(TEST_DIR, "user-template-import-hr.xml");
+    private static final File USER_LARGO_WITH_ASSIGNMENT_FILE = new File(TEST_DIR, "user-largo-with-assignment.xml");
 
     @Override
     public void initSystem(Task initTask, OperationResult initResult) throws Exception {
@@ -81,22 +82,16 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         initDummyResource(RESOURCE_DUMMY_VOLATILE_NAME, RESOURCE_DUMMY_VOLATILE_FILE, RESOURCE_DUMMY_VOLATILE_OID, initTask, initResult);
 
         initDummyResource(RESOURCE_DUMMY_MONSTERIZED_NAME, RESOURCE_DUMMY_MONSTERIZED_FILE, RESOURCE_DUMMY_MONSTERIZED_OID, initTask, initResult);
-    }
 
-    @Test
-    public void test100ImportLiveSyncTaskDummyHr() throws Exception {
-        when();
-        importSyncTask();
-
-        then();
-        waitForSyncTaskNextRun();
+        initTestObjects(initTask, initResult, TASK_LIVE_SYNC_DUMMY_HR);
+        TASK_LIVE_SYNC_DUMMY_HR.rerun(initResult);
     }
 
     @Test
     public void test110AddDummyHrAccountMancomb() throws Exception {
+        var result = getTestOperationResult();
+
         given();
-        // Preconditions
-        //assertUsers(5);
 
         DummyAccount account = new DummyAccount(ACCOUNT_MANCOMB_DUMMY_USERNAME);
         account.setEnabled(true);
@@ -105,8 +100,9 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         when();
 
         getDummyResource(RESOURCE_DUMMY_HR_NAME).addAccount(account);
+        invalidateShadowCacheIfNeeded(RESOURCE_DUMMY_HR_OID);
 
-        waitForSyncTaskNextRun();
+        TASK_LIVE_SYNC_DUMMY_HR.rerun(result);
 
         then();
 
@@ -142,22 +138,23 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         assertEquals("Wrong description on resource account", expectedDescription, descriptionOnResource);
         assertEquals("Wrong description in user record", expectedDescription, descriptionOfUser);
 
-//        assertUsers(6);
-
-        // notifications
         notificationManager.setDisabled(true);
     }
 
     @Test
     public void test120UpdateDummyHrAccountMancomb() throws Exception {
+        var result = getTestOperationResult();
+
         when();
         DummyAccount account = getDummyResource(RESOURCE_DUMMY_HR_NAME).getAccountByName(ACCOUNT_MANCOMB_DUMMY_USERNAME);
         account.replaceAttributeValue(DummyAccount.ATTR_FULLNAME_NAME, "Sir Mancomb Seepgood");
 
         displayValue("Dummy HR resource", getDummyResource(RESOURCE_DUMMY_HR_NAME).debugDump());
 
+        invalidateShadowCacheIfNeeded(RESOURCE_DUMMY_HR_OID);
+
         // Make sure we have steady state
-        waitForSyncTaskNextRun();
+        TASK_LIVE_SYNC_DUMMY_HR.rerun(result);
 
         then();
         PrismObject<ShadowType> accountMancombHr = findAccountByUsername(ACCOUNT_MANCOMB_DUMMY_USERNAME,
@@ -210,9 +207,6 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         Task task = getTestTask();
         OperationResult result = task.getResult();
 
-        // Preconditions
-        //assertUsers(5);
-
         when();
 
         Collection<ObjectDelta<? extends ObjectType>> deltas = new ArrayList<>();
@@ -246,9 +240,6 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         assertEquals("Wrong description on resource account", expectedDescription, descriptionOnResource);
         assertEquals("Wrong description in user record", expectedDescription, descriptionOfUser);
 
-//        assertUsers(6);
-
-        // notifications
         notificationManager.setDisabled(true);
     }
 
@@ -257,9 +248,6 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         given();
         Task task = getTestTask();
         OperationResult result = task.getResult();
-
-        // Preconditions
-        //assertUsers(5);
 
         when();
         PrismObject<UserType> user = PrismTestUtil.parseObject(USER_LARGO_WITH_ASSIGNMENT_FILE);
@@ -293,9 +281,6 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         assertEquals("Wrong description on resource account", expectedDescription, descriptionOnResource);
         assertEquals("Wrong description in user record", expectedDescription, descriptionOfUser);
 
-//        assertUsers(6);
-
-        // notifications
         notificationManager.setDisabled(true);
     }
 
@@ -363,13 +348,5 @@ public class TestVolatility extends AbstractInitializedModelIntegrationTest {
         assertDummyAccountAttribute(RESOURCE_DUMMY_MONSTERIZED_NAME, USER_HERMAN_USERNAME,
                 DummyAccount.ATTR_INTERESTS_NAME,
                 "foo", "bar", DummyResource.VALUE_COOKIE, DummyResource.VALUE_MONSTER);
-    }
-
-    protected void importSyncTask() throws FileNotFoundException {
-        importObjectFromFile(TASK_LIVE_SYNC_DUMMY_HR_FILENAME);
-    }
-
-    protected void waitForSyncTaskNextRun() throws Exception {
-        waitForTaskNextRunAssertSuccess(TASK_LIVE_SYNC_DUMMY_HR_OID, 10000);
     }
 }
