e36dbf4957f34eec9751e5637fc17e9d913e6c09
KaterynaHonchar <honchar@evolveum.com>
2024-10-08 13:16:36 +0200

    fix for 10099 Assignment count is not synchronized in object detail page + conflict resolving

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/application/FocusProjectionsCounter.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/application/FocusProjectionsCounter.java
@@ -7,7 +7,9 @@
 package com.evolveum.midpoint.web.application;
 
 import com.evolveum.midpoint.gui.api.page.PageBase;
+import com.evolveum.midpoint.gui.api.prism.ItemStatus;
 import com.evolveum.midpoint.gui.api.prism.wrapper.PrismObjectWrapper;
+import com.evolveum.midpoint.gui.api.prism.wrapper.ShadowWrapper;
 import com.evolveum.midpoint.gui.impl.page.admin.focus.FocusDetailsModels;
 import com.evolveum.midpoint.gui.impl.util.ProvisioningObjectsUtil;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.FocusType;
@@ -21,12 +23,24 @@ public class FocusProjectionsCounter<F extends FocusType> extends SimpleCounter<
     @Override
     public int count(FocusDetailsModels<F> objectDetailsModels, PageBase pageBase) {
         if (objectDetailsModels.getProjectionModel().isAttached()) {
-            return objectDetailsModels.getProjectionModel().getObject().size();
+            return objectDetailsModels
+                    .getProjectionModel()
+                    .getObject()
+                    .stream()
+                    .filter(shadowWrapper -> !isNewlyAddedShadow(shadowWrapper) && !shadowWrapper.isDead())
+                    .toList()
+                    .size();
         }
 
         PrismObjectWrapper<F> assignmentHolderWrapper = objectDetailsModels.getObjectWrapperModel().getObject();
         F object = assignmentHolderWrapper.getObject().asObjectable();
 
         return ProvisioningObjectsUtil.countLinkForNonDeadShadows(object.getLinkRef());
+    }
+
+    //check if the shadow was newly added
+    //if true, then it is not counted
+    private boolean isNewlyAddedShadow(ShadowWrapper shadowWrapper) {
+        return ItemStatus.ADDED.equals(shadowWrapper.getStatus());
     }
 }
