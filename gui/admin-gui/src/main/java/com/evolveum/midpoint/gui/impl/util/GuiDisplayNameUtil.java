1c8d801c0b115d56749f36769c5ddc6dc0991c2a
Pavol Mederly <mederly@evolveum.com>
2024-10-09 11:34:23 +0200

    Deprecate/remove parts of association definitions
    - Legacy association definition is now deprecated.
    - Definitions for association attributes, references, and activation
    are (maybe temporarily) removed. We'll see later how to implement these.

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/util/GuiDisplayNameUtil.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/util/GuiDisplayNameUtil.java
@@ -286,13 +286,6 @@ public class GuiDisplayNameUtil {
                         ResourceType.F_SCHEMA_HANDLING,
                         SchemaHandlingType.F_OBJECT_TYPE,
                         ResourceObjectTypeDefinitionType.F_CREDENTIALS))
-                || mapping.asPrismContainerValue().getPath().namedSegmentsOnly().isSuperPath(
-                ItemPath.create(
-                        ResourceType.F_SCHEMA_HANDLING,
-                        SchemaHandlingType.F_ASSOCIATION_TYPE,
-                        ShadowAssociationTypeDefinitionType.F_SUBJECT,
-                        ShadowAssociationTypeSubjectDefinitionType.F_ASSOCIATION,
-                        ShadowAssociationDefinitionType.F_ACTIVATION))
                 || mapping.asPrismContainerValue().getPath().namedSegmentsOnly().equivalent(
                 ItemPath.create(
                         ResourceType.F_SCHEMA_HANDLING,
