46a39e89cdcf9be9d966401b3b79e41c462558a3
tchrapovic <chrapovic.tadeas@gmail.com>
2024-10-12 17:47:05 +0200

    Role analysis, improve clustering behavior 1. Failure to meet the required number of properties - add deciding on access or overal noise in case of clustering of rules 2. Avoidance of unnecessarily starting the Jaccard distance calculation in the case of an impossible-to-fulfill condition (causes low number of point properties)

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/mining/algorithm/cluster/mechanism/Clusterer.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/mining/algorithm/cluster/mechanism/Clusterer.java
@@ -33,20 +33,11 @@ public abstract class Clusterer<T extends Clusterable> {
             int minPropertiesOverlap, DensityBasedClustering.PointStatusWrapper pStatusWrapper) {
         List<T> neighbors = new ArrayList<>();
 
-        // Retrieve the properties of the given point
-        Set<String> pointProperties = point.getPoint();
- /*        Check if the number of properties is less than the minimum required overlap
-         If the number of properties is less than the minimum required overlap,
-         set the point status to ACCESS_NOISE and return an empty list of neighbors
-         because the point is not suitable for minIntersection
-         conditions (there are not enough properties to meet the minimum overlap).*/
-        if(pointProperties.size() < minPropertiesOverlap){
-            pStatusWrapper.pStatus = OutlierNoiseCategoryType.ACCESS_NOISE;
-            return neighbors;
-        }
-
         return switch (clusteringMode) {
             case BALANCED -> {
+                boolean conditionMeets = checkPropertiesRequirement(point, minPropertiesOverlap, pStatusWrapper);
+                if (!conditionMeets) {yield neighbors;}
+
                 int numberOfOveralRuleNeighbors = point.getMembersCount();
                 for (T neighbor : points) {
                     boolean notNeighbor = point != neighbor;
@@ -67,6 +58,9 @@ public abstract class Clusterer<T extends Clusterable> {
                 yield neighbors;
             }
             case UNBALANCED -> {
+                boolean conditionMeets = checkPropertiesRequirement(point, minPropertiesOverlap, pStatusWrapper);
+                if (!conditionMeets) {yield neighbors;}
+
                 int numberOfOveralRuleNeighbors = point.getMembersCount();
                 for (T neighbor : points) {
                     boolean notNeighbor = point != neighbor;
@@ -113,6 +107,10 @@ public abstract class Clusterer<T extends Clusterable> {
 
                 }
 
+                boolean conditionMeets = checkPropertiesRequirementIncludeRule(point, minPropertiesOverlap,
+                        numberOfRulesNeighbors, minPts, pStatusWrapper);
+                if (!conditionMeets) {yield neighbors;}
+
                 OutlierNoiseCategoryType pStatus = pStatusWrapper.pStatus;
                 if (numberOfOveralRuleNeighbors > minPts) {
                     pStatusWrapper.pStatus = OutlierNoiseCategoryType.SUITABLE;
@@ -157,6 +155,10 @@ public abstract class Clusterer<T extends Clusterable> {
                         numberOfRulesNeighbors += neighbor.getMembersCount();
                     }
                 }
+
+                boolean conditionMeets = checkPropertiesRequirementIncludeRule(point, minPropertiesOverlap,
+                        numberOfRulesNeighbors, minPts, pStatusWrapper);
+                if (!conditionMeets) {yield neighbors;}
 
                 OutlierNoiseCategoryType pStatus = pStatusWrapper.pStatus;
                 if (numberOfOveralRuleNeighbors > minPts) {
@@ -208,6 +210,10 @@ public abstract class Clusterer<T extends Clusterable> {
                     }
                 }
 
+                boolean conditionMeets = checkPropertiesRequirementIncludeRule(point, minPropertiesOverlap,
+                        numberOfRulesNeighbors, minPts, pStatusWrapper);
+                if (!conditionMeets) {yield neighbors;}
+
                 OutlierNoiseCategoryType pStatus = pStatusWrapper.pStatus;
                 if (numberOfOveralRuleNeighbors > minPts) {
                     pStatusWrapper.pStatus = OutlierNoiseCategoryType.SUITABLE;
@@ -230,6 +236,39 @@ public abstract class Clusterer<T extends Clusterable> {
                 yield neighbors;
             }
         };
+    }
+
+    private static <T extends Clusterable> boolean checkPropertiesRequirement(@NotNull T point,
+            int minPropertiesOverlap,
+            DensityBasedClustering.PointStatusWrapper pStatusWrapper) {
+        // Retrieve the properties of the given point
+        Set<String> pointProperties = point.getPoint();
+ /*        Check if the number of properties is less than the minimum required overlap
+         If the number of properties is less than the minimum required overlap,
+         set the point status to ACCESS_NOISE and return an empty list of neighbors
+         because the point is not suitable for minIntersection
+         conditions (there are not enough properties to meet the minimum overlap).*/
+        if (pointProperties.size() < minPropertiesOverlap) {
+            pStatusWrapper.pStatus = OutlierNoiseCategoryType.ACCESS_NOISE;
+            return false;
+        }
+        return true;
+    }
+
+    private static <T extends Clusterable> boolean checkPropertiesRequirementIncludeRule(@NotNull T point,
+            int minPropertiesOverlap,
+            int numberOfRulesNeighbors,
+            int minPts,
+            DensityBasedClustering.PointStatusWrapper pStatusWrapper) {
+        boolean propertiesCountMeet = checkPropertiesRequirement(point, minPropertiesOverlap, pStatusWrapper);
+        if (!propertiesCountMeet && numberOfRulesNeighbors < minPts) {
+            pStatusWrapper.pStatus = OutlierNoiseCategoryType.OVERAL_NOISE;
+            return false;
+        } else if (!propertiesCountMeet) {
+            pStatusWrapper.pStatus = OutlierNoiseCategoryType.ACCESS_NOISE;
+            return false;
+        }
+        return true;
     }
 
     protected double unbalancedAccessDistance(
