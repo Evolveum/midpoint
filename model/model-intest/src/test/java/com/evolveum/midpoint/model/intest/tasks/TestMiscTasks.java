d299976020618d444e58c2dc85d5ffcfeae471c0
Andrej <84732799+and-za@users.noreply.github.com>
2024-12-11 14:12:11 +0100

    Fix cleanup after finish to remove also subtasks (#238)
    **What**
    
    Remove not just the root task, but also all its subtasks when it is
    cleaned up.
    
    **Why**
    
    When cleanup after finish is set on task, it should be auto-removed
    after specified duration from its completion.
    
    However there was a bug, which caused, that only the root task was
    removed. If task run spawned any persistent subtasks (e.g. when
    number of workers per node is specified), those were not cleaned up.
    This change fixes it.
    
    ---------
    
    (cherry picked from commit 4da3d2184cce9dfa7d066dd750d5e7725ac65865)

--- model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/tasks/TestMiscTasks.java
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/tasks/TestMiscTasks.java
@@ -6,11 +6,11 @@
  */
 package com.evolveum.midpoint.model.intest.tasks;
 
+import static com.evolveum.midpoint.schema.constants.MidPointConstants.NS_RI;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static com.evolveum.midpoint.schema.constants.MidPointConstants.NS_RI;
-
 import java.io.File;
+import java.util.List;
 import javax.xml.namespace.QName;
 
 import org.springframework.test.annotation.DirtiesContext;
@@ -50,6 +50,9 @@ public class TestMiscTasks extends AbstractInitializedModelIntegrationTest {
             TestObject.file(TEST_DIR, "task-delete-incomplete-raw.xml", "d0053e62-9d48-4c1e-ace8-a8feb1f35f91");
     private static final TestObject<TaskType> TASK_DELETE_SELECTED_USERS =
             TestObject.file(TEST_DIR, "task-delete-selected-users.xml", "623f261c-4c63-445b-a714-dcde118f227c");
+    private static final TestObject<TaskType> TASK_CLEANUP_SUBTASKS_AFTER_COMPLETION =
+            TestObject.file(TEST_DIR, "task-cleanup-subtasks-after-completion.xml",
+                    "7f5eb732-9ffc-42c3-a416-ee1754f1b764");
     private static final TestTask TASK_EXECUTE_CHANGES_LEGACY =
             new TestTask(TEST_DIR, "task-execute-changes.xml", "1dce894e-e76c-4db5-9318-0fa5b55261da");
     private static final TestTask TASK_EXECUTE_CHANGES_SINGLE =
@@ -245,6 +248,41 @@ public class TestMiscTasks extends AbstractInitializedModelIntegrationTest {
         assertUser(user2.getOid(), "after")
                 .display()
                 .assertName("user-to-delete-2-indestructible");
+    }
+
+    /**
+     * Tests if cleanup after task finish removes also subtasks, not just parent task
+     *
+     * Test for MID-10272
+     */
+    @Test
+    public void test140CleanSubtasksAfterFinish() throws Exception {
+        given("Noop task with more workers per node is added.");
+        OperationResult result = getTestOperationResult();
+        addTask(TASK_CLEANUP_SUBTASKS_AFTER_COMPLETION, result);
+        waitForTaskCloseOrSuspend(TASK_CLEANUP_SUBTASKS_AFTER_COMPLETION.oid, 10000);
+
+        Task taskTree = this.taskManager.getTaskTree(TASK_CLEANUP_SUBTASKS_AFTER_COMPLETION.oid, result);
+        List<String> subtasksOids = taskTree.listSubtasksDeeply(true, result).stream()
+                .map(Task::getOid)
+                .toList();
+
+        when("Task trigger scanner executes.");
+        importObjectFromFile(TASK_TRIGGER_SCANNER_FILE);
+        waitForTaskStart(TASK_TRIGGER_SCANNER_OID);
+        waitForTaskFinish(TASK_TRIGGER_SCANNER_OID);
+
+        then("After task trigger scanner executes, Noop task should be deleted as well as all its worker "
+                + "subtasks");
+        // Just to make sure, that we have correct task definition which results in subtasks creation.
+        assertThat(subtasksOids)
+                .withFailMessage(() -> "There are no subtasks, but at least one is expected. Check task definition xml")
+                .hasSizeGreaterThan(0);
+
+        assertNoObject(TaskType.class, TASK_CLEANUP_SUBTASKS_AFTER_COMPLETION.oid);
+        for (String subtaskOid : subtasksOids) {
+            assertNoObject(TaskType.class, subtaskOid);
+        }
     }
 
     /** Tests the legacy form of "execute changes" task. */
