12684111351598c1d461dfd4ec85844690cd515a
KaterynaHonchar <honchar@evolveum.com>
2025-01-16 11:23:01 +0100

    #10336 fixing authorization check in gui by using EndPointsUrlMapping actions check

--- model/authentication-api/src/main/java/com/evolveum/midpoint/authentication/api/authorization/AuthorizationActionValue.java
+++ model/authentication-api/src/main/java/com/evolveum/midpoint/authentication/api/authorization/AuthorizationActionValue.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010-2017 Evolveum and contributors
+ * Copyright (c) 2025 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
  */
 
-package com.evolveum.midpoint.authentication.impl.authorization;
+package com.evolveum.midpoint.authentication.api.authorization;
 
 import com.evolveum.midpoint.util.DisplayableValue;
 
