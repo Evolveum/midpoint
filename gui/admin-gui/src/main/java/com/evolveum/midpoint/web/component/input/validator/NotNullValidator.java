bf059cdc9481810ef0bc9ab8ffa5f3741b68fd5b
skublik <lukas.skublik@gmail.com>
2025-01-21 09:42:46 +0100

    MID-10210:move validation of empty parent to validator

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/input/validator/NotNullValidator.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/input/validator/NotNullValidator.java
@@ -6,6 +6,13 @@
  */
 package com.evolveum.midpoint.web.component.input.validator;
 
+import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerValueWrapper;
+import com.evolveum.midpoint.gui.api.prism.wrapper.PrismPropertyWrapper;
+
+import com.evolveum.midpoint.gui.api.util.WebPrismUtil;
+import com.evolveum.midpoint.prism.PrismContainerValue;
+
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.validation.INullAcceptingValidator;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.ValidationError;
@@ -15,8 +22,14 @@ public class NotNullValidator<T> implements INullAcceptingValidator<T>{
     private static final long serialVersionUID = 1L;
     private String key;
     private boolean useModel = false;
+    private IModel<PrismPropertyWrapper<T>> propertyWrapper = null;
 
     public NotNullValidator(String errorMessageKey) {
+        this.key = errorMessageKey;
+    }
+
+    public NotNullValidator(String errorMessageKey, IModel<PrismPropertyWrapper<T>> propertyWrapper) {
+        this.propertyWrapper = propertyWrapper;
         this.key = errorMessageKey;
     }
 
@@ -26,6 +39,10 @@ public class NotNullValidator<T> implements INullAcceptingValidator<T>{
 
     @Override
     public void validate(IValidatable<T> validatable) {
+        if(propertyWrapper != null && skipValidation()) {
+            return;
+        }
+
         if (validatable.getValue() == null) {
             if (useModel && validatable.getModel() != null && validatable.getModel().getObject() != null) {
                 useModel = false;
@@ -34,9 +51,17 @@ public class NotNullValidator<T> implements INullAcceptingValidator<T>{
             ValidationError err = new ValidationError();
             err.addKey(key);
             validatable.error(err);
-
         }
-
     }
 
+    private boolean skipValidation() {
+        PrismContainerValueWrapper parentContainer = propertyWrapper.getObject().getParent();
+        if (parentContainer == null || parentContainer.getNewValue() == null
+                || (parentContainer.getParent() != null && parentContainer.getParent().isMultiValue())) {
+            return false;
+        }
+        PrismContainerValue cleanedUpValue =
+                WebPrismUtil.cleanupEmptyContainerValue(parentContainer.getNewValue().clone());
+        return cleanedUpValue == null || cleanedUpValue.isEmpty();
+    }
 }
