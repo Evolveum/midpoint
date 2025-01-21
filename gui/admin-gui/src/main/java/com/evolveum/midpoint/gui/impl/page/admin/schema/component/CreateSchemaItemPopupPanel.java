7f5a038048ae6f70bc17e5cdcf0227d8af9b98a1
skublik <lukas.skublik@gmail.com>
2025-01-21 12:35:12 +0100

    MID-10283: fix creating of new schema extension item

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/schema/component/CreateSchemaItemPopupPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/schema/component/CreateSchemaItemPopupPanel.java
@@ -1,23 +1,21 @@
 /*
- * Copyright (c) 2023 Evolveum and contributors
+ * Copyright (c) 2010-2025 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
  */
 package com.evolveum.midpoint.gui.impl.page.admin.schema.component;
 
-import com.evolveum.midpoint.gui.api.prism.wrapper.ItemWrapper;
 import com.evolveum.midpoint.gui.api.prism.wrapper.PrismContainerValueWrapper;
 import com.evolveum.midpoint.gui.api.util.LocalizationUtil;
 
 import com.evolveum.midpoint.gui.impl.component.message.FeedbackLabels;
 import com.evolveum.midpoint.gui.impl.prism.panel.ItemPanelSettingsBuilder;
-import com.evolveum.midpoint.gui.impl.prism.panel.vertical.form.VerticalFormPrismContainerValuePanel;
 
 import com.evolveum.midpoint.gui.impl.prism.panel.vertical.form.VerticalFormPrismPropertyPanel;
-import com.evolveum.midpoint.prism.path.ItemName;
+import com.evolveum.midpoint.web.component.AjaxSubmitButton;
+import com.evolveum.midpoint.web.component.form.MidpointForm;
 import com.evolveum.midpoint.web.component.prism.InputPanel;
-import com.evolveum.midpoint.web.component.prism.ItemVisibility;
 import com.evolveum.midpoint.web.model.PrismPropertyWrapperModel;
 import com.evolveum.midpoint.xml.ns._public.prism_schema_3.PrismItemDefinitionType;
 
@@ -33,24 +31,24 @@ import com.evolveum.midpoint.web.component.dialog.Popupable;
 import com.evolveum.midpoint.web.component.dialog.SimplePopupable;
 
 import javax.xml.namespace.QName;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-public abstract class CreateSchemaItemPopupPanel<SI extends PrismItemDefinitionType> extends SimplePopupable {
+public class CreateSchemaItemPopupPanel extends SimplePopupable {
 
     private static final String ID_PANEL = "panel";
     private static final String ID_BUTTON_CANCEL = "cancelButton";
     private static final String ID_BUTTON_CREATE = "createButton";
     private static final String ID_BUTTONS = "buttons";
+    private static final String ID_FORM = "form";
 
     private Fragment footer;
 
-    private final IModel<PrismContainerValueWrapper<SI>> model;
+    private final IModel<PrismContainerValueWrapper<PrismItemDefinitionType>> model;
 
-    public CreateSchemaItemPopupPanel(String id, IModel<PrismContainerValueWrapper<SI>> model) {
+    public CreateSchemaItemPopupPanel(String id, IModel<PrismContainerValueWrapper<PrismItemDefinitionType>> model) {
         super(id, 500, 600, () -> LocalizationUtil.translate("CreateSchemaItemPopupPanel.title"));
         this.model = model;
-        initLayout();
         initFooter();
+        initLayout();
     }
 
     private void initFooter() {
@@ -69,13 +67,27 @@ public abstract class CreateSchemaItemPopupPanel<SI extends PrismItemDefinitionT
         cancel.setOutputMarkupId(true);
         footer.add(cancel);
 
-        AjaxButton create = new AjaxButton(ID_BUTTON_CREATE) {
+        AjaxSubmitButton create = new AjaxSubmitButton(ID_BUTTON_CREATE) {
 
             private static final long serialVersionUID = 1L;
 
             @Override
-            public void onClick(AjaxRequestTarget target) {
-                onClickCreate(target);
+            public void onSubmit(AjaxRequestTarget target) {
+                createPerform(target);
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target) {
+                getPanel().visitChildren(FormComponent.class, (formComponent, object) -> {
+                    ((FormComponent) formComponent).validate();
+                    if (formComponent.hasErrorMessage()) {
+                        target.add(formComponent);
+                        InputPanel inputParent = formComponent.findParent(InputPanel.class);
+                        if (inputParent != null && inputParent.getParent() != null) {
+                            target.addChildren(inputParent.getParent(), FeedbackLabels.class);
+                        }
+                    }
+                });
             }
         };
         create.setOutputMarkupId(true);
@@ -83,37 +95,24 @@ public abstract class CreateSchemaItemPopupPanel<SI extends PrismItemDefinitionT
         this.footer = footer;
     }
 
-    private void onClickCreate(AjaxRequestTarget target) {
-        AtomicBoolean valid = new AtomicBoolean(true);
-        visitChildren(FormComponent.class, (formComponent, object) -> {
-            ((FormComponent) formComponent).setRequired(true);
-            ((FormComponent) formComponent).validate();
-            if (formComponent.hasErrorMessage()) {
-                valid.set(false);
-                target.add(formComponent);
-                InputPanel inputParent = formComponent.findParent(InputPanel.class);
-                if (inputParent != null && inputParent.getParent() != null) {
-                    target.addChildren(inputParent.getParent(), FeedbackLabels.class);
-                }
-            }
-        });
-        if (valid.get()) {
-            createPerform(target);
-        }
-    }
-
     protected void createPerform(AjaxRequestTarget target) {
 
     }
 
     private void initLayout() {
+
+        MidpointForm<Object> form = new MidpointForm<>(ID_FORM);
+        form.setOutputMarkupId(true);
+        form.setDefaultButton((AjaxSubmitButton)footer.get(ID_BUTTON_CREATE));
+        add(form);
+
         VerticalFormPrismPropertyPanel<QName> panel = new VerticalFormPrismPropertyPanel<>(
                 ID_PANEL,
                 PrismPropertyWrapperModel.fromContainerValueWrapper(model, PrismItemDefinitionType.F_TYPE),
                 new ItemPanelSettingsBuilder()
                         .mandatoryHandler(itemWrapper -> true)
                         .build());
-        add(panel);
+        form.add(panel);
     }
 
     @Override
@@ -125,7 +124,7 @@ public abstract class CreateSchemaItemPopupPanel<SI extends PrismItemDefinitionT
         getPageBase().hideMainPopup(target);
     }
 
-    protected VerticalFormPrismContainerValuePanel<SI, PrismContainerValueWrapper<SI>> getPanel() {
-        return (VerticalFormPrismContainerValuePanel<SI, PrismContainerValueWrapper<SI>>) get(ID_PANEL);
+    protected VerticalFormPrismPropertyPanel<QName> getPanel() {
+        return (VerticalFormPrismPropertyPanel<QName>) get(createComponentPath(ID_FORM, ID_PANEL));
     }
 }
