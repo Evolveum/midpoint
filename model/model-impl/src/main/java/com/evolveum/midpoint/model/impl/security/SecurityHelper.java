2c80410f14fac907d98fb19ae6f452ca1e1b6b15
skublik <lukas.skublik@gmail.com>
2025-01-27 09:38:03 +0100

    adding try/catch for calling audit servis during authentication (fix for showing 500 error page if audit throws exception)
    (cherry picked from commit 0c67a87026a644faae8841268476a7ccb25804f3)

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/security/SecurityHelper.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/security/SecurityHelper.java
@@ -84,7 +84,11 @@ public class SecurityHelper implements ModelAuditRecorder {
         record.setMessage(message);
         storeConnectionEnvironment(record, connEnv);
 
-        auditHelper.audit(record, null, task, new OperationResult(SecurityHelper.class.getName() + ".auditLogin"));
+        try {
+            auditHelper.audit(record, null, task, new OperationResult(SecurityHelper.class.getName() + ".auditLogin"));
+        } catch (Exception e) {
+            LOGGER.error("Couldn't audit audit event", e);
+        }
     }
 
     @Override
@@ -98,7 +102,11 @@ public class SecurityHelper implements ModelAuditRecorder {
         record.setTimestamp(System.currentTimeMillis());
         record.setOutcome(OperationResultStatus.SUCCESS);
         storeConnectionEnvironment(record, connEnv);
-        auditHelper.audit(record, null, task, result);
+        try {
+            auditHelper.audit(record, null, task, result);
+        } catch (Exception e) {
+            LOGGER.error("Couldn't audit audit event", e);
+        }
     }
 
     private SystemConfigurationType getSystemConfig() {
