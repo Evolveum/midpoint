5a174a0b6000e9183587b8a241c1b79c7cefdecb
tchrapovic <chrapovic.tadeas@gmail.com>
2025-02-06 14:23:51 +0100

    Role analysis: add missing isOwnedBy method for detected pattern container

--- repo/repo-sqale/src/main/java/com/evolveum/midpoint/repo/sqale/qmodel/mining/cluster/QClusterDetectedPattern.java
+++ repo/repo-sqale/src/main/java/com/evolveum/midpoint/repo/sqale/qmodel/mining/cluster/QClusterDetectedPattern.java
@@ -7,13 +7,11 @@
 package com.evolveum.midpoint.repo.sqale.qmodel.mining.cluster;
 
 import com.evolveum.midpoint.repo.sqale.qmodel.common.QContainer;
-import com.evolveum.midpoint.repo.sqale.qmodel.mining.outlier.MOutlier;
-import com.evolveum.midpoint.repo.sqale.qmodel.object.MObjectType;
-import com.evolveum.midpoint.repo.sqlbase.querydsl.UuidPath;
 
-import com.querydsl.core.types.dsl.EnumPath;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.sql.ColumnMetadata;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.Types;
 
@@ -22,13 +20,18 @@ public class QClusterDetectedPattern extends QContainer<MClusterDetectedPattern,
     public static final String TABLE_NAME = "m_role_analysis_cluster_detected_pattern";
     public static final String ALIAS = "cdp";
 
-
     public static final ColumnMetadata REDUCTION_COUNT =
             ColumnMetadata.named("reductionCount").ofType(Types.DOUBLE);
 
     public QClusterDetectedPattern(String variable) {
-        super(MClusterDetectedPattern.class, variable , DEFAULT_SCHEMA_NAME, TABLE_NAME);
+        super(MClusterDetectedPattern.class, variable, DEFAULT_SCHEMA_NAME, TABLE_NAME);
     }
 
     public NumberPath<Double> reductionCount = createNumber("reductionCount", Double.class, REDUCTION_COUNT);
+
+    @Override
+    public BooleanExpression isOwnedBy(@NotNull MClusterObject ownerRow) {
+        return ownerOid.eq(ownerRow.oid);
+    }
+
 }
