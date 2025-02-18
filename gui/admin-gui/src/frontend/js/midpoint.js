fde9914c7c632f4dd8e16dbde92df7c32ed72c13
skublik <lukas.skublik@gmail.com>
2025-02-18 18:34:54 +0100

    MID-10440: use latest version of sass libraries and hide warnings processed by sass

--- gui/admin-gui/src/frontend/js/midpoint.js
+++ gui/admin-gui/src/frontend/js/midpoint.js
@@ -8,9 +8,7 @@
 import MidPointTheme from './midpoint-theme';
 import MidPointAceEditor from "./ace-editor";
 import MidPointHoneypot from "./honeypot";
-import Popper from '@popperjs/core';
 
 window.MidPointTheme = new MidPointTheme();
 window.MidPointAceEditor = new MidPointAceEditor();
 window.MidPointHoneypot = new MidPointHoneypot();
-window.Popper = Popper;
