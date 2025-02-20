529705d536749b023534f0811b3a7bf0e9a57c11
KaterynaHonchar <honchar@evolveum.com>
2025-02-20 13:43:18 +0100

    #10398 fix for request access: changing person of interest while having shopping cart items leads to authorization check

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/util/WebComponentUtil.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/util/WebComponentUtil.java
@@ -2259,9 +2259,6 @@ public final class WebComponentUtil {
         } finally {
             result.recomputeStatus();
         }
-        if (!result.isSuccess() && !result.isHandledError()) {
-            pageBase.showResult(result);
-        }
         return filter;
     }
 
