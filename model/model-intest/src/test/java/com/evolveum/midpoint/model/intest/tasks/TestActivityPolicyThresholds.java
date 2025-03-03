76446fcab3fea6740135576e2a3247dd2953b8d5
Viliam Repan <vilo.repan@evolveum.com>
2025-03-03 22:59:41 +0100

    MID-10412 placeholder for tests, wip. enabled all other policy thresholds tests

---
+++ model/model-intest/src/test/java/com/evolveum/midpoint/model/intest/tasks/TestActivityPolicyThresholds.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2010-2025 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.model.intest.tasks;
+
+public class TestActivityPolicyThresholds {
+}
