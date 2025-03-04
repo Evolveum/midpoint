1c9199ddc7a671fa57e3e2271a413cb9844fe307
Pavol Mederly <mederly@evolveum.com>
2025-03-04 18:01:03 +0100

    Add repo caching parameters to perf. test
    (cherry picked from commit 6e34641fd1bfd24d2868f27f45c1ded0c106da85)
    (cherry picked from commit 15bbd7cce4f358351712787b1dba4e29410b77bf)

--- testing/story/src/test/java/com/evolveum/midpoint/testing/story/sysperf/OtherParameters.java
+++ testing/story/src/test/java/com/evolveum/midpoint/testing/story/sysperf/OtherParameters.java
@@ -1,9 +1,14 @@
 package com.evolveum.midpoint.testing.story.sysperf;
 
+import com.evolveum.midpoint.schema.constants.ObjectTypes;
+import com.evolveum.midpoint.util.MiscUtil;
+
+import com.google.common.collect.Sets;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.evolveum.midpoint.testing.story.sysperf.TestSystemPerformance.*;
 
@@ -11,17 +16,42 @@ public class OtherParameters {
 
     private static final String PROP_LABEL = "label";
     private static final String PROP_TASK_TIMEOUT = "taskTimeout";
+    private static final String PROP_CACHED = "cached";
+    private static final String PROP_NOT_CACHED = "not-cached";
 
     private static final File SYSTEM_CONFIGURATION_TEMPLATE_FILE = new File(TEST_DIR, "system-configuration.vm.xml");
 
+    private static final Set<ObjectTypes> CACHED_BY_DEFAULT = Set.of(
+            ObjectTypes.SYSTEM_CONFIGURATION,
+            ObjectTypes.ARCHETYPE,
+            ObjectTypes.OBJECT_TEMPLATE,
+            ObjectTypes.SECURITY_POLICY,
+            ObjectTypes.PASSWORD_POLICY, // ValuePolicyType
+            ObjectTypes.RESOURCE,
+            ObjectTypes.ROLE,
+            ObjectTypes.SHADOW,
+            ObjectTypes.MARK,
+            ObjectTypes.CONNECTOR);
+
     final String label;
     final int taskTimeout;
+    final Set<ObjectTypes> cached;
+    final Set<ObjectTypes> notCached;
 
     private OtherParameters() {
         this.label = System.getProperty(PROP_LABEL, createDefaultLabel());
         this.taskTimeout = Integer.parseInt(System.getProperty(PROP_TASK_TIMEOUT, "1800000")); // 30 minutes
+        this.cached = parseObjectTypeNames(System.getProperty(PROP_CACHED));
+        this.notCached = parseObjectTypeNames(System.getProperty(PROP_NOT_CACHED));
 
         createSystemConfigurationFile();
+    }
+
+    private Set<ObjectTypes> parseObjectTypeNames(String stringValue) {
+        return Arrays.stream(MiscUtil.emptyIfNull(stringValue).split(","))
+                .filter(s -> !s.isBlank())
+                .map(name -> ObjectTypes.getObjectType(name.trim()))
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     private String createDefaultLabel() {
@@ -93,7 +123,16 @@ public class OtherParameters {
         VelocityGenerator.generate(
                 SYSTEM_CONFIGURATION_TEMPLATE_FILE,
                 GENERATED_SYSTEM_CONFIGURATION_FILE,
-                Map.of());
+                Map.of("cachedTypeNames", computeCachedTypeNames()));
+    }
+
+    private Collection<String> computeCachedTypeNames() {
+        var effective = Sets.difference(
+                Sets.union(CACHED_BY_DEFAULT, cached),
+                notCached);
+        return effective.stream()
+                .map(ObjectTypes::getValue)
+                .toList();
     }
 
     @Override
@@ -101,6 +140,8 @@ public class OtherParameters {
         return "OtherParameters{" +
                 "label=" + label +
                 ", taskTimeout=" + taskTimeout +
+                ", cached=" + cached +
+                ", notCached=" + notCached +
                 '}';
     }
 }
