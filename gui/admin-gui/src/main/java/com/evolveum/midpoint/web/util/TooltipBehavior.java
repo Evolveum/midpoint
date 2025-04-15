a5d0bef7e3fa218519329c75f894bc36866853c8
idojcinovic <idojcinovic@evolveum.com>
2025-04-15 18:38:43 +0200

    add WCAG support for issues 5.2.5, 5.2.6, 5.2.10

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/util/TooltipBehavior.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/util/TooltipBehavior.java
@@ -10,6 +10,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 
 /**
  * This behavior is used for bootstrap tooltips. Just add this behaviour to {@link org.apache.wicket.markup.html.basic.Label}.
@@ -23,6 +25,7 @@ public class TooltipBehavior extends Behavior {
     public void onConfigure(final Component component) {
         component.setOutputMarkupId(true);
 
+        component.add(AttributeModifier.append("data-html", "true"));
         component.add(AttributeModifier.replace("data-toggle", "tooltip"));
         component.add(new AttributeModifier("data-placement", getDataPlacement()) {
 
@@ -34,6 +37,69 @@ public class TooltipBehavior extends Behavior {
                 return currentValue;
             }
         });
+    }
+
+    /* added for WCAG tooltip issues 5.2.5, 5.2.6 */
+    @Override
+    public void renderHead(Component component, IHeaderResponse response) {
+        String markupId = component.getMarkupId();
+        String js = String.format("""
+            (function() {
+                var $el = $('#%s');
+                $el.attr('tabindex', '0');
+                $el.tooltip({ trigger: 'manual' });
+
+                let isHovered = false;
+                let isTooltipHovered = false;
+
+                function showTooltip() {
+                    $el.tooltip('show');
+
+                    setTimeout(function() {
+                        var $tooltip = $('.tooltip');
+
+                        $tooltip.on('mouseenter', function () {
+                            isTooltipHovered = true;
+                        }).on('mouseleave', function () {
+                            isTooltipHovered = false;
+                            checkHide();
+                        });
+                    }, 100);
+                }
+
+                function checkHide() {
+                    setTimeout(function () {
+                        if (!isHovered && !isTooltipHovered && !$el.is(':focus')) {
+                            $el.tooltip('hide');
+                        }
+                    }, 150);
+                }
+
+                $el.on('mouseenter focus', function () {
+                    isHovered = true;
+                    showTooltip();
+                });
+
+                $el.on('mouseleave blur', function () {
+                    isHovered = false;
+                    checkHide();
+                });
+
+                $(document).on('keydown', function(e) {
+                    if (e.key === "Escape") {
+                        $el.tooltip('hide');
+                    }
+                });
+
+                $(document).on('focusin', function(e) {
+                    if (!$(e.target).closest('.tooltip, #%s').length) {
+                        $el.tooltip('hide');
+                    }
+                });
+            })();
+            """, markupId, markupId);
+
+        response.render(OnDomReadyHeaderItem.forScript(js));
     }
 
     public String getDataPlacement() {
