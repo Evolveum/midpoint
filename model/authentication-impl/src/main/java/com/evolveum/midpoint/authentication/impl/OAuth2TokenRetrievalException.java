527719d0a528c8239ebe9c3ebf199f9d0e33e1c1
Petr Vsetecka <prace@vseta.cz>
2025-07-24 16:53:14 +0200

    feat: add oauth2 credential flow to mail servers
    (cherry picked from commit 5dbc63d0fbb66561c6ebe31154d9f72a040b0681)

---
+++ model/authentication-impl/src/main/java/com/evolveum/midpoint/authentication/impl/OAuth2TokenRetrievalException.java
@@ -0,0 +1,7 @@
+package com.evolveum.midpoint.authentication.impl;
+
+public class OAuth2TokenRetrievalException extends Exception {
+    public OAuth2TokenRetrievalException(String message) {
+        super(message);
+    }
+}
