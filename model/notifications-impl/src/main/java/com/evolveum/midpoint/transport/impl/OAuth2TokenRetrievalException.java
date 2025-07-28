5444b4486006c3e54c76dc901ade17b647f80555
Petr Vsetecka <prace@vseta.cz>
2025-07-28 08:15:47 +0200

    refactor: remove dependency on authentication-impl

--- model/notifications-impl/src/main/java/com/evolveum/midpoint/transport/impl/OAuth2TokenRetrievalException.java
+++ model/notifications-impl/src/main/java/com/evolveum/midpoint/transport/impl/OAuth2TokenRetrievalException.java
@@ -1,4 +1,4 @@
-package com.evolveum.midpoint.authentication.impl;
+package com.evolveum.midpoint.transport.impl;
 
 public class OAuth2TokenRetrievalException extends Exception {
     public OAuth2TokenRetrievalException(String message) {
