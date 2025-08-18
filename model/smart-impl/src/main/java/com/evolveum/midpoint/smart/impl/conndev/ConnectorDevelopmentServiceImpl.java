7886c569aeeba74e14041baa894a38845bcf25a9
Tony Tkáčik <tonydamage@gmail.com>
2025-08-18 14:45:58 +0200

    Smart Integration: Initial Connector Development model

---
+++ model/smart-impl/src/main/java/com/evolveum/midpoint/smart/impl/conndev/ConnectorDevelopmentServiceImpl.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2010-2025 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+package com.evolveum.midpoint.smart.impl.conndev;
+
+public class ConnectorDevelopmentServiceImpl {
+
+
+
+}
