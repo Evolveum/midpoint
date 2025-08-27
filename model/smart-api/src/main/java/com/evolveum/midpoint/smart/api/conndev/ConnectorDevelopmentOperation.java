cdfcacc69e8ed4470301038db6966f22c842c2bb
Tony Tkáčik <tonydamage@gmail.com>
2025-08-27 16:22:10 +0200

    Refactor and expand methods in `ConnectorDevelopmentOperation` to enhance functionality and clarity

--- model/smart-api/src/main/java/com/evolveum/midpoint/smart/api/conndev/ConnectorDevelopmentOperation.java
+++ model/smart-api/src/main/java/com/evolveum/midpoint/smart/api/conndev/ConnectorDevelopmentOperation.java
@@ -1,6 +1,7 @@
 package com.evolveum.midpoint.smart.api.conndev;
 
 import com.evolveum.midpoint.prism.PrismContainer;
+import com.evolveum.midpoint.schema.processor.BareResourceSchema;
 import com.evolveum.midpoint.smart.api.info.StatusInfo;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 
@@ -8,31 +9,62 @@ public interface ConnectorDevelopmentOperation {
 
     ConnectorDevelopmentType getObject();
 
-
+    // AI only
     StatusInfo<PrismContainer<ConnDevDocumentationSourceType>> discoverDocumentation(ConnectorDevelopmentType type);
 
+    // AI only
     StatusInfo<ConnectorDevelopmentType> processDocumentation(PrismContainer<ConnDevDocumentationSourceType> sources);
 
-    ConnectorDevelopmentType updateBasicInformation(ConnDevApplicationInfoType basicInfo);
+    // Midpoint local (AI optional in background)
+    void basicConnectorInfoUpdated(ConnectorDevelopmentType  updated);
 
+    // AI optional
     StatusInfo<PrismContainer<ConnDevAuthInfoType>>  selectBaseApiInformation(String basicInfo);
 
+    // Midpoint local (+ download framework)
     StatusInfo<ConnectorType> updateSupportedAuthTypes(PrismContainer<ConnDevAuthInfoType> basicInfo);
 
-    StatusInfo<ResourceType> testConnection(ConnectorConfigurationType type);
+    // FIXME: Also add operation results
+    // Midpoint local
+    ResourceType testConnection(ConnectorConfigurationType type);
 
-    StatusInfo<PrismContainer<ConnDevBasicObjectClassInfoType>> discoverObjectClasses(ResourceType resourceType);
+    // AI optional
+    StatusInfo<PrismContainer<ConnDevBasicObjectClassInfoType>> discoverObjectClasses();
 
-    StatusInfo<ConnDevArtifactType> generateNativeSchemaScript(ConnDevBasicObjectClassInfoType type, ResourceType resourceType);
+    // AI optional
+    StatusInfo<PrismContainer<ConnDevAttributeInfoType>> generateAttributes(ConnDevBasicObjectClassInfoType type);
 
-    ConnectorDevelopmentType saveNativeSchemaScript(ConnDevArtifactType type);
+    // AI required for REST, could be local for SCIM partially
+    StatusInfo<ConnDevArtifactType> generateNativeSchemaScript(PrismContainer<ConnDevAttributeInfoType> type);
 
+    // local
+    String getArtifactContent(ConnDevArtifactType type);
+
+    // local
+    void saveNativeSchemaScript(ConnDevArtifactType type, String updated);
+
+    // local
+    BareResourceSchema testSchema(ConnDevArtifactType type);
+
+    // AI only
     StatusInfo<PrismContainer<ConnDevHttpEndpointType>> getSearchEndpoints(String objectClass);
 
+    // AI + local skeleton
     StatusInfo<ConnDevArtifactType> generateSearchAll(String objectClass, ConnDevHttpEndpointType endpoint);
 
+    // local
     void testSearchAll(String objectClass, ConnDevArtifactType script);
 
+    // local
+    void saveSearchAll(ConnDevArtifactType script, String body);
+
+    // local
     void saveArtifact(String objectClass, ConnDevArtifactType endpoint);
+
+    StatusInfo<ConnDevArtifactType> generateGet(String objectClass, ConnDevHttpEndpointType endpoint);
+
+    void testGet(String objectClass, ConnDevArtifactType script);
+
+    void saveGet(ConnDevArtifactType script, String body);
 
 }
