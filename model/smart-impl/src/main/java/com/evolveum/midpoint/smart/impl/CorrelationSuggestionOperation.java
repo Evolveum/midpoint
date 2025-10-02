82e6df955892ec1301291e5b690040409d6b4eb0
tchrapovic <chrapovic.tadeas@gmail.com>
2025-10-02 09:56:20 +0200

    Smart integration: improve correlation suggestion properties info
    1. Add missing correlation mapping name (same as normal mapping)
    2. Remove dummy text from correlation description
    3. Use more meaningful correlation name value

--- model/smart-impl/src/main/java/com/evolveum/midpoint/smart/impl/CorrelationSuggestionOperation.java
+++ model/smart-impl/src/main/java/com/evolveum/midpoint/smart/impl/CorrelationSuggestionOperation.java
@@ -7,6 +7,8 @@
 
 package com.evolveum.midpoint.smart.impl;
 
+import ch.qos.logback.core.util.StringUtil;
+
 import com.evolveum.midpoint.prism.PrismObjectDefinition;
 import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.schema.config.ConfigurationItemOrigin;
@@ -63,12 +65,15 @@ class CorrelationSuggestionOperation {
                 // no need to mark it as AI-provided, as it should already marked as such
                 suggestionBean.getAttributes().add(suggestion.attributeDefinitionBean());
             }
+
+            String lastName = suggestion.focusItemPath.lastName().getLocalPart();
+            String correlatorName = StringUtil.capitalizeFirstLetter(lastName) + " correlator";
             var correlationDefinition = new CorrelationDefinitionType()
                     .correlators(new CompositeCorrelatorType()
                             .items(new ItemsSubCorrelatorType()
-                                    .name("Dummy name") //TODO change after (v2) correlation LLM integration
-                                    .displayName("Dummy display name") //TODO
-                                    .description("Dummy description. Suggested based on matching of "
+                                    .name(correlatorName) //TODO change after (v2) correlation LLM integration
+                                    .displayName(correlatorName) //TODO
+                                    .description("Suggested based on matching of "
                                             + suggestion.resourceAttrPath() + " to "
                                             + suggestion.focusItemPath()) //TODO
                                     .composition(new CorrelatorCompositionDefinitionType()
@@ -110,6 +115,8 @@ class CorrelationSuggestionOperation {
                         var resourceAttrPath = matchingOp.getApplicationItemPath(siAttributeMatch.getApplicationAttribute());
                         var resourceAttrName = resourceAttrPath.rest(); // skipping "c:attributes"; TODO handle or skip other cases
                         var inbound = new InboundMappingType()
+                                .name(resourceAttrPath.lastName().getLocalPart()
+                                        + "-to-" + focusItemPath) //TODO TBD
                                 .target(new VariableBindingDefinitionType()
                                         .path(focusItemPath.toBean()))
                                 .use(InboundMappingUseType.CORRELATION);
