691c9ccf24e4486e3a83efc22bf6eaebf9a65c46
Andrej Zan <andrej.zan@evolveum.com>
2025-10-10 16:47:47 +0200

    Focus statistics improvements and changes
    **What**
    
    - Collect statistics also for properties in custom schema extensions.
    - Change all counts to ratios (except objects count).
    - Add new stats like ratio of multi valued properties.
    - Use order of magnitudes for object counts instead of exact counts.
    - Remove "floating" threshold for distribution threshold.
    - Do not calculate cardinality if distribution is shown.
    - Do not consider null values (basically missing property) as a
      "category" (each set of identical values is one category). That means:
      - Missing properties are not counted in cardinality.
      - Missing properties are not part of the distribution array.
    
    **Why**
    
    Most of the changes were done from compliance reasons. We do not want to
    expose potentially sensitive statistics.

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/stats/FocusTypeCounter.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/stats/FocusTypeCounter.java
@@ -1,11 +1,20 @@
 package com.evolveum.midpoint.ninja.action.stats;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.evolveum.midpoint.prism.ComplexTypeDefinition;
+import com.evolveum.midpoint.prism.Containerable;
+import com.evolveum.midpoint.prism.ItemDefinition;
+import com.evolveum.midpoint.prism.PrismContainer;
+import com.evolveum.midpoint.prism.PrismContainerDefinition;
+import com.evolveum.midpoint.prism.PrismContainerValue;
 import com.evolveum.midpoint.prism.PrismObject;
-import com.evolveum.midpoint.prism.PrismObjectValue;
+import com.evolveum.midpoint.prism.PrismProperty;
+import com.evolveum.midpoint.prism.PrismPropertyDefinition;
 import com.evolveum.midpoint.util.exception.SchemaException;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 
@@ -13,40 +22,79 @@ class FocusTypeCounter {
 
     private final String focusType;
     private final Map<String, PropertyStatsCounter> statsCounterMap;
-    private int totalCount;
+    private final MagnitudeCounter totalCount;
 
     FocusTypeCounter(String focusType) {
         this.focusType = focusType;
-        this.totalCount = 0;
+        this.totalCount = new MagnitudeCounter();
         this.statsCounterMap = new HashMap<>();
     }
 
     void count(PrismObject<? extends ObjectType> object) {
-        this.totalCount++;
-        final PrismObjectValue<? extends ObjectType> objectValue = object.getValue();
-        object.getDefinition().getPropertyDefinitions().stream()
-                .map(propertyDef -> {
-                    try {
-                        return objectValue.findOrCreateProperty(propertyDef);
-                    } catch (SchemaException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .filter(property -> property.size() <= 1)
-                .forEach(property -> {
-                    final String propertyName = property.getElementName().getLocalPart();
-                    final PropertyStatsCounter counter = this.statsCounterMap.computeIfAbsent(
-                            propertyName, key -> new PropertyStatsCounter(propertyName));
-                    final String value = property.getRealValue() == null ? null : property.getRealValue().toString();
-                    counter.count(value);
-                });
+        this.totalCount.increment();
+        final List<PrismPropertyDefinition<?>> propertyDefinitions = object.getDefinition().getPropertyDefinitions();
+        final Collection<PrismProperty<?>> properties = collectProperties(object.getValue(), propertyDefinitions);
+        try {
+            final PrismContainer<?> extension = object.getOrCreateExtension();
+            properties.addAll(flatten(extension));
+        } catch (SchemaException e) {
+            throw new RuntimeException(e);
+        }
+        properties.forEach(property -> {
+            final String propertyPath = property.getPath().toString();
+            final PropertyStatsCounter counter = this.statsCounterMap.computeIfAbsent(
+                    propertyPath, key -> new PropertyStatsCounter(propertyPath));
+            counter.count(property);
+        });
     }
 
     FocusTypeStats calculate() {
         final Collection<PropertyStats> propertiesStats = this.statsCounterMap.values().stream()
                 .map(PropertyStatsCounter::calculate)
                 .toList();
-        return new FocusTypeStats(this.focusType, this.totalCount, propertiesStats);
+        return new FocusTypeStats(this.focusType, this.totalCount.toOrderOfMagnitude(), propertiesStats);
+    }
+
+    private static Collection<PrismProperty<?>> flatten(PrismContainer<?> container) {
+        final ComplexTypeDefinition complexTypeDefinition = container.getComplexTypeDefinition();
+        final PrismContainerValue<?> containerValue = container.hasNoValues()
+                ? container.createNewValue()
+                : container.getValue();
+
+        final Collection<PrismProperty<?>> currentLevelProperties = collectProperties(containerValue,
+                complexTypeDefinition.getPropertyDefinitions());
+        final List<PrismProperty<?>> nestedProperties = new ArrayList<>();
+        for (final ItemDefinition<?> definition : complexTypeDefinition.getDefinitions()) {
+            if (definition instanceof PrismContainerDefinition<?> containerDefinition) {
+                // We decided to not calculate stats for multivalued containers, because it complicates calculation, and we are
+                // not even sure if we would use such stats. We also MUST NOT work with "elaborate" containers,
+                // because they may contain cycles.
+                if (definition.isSingleValue() && !definition.isElaborate()) {
+                    try {
+                        final PrismContainer<Containerable> innerContainer = containerValue.findOrCreateContainer(
+                                containerDefinition.getItemName());
+                        nestedProperties.addAll(flatten(innerContainer));
+                    } catch (SchemaException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+        currentLevelProperties.addAll(nestedProperties);
+        return currentLevelProperties;
+    }
+
+    private static Collection<PrismProperty<?>> collectProperties(PrismContainerValue<?> containerValue,
+            Collection<PrismPropertyDefinition<?>> propertyDefinitions) {
+        final List<PrismProperty<?>> properties = new ArrayList<>();
+        for (PrismPropertyDefinition<?> definition : propertyDefinitions) {
+            try {
+                properties.add(containerValue.findOrCreateProperty(definition));
+            } catch (SchemaException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return properties;
     }
 
 }
--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/stats/PropertyStats.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/stats/PropertyStats.java
@@ -11,17 +11,20 @@ import com.evolveum.midpoint.prism.path.ItemName;
 import com.evolveum.midpoint.schema.constants.SchemaConstants;
 import com.evolveum.midpoint.util.exception.SchemaException;
 
-public record PropertyStats(String path, int missingCount, float cardinality,
+public record PropertyStats(String path, float multiValuedRatio, float missingValuesRatio, @Nullable Float cardinality,
         @Nullable CategoriesDistribution distribution) implements PrismTransformableRecord {
     private static final ItemName C_DISTRIBUTION = new ItemName(SchemaConstants.NS_C, "distribution");
     private static final ItemName F_PATH = new ItemName(SchemaConstants.NS_C, "path");
-    private static final ItemName F_MISSING_COUNT = new ItemName(SchemaConstants.NS_C, "missingCount");
+    private static final ItemName F_MISSING_RATIO = new ItemName(SchemaConstants.NS_C, "missingRatio");
     private static final ItemName F_CARDINALITY = new ItemName(SchemaConstants.NS_C, "cardinality");
+    private static final ItemName F_MULTI_VALUED_RATIO = new ItemName(SchemaConstants.NS_C, "multiValuedRatio");
 
     @Override
     public PrismContainerValue<Containerable> asPrismContainerValue(ItemFactory itemFactory) throws SchemaException {
-        final PrismProperty<Integer> missingCountProperty = itemFactory.createProperty(F_MISSING_COUNT);
-        missingCountProperty.setRealValue(this.missingCount);
+        final PrismProperty<Float> multiValuedRatio = itemFactory.createProperty(F_MULTI_VALUED_RATIO);
+        multiValuedRatio.setRealValue(this.multiValuedRatio);
+        final PrismProperty<Float> missingCountProperty = itemFactory.createProperty(F_MISSING_RATIO);
+        missingCountProperty.setRealValue(this.missingValuesRatio);
         final PrismProperty<Float> cardinalityProperty = itemFactory.createProperty(F_CARDINALITY);
         cardinalityProperty.setRealValue(this.cardinality);
         final PrismProperty<String> pathProperty = itemFactory.createProperty(F_PATH);
@@ -29,6 +32,7 @@ public record PropertyStats(String path, int missingCount, float cardinality,
 
         final PrismContainerValue<Containerable> propertyStatsValue = itemFactory.createContainerValue();
         propertyStatsValue.add(pathProperty);
+        propertyStatsValue.add(multiValuedRatio);
         propertyStatsValue.add(missingCountProperty);
         propertyStatsValue.add(cardinalityProperty);
         final PrismContainer<Containerable> distributionContainer = itemFactory.createContainer(C_DISTRIBUTION);
