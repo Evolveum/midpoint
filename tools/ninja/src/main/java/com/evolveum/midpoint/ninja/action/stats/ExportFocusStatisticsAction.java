a0a9e822d4451c557cf5829ccecdd0d216a544a9
Andrej Zan <andrej.zan@evolveum.com>
2025-10-11 16:05:54 +0200

    Hard-code items allow list to Ninja's focus stats export
    **What**
    
    Add hard-coded list of items, which are by default included in focus
    stats export.
    
    **Why**
    
    Not all items are interesting for our needs, and some items may be also
    sensitive. Because of that we decided to include just some particular
    items. Only these hard-coded items will be included in the export.
    
    Items can still be explicitly excluded by user with the use of `-ei`
    option.
    
    Explicitly excluding items that are not by default included has no
    effect, since they would not be exported anyway.
    
    (cherry picked from commit eba3e89200b522559b7a88bfa5c7cb1407db267a)

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/stats/ExportFocusStatisticsAction.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/stats/ExportFocusStatisticsAction.java
@@ -3,18 +3,47 @@ package com.evolveum.midpoint.ninja.action.stats;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.function.Predicate;
 
 import com.evolveum.midpoint.ninja.action.AbstractRepositorySearchAction;
 import com.evolveum.midpoint.ninja.action.ExportOptions;
 import com.evolveum.midpoint.ninja.util.OperationStatus;
+import com.evolveum.midpoint.prism.ItemDefinition;
+import com.evolveum.midpoint.prism.path.ItemName;
+import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.schema.constants.ObjectTypes;
+import com.evolveum.midpoint.schema.constants.SchemaConstants;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 
 public class ExportFocusStatisticsAction extends AbstractRepositorySearchAction<ExportOptions, Void> {
+    private static final List<ItemName> INCLUDE_ITEMS = List.of(
+            new ItemName(SchemaConstants.NS_C, "description"), new ItemName(SchemaConstants.NS_C, "documentation"),
+            new ItemName(SchemaConstants.NS_C, "subtype"), new ItemName(SchemaConstants.NS_C, "lifecycleState"),
+            new ItemName(SchemaConstants.NS_C, "jpegPhoto"), new ItemName(SchemaConstants.NS_C, "costCenter"),
+            new ItemName(SchemaConstants.NS_C, "locality"), new ItemName(SchemaConstants.NS_C, "preferredLanguage"),
+            new ItemName(SchemaConstants.NS_C, "locale"), new ItemName(SchemaConstants.NS_C, "timezone"),
+            new ItemName(SchemaConstants.NS_C, "emailAddress"), new ItemName(SchemaConstants.NS_C, "telephoneNumber"),
+            new ItemName(SchemaConstants.NS_C, "fullName"), new ItemName(SchemaConstants.NS_C, "givenName"),
+            new ItemName(SchemaConstants.NS_C, "familyName"), new ItemName(SchemaConstants.NS_C, "additionalName"),
+            new ItemName(SchemaConstants.NS_C, "nickName"), new ItemName(SchemaConstants.NS_C, "honorificPrefix"),
+            new ItemName(SchemaConstants.NS_C, "honorificSuffix"), new ItemName(SchemaConstants.NS_C, "title"),
+            new ItemName(SchemaConstants.NS_C, "employeeNumber"), new ItemName(SchemaConstants.NS_C, "personalNumber"),
+            new ItemName(SchemaConstants.NS_C, "organization"),
+            new ItemName(SchemaConstants.NS_C, "organizationalUnit"), new ItemName(SchemaConstants.NS_C, "displayName"),
+            new ItemName(SchemaConstants.NS_C, "identifier"), new ItemName(SchemaConstants.NS_C, "riskLevel"),
+            new ItemName(SchemaConstants.NS_C, "mailDomain"), new ItemName(SchemaConstants.NS_C, "displayOrder"),
+            new ItemName(SchemaConstants.NS_C, "url"), new ItemName(SchemaConstants.NS_C, "extension"));
+
     @Override
     protected Callable<Void> createConsumer(BlockingQueue<ObjectType> queue, OperationStatus operation) {
         return () -> {
-            final StatsCounter statsCounter = new StatsCounter(options.getExcludeItems());
+            final List<ItemPath> excludeItems = this.options.getExcludeItems();
+
+            Predicate<ItemDefinition<?>> includePredicate =
+                    definition -> INCLUDE_ITEMS.stream().anyMatch(definition.getItemName()::isSuperPathOrEquivalent);
+            includePredicate = includePredicate.and(
+                    definition -> excludeItems.stream().noneMatch(definition.getItemName()::isSuperPathOrEquivalent));
+            final StatsCounter statsCounter = new StatsCounter(includePredicate);
             new ExportFocusStatisticsWorker(context, options, queue, operation, statsCounter).run();
             return null;
         };
