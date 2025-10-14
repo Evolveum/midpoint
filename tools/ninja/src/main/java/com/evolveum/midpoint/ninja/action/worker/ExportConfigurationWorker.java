4451c90291ef82aed604b0eb68f3c4b3d1b04646
Pavol Mederly <mederly@evolveum.com>
2025-10-14 13:08:21 +0200

    Update exported configuration items
    1. Resource schema is now correctly exported
    2. Additional connector config is removed
    3. AdminGuiConfiguration is removed
    4. GUI-related things from ArchetypeType are removed
    5. Irrelevant parts of ConnectorType are removed
    6. Removal of protected values is fixed, plus we mask
    protected value embedded deep in JAXB structures.
    
    (cherry picked from commit 198418e5f188a4151fae5cb2d21800f2d65e90d3)

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/worker/ExportConfigurationWorker.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/worker/ExportConfigurationWorker.java
@@ -1,9 +1,13 @@
 package com.evolveum.midpoint.ninja.action.worker;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.function.Consumer;
+import javax.xml.namespace.QName;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import com.evolveum.midpoint.common.cleanup.CleanupPath;
 import com.evolveum.midpoint.common.cleanup.CleanupPathAction;
@@ -11,15 +15,14 @@ import com.evolveum.midpoint.common.cleanup.ObjectCleaner;
 import com.evolveum.midpoint.ninja.action.ExportOptions;
 import com.evolveum.midpoint.ninja.impl.NinjaContext;
 import com.evolveum.midpoint.ninja.util.OperationStatus;
-import com.evolveum.midpoint.prism.PrismObject;
-import com.evolveum.midpoint.prism.PrismProperty;
-import com.evolveum.midpoint.prism.PrismPropertyValue;
-import com.evolveum.midpoint.prism.PrismVisitor;
+import com.evolveum.midpoint.prism.*;
 import com.evolveum.midpoint.prism.path.ItemName;
 import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.prism.path.ItemPathCollectionsUtil;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
+import com.evolveum.prism.xml.ns._public.types_3.ProtectedByteArrayType;
 import com.evolveum.prism.xml.ns._public.types_3.ProtectedDataType;
+import com.evolveum.prism.xml.ns._public.types_3.ProtectedStringType;
 
 public class ExportConfigurationWorker extends ExportConsumerWorker {
 
@@ -39,38 +42,115 @@ public class ExportConfigurationWorker extends ExportConsumerWorker {
         final ObjectCleaner objectCleaner = new ObjectCleaner();
         objectCleaner.setRemoveMetadata(true);
         objectCleaner.setPaths(List.of(
-                new CleanupPath(ResourceType.COMPLEX_TYPE,
-                        ResourceType.F_CONNECTOR_CONFIGURATION, CleanupPathAction.REMOVE),
-                new CleanupPath(AssignmentHolderType.COMPLEX_TYPE,
-                        AssignmentHolderType.F_ROLE_MEMBERSHIP_REF, CleanupPathAction.REMOVE)));
+                remove(ResourceType.COMPLEX_TYPE, ResourceType.F_CONNECTOR_CONFIGURATION),
+                remove(ResourceType.COMPLEX_TYPE,
+                        ResourceType.F_ADDITIONAL_CONNECTOR, ConnectorInstanceSpecificationType.F_CONNECTOR_CONFIGURATION),
+                remove(AssignmentHolderType.COMPLEX_TYPE, AssignmentHolderType.F_ROLE_MEMBERSHIP_REF),
+                remove(AbstractRoleType.COMPLEX_TYPE, AbstractRoleType.F_ADMIN_GUI_CONFIGURATION),
+                remove(ArchetypeType.COMPLEX_TYPE, ArchetypeType.F_ARCHETYPE_POLICY, ArchetypePolicyType.F_DISPLAY),
+                remove(ArchetypeType.COMPLEX_TYPE, ArchetypeType.F_ARCHETYPE_POLICY, ArchetypePolicyType.F_ITEM_CONSTRAINT),
+                remove(ArchetypeType.COMPLEX_TYPE, ArchetypeType.F_ARCHETYPE_POLICY, ArchetypePolicyType.F_ADMIN_GUI_CONFIGURATION),
+                keep(ResourceType.COMPLEX_TYPE, ResourceType.F_SCHEMA, XmlSchemaType.F_DEFINITION) // operational data - must be explicitly allowed
+        ));
         objectCleaner.process(prismObject);
 
-        final List<ItemName> include = List.of(
-                SystemConfigurationType.F_DEFAULT_OBJECT_POLICY_CONFIGURATION, SystemConfigurationType.F_MODEL_HOOKS,
+        keepOnly(prismObject,
+                SystemConfigurationType.class,
+                SystemConfigurationType.F_DEFAULT_OBJECT_POLICY_CONFIGURATION,
+                SystemConfigurationType.F_MODEL_HOOKS,
                 SystemConfigurationType.F_CORRELATION);
 
-        if (prismObject.isOfType(SystemConfigurationType.class)) {
-            prismObject.getValue().getItems().forEach(item -> item.removeIf(
-                    val -> !ItemPathCollectionsUtil.containsSubpathOrEquivalent(include, val.getPath())));
-        }
+        keepOnly(prismObject,
+                ConnectorType.class,
+                ConnectorType.F_NAME,
+                ConnectorType.F_AVAILABLE);
 
+        // Remove items that contains ProtectedDataType values directly
         final Collection<ItemPath> excludeItems = options.getExcludeItems();
         prismObject.acceptVisitor(sensitiveDataCollector(excludeItems::add));
         excludeItems.forEach(path -> prismObject.removeItem(path, PrismProperty.class));
 
+        // Mask other ProtectedDataType values (it is quite hard to remove them)
+        prismObject.accept(new SensitiveDataRemovingVisitor(context));
+    }
 
-        // Based on object type we can remove additional items, which are not interesting to us
+    private static void keepOnly(
+            PrismObject<? extends ObjectType> prismObject, Class<? extends ObjectType> type, ItemName... itemNames) {
+        if (prismObject.isOfType(type)) {
+            final List<ItemName> itemNamesList = List.of(itemNames);
+            prismObject.getValue().getItems().forEach(item -> item.removeIf(
+                    val -> !ItemPathCollectionsUtil.containsSubpathOrEquivalent(itemNamesList, val.getPath())));
+        }
+    }
+
+    private CleanupPath remove(QName type, ItemName... pathSegments) {
+        return new CleanupPath(type, ItemPath.create(List.of(pathSegments)), CleanupPathAction.REMOVE);
+    }
+
+    private CleanupPath keep(QName type, ItemName... pathSegments) {
+        return new CleanupPath(type, ItemPath.create(List.of(pathSegments)), CleanupPathAction.IGNORE);
     }
 
     private PrismVisitor sensitiveDataCollector(Consumer<ItemPath> pathConsumer) {
-        return item -> {
-            if (item instanceof PrismPropertyValue<?> property
-                    && property.getTypeName().equals(ProtectedDataType.COMPLEX_TYPE)) {
-                pathConsumer.accept(property.getPath());
-                return false;
+        return object -> {
+            if (object instanceof PrismPropertyValue<?> propertyValue) {
+                var typeName = propertyValue.getTypeName();
+                if (ProtectedDataType.COMPLEX_TYPE.equals(typeName)
+                        || ProtectedStringType.COMPLEX_TYPE.equals(typeName)) {
+                    // Note: ProtectedByteArrayType has the same type name as ProtectedDataType (why?)
+                    // TODO we might also check using getRealValue() but that may be too fragile - what would we do if
+                    //  that check failed?
+                    pathConsumer.accept(propertyValue.getPath());
+                    return false;
+                }
             }
             return true;
         };
     }
 
+    private static class SensitiveDataRemovingVisitor implements ConfigurableVisitor, JaxbVisitor {
+
+        private final NinjaContext context;
+
+        private SensitiveDataRemovingVisitor(NinjaContext context) {
+            this.context = context;
+        }
+
+        @Override
+        public void visit(JaxbVisitable visitable) {
+            if (visitable instanceof ProtectedDataType<?> protectedData) {
+                protectedData.setEncryptedData(null);
+                protectedData.setExternalData(null);
+                protectedData.setHashedData(null);
+                if (protectedData instanceof ProtectedStringType protectedString) {
+                    protectedString.setClearValue("REDACTED");
+                } else if (protectedData instanceof ProtectedByteArrayType protectedByteArray) {
+                    protectedByteArray.setClearValue(ArrayUtils.toObject("REDACTED".getBytes(StandardCharsets.UTF_8)));
+                } else {
+                    // currently no other subtypes here; I'm not sure if the empty object would be OK if we get here
+                }
+            } else {
+                // Should we parse not-yet-parsed RawType here?
+                JaxbVisitable.visitPrismStructure(visitable, this);
+            }
+        }
+
+        @Override
+        public void visit(Visitable visitable) {
+            if (visitable instanceof PrismPropertyValue<?> propertyValue) {
+                try {
+                    if (propertyValue.getRealValue() instanceof JaxbVisitable jaxbVisitable) {
+                        jaxbVisitable.accept(this);
+                    }
+                } catch (Exception e) {
+                    context.getLog().warn("Couldn't get real value of item @{}: {}", propertyValue, e.getMessage());
+                }
+            }
+        }
+
+        @Override
+        public boolean shouldVisitEmbeddedObjects() {
+            return true;
+        }
+    }
 }
