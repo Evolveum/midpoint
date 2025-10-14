95f09e24cc6be9e672d284948af6109515f686cf
Pavol Mederly <mederly@evolveum.com>
2025-10-14 18:21:55 +0200

    Avoid ObjectCleaner in ninja config export
    This commit replaces ObjectCleaner by own logic when
    removing unwanted items. The reason is that it does not
    support abstract object types, so its configuration
    would get too complex. Another reason is that we need
    to backport this logic to support-4.8, where this
    class does not exist.
    
    Also fixed some minor bugs, like assuming all items
    to be excluded are properties + now supporting item
    paths with intermediate multivalued containers.

--- tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/worker/ExportConfigurationWorker.java
+++ tools/ninja/src/main/java/com/evolveum/midpoint/ninja/action/worker/ExportConfigurationWorker.java
@@ -8,6 +8,8 @@ import java.util.concurrent.BlockingQueue;
 import java.util.function.Consumer;
 import javax.xml.namespace.QName;
 
+import com.evolveum.midpoint.util.QNameUtil;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 import com.evolveum.midpoint.common.cleanup.CleanupPath;
@@ -20,7 +22,6 @@ import com.evolveum.midpoint.ninja.util.OperationStatus;
 import com.evolveum.midpoint.prism.*;
 import com.evolveum.midpoint.prism.path.ItemName;
 import com.evolveum.midpoint.prism.path.ItemPath;
-import com.evolveum.midpoint.prism.path.ItemPathCollectionsUtil;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 import com.evolveum.prism.xml.ns._public.types_3.ProtectedByteArrayType;
 import com.evolveum.prism.xml.ns._public.types_3.ProtectedDataType;
@@ -38,60 +39,107 @@ public class ExportConfigurationWorker extends ExportConsumerWorker {
         final ObjectCleaner objectCleaner = new ObjectCleaner();
         objectCleaner.setRemoveMetadata(true);
         objectCleaner.setPaths(List.of(
-                remove(ResourceType.COMPLEX_TYPE, ResourceType.F_CONNECTOR_CONFIGURATION),
-                remove(ResourceType.COMPLEX_TYPE, ResourceType.F_ADDITIONAL_CONNECTOR,
-                        ConnectorInstanceSpecificationType.F_CONNECTOR_CONFIGURATION),
-                remove(AssignmentHolderType.COMPLEX_TYPE, AssignmentHolderType.F_ROLE_MEMBERSHIP_REF),
-                remove(AbstractRoleType.COMPLEX_TYPE, AbstractRoleType.F_ADMIN_GUI_CONFIGURATION),
-                remove(ArchetypeType.COMPLEX_TYPE, ArchetypeType.F_ARCHETYPE_POLICY, ArchetypePolicyType.F_DISPLAY),
-                remove(ArchetypeType.COMPLEX_TYPE, ArchetypeType.F_ARCHETYPE_POLICY,
-                        ArchetypePolicyType.F_ITEM_CONSTRAINT),
-                remove(ArchetypeType.COMPLEX_TYPE, ArchetypeType.F_ARCHETYPE_POLICY,
-                        ArchetypePolicyType.F_ADMIN_GUI_CONFIGURATION),
-                remove(ObjectType.COMPLEX_TYPE, ObjectType.F_EXTENSION),
-                keep(ResourceType.COMPLEX_TYPE, ResourceType.F_SCHEMA, XmlSchemaType.F_DEFINITION) // operational data - must be explicitly allowed
-        ));
+                // operational data - must be explicitly allowed, otherwise the cleaner will remove them
+                keepInstruction(ResourceType.COMPLEX_TYPE, ResourceType.F_SCHEMA, XmlSchemaType.F_DEFINITION)));
         objectCleaner.process(prismObject);
 
-        keepOnly(prismObject,
+        // removal of the connector configuration (two places)
+        remove(prismObject, ResourceType.class, ResourceType.F_CONNECTOR_CONFIGURATION);
+        remove(prismObject, ResourceType.class, ResourceType.F_ADDITIONAL_CONNECTOR,
+                ConnectorInstanceSpecificationType.F_CONNECTOR_CONFIGURATION);
+
+        // removal of other items
+        remove(prismObject, AssignmentHolderType.class, AssignmentHolderType.F_ROLE_MEMBERSHIP_REF);
+        remove(prismObject, AbstractRoleType.class, AbstractRoleType.F_ADMIN_GUI_CONFIGURATION);
+        remove(prismObject, ArchetypeType.class, ArchetypeType.F_ARCHETYPE_POLICY, ArchetypePolicyType.F_DISPLAY);
+        remove(prismObject, ArchetypeType.class, ArchetypeType.F_ARCHETYPE_POLICY, ArchetypePolicyType.F_ITEM_CONSTRAINT);
+        remove(prismObject, ArchetypeType.class, ArchetypeType.F_ARCHETYPE_POLICY, ArchetypePolicyType.F_ADMIN_GUI_CONFIGURATION);
+        remove(prismObject, ObjectType.class, ObjectType.F_EXTENSION);
+
+        removeAllExcept(prismObject,
                 SystemConfigurationType.class,
                 SystemConfigurationType.F_DEFAULT_OBJECT_POLICY_CONFIGURATION,
                 SystemConfigurationType.F_MODEL_HOOKS,
                 SystemConfigurationType.F_CORRELATION);
 
-        keepOnly(prismObject,
+        removeAllExcept(prismObject,
                 ConnectorType.class,
                 ConnectorType.F_NAME,
                 ConnectorType.F_AVAILABLE);
 
         // Remove items specified by the user.
         final Collection<ItemPath> explicitlyExcludedItems = options.getExcludeItems();
-        explicitlyExcludedItems.forEach(path -> prismObject.removeItem(path, PrismProperty.class));
+        explicitlyExcludedItems.forEach(path -> removeItem(prismObject, path));
 
         // Remove items that contains ProtectedDataType values.
         final Collection<ItemPath> sensitiveItems = new ArrayList<>();
         prismObject.acceptVisitor(sensitivePropertiesCollector(sensitiveItems::add));
-        sensitiveItems.forEach(path -> prismObject.removeItem(path, PrismProperty.class));
+        sensitiveItems.forEach(path -> removeItem(prismObject, path));
 
         // Mask other ProtectedDataType values (it is quite hard to remove them, because they are not "normal"
         // properties)
-        prismObject.accept(new SensitiveDataRemovingVisitor(context.getLog()));
+        //noinspection unchecked
+        prismObject.accept(new SensitiveDataRemovingVisitor<>(context.getLog()));
     }
 
-    private static void keepOnly(
-            PrismObject<? extends ObjectType> prismObject, Class<? extends ObjectType> type, ItemName... itemNames) {
+    /** If object is of given type, we keep only the specified root-level items. */
+    private static void removeAllExcept(
+            PrismObject<? extends ObjectType> prismObject, Class<? extends ObjectType> type, ItemName... rootItemNames) {
         if (prismObject.isOfType(type)) {
-            final List<ItemName> itemNamesList = List.of(itemNames);
-            prismObject.getValue().getItems().forEach(item -> item.removeIf(
-                    val -> !ItemPathCollectionsUtil.containsSubpathOrEquivalent(itemNamesList, val.getPath())));
+            var rootItemNamesToKeep = List.of(rootItemNames);
+            for (QName rootItemName : prismObject.getValue().getItemNames()) {
+                if (!QNameUtil.matchAny(rootItemName, rootItemNamesToKeep)) {
+                    removeItem(prismObject, ItemName.fromQName(rootItemName));
+                }
+            }
         }
     }
 
-    private static CleanupPath remove(QName type, ItemName... pathSegments) {
-        return new CleanupPath(type, ItemPath.create(List.of(pathSegments)), CleanupPathAction.REMOVE);
+    /** If object is of given type, we remove item with given path (specified as path segments). */
+    private static void remove(
+            PrismObject<? extends ObjectType> prismObject, Class<? extends ObjectType> type, ItemName... pathSegments) {
+        if (prismObject.isOfType(type)) {
+            removeItem(prismObject, ItemPath.create(List.of(pathSegments)));
+        }
     }
 
-    private static CleanupPath keep(QName type, ItemName... pathSegments) {
+    private static void removeItem(PrismObject<?> object, ItemPath path) {
+        removeItem(object.getValue(), path);
+    }
+
+    /**
+     * Removes all items matching given (name-only) path.
+     *
+     * Similar to {@link PrismContainerValue#removeItem(ItemPath)} but deals not only with a single value, but
+     * can treat multi-valued intermediate containers.
+     *
+     * Similar to PrismContainerValue.removePaths but that method is more complex + not available in 4.8.
+     */
+    private static void removeItem(PrismContainerValue<?> pcv, ItemPath path) {
+        if (path.isEmpty()) {
+            return; // shouldn't occur
+        }
+        var first = path.firstToName();
+        var rest = path.rest();
+        if (rest.isEmpty()) {
+            pcv.removeItem(first); // Just remove the item, there's nothing to descend to
+        } else {
+            var subItem = pcv.findItem(first);
+            if (subItem != null) {
+                // Remove the item from all values of the subItem
+                for (PrismValue value : subItem.getValues()) {
+                    if (value instanceof PrismContainerValue<?> subPcv) {
+                        removeItem(subPcv, rest);
+                    }
+                }
+            } else {
+                // Intermediate item is not present; nothing to do here.
+            }
+        }
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static CleanupPath keepInstruction(QName type, ItemName... pathSegments) {
         return new CleanupPath(type, ItemPath.create(List.of(pathSegments)), CleanupPathAction.IGNORE);
     }
 
