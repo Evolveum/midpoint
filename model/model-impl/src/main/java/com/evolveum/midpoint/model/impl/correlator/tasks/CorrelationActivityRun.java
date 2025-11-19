2f026d9859d0a47aebe92f853aebd0b77be5f8fc
Andrej Zan <andrej.zan@evolveum.com>
2025-11-19 16:57:33 +0100

    Implement correlation with specified correlation definition
    **What**
    
    Implement new method to the correlation service, which correlate given
    shadow with given correlation definition.
    
    **Why**
    
    In the correlation simulation use case we need to simulate given
    correlation definition on specified set of shadows. The correlation
    definition to use does not have to be stored in the resource, but can
    also be specified explicitly.

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/correlator/tasks/CorrelationActivityRun.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/correlator/tasks/CorrelationActivityRun.java
@@ -9,6 +9,8 @@ package com.evolveum.midpoint.model.impl.correlator.tasks;
 
 import org.jetbrains.annotations.NotNull;
 
+import com.evolveum.midpoint.model.api.correlation.CompleteCorrelationResult;
+import com.evolveum.midpoint.model.api.correlation.CorrelationService;
 import com.evolveum.midpoint.model.api.correlator.CorrelationResult;
 import com.evolveum.midpoint.repo.common.activity.run.ActivityRunException;
 import com.evolveum.midpoint.repo.common.activity.run.ActivityRunInstantiationContext;
@@ -20,15 +22,25 @@ import com.evolveum.midpoint.task.api.RunningTask;
 import com.evolveum.midpoint.task.api.SimulationTransaction;
 import com.evolveum.midpoint.task.api.TaskRunResult;
 import com.evolveum.midpoint.util.exception.CommonException;
+import com.evolveum.midpoint.util.exception.CommunicationException;
+import com.evolveum.midpoint.util.exception.ConfigurationException;
+import com.evolveum.midpoint.util.exception.ExpressionEvaluationException;
+import com.evolveum.midpoint.util.exception.ObjectNotFoundException;
+import com.evolveum.midpoint.util.exception.SchemaException;
+import com.evolveum.midpoint.util.exception.SecurityViolationException;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 
 public class CorrelationActivityRun
         extends SearchBasedActivityRun<ShadowType, CorrelationWorkDefinition, CorrelationActivityHandler, AbstractActivityWorkStateType> {
 
+    private final CorrelationService correlationService;
     private CorrelationDefinitionType correlationDefinition;
 
-    public CorrelationActivityRun(ActivityRunInstantiationContext<CorrelationWorkDefinition, CorrelationActivityHandler> ctx) {
+    public CorrelationActivityRun(
+            ActivityRunInstantiationContext<CorrelationWorkDefinition, CorrelationActivityHandler> ctx,
+            CorrelationService correlationService) {
         super(ctx, "Correlation");
+        this.correlationService = correlationService;
         setInstanceReady();
     }
 
@@ -47,8 +59,11 @@ public class CorrelationActivityRun
 
     @Override
     public boolean processItem(@NotNull ShadowType shadow, @NotNull ItemProcessingRequest<ShadowType> req,
-            RunningTask task, OperationResult result) {
-        final CorrelationResult correlationResult = correlate(shadow);
+            RunningTask task, OperationResult result)
+            throws SchemaException, ExpressionEvaluationException, CommunicationException, SecurityViolationException,
+            ConfigurationException, ObjectNotFoundException {
+        final CompleteCorrelationResult correlationResult = this.correlationService.correlate(shadow,
+                this.correlationDefinition, task, result);
 
         final SimulationTransaction simulationTransaction = getSimulationTransaction();
         if (simulationTransaction != null) {
