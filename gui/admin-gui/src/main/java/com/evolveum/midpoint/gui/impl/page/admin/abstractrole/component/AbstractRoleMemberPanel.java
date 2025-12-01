11269b6bf83c8ba1bb998ef8dd4d5c61aced3c00
honchar <honchar@evolveum.com>
2025-12-01 12:57:46 +0100

    fixed object types configuration usage on the members panel; fix for #10896;
    # Conflicts:
    #	gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/abstractrole/component/AbstractRoleMemberPanel.java

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/abstractrole/component/AbstractRoleMemberPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/page/admin/abstractrole/component/AbstractRoleMemberPanel.java
@@ -184,9 +184,14 @@ public class AbstractRoleMemberPanel<R extends AbstractRoleType> extends Abstrac
         if (panelConfig != null) {
             CompiledObjectCollectionView view = WebComponentUtil.getCompiledObjectCollectionView(panelConfig.getListView(),
                     panelConfig, getPageBase());
-            if (view != null && view.getTargetClass() != null
-                    && AssignmentHolderType.class.isAssignableFrom(view.getTargetClass())) {
-                return view.getTargetClass();
+            if (view != null) {
+                if (view.getTargetClass() != null && AssignmentHolderType.class.isAssignableFrom(view.getTargetClass())) {
+                    return view.getTargetClass();
+                }
+                var defaultObjectTypeFromSearchBoxConfig = getDefaultValueFromSearchBoxConfig(view.getSearchBoxConfiguration());
+                if (defaultObjectTypeFromSearchBoxConfig != null) {
+                    return (Class<AH>) defaultObjectTypeFromSearchBoxConfig;
+                }
             }
         }
         return (Class<AH>) UserType.class;
@@ -1726,5 +1731,22 @@ public class AbstractRoleMemberPanel<R extends AbstractRoleType> extends Abstrac
                 buttonDescriptionsModel.getObject() != null ?
                 buttonDescriptionsModel.getObject().getAdditionalButtons() : null;
         return CollectionUtils.isNotEmpty(additionalButtons);
+    }
+
+    private <AH extends AssignmentHolderType> Class<? extends AssignmentHolderType>  getDefaultValueFromSearchBoxConfig(SearchBoxConfigurationType config) {
+        if (config == null || config.getObjectTypeConfiguration() == null) {
+            return null;
+        }
+        QName defaultValue = null;
+        if (config.getObjectTypeConfiguration().getDefaultValue() != null) {
+            defaultValue = config.getObjectTypeConfiguration().getDefaultValue();
+        }
+        if (config.getDefaultObjectType() != null) {
+            defaultValue = config.getObjectTypeConfiguration().getDefaultValue();
+        }
+        if (defaultValue != null) {
+            return (Class<AH>) WebComponentUtil.qnameToClass(PrismContext.get(), defaultValue);
+        }
+        return null;
     }
 }
