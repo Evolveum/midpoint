1c9e5516b19534bc7f77b505abd8ed2049c8306e
honchar <honchar@evolveum.com>
2025-12-02 11:24:33 +0100

    npe fix

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/ContainerableListPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/ContainerableListPanel.java
@@ -1750,7 +1750,7 @@ public abstract class ContainerableListPanel<C extends Serializable, PO extends 
      */
     protected Component createPanelForNoValue() {
         NoValuePanel components = new NoValuePanel(ID_NO_VALUE_PANEL, () -> new NoValuePanelDto(
-                defaultType.getSimpleName())) {
+                defaultType != null ? defaultType.getSimpleName() : "")) {
 
             @Override
             protected List<Component> createToolbarButtons(String buttonsId) {
