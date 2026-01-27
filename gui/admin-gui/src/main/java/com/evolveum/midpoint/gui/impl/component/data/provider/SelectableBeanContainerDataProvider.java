ab5829314a489a56152a3b79a769a01e78af23e6
Hiroyuki Wada <h2-wada@nri.co.jp>
2026-01-28 00:07:22 +0900

    MID-10990 Add JDBC streaming support for audit log CSV export

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/data/provider/SelectableBeanContainerDataProvider.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/data/provider/SelectableBeanContainerDataProvider.java
@@ -15,6 +15,10 @@ import java.util.Set;
 import com.evolveum.midpoint.gui.api.util.WebComponentUtil;
 import com.evolveum.midpoint.gui.api.util.WebModelServiceUtils;
 
+import com.evolveum.midpoint.prism.PrismContext;
+import com.evolveum.midpoint.schema.ObjectHandler;
+import com.evolveum.midpoint.web.component.util.SelectableBean;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
 import org.jetbrains.annotations.NotNull;
@@ -70,5 +74,52 @@ public class SelectableBeanContainerDataProvider<C extends Containerable> extend
     @Override
     protected boolean match(C selectedValue, C foundValue) {
         return selectedValue.asPrismContainerValue().equivalent(foundValue.asPrismContainerValue());
+    }
+
+    @Override
+    public boolean supportsIterativeExport() {
+        return true;
+    }
+
+    /**
+     * Streaming export using JDBC cursor-based streaming.
+     * This method does not load all data into memory - uses true JDBC streaming.
+     * Streaming is enabled by setting iterationPageSize to -1.
+     */
+    @Override
+    public void exportIterative(
+            ObjectHandler<SelectableBean<C>> handler,
+            Task task,
+            OperationResult result) throws CommonException {
+
+        ObjectQuery query = getQuery();
+        if (query == null) {
+            query = PrismContext.get().queryFactory().createQuery();
+        }
+        // Set ordering from current sort settings (no offset/limit for full export)
+        query.setPaging(createPaging(0, Integer.MAX_VALUE));
+
+        // Enable JDBC streaming mode by setting iterationPageSize to -1
+        Collection<SelectorOptions<GetOperationOptions>> streamingOptions =
+                SelectorOptions.updateRootOptions(getSearchOptions(),
+                        opt -> opt.setIterationPageSize(-1), GetOperationOptions::new);
+
+        searchObjectsIterative(getType(), query,
+                (object, opResult) -> {
+                    SelectableBean<C> wrapper = createDataObjectWrapper(object);
+                    return handler.handle(wrapper, opResult);
+                },
+                streamingOptions, task, result);
+    }
+
+    /**
+     * Override this method to use a different iterative search implementation.
+     * Default implementation uses ModelService.searchContainersIterative().
+     */
+    protected void searchObjectsIterative(Class<C> type, ObjectQuery query,
+            ObjectHandler<C> handler,
+            Collection<SelectorOptions<GetOperationOptions>> options,
+            Task task, OperationResult result) throws CommonException {
+        getModelService().searchContainersIterative(type, query, handler, options, task, result);
     }
 }
