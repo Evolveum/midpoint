b207d1d84b2062bea0e0092d211e56f13ab771c9
Andrej Zan <andrej.zan@evolveum.com>
2026-02-06 13:42:16 +0100

    Modify work definition of mapping simulation activity
    **What**
    
    Create new complex type, which holds necessary data for the mappings
    simulation. In particular it specifies mappings alongside the item path
    they are supposed to map.
    
    **Why**
    
    Previous definition contained mapping definition, but that does not
    contain information about what item it is mapping. But that is crucial
    information to evaluate the mapping on a correct item.
    
    **Notes**
    
    This change also fixed few `since` annotations, because they had wrong
    version set.
    
    **Task**: 10992

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/mappings/tasks/MappingWorkDefinition.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/mappings/tasks/MappingWorkDefinition.java
@@ -7,18 +7,22 @@
 
 package com.evolveum.midpoint.model.impl.mappings.tasks;
 
+import static com.evolveum.midpoint.xml.ns._public.common.common_3.ResourceObjectSetQueryApplicationModeType.*;
+
+import java.util.List;
+
 import com.evolveum.midpoint.model.impl.sync.tasks.ResourceSetTaskWorkDefinition;
 import com.evolveum.midpoint.repo.common.activity.definition.WorkDefinitionFactory;
+import com.evolveum.midpoint.schema.util.task.work.ResourceObjectSetUtil;
 import com.evolveum.midpoint.util.DebugUtil;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.MappingType;
-import com.evolveum.midpoint.xml.ns._public.common.common_3.MappingWorkDefinitionType;
+import com.evolveum.midpoint.xml.ns._public.common.common_3.*;
 
 /**
  * Work definition for mapping simulation activity.
  */
 public class MappingWorkDefinition extends ResourceSetTaskWorkDefinition {
 
-    private final MappingType inlineMapping;
+    private final SimulatedMappingsType mappingsToUse;
 
     public MappingWorkDefinition(WorkDefinitionFactory.WorkDefinitionInfo info) {
         super(info);
@@ -29,15 +33,18 @@ public class MappingWorkDefinition extends ResourceSetTaskWorkDefinition {
                     + " but got: " + workDefBean.getClass());
         }
 
-        this.inlineMapping = workDef.getInlineMapping();
-        if (this.inlineMapping == null) {
-            throw new IllegalArgumentException("Inline mapping must be specified");
-        }
+        ResourceObjectSetUtil.setDefaultQueryApplicationMode(getResourceObjectSetSpecification(), APPEND);
+
+        this.mappingsToUse = workDef.getMappings();
+    }
+
+    public List<InlineMappingDefinitionType> provideMappings() {
+        return this.mappingsToUse.getInlineMappings();
     }
 
     @Override
     protected void debugDumpContent(StringBuilder sb, int indent) {
         super.debugDumpContent(sb, indent);
-        DebugUtil.debugDumpWithLabel(sb, "inlineMapping", inlineMapping, indent + 1);
+        DebugUtil.debugDumpWithLabel(sb, "mappings", this.mappingsToUse, indent + 1);
     }
 }
