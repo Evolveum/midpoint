8e1efd5beb68e92a857b9b86e37cd567ef758aa5
Viliam Repan <vilo.repan@evolveum.com>
2026-03-05 16:48:33 +0100

    sidebar help panel added close button, disabled escaping for help content

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/menu/RightSidebarHelpPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/menu/RightSidebarHelpPanel.java
@@ -19,6 +19,7 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
 import com.evolveum.midpoint.gui.api.component.BasePanel;
+import com.evolveum.midpoint.web.component.AjaxIconButton;
 import com.evolveum.midpoint.web.component.util.SerializableFunction;
 import com.evolveum.midpoint.web.component.util.VisibleBehaviour;
 
@@ -27,6 +28,7 @@ public class RightSidebarHelpPanel extends BasePanel<Void> {
     @Serial private static final long serialVersionUID = 1L;
 
     private static final String ID_TITLE = "title";
+    private static final String ID_CLOSE = "close";
     private static final String ID_CONTENT = "content";
 
     private final IModel<Boolean> visible = Model.of(false);
@@ -54,7 +56,23 @@ public class RightSidebarHelpPanel extends BasePanel<Void> {
         title.add(new VisibleBehaviour(() -> titleModel.getObject() != null && StringUtils.isNotEmpty(titleModel.getObject())));
         add(title);
 
+        AjaxIconButton close = new AjaxIconButton(
+                ID_CLOSE,
+                Model.of("fa fa-times text-danger"),
+                createStringResource("RightSidebarHelpPanel.close")) {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                closePerformed(target);
+            }
+        };
+        add(close);
+
         add(new WebMarkupContainer(ID_CONTENT));
+    }
+
+    private void closePerformed(AjaxRequestTarget target) {
+        close(target);
     }
 
     public void open(AjaxRequestTarget target) {
