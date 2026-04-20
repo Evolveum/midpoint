e98069a3035c002df4744e3f930da1c187bc90b5
matisovaa <antonia.matisova@gmail.com>
2026-04-20 08:04:14 +0200

    ImageSanitizationTest

---
+++ gui/admin-gui/src/test/java/com/evolveum/midpoint/web/component/ImageSanitizationTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2010-2026 Evolveum and contributors
+ *
+ * Licensed under the EUPL-1.2 or later.
+ */
+
+package com.evolveum.midpoint.web.component;
+
+import com.evolveum.midpoint.web.component.input.ImageSanitizationUtil;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.testng.annotations.Test;
+
+import java.util.Objects;
+
+import static com.evolveum.midpoint.web.component.FileTestConstants.JPG_METADATA_ARRAY;
+
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertSame;
+
+/**
+ * @author matisovaa
+ *
+ */
+@ActiveProfiles("test")
+public class ImageSanitizationTest {
+
+    @Test
+    public void test4299_2ImageSanitization_removeEXIF_notNeeded() throws Exception {
+        // asserts if it returns the same reference, so no processing was performed at all
+        assertSame(ImageSanitizationUtil.sanitizeImage(JPG_METADATA_ARRAY, false), JPG_METADATA_ARRAY);
+    }
+
+    @Test
+    public void test4299_2ImageSanitization_removeEXIF() throws Exception {
+        final byte[] jpgWithoutMetadata = ImageSanitizationUtil.sanitizeImage(JPG_METADATA_ARRAY, true);
+        assertNotSame(JPG_METADATA_ARRAY, jpgWithoutMetadata);
+        assertTrue(JPG_METADATA_ARRAY.length > Objects.requireNonNull(jpgWithoutMetadata).length);
+    }
+}
