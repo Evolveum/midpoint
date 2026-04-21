764b85c9b7a1cf33491dc5692f44a064fdeda83a
Pavol Mederly <mederly@evolveum.com>
2026-04-22 00:17:55 +0200

    Make EvaluatedPolicyRuleImpl#getRawActions nullable
    The @NotNull annotation was recently added probably by mistake.
    The returned value can be null.

--- model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/EvaluatedPolicyRuleImpl.java
+++ model/model-impl/src/main/java/com/evolveum/midpoint/model/impl/lens/EvaluatedPolicyRuleImpl.java
@@ -348,7 +348,7 @@ public class EvaluatedPolicyRuleImpl implements EvaluatedPolicyRule, AssociatedP
     }
 
     @Override
-    @NotNull public PolicyActionsType getRawActions() {
+    public PolicyActionsType getRawActions() {
         return policyRuleBean.getPolicyActions();
     }
 
