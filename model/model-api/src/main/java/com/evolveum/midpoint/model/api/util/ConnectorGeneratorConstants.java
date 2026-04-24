410e7e7ccd7ec101da20a583259314396b44d4cf
dominikhorvath <dominikhorvath138@gmail.com>
2026-04-24 08:07:55 +0200

    adding a new endpoint for ConnectorDevelopmentRestController

--- model/model-api/src/main/java/com/evolveum/midpoint/model/api/util/ConnectorGeneratorConstants.java
+++ model/model-api/src/main/java/com/evolveum/midpoint/model/api/util/ConnectorGeneratorConstants.java
@@ -2,6 +2,12 @@ package com.evolveum.midpoint.model.api.util;
 
 public class ConnectorGeneratorConstants {
 
-    public static final String RPC_CONNECTOR_GENERATOR_CREATE_CONNECTOR_DEVELOPMENT = "/rpc/createConnectorDevelopmentType";
-    public static final String RPC_CONNECTOR_GENERATOR_DISCOVER_DOCUMENTATION = "/rpc/discoverDocumentationConnector";
+    public static final String RPC_UPSERT_CONNECTOR_DEVELOPMENT_TYPE = "/rpc/upsertConnectorDevelopmentType";
+    public static final String RPC_CREATE_CONNECTOR_STATUS = "/rpc/createConnectorStatus";
+    public static final String RPC_DISCOVER_BASIC_INFORMATION_STATUS = "/rpc/discoverBasicInformationStatus";
+    public static final String RPC_DISCOVER_DOCUMENTATION_STATUS = "/rpc/discoverDocumentationStatus";
+    public static final String RPC_PROCESS_DOCUMENTATION_STATUS = "/rpc/processDocumentationStatus";
+    public static final String RPC_GENERATE_ARTIFACT_STATUS = "/rpc/generateArtifactStatus";
+    public static final String RPC_DISCOVER_OBJECT_CLASS_INFORMATION_STATUS = "/rpc/discoverObjectClassInformationStatus";
+    public static final String RPC_DISCOVER_OBJECT_CLASS_ATTRIBUTES_STATUS = "/rpc/discoverObjectClassAttributesStatus";
 }
