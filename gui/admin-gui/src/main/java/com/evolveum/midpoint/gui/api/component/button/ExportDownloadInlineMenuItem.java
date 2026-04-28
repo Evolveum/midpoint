d670f5487d991e2508cbd178e858daf9a9f13d50
honchar <honchar@evolveum.com>
2026-04-28 14:15:13 +0200

    Removed timer behavior for xlsx export
    Related to #10990

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/button/ExportDownloadInlineMenuItem.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/api/component/button/ExportDownloadInlineMenuItem.java
@@ -20,10 +20,6 @@ import com.evolveum.midpoint.web.component.menu.cog.InlineMenuItem;
 import com.evolveum.midpoint.web.component.menu.cog.InlineMenuItemAction;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.wicket.Application;
-import org.apache.wicket.Session;
-import org.apache.wicket.ThreadContext;
-import org.apache.wicket.ajax.AbstractAjaxTimerBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
@@ -33,22 +29,12 @@ import org.apache.wicket.extensions.markup.html.repeater.data.table.export.IExpo
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.util.resource.FileResourceStream;
 import org.apache.wicket.util.resource.IResourceStream;
 
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.io.Serial;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.io.File;
-import java.util.concurrent.Future;
 import org.apache.wicket.util.*;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 public abstract class ExportDownloadInlineMenuItem extends InlineMenuItem {
 
@@ -58,9 +44,6 @@ public abstract class ExportDownloadInlineMenuItem extends InlineMenuItem {
     private AbstractAjaxDownloadBehavior ajaxDownloadBehavior;
     private IModel<String> name;
     protected List<Integer> exportableColumnsIndex = new ArrayList<>();
-
-    private static final ExecutorService EXPORT_EXECUTOR = Executors.newSingleThreadExecutor();
-    private Future<File> future;
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -79,15 +62,7 @@ public abstract class ExportDownloadInlineMenuItem extends InlineMenuItem {
 
             @Override
             public IResourceStream getResourceStream() {
-                if (future == null || !future.isDone()) {
-                    LOGGER.error("Failed to load the file.");
-                }
-                try {
-                    return new FileResourceStream(future.get());
-                } catch (Exception e) {
-                    LOGGER.error("Failed to load the file.");
-                }
-                return null;
+                return new ExportToolbar.DataExportResourceStreamWriter(getDataExporter(), getDataTable());
             }
 
             public String getFileName() {
@@ -98,20 +73,6 @@ public abstract class ExportDownloadInlineMenuItem extends InlineMenuItem {
             }
         };
         component.add(ajaxDownloadBehavior);
-
-        component.add(new AbstractAjaxTimerBehavior(Duration.ofMillis(5000)) {
-            @Serial private static final long serialVersionUID = 1L;
-
-            @Override
-            protected void onTimer(AjaxRequestTarget target) {
-                if (future != null && future.isDone()) {
-                    stop(target); // stop polling
-
-                    // trigger download
-                    ajaxDownloadBehavior.initiate(target);
-                }
-            }
-        });
     }
 
     @Override
@@ -143,32 +104,11 @@ public abstract class ExportDownloadInlineMenuItem extends InlineMenuItem {
                 exportableColumnsIndex.clear();
                 ExportingPanel exportingPanel = new ExportingPanel(WebComponentUtil.getPageBase(component).getMainPopupBodyId(),
                         getDataTable(), exportableColumnsIndex, useExportSizeLimit, name) {
-                    private static final long serialVersionUID = 1L;
+                    @Serial private static final long serialVersionUID = 1L;
 
                     @Override
                     public void exportPerformed(AjaxRequestTarget target) {
-                        Session session = Session.get();
-                        SecurityContext context = SecurityContextHolder.getContext();
-                        Application application = Application.get();
-
-                        component.getPageBase().runPrivileged(() -> {
-                                    future = EXPORT_EXECUTOR.submit(() -> {
-                                        ThreadContext.setApplication(application);
-                                        ThreadContext.setSession(session);
-                                        SecurityContextHolder.setContext(context);
-
-                                        File file = File.createTempFile("export-", ".xlsx");
-
-                                        IDataProvider<?> provider = getDataTable().getDataProvider();
-
-                                        try (OutputStream os = new FileOutputStream(file)) {
-                                            getDataExporter().exportData(provider, getExportableColumns(), os);
-                                        }
-
-                                        return file;
-                                    });
-                                    return null;
-                                });
+                        ajaxDownloadBehavior.initiate(target);
                     }
 
                     @Override
