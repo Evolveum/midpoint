b0d04c247fca97da7130c98f55ff8591d04772a5
dominikhorvath <dominikhorvath138@gmail.com>
2026-05-04 07:23:02 +0200

    updated generate connector controller of rest api & improvements code quality

--- model/model-api/src/main/java/com/evolveum/midpoint/model/api/util/ConnectorGeneratorConstants.java
+++ model/model-api/src/main/java/com/evolveum/midpoint/model/api/util/ConnectorGeneratorConstants.java
@@ -2,6 +2,7 @@ package com.evolveum.midpoint.model.api.util;
 
 public class ConnectorGeneratorConstants {
 
+    public static final String RPC_SUBMIT_OPERATION = "/rpc/submitOperation";
     public static final String RPC_UPSERT_CONNECTOR_DEVELOPMENT_TYPE = "/rpc/upsertConnectorDevelopmentType";
     public static final String RPC_CREATE_CONNECTOR_STATUS = "/rpc/createConnectorStatus";
     public static final String RPC_DISCOVER_BASIC_INFORMATION_STATUS = "/rpc/discoverBasicInformationStatus";
@@ -10,4 +11,13 @@ public class ConnectorGeneratorConstants {
     public static final String RPC_GENERATE_ARTIFACT_STATUS = "/rpc/generateArtifactStatus";
     public static final String RPC_DISCOVER_OBJECT_CLASS_INFORMATION_STATUS = "/rpc/discoverObjectClassInformationStatus";
     public static final String RPC_DISCOVER_OBJECT_CLASS_ATTRIBUTES_STATUS = "/rpc/discoverObjectClassAttributesStatus";
+
+    public static final String OPERATION_UPSERT_CONNECTOR_DEVELOPMENT_TYPE = "UpsertConnectorDevelopmentType";
+    public static final String OPERATION_CREATE_CONNECTOR = "SubmitCreateConnector";
+    public static final String OPERATION_DISCOVER_BASIC_INFORMATION = "SubmitDiscoverBasicInformation";
+    public static final String OPERATION_DISCOVER_DOCUMENTATION = "SubmitDiscoverDocumentation";
+    public static final String OPERATION_PROCESS_DOCUMENTATION = "SubmitProcessDocumentation";
+    public static final String OPERATION_GENERATE_ARTIFACT = "SubmitGenerateArtifact";
+    public static final String OPERATION_DISCOVER_OBJECT_CLASS_INFORMATION = "SubmitDiscoverObjectClassInformation";
+    public static final String OPERATION_DISCOVER_OBJECT_CLASS_ATTRIBUTES = "SubmitDiscoverObjectClassAttribute";
 }
