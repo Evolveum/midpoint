80bcf15dc6b5c004146a4949476e0d46fef9c391
tchrapovic <chrapovic.tadeas@gmail.com>
2026-08-11 12:38:10 +0200

    fix: apply expression validation to read-only expression panels
    Task: 11712 & 11753

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/input/expression/ExpressionPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/component/input/expression/ExpressionPanel.java
@@ -326,7 +326,9 @@ public class ExpressionPanel extends BasePanel<ExpressionType> {
                     ? getPageBase().createStringResource(typeModel.getObject().type).getString()
                     : ExpressionPanel.this.getString(RecognizedEvaluator.AS_IS.type));
             label.setOutputMarkupId(true);
-            label.add(AttributeModifier.replace("class", "form-select form-select-sm text-nowrap"));
+            label.add(AttributeModifier.replace("class", "form-select form-select-sm text-nowrap "));
+            label.add(AttributeModifier.replace("style", ""));
+            label.add(new ExpressionValidationBehavior(typeModel, getModel()));
             return label;
         }
 
