f9d512369ab100617f9fb7620a4691c8d54d25d4
skublik <lukas.skublik@gmail.com>
2026-09-02 13:38:20 +0200

    Introduce IdentificationModuleAuthentication marker interface
    Add the IdentificationModuleAuthentication interface to mark
    authentication modules that establish the identity used for resolving
    the security policy of the authentication flow.
    
    Task: 10821

--- model/authentication-api/src/main/java/com/evolveum/midpoint/authentication/api/config/ArchetypeSelectionModuleAuthentication.java
+++ model/authentication-api/src/main/java/com/evolveum/midpoint/authentication/api/config/ArchetypeSelectionModuleAuthentication.java
@@ -2,7 +2,7 @@ package com.evolveum.midpoint.authentication.api.config;
 
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ArchetypeSelectionType;
 
-public interface ArchetypeSelectionModuleAuthentication extends ModuleAuthentication{
+public interface ArchetypeSelectionModuleAuthentication extends IdentificationModuleAuthentication{
 
     boolean isAllowUndefined();
 
