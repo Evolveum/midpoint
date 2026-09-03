a133fc97c3a2684e35dcaca12b6d98dc13dab9de
Pavol Mederly <mederly@evolveum.com>
2026-09-03 13:16:28 +0200

    Fix change level in "full" DB scripts
    Change level was updated in the code and in upgrade scripts but not
    in the "full" scripts.
    
    Task: 11337

--- config/sql/native/postgres-audit.sql
+++ config/sql/native/postgres-audit.sql
@@ -553,4 +553,4 @@ END $$;
 -- This is important to avoid applying any change more than once.
 -- Also update SqaleUtils.CURRENT_SCHEMA_AUDIT_CHANGE_NUMBER
 -- repo/repo-sqale/src/main/java/com/evolveum/midpoint/repo/sqale/SqaleUtils.java
-call apply_audit_change(15, $$ SELECT 1 $$, true);
+call apply_audit_change(16, $$ SELECT 1 $$, true);
