a44d0234ea3b8a94a28763ee49686686fc404574
honchar <honchar@evolveum.com>
2026-09-07 16:21:09 +0200

    Fixed the width of the property label
    Task: 11628

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/prism/panel/ItemPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/gui/impl/prism/panel/ItemPanel.java
@@ -124,7 +124,7 @@ public abstract class ItemPanel<VW extends PrismValueWrapper<?>, IW extends Item
         if (isDisplayedInColumn()) {
             css.append("col-12");
         } else {
-            css.append("col-4xl-9 col-xxl-8 col-xl-7 col-md-6 col-sm-12");
+            css.append("col-4xl-10 col-3xl-9 col-xxl-8 col-xl-7 col-md-6 col-sm-12");
         }
 
         if (isMultiValueLayout()) {
