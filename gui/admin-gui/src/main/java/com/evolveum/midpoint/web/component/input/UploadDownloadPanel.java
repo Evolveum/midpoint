adf292fe49d57caa8ad0e1db87ffba3fa41b8c8f
honchar <honchar@evolveum.com>
2026-09-14 15:03:15 +0200

    Unblock file upload panel from unrelated errors
    Each of upload/download/delete actions submitted the whole form,
    so that the form submitting failed in case of any field has an error
    (e.g. an empty required field generated an error while submitting).
     UploadDownloadPanel has nothing to do with it so the changes provide a
     fix for it.
    
    Task: 12031 (WCAG report - 8.33)

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/input/UploadDownloadPanel.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/component/input/UploadDownloadPanel.java
@@ -110,7 +110,17 @@ public class UploadDownloadPanel extends InputPanel {
             @Override
             protected void onError(AjaxRequestTarget target) {
                 super.onError(target);
-                UploadDownloadPanel.this.uploadFileFailed(target);
+                if (!fileUpload.hasErrorMessage()) {
+                    //we don't need to validate other fields and show their errors when upload file action is produced
+                    fileUpload.getForm().getRootForm().visitChildren(FormComponent.class, (otherComponent, visit) -> {
+                        if (otherComponent != fileUpload) {
+                            otherComponent.getFeedbackMessages().clear();
+                        }
+                    });
+                    UploadDownloadPanel.this.uploadFilePerformed(target);
+                } else {
+                    UploadDownloadPanel.this.uploadFileFailed(target);
+                }
             }
         });
         fileUpload.add(new VisibleBehaviour(() -> !isReadOnly));
@@ -164,7 +174,7 @@ public class UploadDownloadPanel extends InputPanel {
         downloadBehavior.setFileName(getDownloadFileName());
         add(downloadBehavior);
 
-        add(new AjaxSubmitButton(ID_BUTTON_DOWNLOAD) {
+        AjaxSubmitButton downloadButton = new AjaxSubmitButton(ID_BUTTON_DOWNLOAD) {
 
             @Serial
             private static final long serialVersionUID = 1L;
@@ -173,7 +183,9 @@ public class UploadDownloadPanel extends InputPanel {
             protected void onSubmit(AjaxRequestTarget target) {
                 downloadPerformed(downloadBehavior, target);
             }
-        });
+        };
+        downloadButton.setDefaultFormProcessing(false);
+        add(downloadButton);
 
         AjaxSubmitButton deleteButton = new AjaxSubmitButton(ID_BUTTON_DELETE) {
 
@@ -185,6 +197,7 @@ public class UploadDownloadPanel extends InputPanel {
                 removeFilePerformed(target);
             }
         };
+        deleteButton.setDefaultFormProcessing(false);
         deleteButton.add(new VisibleBehaviour(() -> !isReadOnly));
         add(deleteButton);
 
@@ -240,6 +253,7 @@ public class UploadDownloadPanel extends InputPanel {
             updateValue(null);
             LOGGER.trace("Remove file success.");
             input.success(getString("UploadPanel.message.removeSuccess"));
+            target.add(UploadDownloadPanel.this);
         } catch (Exception e) {
             LOGGER.trace("Remove file error.", e);
             input.error(getString("UploadPanel.message.removeError") + " " + e.getMessage());
